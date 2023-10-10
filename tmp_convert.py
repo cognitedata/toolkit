1a36287d51b1099bc0f96f4b9264c2d6ba35a902
anders-albert <anders0albert@gmail.com>
2023-10-10 14:15:38 +0200

    refactor: Converted to yaml and from read to write version

---
+++ tmp_convert.py
@@ -0,0 +1,34 @@
+import json
+
+from cognite.client.data_classes.data_modeling import DataModelList, ViewId, ViewList, ContainerList, View, Container, DataModel
+from pathlib import Path
+import re
+import yaml
+ROOT = Path(__file__).parent
+
+JSON_PATTERN = re.compile(r"^(\w+\.)?(container|view|datamodel)\.json$")
+
+
+def main():
+    for file in (ROOT / "modules").glob("**/*.json"):
+        if not(match := JSON_PATTERN.match(file.name)):
+            continue
+        type_ = match.group(2)
+        data = json.loads(file.read_text())
+        if type_ == "container":
+            items =  Container.load(data)
+        elif type_ == "view":
+            items = View.load(data)
+        elif type_ == "datamodel":
+            items = DataModel.load(data)
+        else:
+            raise NotImplementedError
+
+        yaml_file = file.parent / f"{match.group(1)}{type_}.yaml"
+        yaml_file.write_text(yaml.dump(items.as_apply().dump(camel_case=True)))
+        print("Converted", file, "to", yaml_file)
+
+
+
+if __name__ == '__main__':
+    main()
