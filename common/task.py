e877ac2d1c0ea58c2a5e1efb4d7a683dfc6798ce
Pål Rønning <pal.ronning@cognite.com>
2023-10-13 23:20:34 +0200

    Couple of basic tests and first cut Pydantic implementation

--- common/task.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from typing import Optional
-from pydantic import BaseModel
-from cognite.client import CogniteClient
-
-class TaskBaseModel(BaseModel):
-    client: CogniteClient
-    
-    
--- tests/modules/cdf_apm_simple/raw/test_deploy.py
+++ tests/modules/cdf_apm_simple/raw/test_deploy.py
@@ -1,4 +1,43 @@
+import pandas
+from pydantic import ValidationError
 import pytest
+import os
 
-def test_load_content():
-    pass
+from modules.cdf_apm_simple.raw.worker import RawDatabaseContent, Worker
+from cognite.client.testing import CogniteClientMock
+
+
+@pytest.fixture
+def file():
+    dir = os.path.dirname(os.path.abspath(__file__))
+    return f"{dir}/data/workitems.csv"
+
+
+def test_load_content(file):
+    df = pandas.read_csv(file)
+    assert df is not None
+
+    data = RawDatabaseContent(database_name="workitems", table_name="workitems", data=df)
+    assert data is not None
+
+
+def test_write_content(file):
+
+    worker = Worker(
+        client=CogniteClientMock(),
+        content=[
+            RawDatabaseContent(
+                database_name="workitems",
+                table_name="workitems",
+                data=pandas.read_csv(file),
+            )
+        ],
+        reset=True
+    )
+
+    assert worker is not None
+
+    worker.createtables()
+
+    #todo: assert that table api is called with delete/write
+
