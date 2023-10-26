0787682fa1d78b6bdc6a811be8f0bf8dbde9d051
Greger Wedel <greger.wedel@cognite.com>
2023-10-26 12:53:58 +0200

    Rename to apm_simple and fix up to use new auth and schedule functionality

---
+++ modules/cdf_apm_simple/transformations/apm_simple-load-asset2children.sql
@@ -0,0 +1,6 @@
+select
+  cast(`externalId` as STRING) as externalId,
+  node_reference('{{datamodel}}', `sourceExternalId`) as startNode,
+  node_reference('{{datamodel}}', `targetExternalId`) as endNode
+from
+  `{{raw_db}}`.`asset2children`;
