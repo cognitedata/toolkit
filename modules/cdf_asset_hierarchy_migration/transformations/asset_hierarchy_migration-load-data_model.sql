f0231c381a968212202b685157e5f9966dfdc443
anders-albert <anders0albert@gmail.com>
2023-10-30 16:21:59 +0100

    refactor: Fix bugs in SQL

--- modules/cdf_asset_hierarchy_migration/transformations/asset_hierarchy_migration-load-data_model.sql
+++ modules/cdf_asset_hierarchy_migration/transformations/asset_hierarchy_migration-load-data_model.sql
@@ -1,10 +1,12 @@
 select
   cast(`externalId` as STRING) as externalId,
-  node_reference('cdfTemplate', `parentExternalId`) as parent,
+  node_reference('{{space}}', `parentExternalId`) as parent,
   cast(`name` as STRING) as name,
   cast(`source` as STRING) as source,
   cast(`description` as STRING) as description,
   cast(`labels` as ARRAY < STRING >) as labels,
   to_json(`metadata`) as metadata
 from
-  `_cdf`.`assets`;
+  `_cdf`.`assets`
+where
+ isnotnull(`externalId`) and isnotnull(`parentExternalId`);
