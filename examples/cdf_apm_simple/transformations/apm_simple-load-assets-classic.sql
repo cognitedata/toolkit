4d9ce75bf5297ee8b04b4134d7650438b8145111
Greger Wedel <greger.wedel@cognite.com>
2023-10-31 14:44:35 +0100

    Delete duplicate

--- examples/cdf_apm_simple/transformations/apm_simple-load-assets-classic.sql
+++ examples/cdf_apm_simple/transformations/apm_simple-load-assets-classic.sql
@@ -2,7 +2,6 @@ select
   cast(`externalId` as STRING) as name,
   cast(`description` as STRING) as description,
   cast(`sourceDb` as STRING) as source,
-  cast(`externalId` as STRING) as externalId,
   cast(`parentExternalId` as STRING) as parentExternalId
 from
   `{{raw_db}}`.`assets`;
