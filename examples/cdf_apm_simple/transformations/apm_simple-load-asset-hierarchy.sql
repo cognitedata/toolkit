30c3edb95c20d3b92fadbdfa50fb455f8df71ba8
Pål Rønning <pal.ronning@cognite.com>
2023-10-31 14:41:28 +0100

    Updating to use asset hierarchy target

--- examples/cdf_apm_simple/transformations/apm_simple-load-asset-hierarchy.sql
+++ examples/cdf_apm_simple/transformations/apm_simple-load-asset-hierarchy.sql
@@ -1,6 +1,8 @@
 select
+  select
   cast(`externalId` as STRING) as externalId,
   cast(`externalId` as STRING) as name,
   cast(`description` as STRING) as description,
+  cast(`parentExternalId` as STRING) as parentExternalId
 from
   `{{raw_db}}`.`assets`;
