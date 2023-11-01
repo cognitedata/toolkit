6052bfd630a98b5149ec719a7be47a54d6e94e55
anders-albert <anders0albert@gmail.com>
2023-11-01 08:30:06 +0100

    refactor: Update transformation

--- experimental/cdf_asset_hierarchy_replication/transformations/asset_hierarchy_replication-load-data_model.sql
+++ experimental/cdf_asset_hierarchy_replication/transformations/asset_hierarchy_replication-load-data_model.sql
@@ -1,12 +1,13 @@
 select
   cast(`externalId` as STRING) as externalId,
   node_reference('{{space}}', `parentExternalId`) as parent,
+  node_reference('{{space}}', '{{root_asset_external_id}}') as root,
   cast(`name` as STRING) as name,
   cast(`source` as STRING) as source,
   cast(`description` as STRING) as description,
   cast(`labels` as ARRAY < STRING >) as labels,
   to_json(`metadata`) as metadata
 from
-  `_cdf`.`assets`
+  cdf_assetSubtree('{{root_asset_external_id}}')
 where
  isnotnull(`externalId`) and isnotnull(`parentExternalId`);
