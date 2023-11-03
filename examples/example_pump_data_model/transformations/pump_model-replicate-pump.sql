f854bd0e96f96a899809303a05bbe3a531cf6850
anders-albert <anders0albert@gmail.com>
2023-11-03 13:26:55 +0100

    feat: Pump transformation

---
+++ examples/example_pump_data_model/transformations/pump_model-replicate-pump.sql
@@ -0,0 +1,13 @@
+select
+  cast(`externalId` as STRING) as externalId,
+  cast(`name` as STRING) as name,
+  cast(`description` as STRING) as description,
+  cast(get_json_object(`metadata`, '$.DesignPointHeadFT') as DOUBLE) as DesignPointHeadFT,
+  cast(get_json_object(`metadata`, '$.LowHeadFT') as DOUBLE) as LowHeadFT,
+  cast(get_json_object(`metadata`, '$.DesignPointFlowGPM') as DOUBLE) as DesignPointFlowGPM,
+  cast(get_json_object(`metadata`, '$.LowHeadFlowGPM') as DOUBLE) as LowHeadFlowGPM,
+  node_reference('cdfTemplate', cast(`parent` as STRING)) as liftStation
+from
+  cdf_data_models("cdfTemplate", "AssetHierarchy", "1", "Asset")
+where
+  startswith(name, 'Pump')
