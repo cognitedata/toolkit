62c81ba3167c7d3183ac4957c6cd17ea1ef9b79c
Pål Rønning <pal.ronning@cognite.com>
2023-11-03 15:31:53 +0100

    Fixed variable substitution

--- modules/cdf_infield_location/transformations/sync_assets_from_hierarchy_to_apm.sql
+++ modules/cdf_infield_location/transformations/sync_assets_from_hierarchy_to_apm.sql
@@ -2,13 +2,13 @@ select
   cast(asset.externalId as STRING) as externalId,
   (case
     when isnull(asset.parentExternalId) then null
-    else node_reference('infield_default_location_source_data_space', asset.parentExternalId) 
+    else node_reference('infield_{{location_name}}_source_data_space', asset.parentExternalId) 
   end) as parent,  
-  cast("CDF Classic" as STRING) as source,
-  node_reference('infield_default_location_source_data_space', cast(rootAsset.externalId as STRING)) as root,
+  cast("Asset Hierarachy" as STRING) as source,
+  node_reference('infield_{{location_name}}_source_data_space', cast(rootAsset.externalId as STRING)) as root,
   cast(asset.description as STRING) as description,
   cast(asset.name as STRING) as title,
   cast(asset.externalId as STRING) as sourceId
 from
-  cdf_assetSubtree('WMT:VAL') as asset
-  inner join cdf_assetSubtree('WMT:VAL') as rootAsset on asset.rootId = rootAsset.id
+  cdf_assetSubtree('{{root_asset_external_id}}') as asset
+  inner join cdf_assetSubtree('{{root_asset_external_id}}') as rootAsset on asset.rootId = rootAsset.id
