2017b229037d065032d0147de133a3ead9960b96
anders-albert <anders0albert@gmail.com>
2023-11-03 14:10:27 +0100

    fix: bug in SQL

--- examples/example_pump_data_model/transformations/pump_model-replicate-lift_station_pumps.sql
+++ examples/example_pump_data_model/transformations/pump_model-replicate-lift_station_pumps.sql
@@ -1,8 +1,8 @@
 select
   concat(cast(`parent` as STRING), ':', cast(`externalId` as STRING)) as externalId,
   `parent` as startNode,
-  node_reference('cdfTemplate', cast(`externalId` as STRING)) as endNode as endNode
+  node_reference('{{space}}', cast(`externalId` as STRING)) as endNode
 from
-  cdf_data_models("cdfTemplate", "AssetHierarchy", "1", "Asset")
+  cdf_data_models("{{space}}", "AssetHierarchy", "1", "Asset")
 where
   startswith(name, 'Pump')
--- examples/example_pump_data_model/transformations/pump_model-replicate-pump.sql
+++ examples/example_pump_data_model/transformations/pump_model-replicate-pump.sql
@@ -7,7 +7,7 @@ select
   cast(get_json_object(`metadata`, '$.LowHeadFT') as DOUBLE) as LowHeadFT,
   cast(get_json_object(`metadata`, '$.DesignPointFlowGPM') as DOUBLE) as DesignPointFlowGPM,
   cast(get_json_object(`metadata`, '$.LowHeadFlowGPM') as DOUBLE) as LowHeadFlowGPM,
-  node_reference('cdfTemplate', cast(`parent` as STRING)) as liftStation
+  parent as liftStation
 from
   cdf_data_models("cdfTemplate", "AssetHierarchy", "1", "Asset")
 where
