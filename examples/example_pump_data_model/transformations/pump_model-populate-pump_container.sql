c3371f756541667127a9496cfbc3794319688215
anders-albert <anders0albert@gmail.com>
2023-11-11 15:24:28 +0100

    refactor: Finished pump data model example

--- examples/example_pump_data_model/transformations/pump_model-populate-pump_container.sql
+++ examples/example_pump_data_model/transformations/pump_model-populate-pump_container.sql
@@ -1,13 +1,10 @@
 select
   cast(`externalId` as STRING) as externalId,
-  cast(`name` as STRING) as name,
-  cast(`description` as STRING) as description,
   cast(get_json_object(`metadata`, '$.DesignPointHeadFT') as DOUBLE) as DesignPointHeadFT,
   cast(get_json_object(`metadata`, '$.LowHeadFT') as DOUBLE) as LowHeadFT,
   cast(get_json_object(`metadata`, '$.DesignPointFlowGPM') as DOUBLE) as DesignPointFlowGPM,
-  cast(get_json_object(`metadata`, '$.LowHeadFlowGPM') as DOUBLE) as LowHeadFlowGPM,
-  parent as liftStation
+  cast(get_json_object(`metadata`, '$.LowHeadFlowGPM') as DOUBLE) as LowHeadFlowGPM
 from
-  cdf_data_models("{{model_space}}", "AssetHierarchy", "1", "Asset")
+  cdf_data_models("{{source_model_space}}", "{{source_model}}", "1", "Asset")
 where
-  startswith(name, 'Pump')
+  startswith(title, 'Pump')
--- examples/example_pump_data_model/transformations/pump_model-replicate-lift_station_pumps.sql
+++
@@ -1,8 +0,0 @@
-select
-  concat(cast(`parent` as STRING), ':', cast(`externalId` as STRING)) as externalId,
-  `parent` as startNode,
-  node_reference('{{instance_space}}', cast(`externalId` as STRING)) as endNode
-from
-  cdf_data_models("{{model_space}}", "AssetHierarchy", "1", "Asset")
-where
-  startswith(name, 'Pump')
