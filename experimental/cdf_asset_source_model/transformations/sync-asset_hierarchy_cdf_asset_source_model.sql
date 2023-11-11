c2b5a5b31daea7b0f7de4339f4bd4fe98220f944
anders-albert <anders0albert@gmail.com>
2023-11-11 13:07:35 +0100

    fix: Transformation and added instance space"

--- experimental/cdf_asset_source_model/transformations/sync-asset_hierarchy_cdf_asset_source_model.sql
+++ experimental/cdf_asset_source_model/transformations/sync-asset_hierarchy_cdf_asset_source_model.sql
@@ -1,13 +1,46 @@
+-- Root Asset
 select
   cast(`externalId` as STRING) as externalId,
-  node_reference('{{instance_space}}', `parentExternalId`) as parent,
-  node_reference('{{instance_space}}', '{{root_asset_external_id}}') as root,
+  null as parent,
+  node_reference('{{instance_space}}', '{{root_asset_external_id}}}}') as root,
   cast(`name` as STRING) as name,
   cast(`source` as STRING) as source,
   cast(`description` as STRING) as description,
   cast(`labels` as ARRAY < STRING >) as labels,
   to_json(`metadata`) as metadata
 from
-  cdf_assetSubtree('{{root_asset_external_id}}')
+  cdf_assetSubtree("{{root_asset_external_id}}}}")
 where
- isnotnull(`externalId`) and isnotnull(`parentExternalId`);
+  isnull(`parentExternalId`)
+  
+UNION ALL
+-- Pump Stations
+select
+  cast(`externalId` as STRING) as externalId,
+  node_reference('{{instance_space}}', `parentExternalId`) as parent,
+  node_reference('{{instance_space}}', '{{root_asset_external_id}}}}') as root,
+  cast(`name` as STRING) as name,
+  cast(`source` as STRING) as source,
+  cast(`description` as STRING) as description,
+  cast(`labels` as ARRAY < STRING >) as labels,
+  to_json(`metadata`) as metadata
+from
+  cdf_assetSubtree('{{root_asset_external_id}}}}')
+where
+ isnotnull(`externalId`) and isnotnull(`parentExternalId`) and not startswith(name, 'Pump')
+
+UNION ALL
+-- Pumps
+select
+  cast(`externalId` as STRING) as externalId,
+  node_reference('{{instance_space}}', `parentExternalId`) as parent,
+  node_reference('{{instance_space}}', '{{root_asset_external_id}}}}') as root,
+  cast(`name` as STRING) as name,
+  cast(`source` as STRING) as source,
+  cast(`description` as STRING) as description,
+  cast(`labels` as ARRAY < STRING >) as labels,
+  to_json(`metadata`) as metadata
+from
+  cdf_assetSubtree('{{root_asset_external_id}}}}')
+where
+ isnotnull(`externalId`) and isnotnull(`parentExternalId`) and startswith(name, 'Pump');
