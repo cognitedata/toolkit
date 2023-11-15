8d7ca8b408e78123c8823062ce97178b87b10190
Greger Wedel <greger.wedel@cognite.com>
2023-11-15 16:11:35 +0100

    Too fast...

--- demo/preproc.sh
+++ demo/preproc.sh
@@ -2,7 +2,7 @@
 echo "Running copy commands to prep deployment of demo..."
 pushd `dirname $0`
 echo "Copying my local.yaml to root of repo..."
-cp local.yaml ../
+cp local.yaml ../cognite_toolkit/
 echo "Copying config.yaml into cdf_auth_readwrite_all module..."
 cp config.cdf_auth_readwrite_all.yaml ../cognite_toolkit/common/cdf_auth_readwrite_all/config.yaml
 popd
