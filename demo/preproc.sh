40758fa9c62e53115067d47df25420da4ffaea5a
Pål Rønning <pal.ronning@cognite.com>
2023-11-15 17:27:15 +0100

    Adding missing sourceid

--- demo/preproc.sh
+++ demo/preproc.sh
@@ -5,4 +5,7 @@ echo "Copying my local.yaml to root of repo..."
 cp local.yaml ../cognite_toolkit/
 echo "Copying config.yaml into cdf_auth_readwrite_all module..."
 cp config.cdf_auth_readwrite_all.yaml ../cognite_toolkit/common/cdf_auth_readwrite_all/config.yaml
+echo "Copying config.yaml into cdf_infield_common module..."
+cp config.cdf_infield_common.yaml ../cognite_toolkit/common/cdf_infield_common/config.yaml
+
 popd
