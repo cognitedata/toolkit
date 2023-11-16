b0d73fad4259b9af30fb6a61f0126d6d5fa7cb2e
Jan Inge Bergseth <31886431+BergsethCognite@users.noreply.github.com>
2023-11-16 15:59:21 +0100

    default asset + valhall asset pipeline

---
+++ cognite_toolkit/modules/cdf_data_pipeline_asset_valhall/transformations/asset-valhall-workmate-asset-hierarchy.sql
@@ -0,0 +1,23 @@
+--
+-- Create Asset Hierarchy using Transformation
+--
+-- Input data from RAW DB table (using example data)
+--
+-- Root node has parentExternal id = ''
+-- Transformation is connected to asset data set
+-- All metadata expect selected fileds are added to metadata
+--
+SELECT 
+  sourceDb || ':' || tag          as externalId,
+  if(parentTag is null, 
+     '', 
+     sourceDb || ':' ||parentTag) as parentExternalId,
+  tag                             as name,
+  sourceDb                        as source,
+  description,
+  dataset_id("asset:valhall")     as dataSetId,
+  to_metadata_except(
+    array("sourceDb", "parentTag", "description"), *) 
+                                  as metadata
+FROM 
+  `src-asset-valhall-workmate`.`assets`
