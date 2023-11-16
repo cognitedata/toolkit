2b535afe129236b80a16cb3018dc281a03ad7ac3
Greger Wedel <greger.wedel@cognite.com>
2023-11-16 15:44:03 +0100

    Clean up gh actions and standardize

--- demo/preproc.sh
+++ demo/preproc.sh
@@ -2,10 +2,10 @@
 echo "Running copy commands to prep deployment of demo..."
 pushd `dirname $0`
 echo "Copying my local.yaml to root of repo..."
-cp local.yaml ../cognite_toolkit/
+cp local.yaml ../demo_project/
 echo "Copying config.yaml into cdf_auth_readwrite_all module..."
-cp config.cdf_auth_readwrite_all.yaml ../cognite_toolkit/common/cdf_auth_readwrite_all/config.yaml
+cp config.cdf_auth_readwrite_all.yaml ../demo_project/common/cdf_auth_readwrite_all/config.yaml
 echo "Copying config.yaml into cdf_infield_common module..."
-cp config.cdf_infield_common.yaml ../cognite_toolkit/modules/cdf_infield_common/config.yaml
+cp config.cdf_infield_common.yaml ../demo_project/modules/cdf_infield_common/config.yaml
 
 popd
