a4cc7a9dcea8f1e758917829e7cf2bb6e79ba8bd
Anders Albert <60234212+doctrino@users.noreply.github.com>
2023-11-30 12:01:54 +0100

    Approval test clean (#189)
    * refactor; Moved out fixtures for reuse
    
    * tests: Added testing for clean cmd

--- tests/test_approval_modules.py
+++ tests/test_approval_modules.py
@@ -18,7 +18,7 @@ import typer
 from cognite.client import CogniteClient
 from pytest import MonkeyPatch
 
-from cognite_toolkit.cdf import Common, build, deploy
+from cognite_toolkit.cdf import Common, build, clean, deploy
 from cognite_toolkit.cdf_tk.templates import TMPL_DIRS, read_yaml_files
 from cognite_toolkit.cdf_tk.utils import CDFToolConfig
 
@@ -26,6 +26,8 @@ REPO_ROOT = Path(__file__).parent.parent
 
 SNAPSHOTS_DIR = REPO_ROOT / "tests" / "test_approval_modules_snapshots"
 SNAPSHOTS_DIR.mkdir(exist_ok=True)
+SNAPSHOTS_DIR_CLEAN = REPO_ROOT / "tests" / "test_approval_modules_snapshots_clean"
+SNAPSHOTS_DIR_CLEAN.mkdir(exist_ok=True)
 
 
 @contextlib.contextmanager
@@ -59,14 +61,39 @@ def local_tmp_path():
     return SNAPSHOTS_DIR.parent / "tmp"
 
 
-@pytest.mark.parametrize("module_path", list(find_all_modules()))
-def test_module_approval(
-    module_path: Path,
-    local_tmp_path: Path,
-    monkeypatch: MonkeyPatch,
-    cognite_client_approval: CogniteClient,
-    data_regression,
-) -> None:
+@pytest.fixture
+def cdf_tool_config(cognite_client_approval: CogniteClient, monkeypatch: MonkeyPatch) -> CDFToolConfig:
+    monkeypatch.setenv("CDF_PROJECT", "pytest-project")
+    monkeypatch.setenv("IDP_TOKEN_URL", "dummy")
+    monkeypatch.setenv("IDP_CLIENT_ID", "dummy")
+    monkeypatch.setenv("IDP_CLIENT_SECRET", "dummy")
+
+    with chdir(REPO_ROOT):
+        # Build must always be executed from root of the project
+        cdf_tool = MagicMock(spec=CDFToolConfig)
+        cdf_tool.verify_client.return_value = cognite_client_approval
+        cdf_tool.verify_capabilities.return_value = cognite_client_approval
+        cdf_tool.failed = False
+
+        cdf_tool.verify_dataset.return_value = 42
+        cdf_tool.data_set_id = 999
+        yield cdf_tool
+
+
+@pytest.fixture
+def typer_context(cdf_tool_config: CDFToolConfig) -> typer.Context:
+    context = MagicMock(spec=typer.Context)
+    context.obj = Common(
+        verbose=False,
+        override_env=True,
+        cluster="pytest",
+        project="pytest-project",
+        mockToolGlobals=cdf_tool_config,
+    )
+    return context
+
+
+def mock_read_yaml_files(module_path: Path, monkeypatch: MonkeyPatch) -> None:
     def fake_read_yaml_files(
         yaml_dirs: list[str],
         name: str | None = None,
@@ -76,46 +103,67 @@ def test_module_approval(
         return read_yaml_files(yaml_dirs, name)
 
     monkeypatch.setattr("cognite_toolkit.cdf_tk.templates.read_yaml_files", fake_read_yaml_files)
-    monkeypatch.setenv("CDF_PROJECT", "pytest-project")
-    monkeypatch.setenv("IDP_TOKEN_URL", "dummy")
-    monkeypatch.setenv("IDP_CLIENT_ID", "dummy")
-    monkeypatch.setenv("IDP_CLIENT_SECRET", "dummy")
 
-    with chdir(REPO_ROOT):
-        # Build must always be executed from root of the project
-        context = MagicMock(spec=typer.Context)
-        cdf_tool = MagicMock(spec=CDFToolConfig)
-        cdf_tool.verify_client.return_value = cognite_client_approval
-        cdf_tool.verify_capabilities.return_value = cognite_client_approval
-        cdf_tool.failed = False
 
-        cdf_tool.verify_dataset.return_value = 42
-        cdf_tool.data_set_id = 999
+@pytest.mark.parametrize("module_path", list(find_all_modules()))
+def test_deploy_module_approval(
+    module_path: Path,
+    local_tmp_path: Path,
+    monkeypatch: MonkeyPatch,
+    cognite_client_approval: CogniteClient,
+    cdf_tool_config: CDFToolConfig,
+    typer_context: typer.Context,
+    data_regression,
+) -> None:
+    mock_read_yaml_files(module_path, monkeypatch)
+    build(
+        typer_context,
+        source_dir="./cognite_toolkit",
+        build_dir=str(local_tmp_path),
+        build_env="test",
+        clean=True,
+    )
+    deploy(
+        typer_context,
+        build_dir=str(local_tmp_path),
+        build_env="test",
+        interactive=False,
+        drop=True,
+        drop_data=True,
+        dry_run=False,
+        include=[],
+    )
 
-        context.obj = Common(
-            verbose=False,
-            override_env=True,
-            cluster="pytest",
-            project="pytest-project",
-            mockToolGlobals=cdf_tool,
-        )
-        build(
-            context,
-            source_dir="./cognite_toolkit",
-            build_dir=str(local_tmp_path),
-            build_env="test",
-            clean=True,
-        )
-        deploy(
-            context,
-            build_dir=str(local_tmp_path),
-            build_env="test",
-            interactive=False,
-            drop=True,
-            drop_data=True,
-            dry_run=False,
-            include=[],
-        )
-
-        dump = cognite_client_approval.dump()
+    dump = cognite_client_approval.dump()
     data_regression.check(dump, fullpath=SNAPSHOTS_DIR / f"{module_path.name}.yaml")
+
+
+@pytest.mark.parametrize("module_path", list(find_all_modules()))
+def test_clean_module_approval(
+    module_path: Path,
+    local_tmp_path: Path,
+    monkeypatch: MonkeyPatch,
+    cognite_client_approval: CogniteClient,
+    cdf_tool_config: CDFToolConfig,
+    typer_context: typer.Context,
+    data_regression,
+) -> None:
+    mock_read_yaml_files(module_path, monkeypatch)
+    build(
+        typer_context,
+        source_dir="./cognite_toolkit",
+        build_dir=str(local_tmp_path),
+        build_env="test",
+        clean=True,
+    )
+    clean(
+        typer_context,
+        build_dir=str(local_tmp_path),
+        build_env="test",
+        interactive=False,
+        dry_run=False,
+        include=[],
+    )
+
+    dump = cognite_client_approval.dump()
+    data_regression.check(dump, fullpath=SNAPSHOTS_DIR_CLEAN / f"{module_path.name}.yaml")
