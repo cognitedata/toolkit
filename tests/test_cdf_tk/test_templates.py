0e26c01940d3de61834f7c9da42f22666c5d2fa3
Anders Albert <60234212+doctrino@users.noreply.github.com>
2023-12-11 14:53:33 +0100

    One Config (#200)
    * refactor; Moved modules around
    
    * refactor: update init command to new folder structure
    
    * feat: Created generate_config
    
    * refactor: Generate config
    
    * refactor; Work towards one config
    
    * refactor: Towards one module
    
    * fix: Copy over single file
    
    * fix: iterate modules function
    
    * tests; updated test
    
    * build; Added config yaml
    
    * refactor: populate config
    
    * tests: Added test to check config.yaml is uptodate
    
    * build: ruamel.yaml
    
    * refactor: preserve comments
    
    * refactor; update test data
    
    * refactor: print out on init
    
    * fix: no existing config
    
    * style: fix typo
    
    * tests: updated tests
    
    * refactor: better error message
    
    * refactor: update auth command
    
    * fix; path'
    
    * Update README
    
    * Move cdf_apm_base into separate folder
    
    * refactor: review feedback graceful exit
    
    * refactor; rename to cognite and custom modules
    
    * refactor: Added example module
    
    * refactor: Finish renaming and adjust example
    
    * fix: place variables correctly in hierarchy
    
    * refactor: indent comments
    
    * fix: indent of comments
    
    * docs: changelog and documentation
    
    * poetry update
    
    ---------
    
    Co-authored-by: Greger Wedel <greger.wedel@cognite.com>

---
+++ tests/test_cdf_tk/test_templates.py
@@ -0,0 +1,87 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Any
+
+import pytest
+import yaml
+
+from cognite_toolkit.cdf_tk.templates import COGNITE_MODULES, create_local_config, generate_config, split_config
+
+BUILD_CONFIG = Path(__file__).parent / "project_configs"
+
+
+def generate_config_test_cases():
+    expected = {
+        COGNITE_MODULES: {
+            "a_module": {
+                "readwrite_source_id": "<change_me>",
+                "readonly_source_id": "<change_me>",
+            },
+            "another_module": {
+                "default_location": "oid",
+                "source_asset": "workmate",
+                "source_workorder": "workmate",
+                "source_files": "fileshare",
+                "source_timeseries": "pi",
+            },
+            "top_variable": "<top_variable>",
+            "parent_module": {"child_module": {"child_variable": "<change_me>"}},
+        },
+    }
+
+    yield pytest.param(yaml.safe_dump(expected, sort_keys=False), None, id="Include all")
+
+    only_a_module = {
+        COGNITE_MODULES: {
+            "a_module": {
+                "readwrite_source_id": "<change_me>",
+                "readonly_source_id": "<change_me>",
+            },
+        }
+    }
+    yield pytest.param(yaml.safe_dump(only_a_module, sort_keys=False), {"a_module"}, id="Include one module")
+
+
+@pytest.mark.parametrize(
+    "expected, include",
+    list(generate_config_test_cases()),
+)
+def test_generate_config(expected: str, include: set[str] | None) -> None:
+    actual, _ = generate_config(BUILD_CONFIG, include_modules=include)
+
+    assert actual == expected
+
+
+@pytest.fixture()
+def my_config():
+    return {
+        "top_variable": "my_top_variable",
+        "module_a": {
+            "readwrite_source_id": "my_readwrite_source_id",
+            "readonly_source_id": "my_readonly_source_id",
+        },
+        "parent": {"child": {"child_variable": "my_child_variable"}},
+    }
+
+
+def test_split_config(my_config: dict[str, Any]) -> None:
+    expected = {
+        "": {"top_variable": "my_top_variable"},
+        "module_a": {
+            "readwrite_source_id": "my_readwrite_source_id",
+            "readonly_source_id": "my_readonly_source_id",
+        },
+        "parent.child": {"child_variable": "my_child_variable"},
+    }
+    actual = split_config(my_config)
+
+    assert actual == expected
+
+
+def test_create_local_config(my_config: dict[str, Any]):
+    configs = split_config(my_config)
+
+    local_config = create_local_config(configs, Path("parent/child/auth/"))
+
+    assert dict(local_config.items()) == {"top_variable": "my_top_variable", "child_variable": "my_child_variable"}
