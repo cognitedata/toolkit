8a10498cc7fa664e20c18076f6275f2fbe48ace9
Jan Inge Bergseth <31886431+BergsethCognite@users.noreply.github.com>
2023-12-12 15:11:53 +0100

    config update

--- cognite_toolkit/examples/cdf_data_pipeline_files_oid/transformations/tr_files_oid_fileshare_file_metadata.sql
+++ cognite_toolkit/examples/cdf_data_pipeline_files_oid/transformations/tr_files_oid_fileshare_file_metadata.sql
@@ -4,15 +4,15 @@
 -- Input data from RAW DB table (using example data) and uploaded files
 --
 SELECT
-  file.id                         as id,
-  file.name                       as name,
-  'valhall:'|| file.name          as  externalId,
-  meta.source                     as source,
-  meta.`mime_type`                as mimeType,
-  dataset_id("ds-files:valhall")  as dataSetId,
-  to_metadata(doc_type)           as metadata
+  file.id                          as id,
+  file.name                        as name,
+  '{{location_name}}_'|| file.name as  externalId,
+  meta.source                      as source,
+  meta.`mime_type`                 as mimeType,
+  dataset_id('{{files_dataset}}')  as dataSetId,
+  to_metadata(doc_type)            as metadata
 FROM 
-  `src-files-valhall-fileshare`.`files_meta` meta,
+  `{{files_raw_input_db}}`.`{{files_raw_input_table}}` meta,
   _cdf.files                                 file 
 WHERE 
   file.name = meta.name
