27438bf914f7b2e17e1fd2baadee7ff5010b3496
Pål Rønning <pal.ronning@cognite.com>
2023-12-13 14:55:21 +0100

    re-adding demo/config.yaml

--- demo/preproc.py
+++ demo/preproc.py
@@ -10,7 +10,7 @@ DEMO_PROJECT = THIS_FOLDER.parent / "demo_project"
 def run() -> None:
     print("Running copy commands to prep deployment of demo...")
     os.makedirs(DEMO_PROJECT, exist_ok=True)
-    print("Copying my enviroments.yaml to root of repo...")
+    print("Copying my environments.yaml to root of repo...")
     shutil.copy(THIS_FOLDER / "environments.yaml", DEMO_PROJECT / "environments.yaml")
     print("Copying config.yaml into demo project...")
     shutil.copy(THIS_FOLDER / "config.yaml", DEMO_PROJECT / "config.yaml")
