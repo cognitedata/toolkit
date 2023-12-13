d69ddd864b742c04f8e9caabea1f2ba463e9d577
Pål Rønning <pal.ronning@cognite.com>
2023-12-13 13:28:09 +0100

    Asset pipeline in demo project

--- demo/preproc.py
+++ demo/preproc.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import shutil
+import os, shutil
 from pathlib import Path
 
 import yaml
@@ -10,14 +10,18 @@ DEMO_PROJECT = THIS_FOLDER.parent / "demo_project"
 
 def run() -> None:
     print("Running copy commands to prep deployment of demo...")
+    os.makedirs(DEMO_PROJECT, exist_ok=True)
     print("Copying my enviroments.yaml to root of repo...")
-    shutil.copy(THIS_FOLDER / "environments.yaml", DEMO_PROJECT)
+    shutil.copy(THIS_FOLDER / "environments.yaml", DEMO_PROJECT / "environments.yaml")
     print("Copying config.yaml into demo project...")
+    shutil.copy(THIS_FOLDER / "config.yaml", DEMO_PROJECT / "config.yaml")
     config_yaml_path = DEMO_PROJECT / "config.yaml"
+
     variables = yaml.safe_load((THIS_FOLDER / "config.yaml").read_text())
     config_yaml = config_yaml_path.read_text()
     for key, value in variables.items():
         config_yaml = config_yaml.replace(f"{key}: <change_me>", f"{key}: {value}")
+    
     config_yaml_path.write_text(config_yaml)
 
 
