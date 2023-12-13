19f46ff5504b69b54b081613d218276e0e6aec54
Pål Rønning <pal.ronning@cognite.com>
2023-12-13 14:17:40 +0100

    adjusting demo deploy preprocessing

--- demo/preproc.py
+++ demo/preproc.py
@@ -6,20 +6,24 @@ import yaml
 
 THIS_FOLDER = Path(__file__).parent.absolute()
 DEMO_PROJECT = THIS_FOLDER.parent / "demo_project"
-
+TOOLKIT_FOLDER = THIS_FOLDER.parent / "cognite_toolkit"
 
 def run() -> None:
+
+    print(TOOLKIT_FOLDER)
+
     print("Running copy commands to prep deployment of demo...")
     os.makedirs(DEMO_PROJECT, exist_ok=True)
     print("Copying my enviroments.yaml to root of repo...")
     shutil.copy(THIS_FOLDER / "environments.yaml", DEMO_PROJECT / "environments.yaml")
     print("Copying config.yaml into demo project...")
-    shutil.copy(THIS_FOLDER / "config.yaml", DEMO_PROJECT / "config.yaml")
+    shutil.copy(TOOLKIT_FOLDER / "config.yaml", DEMO_PROJECT / "config.yaml")
     config_yaml_path = DEMO_PROJECT / "config.yaml"
 
-    variables = yaml.safe_load((THIS_FOLDER / "config.yaml").read_text())
+    variables = yaml.safe_load((THIS_FOLDER / "demo_config.yaml").read_text())
     config_yaml = config_yaml_path.read_text()
     for key, value in variables.items():
+        print(f"updating {key}")
         config_yaml = config_yaml.replace(f"{key}: <change_me>", f"{key}: {value}")
     
     config_yaml_path.write_text(config_yaml)
