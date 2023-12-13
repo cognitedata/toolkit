1ea554edf451ee316ebe4bc9a8c7c40ddc0fd6df
anders-albert <anders0albert@gmail.com>
2023-12-13 08:00:01 +0100

    refactor; Single transformation

--- cognite_toolkit/cognite_modules/infield/cdf_infield_location/transformations/tr_asset_oid_workmate_infield_sync_assets_from_hierarchy_to_apm.sql
+++ cognite_toolkit/cognite_modules/infield/cdf_infield_location/transformations/tr_asset_oid_workmate_infield_sync_assets_from_hierarchy_to_apm.sql
@@ -1,5 +1,24 @@
+--- Parents
 select
-  cast(asset.externalId as STRING) as externalId, 
+  cast(asset.externalId as STRING) as externalId,
+  (case
+    when isnull(asset.parentExternalId) then null
+    else node_reference('sp_asset_{{default_location}}_source', asset.parentExternalId)
+  end) as parent,
+  null as source,
+  null as root,
+  null as description,
+  null as title,
+  null as sourceId
+from
+  cdf_assetSubtree('{{root_asset_external_id}}') as asset
+  inner join cdf_assetSubtree('{{root_asset_external_id}}') as rootAsset on asset.rootId = rootAsset.id
+
+UNION ALL
+--- Children
+select
+  cast(asset.externalId as STRING) as externalId,
+  null as parent,
   cast("Asset Hierarachy" as STRING) as source,
   node_reference('sp_asset_{{default_location}}_source', cast(rootAsset.externalId as STRING)) as root,
   cast(asset.description as STRING) as description,
