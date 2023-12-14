a491a567a20818e889f2dce45647dbf434e902e6
anders-albert <anders0albert@gmail.com>
2023-12-14 09:08:24 +0100

    refactor: a few more fixes

--- cognite_toolkit/cognite_modules/examples/cdf_apm_simple_data_model/transformations/tr_timeseries_oid_pi_apm_simple_load_timeseries2assets.sql
+++ cognite_toolkit/cognite_modules/examples/cdf_apm_simple_data_model/transformations/tr_timeseries_oid_pi_apm_simple_load_timeseries2assets.sql
@@ -2,4 +2,4 @@ select
   cast(`asset` as STRING) as externalId,
   array(timeseries) as metrics
 from
-  `files_{{default_location}}_{{source_files}}`.`timeseries2assets`;
+  `files_{{default_location}}_{{source_timeseries}}`.`timeseries2assets`;
