5ed6f9622ff3345b0abad89318578d9a078b5410
Greger Wedel <greger.wedel@cognite.com>
2024-01-08 11:48:32 +0100

    Refactored with ApprovalCogniteClient

--- tests/test_cdf_tk/test_run.py
+++ tests/test_cdf_tk/test_run.py
@@ -1,35 +1,24 @@
 from unittest.mock import MagicMock
 
-from cognite.client.data_classes.capabilities import (
-    SessionsAcl,
-)
+from requests import Response
 
 from cognite_toolkit.cdf_tk.run import get_oneshot_session
 from cognite_toolkit.cdf_tk.utils import CDFToolConfig
 from tests.conftest import ApprovalCogniteClient
-from tests.test_cdf_tk.conftest import get_capabilities_mock, get_post_mock
 
 
-def test_get_oneshot_session(MockCDFToolConfig: CDFToolConfig):
-    MockCDFToolConfig._client.iam.token.inspect = get_capabilities_mock(
-        [
-            SessionsAcl(
-                [SessionsAcl.Action.Create, SessionsAcl.Action.Delete, SessionsAcl.Action.List],
-                scope=SessionsAcl.Scope.All(),
-            ),
-        ]
-    )
-    MockCDFToolConfig._client.post = get_post_mock(
-        {
-            "items": [
-                {
-                    "oneshotTokenExchange": True,
-                },
-            ],
-        },
-        b'{"items":[{"id":5192234284402249,"nonce":"QhlCnImCBwBNc72N","status":"READY","type":"ONESHOT_TOKEN_EXCHANGE"}]}',
-    )
-    session = get_oneshot_session(MockCDFToolConfig)
+def test_get_oneshot_session(cognite_client_approval: ApprovalCogniteClient):
+    cdf_tool = MagicMock(spec=CDFToolConfig)
+    cdf_tool.client = cognite_client_approval.mock_client
+    cdf_tool.verify_client.return_value = cognite_client_approval.mock_client
+    cdf_tool.verify_capabilities.return_value = cognite_client_approval.mock_client
+    cdf_tool.oauth_credentials = cognite_client_approval.mock_client
+    cdf_tool.oauth_credentials.authorization_header.return_value = ("Bearer", "123")
+    sessionResponse = Response()
+    sessionResponse.status_code = 200
+    sessionResponse._content = b'{"items":[{"id":5192234284402249,"nonce":"QhlCnImCBwBNc72N","status":"READY","type":"ONESHOT_TOKEN_EXCHANGE"}]}'
+    cdf_tool.client.post.return_value = sessionResponse
+    session = get_oneshot_session(cdf_tool)
     assert session.id == 5192234284402249
     assert session.nonce == "QhlCnImCBwBNc72N"
     assert session.status == "READY"
