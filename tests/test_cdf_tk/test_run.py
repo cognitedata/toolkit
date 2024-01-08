a522be08fb24b82c1291c62c7b937e62a9dfb083
Greger Wedel <greger.wedel@cognite.com>
2024-01-08 13:41:44 +0100

    Add test of run_transformation

--- tests/test_cdf_tk/test_run.py
+++ tests/test_cdf_tk/test_run.py
@@ -1,8 +1,9 @@
 from unittest.mock import MagicMock, Mock
 
+from cognite.client.data_classes.transformations import Transformation, TransformationList
 from requests import Response
 
-from cognite_toolkit.cdf_tk.run import get_oneshot_session
+from cognite_toolkit.cdf_tk.run import get_oneshot_session, run_transformation
 from cognite_toolkit.cdf_tk.utils import CDFToolConfig
 from tests.conftest import ApprovalCogniteClient
 
@@ -27,7 +28,20 @@ def test_get_oneshot_session(cognite_client_approval: ApprovalCogniteClient):
 
 def test_run_transformation(cognite_client_approval: ApprovalCogniteClient):
     cdf_tool = MagicMock(spec=CDFToolConfig)
+    cdf_tool.client = cognite_client_approval.mock_client
     cdf_tool.verify_client.return_value = cognite_client_approval.mock_client
     cdf_tool.verify_capabilities.return_value = cognite_client_approval.mock_client
+    cdf_tool.oauth_credentials = Mock()
+    cdf_tool.oauth_credentials.authorization_header.return_value = ("Bearer", "123")
+    sessionResponse = Response()
+    sessionResponse.status_code = 200
+    sessionResponse._content = b'{"items":[{"id":5192234284402249,"nonce":"QhlCnImCBwBNc72N","status":"READY","type":"ONESHOT_TOKEN_EXCHANGE"}]}'
+    cdf_tool.client.post.return_value = sessionResponse
+    transformation = Transformation(
+        name="Test transformation",
+        external_id="test",
+        query="SELECT * FROM timeseries",
+    )
+    cognite_client_approval.client.transformations.retrieve_multiple.return_value = TransformationList([transformation])
 
-    # cognite_client_approval.append(DataSet, first)
+    run_transformation(cdf_tool, "test")
