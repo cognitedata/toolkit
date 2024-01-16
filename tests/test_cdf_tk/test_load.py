92cdda692b94ca258c113454e095827d15819cf7
anders-albert <anders0albert@gmail.com>
2024-01-16 10:46:36 +0100

    test: failing test for existing dataset

--- tests/test_cdf_tk/test_load.py
+++ tests/test_cdf_tk/test_load.py
@@ -12,6 +12,7 @@ from cognite_toolkit.cdf_tk.load import (
     DataSetsLoader,
     FileMetadataLoader,
     ResourceLoader,
+    TimeSeriesLoader,
 )
 from cognite_toolkit.cdf_tk.utils import CDFToolConfig
 from tests.approval_client import ApprovalCogniteClient
@@ -44,28 +45,29 @@ def test_loader_class(
     data_regression.check(dump, fullpath=SNAPSHOTS_DIR / f"{directory.name}.yaml")
 
 
-def test_upsert_data_set(cognite_client_approval: ApprovalCogniteClient):
-    cdf_tool = MagicMock(spec=CDFToolConfig)
-    cdf_tool.verify_client.return_value = cognite_client_approval.mock_client
-    cdf_tool.verify_capabilities.return_value = cognite_client_approval.mock_client
+class DataSetsLoaderTest:
+    def test_upsert_data_set(self, cognite_client_approval: ApprovalCogniteClient):
+        cdf_tool = MagicMock(spec=CDFToolConfig)
+        cdf_tool.verify_client.return_value = cognite_client_approval.mock_client
+        cdf_tool.verify_capabilities.return_value = cognite_client_approval.mock_client
 
-    loader = DataSetsLoader.create_loader(cdf_tool)
-    loaded = loader.load_resource(DATA_FOLDER / "data_sets" / "1.my_datasets.yaml", cdf_tool, skip_validation=False)
-    assert len(loaded) == 2
+        loader = DataSetsLoader.create_loader(cdf_tool)
+        loaded = loader.load_resource(DATA_FOLDER / "data_sets" / "1.my_datasets.yaml", cdf_tool, skip_validation=False)
+        assert len(loaded) == 2
 
-    first = DataSet.load(loaded[0].dump())
-    # Set the properties that are set on the server side
-    first.id = 42
-    first.created_time = 42
-    first.last_updated_time = 42
-    # Simulate that the data set is already in CDF
-    cognite_client_approval.append(DataSet, first)
+        first = DataSet.load(loaded[0].dump())
+        # Set the properties that are set on the server side
+        first.id = 42
+        first.created_time = 42
+        first.last_updated_time = 42
+        # Simulate that the data set is already in CDF
+        cognite_client_approval.append(DataSet, first)
 
-    to_create, to_change, unchanged = loader.to_create_changed_unchanged_triple(loaded)
+        to_create, to_change, unchanged = loader.to_create_changed_unchanged_triple(loaded)
 
-    assert len(to_create) == 1
-    assert len(to_change) == 0
-    assert len(unchanged) == 1
+        assert len(to_create) == 1
+        assert len(to_change) == 0
+        assert len(unchanged) == 1
 
 
 class TestAuthLoader:
@@ -89,3 +91,41 @@ capabilities:
         loaded = loader.load_resource(Path("group_file.yaml"), cdf_tool_config, skip_validation=True)
 
         assert loaded.name == "some_name"
+
+
+class TestTimeSeriesLoader:
+    timeseries_yaml = """
+externalId: pi_160696
+name: VAL_23-PT-92504:X.Value
+dataSetExternalId: ds_timeseries_oid
+isString: false
+metadata:
+  compdev: '0'
+  location5: '2'
+isStep: false
+description: PH 1stStgSuctCool Gas Out
+"""
+
+    def test_load_skip_validation_no_preexisting_dataset(
+        self, cognite_client_approval: ApprovalCogniteClient, cdf_tool_config: CDFToolConfig, monkeypatch: MonkeyPatch
+    ) -> None:
+        loader = TimeSeriesLoader(cognite_client_approval.mock_client)
+        mock_read_yaml_file({"timeseries.yaml": yaml.safe_load(self.timeseries_yaml)}, monkeypatch)
+
+        loaded = loader.load_resource(Path("timeseries.yaml"), cdf_tool_config, skip_validation=True)
+
+        assert len(loaded) == 1
+        assert loaded[0].data_set_id == -1
+
+    def test_load_skip_validation_with_preexisting_dataset(
+        self, cognite_client_approval: ApprovalCogniteClient, cdf_tool_config: CDFToolConfig, monkeypatch: MonkeyPatch
+    ) -> None:
+        cognite_client_approval.append(DataSet, DataSet(id=12345, external_id="ds_timeseries_oid"))
+        loader = TimeSeriesLoader(cognite_client_approval.mock_client)
+
+        mock_read_yaml_file({"timeseries.yaml": yaml.safe_load(self.timeseries_yaml)}, monkeypatch)
+
+        loaded = loader.load_resource(Path("timeseries.yaml"), cdf_tool_config, skip_validation=True)
+
+        assert len(loaded) == 1
+        assert loaded[0].data_set_id == 12345
