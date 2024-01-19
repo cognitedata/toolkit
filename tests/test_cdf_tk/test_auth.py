1228c3549cc319eda67520f002d6dcf83f9a8620
Greger Wedel <greger.wedel@cognite.com>
2024-01-19 13:56:07 +0100

    Add more tests

--- tests/test_cdf_tk/test_auth.py
+++ tests/test_cdf_tk/test_auth.py
@@ -1,10 +1,13 @@
 import json
 import platform
+from dataclasses import dataclass
 from pathlib import Path
 from unittest.mock import MagicMock
 
 import pytest
 import yaml
+from cognite.client._api.iam import IAMAPI
+from cognite.client.data_classes._base import CogniteResource, CogniteResourceList
 from cognite.client.data_classes.capabilities import (
     AllProjectsScope,
     ProjectCapability,
@@ -25,11 +28,25 @@ SNAPSHOTS_DIR = THIS_FOLDER / f"{TEST_PREFIX}_data_snapshots"
 SNAPSHOTS_DIR.mkdir(exist_ok=True)
 
 
+@dataclass
+class CDFDataFixture:
+    cdf_tool: CDFToolConfig = None
+    approval_client: ApprovalCogniteClient = None
+    # The data dict is used to keep the pre-loaded CDF project data.
+    # Each CogniteResource needs to be added to the approval_client after the test
+    # has modified the data for its test purposes.
+    # E.g.
+    #
+    # approval_client.append(TokenInspection, modified_inspect_result)
+    # approval_client.append(Group, modified_group_list)
+    data: dict[CogniteResource, CogniteResource | CogniteResourceList] = None
+
+
 @pytest.fixture
-def cdf_tool(
+def cdf_data_fixture(
     cognite_client_approval: ApprovalCogniteClient,
     monkeypatch: MonkeyPatch,
-) -> CDFToolConfig:
+) -> CDFDataFixture:
     monkeypatch.setenv("CDF_PROJECT", "pytest-project")
     monkeypatch.setenv("CDF_CLUSTER", "bluefield")
     monkeypatch.setenv("IDP_TOKEN_URL", "dummy")
@@ -42,8 +59,6 @@ def cdf_tool(
     cdf_tool = MagicMock(spec=CDFToolConfig)
     cdf_tool.failed = False
     cdf_tool.environment_variables.side_effect = real_config.environment_variables
-    cdf_tool.cognite_approval_client = cognite_client_approval
-    cdf_tool.client = cognite_client_approval.mock_client
     # Load the rw-group and add it to the mock client as an existing resource
     group = Group.load(yaml.safe_load((DATA_FOLDER / "rw-group.yaml").read_text()))
     project_capabilities: ProjectCapabilityList = []
@@ -52,12 +67,8 @@ def cdf_tool(
     inspect_result = TokenInspection(
         subject="test@test.com",
         projects=[ProjectSpec("pytest-project", groups=[1234567890])],
-        capabilities=project_capabilities,
+        capabilities=ProjectCapabilityList(project_capabilities),
     )
-    # This is to mock the iam.token.inspect call
-    cognite_client_approval.append(TokenInspection, inspect_result)
-    # This is to mock the iam.groups.create call
-    cognite_client_approval.append(Group, GroupList([group]))
 
     # Mock the get call to return the project info
     def mock_get_json(*args, **kwargs):
@@ -67,26 +78,125 @@ def cdf_tool(
 
     # Set the mock get call to return the project info
     cognite_client_approval.client.get.side_effect = mock_get_json
-    return cdf_tool
+
+    # Set the side effect of the MagicMock to the real method
+    cognite_client_approval.client.iam.compare_capabilities.side_effect = IAMAPI.compare_capabilities
+
+    return CDFDataFixture(
+        cdf_tool=cdf_tool,
+        approval_client=cognite_client_approval,
+        data={
+            TokenInspection: inspect_result,
+            Group: GroupList([group]),
+            # NOTE! If you add more resources to be pre-loaded in the CDF project, add them here.
+        },
+    )
 
 
-def test_auth_verify(
-    data_regression,
+def test_auth_verify_happypath(
     file_regression,
-    cdf_tool: CDFToolConfig,
+    cdf_data_fixture: CDFDataFixture,
     capfd,
 ):
-    check_auth(cdf_tool, group_file=DATA_FOLDER / "rw-group.yaml")
-
+    # First add the pre-loaded data to the approval_client
+    for resource, data in cdf_data_fixture.data.items():
+        cdf_data_fixture.approval_client.append(resource, data)
+    # Then make sure that the CogniteClient used is the one mocked by
+    # the approval_client
+    cdf_data_fixture.cdf_tool.client = cdf_data_fixture.approval_client.mock_client
+    check_auth(cdf_data_fixture.cdf_tool, group_file=DATA_FOLDER / "rw-group.yaml")
     out, _ = capfd.readouterr()
     if platform.system() == "Windows":
         # Windows console use different characters for tables in rich.
-        fullpath = SNAPSHOTS_DIR / f"{TEST_PREFIX}_auth_verify_windows.txt"
+        fullpath = SNAPSHOTS_DIR / f"{TEST_PREFIX}_auth_verify_happypath_windows.txt"
     else:
-        fullpath = SNAPSHOTS_DIR / f"{TEST_PREFIX}_auth_verify.txt"
+        fullpath = SNAPSHOTS_DIR / f"{TEST_PREFIX}_auth_verify_happypath.txt"
     file_regression.check(out, encoding="utf-8", fullpath=fullpath)
 
-    dump = cdf_tool.cognite_approval_client.dump()
+    dump = cdf_data_fixture.approval_client.dump()
     assert dump == {}
-    # calls = cdf_tool.cognite_approval_client.retrieve_calls()
-    # data_regression.check(calls, fullpath=SNAPSHOTS_DIR / "auth_verify.yaml")
+
+
+def test_auth_verify_wrong_capabilities(
+    file_regression,
+    cdf_data_fixture: CDFDataFixture,
+    capfd,
+):
+    # Remove the last 3 capabilities from the inspect result to make a test case
+    # with wrong capabilities in the current CDF group.
+    for _ in range(1, 4):
+        del cdf_data_fixture.data[TokenInspection].capabilities[-1]
+    # Add the pre-loaded data to the approval_client
+    for resource, data in cdf_data_fixture.data.items():
+        cdf_data_fixture.approval_client.append(resource, data)
+    # Then make sure that the CogniteClient used is the one mocked by
+    # the approval_client
+    cdf_data_fixture.cdf_tool.client = cdf_data_fixture.approval_client.mock_client
+    check_auth(cdf_data_fixture.cdf_tool, group_file=DATA_FOLDER / "rw-group.yaml")
+    out, _ = capfd.readouterr()
+    if platform.system() == "Windows":
+        # Windows console use different characters for tables in rich.
+        fullpath = SNAPSHOTS_DIR / f"{TEST_PREFIX}_auth_verify_wrong_capabilities_windows.txt"
+    else:
+        fullpath = SNAPSHOTS_DIR / f"{TEST_PREFIX}_auth_verify_wrong_capabilities.txt"
+    file_regression.check(out, encoding="utf-8", fullpath=fullpath)
+
+    dump = cdf_data_fixture.approval_client.dump()
+    assert dump == {}
+
+
+def test_auth_verify_two_groups(
+    file_regression,
+    cdf_data_fixture: CDFDataFixture,
+    capfd,
+):
+    # Add another group
+    cdf_data_fixture.data[Group].append(Group.load(yaml.safe_load((DATA_FOLDER / "rw-group.yaml").read_text())))
+    cdf_data_fixture.data[Group][1].name = "2nd group"
+    # Add the pre-loaded data to the approval_client
+    for resource, data in cdf_data_fixture.data.items():
+        cdf_data_fixture.approval_client.append(resource, data)
+    # Then make sure that the CogniteClient used is the one mocked by
+    # the approval_client
+    cdf_data_fixture.cdf_tool.client = cdf_data_fixture.approval_client.mock_client
+    check_auth(cdf_data_fixture.cdf_tool, group_file=DATA_FOLDER / "rw-group.yaml")
+    out, _ = capfd.readouterr()
+    if platform.system() == "Windows":
+        # Windows console use different characters for tables in rich.
+        fullpath = SNAPSHOTS_DIR / f"{TEST_PREFIX}_auth_verify_two_groups_windows.txt"
+    else:
+        fullpath = SNAPSHOTS_DIR / f"{TEST_PREFIX}_auth_verify_two_groups.txt"
+    file_regression.check(out, encoding="utf-8", fullpath=fullpath)
+
+    dump = cdf_data_fixture.approval_client.dump()
+    assert dump == {}
+
+
+def test_auth_verify_no_capabilities(
+    file_regression,
+    cdf_data_fixture: CDFDataFixture,
+    capfd,
+):
+    # Add the pre-loaded data to the approval_client
+    for resource, data in cdf_data_fixture.data.items():
+        cdf_data_fixture.approval_client.append(resource, data)
+    # Then make sure that the CogniteClient used is the one mocked by
+    # the approval_client
+    cdf_data_fixture.cdf_tool.client = cdf_data_fixture.approval_client.mock_client
+
+    def mock_verify_client(*args, **kwargs):
+        raise Exception("No capabilities")
+
+    cdf_data_fixture.cdf_tool.verify_client.side_effect = mock_verify_client
+
+    check_auth(cdf_data_fixture.cdf_tool, group_file=DATA_FOLDER / "rw-group.yaml")
+    out, _ = capfd.readouterr()
+    if platform.system() == "Windows":
+        # Windows console use different characters for tables in rich.
+        fullpath = SNAPSHOTS_DIR / f"{TEST_PREFIX}_auth_verify_no_capabilities_windows.txt"
+    else:
+        fullpath = SNAPSHOTS_DIR / f"{TEST_PREFIX}_auth_verify_no_capabilities.txt"
+    file_regression.check(out, encoding="utf-8", fullpath=fullpath)
+
+    dump = cdf_data_fixture.approval_client.dump()
+    assert dump == {}
