6c0e03ecfdd32385167153e8dc5c23b49d718270
anders-albert <anders0albert@gmail.com>
2024-01-22 09:52:28 +0100

    refactor: supported naming restrictions for containers

--- tests/test_integration/constants.py
+++ tests/test_integration/constants.py
@@ -3,4 +3,4 @@ import sys
 
 # This is needed as we run tests for two different versions of Python in parallel.
 # The platform.system() is not used, but is here in case we start testing on Windows as well.
-RUN_UNIQUE_ID = f"{platform.system()}-{sys.version_info.major}-{sys.version_info.minor}"
+RUN_UNIQUE_ID = f"{platform.system()}_{sys.version_info.major}_{sys.version_info.minor}"
