ac33c1fbcaa606f03cd8c0d2edd71dcd4d1aede0
anders-albert <anders0albert@gmail.com>
2024-01-23 18:07:38 +0100

    refactor: update demo

--- demo/preproc.py
+++ demo/preproc.py
@@ -10,10 +10,8 @@ DEMO_PROJECT = THIS_FOLDER.parent / "demo_project"
 def run() -> None:
     print("Running copy commands to prep deployment of demo...")
     os.makedirs(DEMO_PROJECT, exist_ok=True)
-    print("Copying my environments.yaml to root of repo...")
-    shutil.copy(THIS_FOLDER / "environments.yaml", DEMO_PROJECT / "environments.yaml")
-    print("Copying config.yaml into demo project...")
-    shutil.copy(THIS_FOLDER / "config.yaml", DEMO_PROJECT / "config.yaml")
+    print("Copying my demo.config.yaml to root of repo...")
+    shutil.copy(THIS_FOLDER / "demo.config.yaml", DEMO_PROJECT / "demo.config.yaml")
 
 
 if __name__ == "__main__":
