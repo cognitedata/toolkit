14d30d73c121850674dfc010b418b05ec80c3b45
anders-albert <anders0albert@gmail.com>
2024-02-01 17:40:27 +0100

    revert: changes to init command

--- cognite_toolkit/cdf.py
+++ cognite_toolkit/cdf.py
@@ -29,6 +29,7 @@ from cognite_toolkit.cdf_tk.load import (
     DeployResults,
     ResourceLoader,
 )
+from cognite_toolkit.cdf_tk.migration_help import print_changes
 from cognite_toolkit.cdf_tk.run import run_function, run_transformation
 from cognite_toolkit.cdf_tk.templates import (
     BUILD_ENVIRONMENT_FILE,
@@ -696,13 +697,15 @@ def main_init(
     """Initialize or upgrade a new CDF project with templates."""
     project_dir = Path.cwd() / f"{init_dir}"
 
-    # if upgrade:
-    #     print_changes(project_dir)
-    #     exit(0)
+    if upgrade:
+        print_changes(project_dir)
+        exit(0)
 
     target_dir_display = f"'{project_dir.relative_to(Path.cwd())!s}'"
 
     template_source = Path(resources.files("cognite_toolkit"))  # type: ignore[arg-type]
+    if upgrade and git_branch is not None:
+        template_source = _download_templates(git_branch, dry_run)
 
     files_to_copy: list[str] = [
         "README.md",
@@ -715,7 +718,9 @@ def main_init(
     ]
 
     if project_dir.exists():
-        if clean and dry_run:
+        if upgrade:
+            print(f"[bold]Upgrading directory {target_dir_display}...[/b]")
+        elif clean and dry_run:
             print(f"Would clean out directory {target_dir_display}...")
         elif clean:
             print(f"Cleaning out directory {target_dir_display}...")
@@ -723,9 +728,15 @@ def main_init(
         else:
             print(f"Directory {target_dir_display} already exists.")
             exit(1)
+    elif not project_dir.exists() and upgrade:
+        print(f"Found no directory {target_dir_display} to upgrade.")
+        exit(1)
 
-    if not dry_run:
+    if not dry_run and not upgrade:
         project_dir.mkdir(exist_ok=True)
+
+    if upgrade:
+        print("  Will upgrade modules and files in place.")
 
     copy_prefix = "Would" if dry_run else "Will"
     print(f"{copy_prefix} copy these files to {target_dir_display}:")
@@ -750,6 +761,18 @@ def main_init(
             else:
                 shutil.copyfile(template_source / filename, project_dir / filename)
 
+    if upgrade and not no_backup:
+        prefix = "Would have backed up" if dry_run else "Backing up"
+        if ctx.obj.verbose:
+            print(f"{prefix} {target_dir_display}")
+        if not dry_run:
+            backup_dir = tempfile.mkdtemp(prefix=f"{project_dir.name}.", suffix=".bck", dir=Path.cwd())
+            shutil.copytree(project_dir, Path(backup_dir), dirs_exist_ok=True)
+    elif upgrade:
+        print(
+            f"[bold yellow]WARNING:[/] --no-backup is specified, no backup {'would have been' if dry_run else 'will be'} be."
+        )
+
     for root_module in root_modules:
         if ctx.obj.verbose:
             print(f"{copy_prefix} the following modules from  {root_module} to {target_dir_display}")
@@ -764,7 +787,9 @@ def main_init(
                 ignore=shutil.ignore_patterns("default.*"),
             )
 
-    if not dry_run:
+    if not dry_run and upgrade:
+        print(f"You project in {target_dir_display} was upgraded.")
+    elif not dry_run:
         print(f"A new project was created in {target_dir_display}.")
 
     # Creating the config.[environment].yaml files
@@ -775,15 +800,25 @@ def main_init(
             f"There is something wrong with your installation, try to reinstall `cognite-tk`, and if the problem persists, please contact support."
         )
         exit(1)
+    if upgrade and not clean:
+        existing_environments = list(project_dir.glob("config.*.yaml"))
+        if len(existing_environments) >= 1:
+            config_yamls = ConfigYAMLs.load_existing_environments(existing_environments)
+        else:
+            print("  [bold yellow]WARNING:[/] No existing config.[env].yaml files found, creating from the defaults.")
+            config_yamls = ConfigYAMLs.load_default_environments(read_yaml_file(environment_default))
+    else:
+        config_yamls = ConfigYAMLs.load_default_environments(read_yaml_file(environment_default))
 
-    config_yamls = ConfigYAMLs.load_default_environments(read_yaml_file(environment_default))
-
-    config_yamls.load_default_variables(template_source)
-    config_yamls.load_variables(template_source)
+    if not upgrade:
+        config_yamls.load_default_variables(template_source)
+        config_yamls.load_variables(template_source)
 
     for environment, config_yaml in config_yamls.items():
         config_filepath = project_dir / f"config.{environment}.yaml"
 
+        if not upgrade:
+            print(f"Created config for {environment!r} environment.")
         if dry_run:
             print(f"Would write {config_filepath.name!r} to {target_dir_display}")
         else:
