7eda3c661d0400481b0dd0db8222be3f69a96731
anders-albert <anders0albert@gmail.com>
2024-02-01 12:36:15 +0100

    refactor; setup structure for migration test

--- tests_migrations/migrations.py
+++ tests_migrations/migrations.py
@@ -1,5 +1,5 @@
 import re
-from collections.abc import Callable
+from collections.abc import Callable, Iterable
 from pathlib import Path
 from typing import Any
 
@@ -27,14 +27,55 @@ def modify_environment_to_run_all_modules(project_path: Path) -> None:
 
 def get_migration(previous_version: str, current_version: str) -> Callable[[Path], None]:
     previous_version = _version_str_to_tuple(previous_version)
-    if previous_version <= (0, 1, 0, "b", 6):
-        return _migrate_pre_b6_to_b7
-    else:
+    changes = Changes()
+    if previous_version > (0, 1, 0, "b", 6):
         raise NotImplementedError(f"Migration from {previous_version} to {current_version} is not implemented.")
 
+    if previous_version <= (0, 1, 0, "b", 1):
+        changes.append(_add_name_to_file_configs)
 
-def _migrate_pre_b6_to_b7(project_path: Path) -> None:
-    """Migrate from pre-b6 to b7."""
+    if previous_version <= (0, 1, 0, "b", 4):
+        changes.append(_add_ignore_null_fields_to_transformation_configs)
+
+    if previous_version <= (0, 1, 0, "b", 6):
+        changes.append(_to_config_env_yaml)
+
+    return changes
+
+
+class Changes:
+    def __init__(self) -> None:
+        self._changes: list[Callable[[Path], None]] = []
+
+    def append(self, change: Callable[[Path], None]) -> None:
+        self._changes.append(change)
+
+    def __call__(self, project_path: Path) -> None:
+        for change in self._changes:
+            change(project_path)
+
+
+def _add_name_to_file_configs(project_path: Path) -> None:
+    # Added required field 'name' to files
+    for file_yaml in _config_yaml_from_folder_name(project_path, "files"):
+        data = yaml.safe_load(file_yaml.read_text().replace("{{", "").replace("}}", ""))
+        for entry in data:
+            if "name" not in entry:
+                entry["name"] = entry["externalId"]
+        file_yaml.write_text(yaml.dump(data))
+
+
+def _add_ignore_null_fields_to_transformation_configs(project_path: Path) -> None:
+    for transformation_yaml in _config_yaml_from_folder_name(project_path, "transformations"):
+        data = yaml.safe_load(transformation_yaml.read_text().replace("{{", "").replace("}}", ""))
+        for entry in data:
+            if "ignoreNullFields" not in entry:
+                entry["ignoreNullFields"] = False
+        transformation_yaml.write_text(yaml.dump(data))
+
+
+def _to_config_env_yaml(project_path: Path) -> None:
+    """Change introduced in b7"""
     default_packages_path = project_path / "cognite_modules" / "default.packages.yaml"
     environments_path = project_path / "environments.yaml"
     config_path = project_path / "config.yaml"
@@ -46,12 +87,10 @@ def _migrate_pre_b6_to_b7(project_path: Path) -> None:
         raise FileNotFoundError(
             "Could not find one of the required files: default.packages.yaml, environments.yaml, config.yaml"
         )
-
     # Create _system.yaml
     system_yaml = default_packages.copy()
     system_yaml["cdf_toolkit_version"] = __version__
     (project_path / "cognite_modules" / "_system.yaml").write_text(yaml.dump(system_yaml))
-
     # Create config.[env].yaml
     for env_name, env_config in environments.items():
         if env_name == "__system":
@@ -63,23 +102,20 @@ def _migrate_pre_b6_to_b7(project_path: Path) -> None:
             "modules": config_yaml,
         }
         (project_path / f"config.{env_name}.yaml").write_text(yaml.dump(config_env_yaml))
-
     # Delete
     default_packages_path.unlink()
     environments_path.unlink()
     config_path.unlink()
-
     # Delete all default files
     for file in project_path.glob("**/default.*"):
         file.unlink()
 
-    # Added required field 'name' to files
-    for file_yaml in project_path.glob("**/*files.yaml"):
-        data = yaml.safe_load(file_yaml.read_text().replace("{{", "").replace("}}", ""))
-        for entry in data:
-            if "name" not in entry:
-                entry["name"] = entry["externalId"]
-        file_yaml.write_text(yaml.dump(data))
+
+def _config_yaml_from_folder_name(project: Path, folder_name: str) -> Iterable[Path]:
+    for module_name, module_files in iterate_modules(project):
+        for module_file in module_files:
+            if module_file.parent == folder_name:
+                yield module_file
 
 
 def _version_str_to_tuple(version_str: str) -> tuple[int | str, ...]:
