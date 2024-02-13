5305704a3888dbdc5403655b2052de14b6b1ebbd
Jan Inge Bergseth <31886431+BergsethCognite@users.noreply.github.com>
2024-02-13 10:58:00 +0100

    Delete cognite_toolkit/jib03 directory

--- cognite_toolkit/jib03/cognite_modules/examples/cdf_apm_simple_data_model/transformations/tr_workorder_oid_workmate_apm_simple_load_workorders2assets.sql
+++
@@ -1,6 +0,0 @@
-select
-  cast(`externalId` as STRING) as externalId,
-  node_reference('{{datamodel}}', `sourceExternalId`) as startNode,
-  node_reference('{{datamodel}}', `targetExternalId`) as endNode
-from
-  `workorder_{{default_location}}_{{source_workorder}}`.`workorder2assets`;
