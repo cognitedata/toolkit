76c94c7c6e8cd707d48241548ed423fdfef77dd8
anders-albert <anders0albert@gmail.com>
2024-02-13 19:50:46 +0100

    style: fix?

--- cognite_toolkit/cdf_tk/utils.py
+++ cognite_toolkit/cdf_tk/utils.py
@@ -850,7 +850,7 @@ T_Key = TypeVar("T_Key")
 T_Value = TypeVar("T_Value")
 
 
-class YAMLWithComments(Generic[T_Key, T_Value], UserDict[T_Key, T_Value]):
+class YAMLWithComments(UserDict, Generic[T_Key, T_Value]):
     @staticmethod
     def _extract_comments(raw_file: str, key_prefix: tuple[str, ...] = tuple()) -> dict[tuple[str, ...], YAMLComment]:
         """Extract comments from a raw file and return a dictionary with the comments."""
