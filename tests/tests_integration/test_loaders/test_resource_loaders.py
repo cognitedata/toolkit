7ca021cf9e415213a11f261f82e46002ab4669c9
Pål Rønning <pal.ronning@cognite.com>
2024-02-14 16:07:45 +0100

    Bugfix

--- tests/tests_integration/test_loaders/test_resource_loaders.py
+++ tests/tests_integration/test_loaders/test_resource_loaders.py
@@ -1,6 +1,7 @@
+import pytest
 from cognite.client import CogniteClient
 
-from cognite_toolkit.cdf_tk.load import DataSetsLoader
+from cognite_toolkit.cdf_tk.load import DataSetsLoader, ExtractionPipelineConfigLoader
 
 
 class TestDataSetsLoader:
@@ -12,4 +13,22 @@ class TestDataSetsLoader:
 
         assert len(unchanged) == len(data_sets)
         assert len(created) == 0
+        assert len(changed) == 0
+
+
+class TestExtractionPipelineLoader:
+    def test_existing_unchanged(self, cognite_client: CogniteClient):
+
+        extraction_pipeline = cognite_client.extraction_pipelines.list(limit=1)
+        if len(extraction_pipeline) == 0:
+            pytest.skip("No extraction pipelines found")
+        extraction_pipeline_config = cognite_client.extraction_pipelines.config.retrieve(
+            extraction_pipeline[0].external_id
+        )
+        loader = ExtractionPipelineConfigLoader(client=CogniteClient)
+
+        created, changed, unchanged = loader.to_create_changed_unchanged_triple(extraction_pipeline_config.as_write())
+
+        assert len(unchanged) == 0
+        assert len(created) == 1
         assert len(changed) == 0
