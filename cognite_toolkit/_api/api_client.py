26c7e43d176b80cde493c4207fb5f2caeb6188e1
anders-albert <anders0albert@gmail.com>
2024-02-24 17:34:43 +0100

    refactor: setup run API

--- cognite_toolkit/_api/api_client.py
+++ cognite_toolkit/_api/api_client.py
@@ -8,5 +8,6 @@ from cognite_toolkit._api.run_api import RunAPI
 
 class CogniteToolkit:
     def __init__(self, client: CogniteClient, url: str | None = None):
+
         self.modules = ModulesAPI(client, url)
         self.run = RunAPI(client)
--- cognite_toolkit/_api/run_api.py
+++ cognite_toolkit/_api/run_api.py
@@ -1,10 +1,31 @@
+import json
+
 from cognite.client import CogniteClient
+
+from cognite_toolkit._cdf_tk.run import run_function, run_transformation
+from cognite_toolkit._cdf_tk.utils import CDFToolConfig
 
 
 class RunAPI:
     def __init__(self, cognite_client: CogniteClient):
         self._client = cognite_client
 
-    def transformation(self, external_id: str) -> None: ...
+    def _create_tool_config(self) -> CDFToolConfig:
+        cluster = self._client.config.base_url.removeprefix("https://").split(".", maxsplit=1)[0]
+        cdf_tool_config = CDFToolConfig(cluster=cluster, project=self._client.config.project)
+        cdf_tool_config._client = self._client
+        return cdf_tool_config
 
-    def function(self, external_id: str) -> None: ...
+    def transformation(self, external_id: str) -> bool:
+        return run_transformation(
+            self._create_tool_config(),
+            external_id,
+        )
+
+    def function(self, external_id: str, payload: dict, follow: bool = False) -> bool:
+        return run_function(
+            self._create_tool_config(),
+            external_id,
+            json.dumps(payload),
+            follow,
+        )
