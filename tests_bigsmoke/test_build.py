045793b276b5d1a82744459d529855d13dac0e57
Greger Wedel <greger.wedel@cognite.com>
2024-03-08 08:45:02 +0100

    Fix test

--- tests_bigsmoke/test_build.py
+++ tests_bigsmoke/test_build.py
@@ -9,4 +9,4 @@ from cognite_toolkit._cdf_tk.utils import CDFToolConfig
 def test_tool_globals(read_write_group_token):
     ToolGlobals = CDFToolConfig(token=read_write_group_token, project="big-smoke-test")
     print(ToolGlobals)
-    assert True is True
+    assert True
