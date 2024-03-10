2b1cc61f93fbf10d66a13fd7c409d12ccc5db8c3
Håkon V. Treider <haakonvt@gmail.com>
2024-03-10 12:34:24 +0100

    started refactoring fn_context_timeseries_oid_opcua_asset/get_resources.py

--- cognite_toolkit/cognite_modules/examples/cdf_data_pipeline_timeseries_valhall/functions/fn_context_timeseries_oid_opcua_asset/get_resources.py
+++ cognite_toolkit/cognite_modules/examples/cdf_data_pipeline_timeseries_valhall/functions/fn_context_timeseries_oid_opcua_asset/get_resources.py
@@ -20,117 +20,104 @@ from constants import (
 )
 
 
-def read_manual_mappings(cognite_client: CogniteClient, config: ContextConfig) -> list[Row]:
+def manual_table_exists(client: CogniteClient, config: str) -> bool:
+    tables = client.raw.tables.list(config.rawdb, limit=None)
+    return any(tbl.name == config.raw_table_manual for tbl in tables)
+
+
+def read_manual_mappings(client: CogniteClient, config: ContextConfig) -> list[Row]:
     """
     Read manual mappings from RAW DB and add to list of manual mappings if not already contextualized
 
     Args:
-        cognite_client: Instance of CogniteClient
+        client: Instance of CogniteClient
         config: Instance of ContextConfig
 
     Returns:
         list of manual mappings or empty list if no mappings are found
     """
     manual_mappings = []
-    man_table_true = False
     try:
-        table_list = cognite_client.raw.tables.list(config.rawdb, limit=-1)
-        for table in table_list:
-            if table.name == config.raw_table_manual:
-                man_table_true = True
-                break
+        if not manual_table_exists(client, config):
+            return manual_mappings
 
-        if man_table_true:
-            row_list = cognite_client.raw.rows.list(config.rawdb, config.raw_table_manual, limit=-1)
+        row_list = client.raw.rows.list(config.rawdb, config.raw_table_manual, limit=-1)
+        for row in row_list:
+            if not (
+                config.run_all
+                or COL_KEY_MAN_CONTEXTUALIZED not in row.columns
+                or row.columns[COL_KEY_MAN_CONTEXTUALIZED] is not True
+            ):
+                continue
 
-            for row in row_list:
-                if config.run_all or (
-                    COL_KEY_MAN_CONTEXTUALIZED not in row.columns or row.columns[COL_KEY_MAN_CONTEXTUALIZED] is not True
-                ):
-                    strip_value_rows = {}
-                    for key, value in row.columns.items():
-
-                        if isinstance(value, str):
-                            strip_value_rows[key.strip()] = value.strip()
-                        else:
-                            strip_value_rows[key.strip()] = value
-
-                    manual_mappings.append(strip_value_rows)
-
-            print(f"INFO: Number of manual mappings: {len(manual_mappings)}")
+            manual_mappings.append({k.strip(): v.strip() if isinstance(v, str) else v for k, v in row.columns.items()})
+        print(f"INFO: Number of manual mappings: {len(manual_mappings)}")
 
     except Exception as e:
-        print(f"ERROR: Read manual mappings. Error: {e}")
+        print(f"ERROR: Read manual mappings. Error: {type(e)}({e})")
 
     return manual_mappings
 
 
-def get_asset_id_ext_id_mapping(cognite_client: CogniteClient, manual_mappings: list[Row]) -> dict[str, int]:
+def get_asset_id_ext_id_mapping(client: CogniteClient, manual_mappings: list[Row]) -> dict[str, int]:
     """
     Read assets specified in manual mapping input based on external ID and find the corresponding asset internal ID
     Internal ID is used to update time series with asset ID
 
     Args:
-        cognite_client: Instance of CogniteClient
+        client: Instance of CogniteClient
         manual_mappings: list of manual mappings
 
     Returns:
         dictionary with asset external id as key and asset id as value
 
     """
-    asset_ext_id_mapping = {}
     try:
-
         asset_ext_id_list = [mapping[COL_KEY_MAN_MAPPING_ASSET_EXTID] for mapping in manual_mappings]
-        asset_ext_id_list_no_dups = list(set(asset_ext_id_list))
-        asset_list = cognite_client.assets.retrieve_multiple(external_ids=asset_ext_id_list_no_dups)
-
-        asset_ext_id_mapping = {asset.external_id: asset.id for asset in asset_list}
+        asset_list = client.assets.retrieve_multiple(external_ids=list(set(asset_ext_id_list)))
+        return {asset.external_id: asset.id for asset in asset_list}
 
     except Exception as e:
-        print(f"ERROR: Not able read list of assets from {manual_mappings} - error: {e}")
+        print(f"ERROR: Not able read list of assets from {manual_mappings}. Error: {type(e)}({e})")
+        return {}
 
-    return asset_ext_id_mapping
 
-
-def get_ts_list_manual_mapping(cognite_client: CogniteClient, manual_mappings: list[Row]) -> list[TimeSeries]:
+def get_ts_list_manual_mapping(client: CogniteClient, manual_mappings: list[Row]) -> list[TimeSeries]:
     """
     Read time series related to external time series ID specified in manual mapping input
 
     Args:
-        cognite_client: Instance of CogniteClient
+        client: Instance of CogniteClient
         manual_mappings: list of manual mappings
 
     Returns:
         list of TimeSeries
     """
-    ts_list_manual_mapping = []
     try:
-        ts_ext_id_list = [mapping[COL_KEY_MAN_MAPPING_TS_EXTID] for mapping in manual_mappings]
-
-        ts_list_manual_mapping = cognite_client.time_series.retrieve_multiple(external_ids=ts_ext_id_list)
-
-        if len(ts_list_manual_mapping) != len(ts_ext_id_list):
-            raise ValueError(
-                f"Number of time series found {len(ts_list_manual_mapping)} does not match number of "
-                f"time series in input list {len(ts_ext_id_list)}"
-            )
-
+        ts_xid_list = [mapping[COL_KEY_MAN_MAPPING_TS_EXTID] for mapping in manual_mappings]
+        ts_list_manual_mapping = client.time_series.retrieve_multiple(external_ids=ts_xid_list)
     except Exception as e:
         print(f"ERROR: Not able read list of time series from {manual_mappings} - error: {e}")
+        return []
 
-    return ts_list_manual_mapping
+    if len(ts_list_manual_mapping) == len(ts_xid_list):
+        return ts_list_manual_mapping
+
+    raise ValueError(
+        f"Number of time series found {len(ts_list_manual_mapping)} does not match number of "
+        f"time series in input list {len(ts_xid_list)}"
+    )
 
 
 def get_time_series(
-    cognite_client: CogniteClient, config: ContextConfig, manual_matches: list[Row]
+    client: CogniteClient, config: ContextConfig, manual_matches: list[Row]
 ) -> tuple[list[dict[str, Any]], list[TimeSeries]]:
     """
     Read time series based on root ASSET id
     Read all if config property readAll = True, else only read time series not contextualized ( connected to asset)
 
     Args:
-        cognite_client: Instance of CogniteClient
+        client: Instance of CogniteClient
         config: Instance of ContextConfig
         manual_matches: list of manual mappings
 
@@ -143,45 +130,43 @@ def get_time_series(
     ts_meta_dict = {}
     try:
         for ts_prefix in config.time_series_prefix:
-            ts_list = cognite_client.time_series.list(
-                data_set_external_ids=[config.time_series_data_set_ext_id], external_id_prefix=ts_prefix
+            ts_list = client.time_series.list(
+                data_set_external_ids=[config.time_series_data_set_ext_id],
+                external_id_prefix=ts_prefix,
+                limit=None,
             )
             # get list of time series in manual mappings to exclude from matching
-            ts_ext_id_manual_match_list = [m_match[COL_KEY_MAN_MAPPING_TS_EXTID] for m_match in manual_matches]
+            to_skip = set(m_match[COL_KEY_MAN_MAPPING_TS_EXTID] for m_match in manual_matches)
 
             for ts in ts_list:
                 # if manual match exists, skip matching based on entity matching
-                if ts.external_id in ts_ext_id_manual_match_list:
+                if ts.external_id in to_skip or ts.external_id is None:
                     continue
 
-                if TS_CONTEXTUALIZED_METADATA_KEY is not None and TS_CONTEXTUALIZED_METADATA_KEY not in (
-                    ts.metadata or {}
-                ):
-                    if ts.external_id is not None:
-                        entities = get_ts_entities(ts, entities)
-                        ts_meta_dict[ts.id] = ts.metadata
+                if TS_CONTEXTUALIZED_METADATA_KEY not in ts.metadata or {}:
+                    entities = get_ts_entities(ts, entities)
+                    ts_meta_dict[ts.id] = ts.metadata
 
                 # if run all - remove metadata element from last annotation
                 elif config.run_all:
-                    if not config.debug and TS_CONTEXTUALIZED_METADATA_KEY is not None:
-                        ts_meta_update = TimeSeriesUpdate(external_id=ts.external_id).metadata.remove(
-                            [TS_CONTEXTUALIZED_METADATA_KEY]
+                    if not config.debug:
+                        meta_ts_update.append(
+                            TimeSeriesUpdate(external_id=ts.external_id).metadata.remove(
+                                [TS_CONTEXTUALIZED_METADATA_KEY]
+                            )
                         )
-                        meta_ts_update.append(ts_meta_update)
-                    if ts.external_id is not None:
-                        entities = get_ts_entities(ts, entities)
-                        ts_meta_dict[ts.id] = ts.metadata
+                    entities = get_ts_entities(ts, entities)
+                    ts_meta_dict[ts.id] = ts.metadata
 
-        if len(meta_ts_update) > 0:
-            cognite_client.time_series.update(meta_ts_update)
+        client.time_series.update(meta_ts_update)
+        return entities, ts_meta_dict
 
     except Exception as e:
         print(
             f"ERROR: Not able to get entities for time series in data set: {config.time_series_data_set_ext_id} "
             f"with prefix: {config.time_series_prefix}- error: {e}"
         )
-
-    return entities, ts_meta_dict
+        return [], {}
 
 
 def get_ts_entities(ts: TimeSeries, entities: list[dict[str, Any]]) -> list[dict[str, Any]]:
@@ -201,22 +186,20 @@ def get_ts_entities(ts: TimeSeries, entities: list[dict[str, Any]]) -> list[dict
 
     # build list with possible file name variations used in P&ID to refer to other P&ID
     split_name = re.split("[,._ \\-!?:]+", ts.name)
-
     modName = ts.name[len(split_name[0]) + 1 :]  # remove prefix
     modName = modName[: -(len(split_name[-1]) + 1)]  # remove postfix
 
     # add entities for files used to match between file references in P&ID to other files
     entities.append({"id": ts.id, "name": modName, "external_id": ts.external_id, "org_name": ts.name, "type": "ts"})
-
     return entities
 
 
-def get_assets(cognite_client: CogniteClient, asset_root_ext_id: str, numAsset: int) -> list[dict[str, Any]]:
+def get_assets(client: CogniteClient, asset_root_ext_id: str, numAsset: int) -> list[dict[str, Any]]:
     """
     Get Asset used as input to contextualization and build list of entities
 
     Args:
-        cognite_client: Instance of CogniteClient
+        client: Instance of CogniteClient
         asset_root_ext_id: external root asset ID
         numAsset : number of assets to read
 
@@ -225,7 +208,7 @@ def get_assets(cognite_client: CogniteClient, asset_root_ext_id: str, numAsset: 
     """
     entities: list[dict[str, Any]] = []
     try:
-        assets = cognite_client.assets.list(asset_subtree_external_ids=[asset_root_ext_id], limit=numAsset)
+        assets = client.assets.list(asset_subtree_external_ids=[asset_root_ext_id], limit=numAsset)
 
         # clean up dummy tags and system numbers
         for asset in assets:
@@ -262,13 +245,13 @@ def get_assets(cognite_client: CogniteClient, asset_root_ext_id: str, numAsset: 
 
 
 def get_matches(
-    cognite_client: CogniteClient, match_to: list[dict[str, Any]], match_from: list[dict[str, Any]]
+    client: CogniteClient, match_to: list[dict[str, Any]], match_from: list[dict[str, Any]]
 ) -> list[ContextualizationJob]:
     """
     Create / Update entity matching model and run job to get matches
 
     Args:
-        cognite_client: Instance of CogniteClient
+        client: Instance of CogniteClient
         match_to: list of entities to match to (target)
         match_from: list of entities to match from (source)
 
@@ -276,7 +259,7 @@ def get_matches(
         list of matches
     """
     try:
-        model = cognite_client.entity_matching.fit(
+        model = client.entity_matching.fit(
             sources=match_from,
             targets=match_to,
             match_fields=[(COL_MATCH_KEY, COL_MATCH_KEY)],
