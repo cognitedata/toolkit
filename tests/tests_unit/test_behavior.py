654c9a5afaed83bf155af69cb351fcb402430de0
anders-albert <anders0albert@gmail.com>
2024-04-01 17:53:38 +0200

    tests: Added test

--- tests/tests_unit/test_behavior.py
+++ tests/tests_unit/test_behavior.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+from unittest.mock import MagicMock
 
+import pytest
 import typer
 import yaml
 from cognite.client import data_modeling as dm
@@ -8,10 +10,13 @@ from pytest import MonkeyPatch
 
 from cognite_toolkit._cdf import build, deploy, dump_datamodel_cmd, pull_transformation_cmd
 from cognite_toolkit._cdf_tk.load import TransformationLoader
+from cognite_toolkit._cdf_tk.templates import build_config
+from cognite_toolkit._cdf_tk.templates.data_classes import BuildConfigYAML, SystemYAML
 from cognite_toolkit._cdf_tk.utils import CDFToolConfig
 from tests.tests_unit.approval_client import ApprovalCogniteClient
-from tests.tests_unit.test_cdf_tk.constants import CUSTOM_PROJECT
+from tests.tests_unit.test_cdf_tk.constants import CUSTOM_PROJECT, PROJECT_WITH_DUPLICATES
 from tests.tests_unit.utils import mock_read_yaml_file
+from tests.tests_unit.utils import PrintCapture, mock_read_yaml_file
 
 
 def test_inject_custom_environmental_variables(
@@ -54,6 +59,19 @@ def test_inject_custom_environmental_variables(
 
     transformation = cognite_client_approval.created_resources_of_type(Transformation)[0]
     assert transformation.source_oidc_credentials.client_id == "my_environment_variable_value"
+
+
+def test_duplicated_modules(local_tmp_path: Path, typer_context: typer.Context, capture_print: PrintCapture) -> None:
+    with pytest.raises(SystemExit):
+        build_config(
+            build_dir=local_tmp_path,
+            source_dir=PROJECT_WITH_DUPLICATES,
+            config=MagicMock(spec=BuildConfigYAML),
+            system_config=MagicMock(spec=SystemYAML),
+        )
+    # Check that the error message is printed
+    assert "module1" in capture_print.messages[-1]
+    assert "duplicated module names" in capture_print.messages[-2]
 
 
 def test_pull_transformation(
--- tests/tests_unit/test_cdf_tk/constants.py
+++ tests/tests_unit/test_cdf_tk/constants.py
@@ -3,3 +3,4 @@ from pathlib import Path
 PYTEST_PROJECT = Path(__file__).parent / "project_for_test"
 BUILD_DIR = PYTEST_PROJECT.parent / "tmp"
 CUSTOM_PROJECT = Path(__file__).parent / "project_no_cognite_modules"
+PROJECT_WITH_DUPLICATES = Path(__file__).parent / "project_with_duplicates"
