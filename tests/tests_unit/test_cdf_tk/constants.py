b0e94cb67f10e3d9af1c1c6502a7be7610d3e32a
Anders Albert <60234212+doctrino@users.noreply.github.com>
2024-04-02 12:14:35 +0200

    [CDF-21186] Remove system yaml Requirement (#431)
    * refactor: added test data for new case
    
    * tests: added failing test
    
    * feat: Allow no cognite-modules
    
    * build: changelog

--- tests/tests_unit/test_cdf_tk/constants.py
+++ tests/tests_unit/test_cdf_tk/constants.py
@@ -2,3 +2,4 @@ from pathlib import Path
 
 PYTEST_PROJECT = Path(__file__).parent / "project_for_test"
 BUILD_DIR = PYTEST_PROJECT.parent / "tmp"
+CUSTOM_PROJECT = Path(__file__).parent / "project_no_cognite_modules"
