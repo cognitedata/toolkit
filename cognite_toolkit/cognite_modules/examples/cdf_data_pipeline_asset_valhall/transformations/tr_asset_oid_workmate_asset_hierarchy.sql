818854980325e4644b41fd79fc88241b4e588dea
Jan Inge Bergseth <31886431+BergsethCognite@users.noreply.github.com>
2024-04-04 11:25:49 +0200

    div bug fixes and alignment with oid test data

--- cognite_toolkit/cognite_modules/examples/cdf_data_pipeline_asset_valhall/transformations/tr_asset_oid_workmate_asset_hierarchy.sql
+++ cognite_toolkit/cognite_modules/examples/cdf_data_pipeline_asset_valhall/transformations/tr_asset_oid_workmate_asset_hierarchy.sql
@@ -8,16 +8,16 @@
 -- All metadata expect selected fileds are added to metadata
 --
 SELECT 
-  sourceDb || ':' || tag          as externalId,
-  if(parentTag is null, 
+  externalId                      as externalId,
+  if(parentExternalId is null, 
      '', 
-     sourceDb || ':' ||parentTag) as parentExternalId,
+     parentExternalId)            as parentExternalId,
   tag                             as name,
   sourceDb                        as source,
   description,
   dataset_id('{{asset_dataset}}')     as dataSetId,
   to_metadata_except(
-    array("sourceDb", "parentTag", "description"), *) 
+    array("sourceDb", "parentExternalId", "description"), *) 
                                   as metadata
 FROM 
   `{{asset_raw_input_db}}`.`{{asset_raw_input_table}}`
