870925bcd99e7ffb6be1f90cd5203fa4192f50b1
Anders Albert <60234212+doctrino@users.noreply.github.com>
2024-05-02 13:50:07 +0200

    [CDF-21457] Speedup Tests ⚡ (#510)
    * tests: avoid rerunning init
    
    * tests: fix mmutable
    
    * build: added pytest-xdist
    
    * refactor: support parallel running
    
    * ci: use paralell
    
    * fix; added missing URLs

--- tests/tests_unit/test_cdf_tk/test_load.py
+++ tests/tests_unit/test_cdf_tk/test_load.py
@@ -1,4 +1,3 @@
-import abc
 from pathlib import Path
 from unittest.mock import MagicMock
 
@@ -27,6 +26,8 @@ from cognite_toolkit._cdf_tk.load import (
     DataSetsLoader,
     FileMetadataLoader,
     FunctionLoader,
+    Loader,
+    ResourceContainerLoader,
     ResourceLoader,
     ResourceTypes,
     TimeSeriesLoader,
@@ -436,18 +437,37 @@ class TestDeployResources:
         assert actual_order == expected_order
 
 
-def find_subclasses(cls):
-    subclasses = set()
-    for subclass in cls.__subclasses__():
-        subclasses |= find_subclasses(subclass)  # Recursive call to find indirect subclasses
-        if abc.ABC in subclass.__bases__:
-            continue
-        subclasses.add(subclass)
-    return subclasses
+RESOURCE_LOADERS = sorted(
+    [
+        pytest.param(loader, id=loader.__name__)
+        for loaders in LOADER_BY_FOLDER_NAME.values()
+        for loader in loaders
+        if issubclass(loader, ResourceLoader)
+    ],
+    key=lambda x: x.id,
+)
+RESOURCE_CONTAINER_LOADERS = sorted(
+    [
+        pytest.param(loader, id=loader.__name__)
+        for loaders in LOADER_BY_FOLDER_NAME.values()
+        for loader in loaders
+        if issubclass(loader, ResourceContainerLoader)
+    ],
+    key=lambda x: x.id,
+)
+ALL_LOADERS = sorted(
+    [
+        pytest.param(loader, id=loader.__name__)
+        for loaders in LOADER_BY_FOLDER_NAME.values()
+        for loader in loaders
+        if issubclass(loader, Loader)
+    ],
+    key=lambda x: x.id,
+)
 
 
 class TestFormatConsistency:
-    @pytest.mark.parametrize("Loader", sorted(find_subclasses(ResourceLoader), key=lambda x: x.folder_name))
+    @pytest.mark.parametrize("Loader", RESOURCE_LOADERS)
     def test_fake_resource_generator(
         self, Loader: type[ResourceLoader], cdf_tool_config: CDFToolConfig, monkeypatch: MonkeyPatch
     ):
@@ -458,7 +478,7 @@ class TestFormatConsistency:
 
         assert isinstance(instance, loader.resource_write_cls)
 
-    @pytest.mark.parametrize("Loader", sorted(find_subclasses(ResourceLoader), key=lambda x: x.folder_name))
+    @pytest.mark.parametrize("Loader", RESOURCE_LOADERS)
     def test_loader_takes_dict(
         self, Loader: type[ResourceLoader], cdf_tool_config: CDFToolConfig, monkeypatch: MonkeyPatch
     ):
@@ -482,7 +502,7 @@ class TestFormatConsistency:
             loaded, (loader.resource_write_cls, loader.list_write_cls)
         ), f"loaded must be an instance of {loader.list_write_cls} or {loader.resource_write_cls} but is {type(loaded)}"
 
-    @pytest.mark.parametrize("Loader", sorted(find_subclasses(ResourceLoader), key=lambda x: x.folder_name))
+    @pytest.mark.parametrize("Loader", RESOURCE_LOADERS)
     def test_loader_takes_list(
         self, Loader: type[ResourceLoader], cdf_tool_config: CDFToolConfig, monkeypatch: MonkeyPatch
     ):
@@ -507,17 +527,16 @@ class TestFormatConsistency:
             loaded, (loader.resource_write_cls, loader.list_write_cls)
         ), f"loaded must be an instance of {loader.list_write_cls} or {loader.resource_write_cls} but is {type(loaded)}"
 
-    def check_url(self, url) -> bool:
+    @staticmethod
+    def check_url(url) -> bool:
         try:
             response = requests.get(url, allow_redirects=True)
             return response.status_code >= 200 and response.status_code <= 300
         except requests.exceptions.RequestException:
             return False
 
-    @pytest.mark.parametrize("Loader", sorted(find_subclasses(ResourceLoader), key=lambda x: x.folder_name))
-    def test_loader_has_doc_url(
-        self, Loader: type[ResourceLoader], cdf_tool_config: CDFToolConfig, monkeypatch: MonkeyPatch
-    ):
+    @pytest.mark.parametrize("Loader", ALL_LOADERS)
+    def test_loader_has_doc_url(self, Loader: type[Loader], cdf_tool_config: CDFToolConfig, monkeypatch: MonkeyPatch):
         loader = Loader.create_loader(cdf_tool_config)
         assert loader.doc_url() != loader._doc_base_url, f"{Loader.folder_name} is missing doc_url deep link"
         assert self.check_url(loader.doc_url()), f"{Loader.folder_name} doc_url is not accessible"
