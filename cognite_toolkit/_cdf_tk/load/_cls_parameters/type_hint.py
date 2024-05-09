0e222c0077c5e6c0240a35c10d235e6519dbf3d4
anders-albert <anders0albert@gmail.com>
2024-05-09 09:05:26 +0200

    refactor: cleaned up type hints

--- cognite_toolkit/_cdf_tk/load/_cls_parameters/type_hint.py
+++ cognite_toolkit/_cdf_tk/load/_cls_parameters/type_hint.py
@@ -9,63 +9,84 @@ from typing import Any, ClassVar, get_origin
 class TypeHint:
     _BASE_TYPES: ClassVar[set[str]] = {t.__name__ for t in (str, int, float, bool)}
     _CONTAINER_TYPES: ClassVar[set[str]] = {t.__name__ for t in (list, dict)}
+    _TYPES: ClassVar[set[str]] = _BASE_TYPES | _CONTAINER_TYPES
 
     def __init__(self, raw: Any) -> None:
         self.raw = raw
         self._container_type = get_origin(raw)
-        self.args = typing.get_args(raw)
-        self._is_nullable = False
-        if self._is_union(self._container_type) and self.args:
-            inner_container = typing.get_origin(self.args[0])
-            if inner_container:
-                self._is_nullable = any(arg in [None, types.NoneType] for arg in self.args)
-                self._container_type = inner_container
-                self.args = typing.get_args(self.args[0])
+        self.is_union = self._is_union(self._container_type)
+        if self.is_union:
+            self.args = typing.get_args(raw)
+        else:
+            self.args = (raw,)
+
+        # self._is_nullable = False
+        # if self._is_union(self._container_type) and self.args:
+        #     inner_container = typing.get_origin(self.args[0])
+        #     if inner_container:
+        #         self._is_nullable = any(arg in [None, types.NoneType] for arg in self.args)
+        #         self._container_type = inner_container
+        #         self.args = typing.get_args(self.args[0])
 
     def __str__(self) -> str:
-        if self._container_type and self._container_type not in [types.UnionType, typing.Union]:
-            value = self._container_type.__name__
-        else:
-            value = self.arg.__name__
-        if value in self._CONTAINER_TYPES or value in self._BASE_TYPES:
-            return value
-        elif value == "Literal":
-            return "str"
-        return "dict"
+        return ",".join(self.types)
 
     @classmethod
     def _is_union(cls, x: Any) -> bool:
         return x in [types.UnionType, typing.Union]
 
+    @classmethod
+    def _is_none_type(cls, x: Any) -> bool:
+        return x in [None, types.NoneType]
+
+    @classmethod
+    def _as_str(cls, arg: Any) -> str:
+        value = arg.__name__
+        if value in cls._TYPES:
+            return value
+        elif value == "Literal":
+            return "str"
+        return "dict"
+
     @property
-    def arg(self) -> Any:
-        if self._is_union(self._container_type) and self.args:
-            value = self.args[0]
-        else:
-            value = self.raw
-        if self._is_union(value):
-            value = value.__args__[0]
-        return value
+    def types(self) -> list[str]:
+        return [self._as_str(arg) for arg in self.args if not self._is_none_type(arg)]
+
+    # @property
+    # def arg(self) -> Any:
+    #     if self._is_union(self._container_type) and self.args:
+    #         value = self.args[0]
+    #     else:
+    #         value = self.raw
+    #     if self._is_union(value):
+    #         value = value.__args__[0]
+    #     return value
 
     @property
     def is_base_type(self) -> bool:
-        return str(self) in self._BASE_TYPES
+        return any(type_ in self._BASE_TYPES for type_ in self.types)
 
     @property
     def is_nullable(self) -> bool:
-        return self._is_nullable or any(arg is None or arg is types.NoneType for arg in self.args)
+        return any(self._is_none_type(arg) for arg in self.args)
 
     @property
     def is_class(self) -> bool:
-        return inspect.isclass(self.arg)
+        if self.is_union:
+            return False
+        return inspect.isclass(self.args[0])
+
+    @property
+    def _get_origins(self) -> tuple[Any, ...]:
+        return tuple(get_origin(arg) for arg in self.args)
 
     @property
     def is_dict_type(self) -> bool:
-        return self._container_type is dict
+        return any(arg is dict for arg in self._get_origins)
 
     @property
     def is_list_type(self) -> bool:
-        return self._container_type is list
+        return any(arg is list for arg in self._get_origins)
 
     def __repr__(self) -> str:
-        return repr(self.arg)
+        return repr(self.raw)
--- tests/tests_unit/test_cdf_tk/test_cls_parameters/test_type_hint.py
+++ tests/tests_unit/test_cdf_tk/test_cls_parameters/test_type_hint.py
@@ -1,4 +1,6 @@
-from typing import Any
+from __future__ import annotations
+
+from typing import Any, Literal, Union
 
 import pytest
 
@@ -7,15 +9,22 @@ from cognite_toolkit._cdf_tk.load._cls_parameters.type_hint import TypeHint
 
 class TestTypeHint:
     @pytest.mark.parametrize(
-        "raw, as_str, is_base_type, is_nullable, is_class, is_dict_type, is_list_type",
+        "raw, types, is_base_type, is_nullable, is_class, is_dict_type, is_list_type",
         [
-            (str, "str", True, False, True, False, False),
+            (str, ["str"], True, False, True, False, False),
+            (Literal["a", "b"], ["str"], True, False, False, False, False),
+            (str | None, ["str"], True, True, False, False, False),
+            (Union[str, int], ["str", "int"], True, False, False, False, False),
+            (str | int | bool, ["str", "int", "bool"], True, False, False, False, False),
+            (dict[str, int], ["dict"], False, False, False, True, False),
+            (list[str] | None, ["list"], False, True, False, False, True),
+            (list[str] | dict[str, int] | None, ["list", "dict"], False, True, False, True, True),
         ],
     )
     def test_type_hint(
         self,
         raw: Any,
-        as_str: str,
+        types: list[str],
         is_base_type: bool,
         is_nullable: bool,
         is_class: bool,
@@ -23,7 +32,7 @@ class TestTypeHint:
         is_list_type: bool,
     ):
         hint = TypeHint(raw)
-        assert str(hint) == as_str
+        assert hint.types == types
         assert hint.is_base_type == is_base_type
         assert hint.is_nullable == is_nullable
         assert hint.is_class == is_class
