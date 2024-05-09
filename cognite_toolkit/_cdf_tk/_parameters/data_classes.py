eab59b40a0959ac44f26b9743c7b3d22ab87d698
anders-albert <anders0albert@gmail.com>
2024-05-09 18:58:34 +0200

    refactor: second implementation read hints

--- cognite_toolkit/_cdf_tk/_parameters/data_classes.py
+++ cognite_toolkit/_cdf_tk/_parameters/data_classes.py
@@ -4,7 +4,9 @@ import typing
 from collections.abc import Hashable, Iterable, MutableSet
 from dataclasses import dataclass
 from functools import total_ordering
-from typing import Generic, TypeVar, final
+
+# Ruff wants to get AbstractSet from collections.abc, but mypy does not allow it.
+from typing import AbstractSet, Generic, TypeVar, final  # noqa: UP035
 
 from cognite.client.utils._text import to_camel_case
 
@@ -25,6 +27,10 @@ class Parameter:
         if not isinstance(other, Parameter):
             return NotImplemented
         return self.path == other.path
+
+    @property
+    def has_any_type(self) -> bool:
+        return any(name in {ANY_STR, ANY_INT} for name in self.path)
 
 
 @final
@@ -80,6 +86,13 @@ class ParameterSet(Hashable, MutableSet, Generic[T_Parameter]):
     def __init__(self, iterable: Iterable[T_Parameter] = ()) -> None:
         self.data: set[T_Parameter] = set(iterable)
 
+    @property
+    def has_any_type(self) -> bool:
+        return any(parameter.has_any_type for parameter in self)
+
+    def subset_any_type(self) -> ParameterSet[T_Parameter]:
+        return type(self)(parameter for parameter in self if parameter.has_any_type)
+
     def subset(self, path: tuple[str | int, ...] | int) -> ParameterSet[T_Parameter]:
         if isinstance(path, tuple):
             return type(self)(parameter for parameter in self if parameter.path[: len(path)] == path)
@@ -112,7 +125,28 @@ class ParameterSet(Hashable, MutableSet, Generic[T_Parameter]):
         self.data.update(other.data)
 
     def difference(self, other: ParameterSet[T_Parameter]) -> ParameterSet[T_Parameter]:
-        return type(self)(self.data.difference(other.data))
+        output = type(self)(self.data.difference(other.data))
+        self_any = self.subset_any_type()
+        other_any = other.subset_any_type()
+        if not self_any and not other_any:
+            # Operation done using hashes
+            return output
+        # Operation done using equality as ANY_STR and ANY_INT does not match on hash
+        # Note that this a Parameter with ANY_STR in the path can remove multiple items.
+        discard = set()
+        for item in output:
+            for any_ in other_any:
+                if item == any_:
+                    discard.add(item)
+            for any_ in self_any:
+                if item == any_:
+                    discard.add(item)
+        for d in discard:
+            output.discard(d)
+        return output
+
+    def __sub__(self, other: AbstractSet) -> ParameterSet[T_Parameter]:
+        return self.difference(type(self)(other))
 
 
 class ParameterSpecSet(ParameterSet[ParameterSpec]):
