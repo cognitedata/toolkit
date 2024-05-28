629658f587c63620b6085333983063e13387686e
Pål Rønning <ronpal@users.noreply.github.com>
2024-05-28 08:45:29 +0200

    Update tests/tests_unit/test_cdf_tk/test_load.py
    Co-authored-by: Anders Albert <60234212+doctrino@users.noreply.github.com>

--- tests/tests_unit/test_cdf_tk/test_load.py
+++ tests/tests_unit/test_cdf_tk/test_load.py
@@ -609,7 +609,6 @@ class TestExtractionPipelineDependencies:
             {"extraction_pipeline.config.yaml": yaml.CSafeLoader(self.config_yaml).get_data()}, monkeypatch
         )
 
-        loader = ExtractionPipelineConfigLoader.create_loader(cdf_tool, None)
 
         cmd = DeployCommand(print_warning=False)
         loader = ExtractionPipelineConfigLoader.create_loader(cdf_tool, None)
