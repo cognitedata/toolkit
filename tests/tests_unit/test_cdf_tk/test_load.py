52092b98e2f607d618e708f849d9d73ad619fdb7
anders-albert <anders0albert@gmail.com>
2024-06-03 08:27:37 +0200

    tests: added failing test

--- tests/tests_unit/test_cdf_tk/test_load.py
+++ tests/tests_unit/test_cdf_tk/test_load.py
@@ -30,6 +30,7 @@ from cognite_toolkit._cdf_tk.loaders import (
     LOADER_BY_FOLDER_NAME,
     LOADER_LIST,
     RESOURCE_LOADER_LIST,
+    ContainerLoader,
     DataModelLoader,
     DatapointsLoader,
     DataSetsLoader,
@@ -138,6 +139,36 @@ class TestDataSetsLoader:
         assert len(to_create) == 1
         assert len(to_change) == 0
         assert len(unchanged) == 1
+
+
+class TestContainerLoader:
+    @pytest.mark.parametrize(
+        "item",
+        [
+            pytest.param(
+                {
+                    "properties": {
+                        "myDirectRelation": {
+                            "type": "direct",
+                            "container": {
+                                "type": "container",
+                                "space": "sp_my_space",
+                                "externalId": "my_container",
+                            },
+                        }
+                    }
+                },
+                id="Direct relation property",
+            ),
+        ],
+    )
+    def test_valid_spec(self, item: dict):
+        spec = ContainerLoader.get_write_cls_parameter_spec()
+        dumped = read_parameters_from_dict(item)
+
+        extra = dumped - spec
+
+        assert not extra, f"Extra keys: {extra}"
 
 
 class TestViewLoader:
