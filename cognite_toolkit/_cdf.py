da3649b31994788727edc849cc171cdb758a3a5b
Anders Albert <60234212+doctrino@users.noreply.github.com>
2024-06-04 10:55:25 +0200

    [CDF-21663] 🧑‍🔬Dump command to class based (#601)
    refactor: moved out dump command

--- cognite_toolkit/_cdf.py
+++ cognite_toolkit/_cdf.py
@@ -15,8 +15,14 @@ from dotenv import load_dotenv
 from rich import print
 from rich.panel import Panel
 
-from cognite_toolkit._cdf_tk.commands import BuildCommand, CleanCommand, DeployCommand, DescribeCommand, auth
-from cognite_toolkit._cdf_tk.commands.dump import dump_datamodel_command
+from cognite_toolkit._cdf_tk.commands import (
+    BuildCommand,
+    CleanCommand,
+    DeployCommand,
+    DescribeCommand,
+    DumpCommand,
+    auth,
+)
 from cognite_toolkit._cdf_tk.commands.pull import pull_command
 from cognite_toolkit._cdf_tk.commands.run import run_function, run_local_function, run_transformation
 from cognite_toolkit._cdf_tk.exceptions import (
@@ -810,6 +816,14 @@ def pull_node_cmd(
     )
 
 
+@dump_app.callback(invoke_without_command=True)
+def dump_main(ctx: typer.Context) -> None:
+    """Commands to dump resource configurations from CDF into a temporary directory."""
+    if ctx.invoked_subcommand is None:
+        print("Use [bold yellow]cdf-tk dump --help[/] for more information.")
+    return None
+
+
 @dump_app.command("datamodel")
 def dump_datamodel_cmd(
     ctx: typer.Context,
@@ -856,7 +870,8 @@ def dump_datamodel_cmd(
     ] = "tmp",
 ) -> None:
     """This command will dump the selected data model as yaml to the folder specified, defaults to /tmp."""
-    dump_datamodel_command(
+    cmd = DumpCommand()
+    cmd.execute(
         CDFToolConfig.from_context(ctx),
         DataModelId(space, external_id, version),
         Path(output_dir),
--- cognite_toolkit/_cdf_tk/commands/__init__.py
+++ cognite_toolkit/_cdf_tk/commands/__init__.py
@@ -2,5 +2,6 @@ from .build import BuildCommand
 from .clean import CleanCommand
 from .deploy import DeployCommand
 from .describe import DescribeCommand
+from .dump import DumpCommand
 
-__all__ = ["BuildCommand", "CleanCommand", "DeployCommand", "DescribeCommand"]
+__all__ = ["BuildCommand", "CleanCommand", "DeployCommand", "DescribeCommand", "DumpCommand"]
