641307acf2f71db1b357b805a4c0bc1a0c013139
Anders Albert <60234212+doctrino@users.noreply.github.com>
2024-06-10 14:13:46 +0200

    Prepare 0.2.0 (#632)
    * refactor: upgrade migration
    
    * build; bump
    
    * build: poetry lock

--- tests_migrations/constants.py
+++ tests_migrations/constants.py
@@ -6,10 +6,6 @@ from pathlib import Path
 TEST_DIR_ROOT = Path(__file__).resolve().parent
 
 SUPPORTED_TOOLKIT_VERSIONS = [
-    "0.1.0",
-    "0.1.1",
-    "0.1.2",
-    "0.1.3",
     "0.2.0a1",
     "0.2.0a2",
     "0.2.0a3",
@@ -18,6 +14,7 @@ SUPPORTED_TOOLKIT_VERSIONS = [
     "0.2.0b1",
     "0.2.0b2",
     "0.2.0b3",
+    "0.2.0b4",
 ]
 
 
