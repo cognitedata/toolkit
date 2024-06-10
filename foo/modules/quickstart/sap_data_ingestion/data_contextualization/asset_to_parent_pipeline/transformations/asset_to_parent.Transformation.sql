7195cf148e97f0364ba2b5ac474983fffd9450bb
Pål Rønning <ronpal@users.noreply.github.com>
2024-06-10 14:25:34 +0200

    First version of module creation

--- foo/modules/quickstart/sap_data_ingestion/data_contextualization/asset_to_parent_pipeline/transformations/asset_to_parent.Transformation.sql
+++
@@ -1,5 +0,0 @@
-
-select
-  CAST(`Functional Loc.` AS STRING) AS externalId,
-  node_reference('sp_sap_maia', CAST(`SupFunctLoc.` AS STRING)) AS parent
-from `data-dumps`.`data-dumps_dump FLOC RZ14`
