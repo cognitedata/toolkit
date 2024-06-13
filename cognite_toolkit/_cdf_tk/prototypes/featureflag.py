5925af2c0103a281ea2e43eb2e471120de2996c3
Pål Rønning <ronpal@users.noreply.github.com>
2024-06-13 10:26:03 +0200

    disable Featureflag until next PR

--- cognite_toolkit/_cdf_tk/prototypes/featureflag.py
+++ cognite_toolkit/_cdf_tk/prototypes/featureflag.py
@@ -1,33 +1,7 @@
 from __future__ import annotations
 
-from pathlib import Path
-
-import yaml
-
 
 class FeatureFlag:
-    _flags = None
-
-    @staticmethod
-    def _get_system_yaml_path() -> Path | None:
-        loc = Path(__file__)
-        while loc != loc.root:
-            loc = loc.parent
-            if (loc / "_system.yaml").exists():
-                return loc / "_system.yaml"
-        return None
-
     @staticmethod
     def enabled(flag: str) -> bool:
-        if flag == "FF_INTERACTIVE_INIT":
-            return True
-
-        if FeatureFlag._flags is None:
-            system_yaml_path = FeatureFlag._get_system_yaml_path()
-            if system_yaml_path:
-                with open(system_yaml_path) as f:
-                    FeatureFlag._flags = {k: v for k, v in yaml.safe_load(f).items() if k.startswith("FF_")}
-            else:
-                FeatureFlag._flags = {}
-
-        return FeatureFlag._flags.get(flag, False)
+        return False
