dca85a1e97dc9a7581cb250cc514973e82f32e83
anders-albert <anders0albert@gmail.com>
2024-06-14 16:58:35 +0200

    refactor; fix

--- tests/tests_unit/test_cdf_tk/test_commands/test_build.py
+++ tests/tests_unit/test_cdf_tk/test_commands/test_build.py
@@ -25,7 +25,9 @@ class TestBuildCommand:
     def test_get_loader_raises_ambiguous_error(self):
         with pytest.raises(AmbiguousResourceFileError) as e:
             BuildCommand()._get_loader(
-                "transformations", destination=Path("my_module") / "transformations" / "notification.yaml"
+                "transformations",
+                destination=Path("transformation") / "notification.yaml",
+                source_path=Path("my_module") / "transformations" / "notification.yaml",
             )
         assert "Ambiguous resource file" in str(e.value)
 
