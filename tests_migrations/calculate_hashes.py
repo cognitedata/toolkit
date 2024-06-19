679a201ae3e1d1be3170cc7742a094bfa1f511db
anders-albert <anders0albert@gmail.com>
2024-06-19 11:27:32 +0200

    refactor; remove unused

--- tests_migrations/calculate_hashes.py
+++ tests_migrations/calculate_hashes.py
@@ -1,14 +1,18 @@
-from constants import PROJECT_INIT_DIR, SUPPORTED_TOOLKIT_VERSIONS
+from pathlib import Path
 
 from cognite_toolkit._cdf_tk.utils import calculate_directory_hash
 
+TEST_DIR_ROOT = Path(__file__).resolve().parent
+PROJECT_INIT_DIR = TEST_DIR_ROOT / "project_inits"
+
+# Todo this file can be deleted when we go to 0.3.0alpha and remove
+#   the old manual migration, cognite_toolkit/_cdf_tk/_migration.yaml
+
 
 def calculate_hashes():
-    exclude_prefixes = set()
-    for version in SUPPORTED_TOOLKIT_VERSIONS:
-        project_init = PROJECT_INIT_DIR / f"project_{version}"
-
-        version_hash = calculate_directory_hash(project_init / "cognite_modules", exclude_prefixes=exclude_prefixes)
+    for directory in PROJECT_INIT_DIR.iterdir():
+        version = directory.name.split("_")[1]
+        version_hash = calculate_directory_hash(directory / "cognite_modules")
         print(f"Cognite Module Hash for version {version!r}: {version_hash!r}")
 
 
