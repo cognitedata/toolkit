144db546d904568761df6ca3216fe22be48eec13
anders-albert <anders0albert@gmail.com>
2024-06-20 08:21:34 +0200

    tests: Finished test data

--- tests/tests_unit/test_cdf_tk/test_commands/test_import.py
+++ tests/tests_unit/test_cdf_tk/test_commands/test_import.py
@@ -5,6 +5,10 @@ from tests.tests_unit.data import TRANSFORMATION_CLI
 
 
 class TestImportTransformationCLI:
-    def test_import_transformation_cli(self, tmp_path: Path):
+    def test_import_transformation_cli(self, tmp_path: Path) -> None:
         cmd = ImportTransformationCLI(print_warning=False)
-        cmd.execute(TRANSFORMATION_CLI, tmp_path, False, False)
+        cmd.execute(TRANSFORMATION_CLI, tmp_path, False, False, verbose=False)
+
+        file_count = len(list(tmp_path.rglob("*")))
+
+        assert file_count == 3
