325a7136b445069faa09d3fd584fdefe2f50d8c6
anders-albert <anders0albert@gmail.com>
2024-06-28 15:56:36 +0200

    refactor: also turn off for groups

--- cognite_toolkit/_cdf_tk/prototypes/turn_off_naming_check.py
+++ cognite_toolkit/_cdf_tk/prototypes/turn_off_naming_check.py
@@ -1,9 +1,7 @@
 from collections.abc import Hashable
 from pathlib import Path
 
-from cognite_toolkit._cdf_tk.loaders import (
-    RESOURCE_LOADER_LIST,
-)
+from cognite_toolkit._cdf_tk.loaders import RESOURCE_LOADER_LIST, GroupLoader
 from cognite_toolkit._cdf_tk.tk_warnings import WarningList, YAMLFileWarning
 
 
@@ -11,5 +9,7 @@ def do() -> None:
     def no_op(cls: type, identifier: Hashable, filepath: Path, verbose: bool) -> WarningList[YAMLFileWarning]:
         return WarningList[YAMLFileWarning]()
 
-    for loader in RESOURCE_LOADER_LIST:
-        loader.check_identifier_semantics = classmethod(no_op)  # type: ignore[method-assign, assignment, arg-type]
+
+def do() -> None:
+    for loader in [*RESOURCE_LOADER_LIST, GroupLoader]:
+        loader.check_identifier_semantics = classmethod(no_op)  # type: ignore[attr-defined]
