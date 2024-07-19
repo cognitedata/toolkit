24f006fb88f2bdd19ac1e7e2a8d8114e83eddb8d
anders-albert <anders0albert@gmail.com>
2024-07-19 23:02:41 +0200

    refactor: finish prototype

--- cognite_toolkit/_cdf_tk/prototypes/modify_function_schedule.py
+++ cognite_toolkit/_cdf_tk/prototypes/modify_function_schedule.py
@@ -1,16 +1,28 @@
+from collections import defaultdict
 from pathlib import Path
+from typing import cast
 
-from cognite.client.data_classes import FunctionSchedule, FunctionScheduleWrite, FunctionScheduleWriteList
+from cognite.client.data_classes import (
+    ClientCredentials,
+    FunctionSchedule,
+    FunctionSchedulesList,
+    FunctionScheduleWrite,
+    FunctionScheduleWriteList,
+)
+from cognite.client.utils.useful_types import SequenceNotStr
 
 from cognite_toolkit._cdf_tk.exceptions import ToolkitRequiredValueError
 from cognite_toolkit._cdf_tk.loaders import (
+    FunctionLoader,
     FunctionScheduleLoader,
 )
 from cognite_toolkit._cdf_tk.utils import CDFToolConfig, load_yaml_inject_variables
 
 
 def modify_function_schedule_loader() -> None:
-    split_character = "/\\"
+    # The split character is designed to be unique and not present in
+    # the name of the function schedule.
+    split_character = r"""πℇr!"#$%&🎃'()*+,-./:;<=>?🎃@[\]^_`{|}~ℇπ"""
 
     def get_id(cls: type[FunctionScheduleLoader], item: FunctionScheduleWrite | FunctionSchedule | dict) -> str:
         nonlocal split_character
@@ -43,54 +55,47 @@ def modify_function_schedule_loader() -> None:
 
     FunctionScheduleLoader.load_resource = load_resource  # type: ignore[method-assign]
 
-    #
-    # def _resolve_functions_ext_id(self, items: FunctionScheduleWriteList) -> FunctionScheduleWriteList:
-    #     functions = FunctionLoader(self.client, None).retrieve(list(set([item.function_external_id for item in items])))
-    #     for item in items:
-    #         for func in functions:
-    #             if func.external_id == item.function_external_id:
-    #                 item.function_id = func.id  # type: ignore[assignment]
-    #     return items
+    def retrieve(self: FunctionScheduleLoader, ids: SequenceNotStr[str]) -> FunctionSchedulesList:
+        nonlocal split_character
+        names_by_function: dict[str, set[str]] = defaultdict(set)
+        for id_ in ids:
+            function_external_id, name = id_.rsplit(split_character, 1)
+            names_by_function[function_external_id].add(name)
+        functions = FunctionLoader(self.client, None).retrieve(list(names_by_function))
+        schedules = FunctionSchedulesList([])
+        for func in functions:
+            ret = self.client.functions.schedules.list(function_id=func.id, limit=-1)
+            for schedule in ret:
+                schedule.function_external_id = func.external_id
+            schedules.extend(
+                [schedule for schedule in ret if schedule.name in names_by_function[cast(str, func.external_id)]]
+            )
+        return schedules
 
-    # def retrieve(self, ids: SequenceNotStr[str]) -> FunctionSchedulesList:
-    #     crons_by_function: dict[str, set[str]] = defaultdict(set)
-    #     for id_ in ids:
-    #         function_external_id, cron = id_.rsplit(self._split_character, 1)
-    #         crons_by_function[function_external_id].add(cron)
-    #     functions = FunctionLoader(self.client, None).retrieve(list(crons_by_function))
-    #     schedules = FunctionSchedulesList([])
-    #     for func in functions:
-    #         ret = self.client.functions.schedules.list(function_id=func.id, limit=-1)
-    #         for schedule in ret:
-    #             schedule.function_external_id = func.external_id
-    #         schedules.extend(
-    #             [
-    #                 schedule
-    #                 for schedule in ret
-    #                 if schedule.cron_expression in crons_by_function[cast(str, func.external_id)]
-    #             ]
-    #         )
-    #     return schedules
-    #
-    # def create(self, items: FunctionScheduleWriteList) -> FunctionSchedulesList:
-    #     items = self._resolve_functions_ext_id(items)
-    #     created = []
-    #     for item in items:
-    #         key = f"{item.function_external_id}:{item.cron_expression}"
-    #         auth_config = self.extra_configs.get(key, {}).get("authentication", {})
-    #         if "clientId" in auth_config and "clientSecret" in auth_config:
-    #             client_credentials = ClientCredentials(auth_config["clientId"], auth_config["clientSecret"])
-    #         else:
-    #             client_credentials = None
-    #
-    #         created.append(
-    #             self.client.functions.schedules.create(
-    #                 name=item.name or "",
-    #                 description=item.description or "",
-    #                 cron_expression=cast(str, item.cron_expression),
-    #                 function_id=cast(int, item.function_id),
-    #                 data=item.data,
-    #                 client_credentials=client_credentials,
-    #             )
-    #         )
-    #     return FunctionSchedulesList(created)
+    FunctionScheduleLoader.retrieve = retrieve  # type: ignore[method-assign]
+
+    def create(self: FunctionScheduleLoader, items: FunctionScheduleWriteList) -> FunctionSchedulesList:
+        nonlocal split_character
+        items = self._resolve_functions_ext_id(items)
+        created = []
+        for item in items:
+            key = f"{item.function_external_id}{split_character}{item.name}"
+            auth_config = self.extra_configs.get(key, {}).get("authentication", {})
+            if "clientId" in auth_config and "clientSecret" in auth_config:
+                client_credentials = ClientCredentials(auth_config["clientId"], auth_config["clientSecret"])
+            else:
+                client_credentials = None
+
+            created.append(
+                self.client.functions.schedules.create(
+                    name=item.name or "",
+                    description=item.description or "",
+                    cron_expression=item.cron_expression,
+                    function_id=item.function_id,
+                    data=item.data,
+                    client_credentials=client_credentials,
+                )
+            )
+        return FunctionSchedulesList(created)
+
+    FunctionScheduleLoader.create = create  # type: ignore[method-assign]
