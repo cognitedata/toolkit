149e3538bd3007f57309a691e9c4b4667a461616
anders-albert <anders0albert@gmail.com>
2024-08-20 20:21:02 +0200

    tests: failing test

---
+++ tests/test_unit/test_cdf_tk/test_commands/test_deploy.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+from unittest.mock import MagicMock
+
+import pytest
+
+from cognite_toolkit._cdf_tk.commands import DeployCommand
+from cognite_toolkit._cdf_tk.loaders import ViewLoader
+from cognite_toolkit._cdf_tk.utils import CDFToolConfig
+
+
+class TestDeployCommand:
+    def test_load_files(self, cdf_tool_config: CDFToolConfig) -> None:
+        path = MagicMock(spec=Path)
+        path.name = "my.View.yaml"
+        path.read_text.side_effect = VIEW_SOURCE_NONE
+        cmd = DeployCommand(print_warning=False, skip_tracking=True)
+
+        with pytest.raises(TypeError) as e:
+            cmd._load_files(
+                ViewLoader.create_loader(cdf_tool_config, None), [path], cdf_tool_config, skip_validation=True
+            )
+
+        assert e.value
+
+
+VIEW_SOURCE_NONE = """- space: dm_domain_generic
+
+  externalId: Equipment
+  name: Equipment
+  version: v1
+  properties:
+    id:
+      container:
+        space: dm_domain_generic
+        externalId: Tag
+      containerPropertyIdentifier: id
+      type:
+        list: false
+        collation: ucs_basic
+        type: text
+      nullable: true
+      immutable: false
+      autoIncrement: false
+      source:
+      defaultValue:
+      name: id
+      description:
+"""
