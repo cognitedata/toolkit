027b0f0db5321113cbb8ac1bce3f9ceb49d88927
anders-albert <anders0albert@gmail.com>
2024-08-29 05:42:31 +0200

    tests: updated last test

--- tests/test_integration/test_toolkit_client/test_locations.py
+++ tests/test_integration/test_toolkit_client/test_locations.py
@@ -4,7 +4,7 @@ import pytest
 from cognite.client.exceptions import CogniteAPIError, CogniteNotFoundError
 
 from cognite_toolkit._cdf_tk.client import ToolkitClient
-from cognite_toolkit._cdf_tk.client.data_classes.locations import (
+from cognite_toolkit._cdf_tk.client.data_classes.location_filters import (
     LocationFilter,
     LocationFilterList,
     LocationFilterWrite,
@@ -15,13 +15,13 @@ SESSION_EXTERNAL_ID = f"loc_ext_id_{RUN_UNIQUE_ID}"
 
 
 @pytest.fixture(scope="session", autouse=True)
-def cleanup_before_session(toolkit_client: ToolkitClient):
+def cleanup_before_session(toolkit_client: ToolkitClient) -> None:
     # Cleanup previously created locationfilters
 
     try:
-        for location_filter in toolkit_client.locations.filters.list():
+        for location_filter in toolkit_client.location_filters.list():
             if location_filter.external_id.startswith("loc_ext_id_"):
-                toolkit_client.locations.filters.delete(location_filter.id)
+                toolkit_client.location_filters.delete(location_filter.id)
     except CogniteAPIError:
         pass
 
@@ -35,10 +35,10 @@ def existing_location_filter(toolkit_client: ToolkitClient) -> LocationFilter:
         external_id=SESSION_EXTERNAL_ID,
     )
     try:
-        retrieved = toolkit_client.locations.filters.retrieve(RUN_UNIQUE_ID)
+        retrieved = toolkit_client.location_filters.retrieve(RUN_UNIQUE_ID)
         return retrieved
     except CogniteAPIError:
-        created = toolkit_client.locations.filters.create(location_filter)
+        created = toolkit_client.location_filters.create(location_filter)
         return created
 
 
@@ -50,30 +50,30 @@ class TestLocationFilterAPI:
             parent_id=f"loc_par_{SESSION_EXTERNAL_ID}",
         )
         with contextlib.suppress(CogniteAPIError):  # Should be CogniteDuplicatedError, but API throws 500 ATM
-            created = toolkit_client.locations.filters.create(location_filter)
+            created = toolkit_client.location_filters.create(location_filter)
             assert isinstance(created, LocationFilter)
             assert created.as_write().dump() == location_filter.dump()
 
-            retrieved = toolkit_client.locations.filters.retrieve(created.id)
+            retrieved = toolkit_client.location_filters.retrieve(created.id)
             assert isinstance(retrieved, LocationFilter)
             assert retrieved.as_write().dump() == location_filter.dump()
 
-            toolkit_client.locations.filters.delete(created.id)
+            toolkit_client.location_filters.delete(created.id)
 
             with pytest.raises(CogniteNotFoundError):
-                toolkit_client.locations.filters.retrieve(created.id)
+                toolkit_client.location_filters.retrieve(created.id)
 
     def test_list_location_filters(
         self, toolkit_client: ToolkitClient, existing_location_filter: LocationFilter
     ) -> None:
-        location_filters = toolkit_client.locations.filters.list()
+        location_filters = toolkit_client.location_filters.list()
         assert isinstance(location_filters, LocationFilterList)
         assert len(location_filters) > 0
 
     def test_iterate_location_filters(
         self, toolkit_client: ToolkitClient, existing_location_filter: LocationFilter
     ) -> None:
-        for location_filters in toolkit_client.locations.filters:
+        for location_filters in toolkit_client.location_filters:
             assert isinstance(location_filters, LocationFilter)
             break
         else:
@@ -84,5 +84,5 @@ class TestLocationFilterAPI:
     ) -> None:
         update = existing_location_filter
         update.description = "New description"
-        updated = toolkit_client.locations.filters.update(update.id, update.as_write())
+        updated = toolkit_client.location_filters.update(update.id, update.as_write())
         assert updated.description == update.description
