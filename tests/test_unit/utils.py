d701bc77a5dd0b225e62f147395b8b89715628bc
Pål Rønning <ronpal@users.noreply.github.com>
2024-08-29 06:17:27 +0200

    Location filter support  (#833)
    * Savepoint
    
    * Savepoint
    
    * proper conditional loading
    
    * Create and update work
    
    * Loading tests
    
    * syncing
    
    * Future
    
    * Moving location filters to core
    
    * Moved to core
    
    * Tests almost done
    
    * covered some API quirks
    
    * covered some API quirks
    
    * into the future
    
    * remembered to use session on tests
    
    * test stability
    
    * fix: type hint 3.9 compatible
    
    * Update cognite_toolkit/_cdf_tk/client/data_classes/locations.py
    
    Following the API spec
    
    Co-authored-by: Anders Albert <60234212+doctrino@users.noreply.github.com>
    
    * Update cognite_toolkit/_cdf_tk/loaders/_resource_loaders/location_loaders.py
    
    Co-authored-by: Anders Albert <60234212+doctrino@users.noreply.github.com>
    
    * Fixing according to review feedback
    
    * Fixed tests
    
    * Fixing and cleaning
    
    * Upgraded SDK and switched to SDK ACL
    
    * Savepoint
    
    * Mocking problems
    
    * Mocking problems
    
    * Fixing tests
    
    * correct mocking
    
    * Fixing tests
    
    * greener still
    
    * added locationFiltersAcl to admin groups
    
    * build: lower bound cognite-sdk
    
    * fix: added missing dump methods
    
    * Ãtests: regen
    
    * fix: missing _load methods
    
    * fix: Only identifier immutable
    
    * Update cognite_toolkit/_cdf_tk/client/api/locations/location_filters.py
    
    * Update cognite_toolkit/_cdf_tk/client/api/locations/location_filters.py
    
    * refactor: extended example
    
    * fix: instansiate LocationFilter
    
    * refactor; finish dependencies
    
    * refactor: spec for LocationLoader
    
    * refactor: fix move location filter to correct place
    
    * fix: imports
    
    * tests: update Location filter
    
    * tests: updated last test
    
    * refactor; fix
    
    * fix: get correct type hint
    
    * refactor: added sap data space
    
    * fix: bug when running dry run
    
    * build: changelog
    
    * tests: regen
    
    ---------
    
    Co-authored-by: anders-albert <anders0albert@gmail.com>
    Co-authored-by: Anders Albert <60234212+doctrino@users.noreply.github.com>

--- tests/test_unit/utils.py
+++ tests/test_unit/utils.py
@@ -44,6 +44,7 @@ from cognite.client.data_classes.workflows import WorkflowTaskOutput, WorkflowTa
 from cognite.client.testing import CogniteClientMock
 
 from cognite_toolkit._cdf_tk._parameters.get_type_hints import _TypeHints
+from cognite_toolkit._cdf_tk.client.data_classes.location_filters import LocationFilterScene
 from cognite_toolkit._cdf_tk.utils import load_yaml_inject_variables, read_yaml_file
 
 
@@ -91,6 +92,7 @@ def mock_read_yaml_file(
         "timeseries_loaders",
         "transformation_loaders",
         "three_d_model_loaders",
+        "location_loaders",
         "workflow_loaders",
     ]:
         monkeypatch.setattr(
@@ -186,6 +188,8 @@ class FakeCogniteResourceGenerator:
             if resource_cls is Geometry and name == "geometries":
                 # Special case for Geometry to avoid recursion.
                 value = None
+            elif name == "scene":
+                value = self.create_value(LocationFilterScene, var_name=name)
             else:
                 value = self.create_value(type_hint_by_name[name], var_name=name)
 
