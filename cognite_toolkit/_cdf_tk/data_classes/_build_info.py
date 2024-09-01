a0ebd219ef8df4b8a77672440ba95cf502826618
anders-albert <anders0albert@gmail.com>
2024-09-01 11:07:30 +0200

    refactor: shell for ModuleResources

--- cognite_toolkit/_cdf_tk/data_classes/_build_info.py
+++ cognite_toolkit/_cdf_tk/data_classes/_build_info.py
@@ -8,6 +8,7 @@ from typing import Any, ClassVar, Generic, SupportsIndex, overload
 from cognite_toolkit._cdf_tk.loaders._base_loaders import T_ID
 
 from ._base import ConfigCore
+from ._module_directories import ModuleDirectories
 
 
 @dataclass
@@ -71,6 +72,13 @@ class BuildInfo(ConfigCore):
     def load(cls, data: dict[str, Any], build_env: str, filepath: Path) -> BuildInfo:
         return cls(filepath, ModulesInfo.load(data))
 
+    @classmethod
+    def rebuild(cls, project_dir: Path, build_env: str, needs_rebuild: set[Path] | None = None) -> BuildInfo:
+        raise NotImplementedError()
+
+    def compare_modules(self, current_modules: ModuleDirectories) -> set[Path]:
+        raise NotImplementedError()
+
 
 class ModuleResources:
     """This class is used to retrieve resource information from the build info.
@@ -80,13 +88,17 @@ class ModuleResources:
     """
 
     def __init__(self, project_dir: Path, build_env: str) -> None:
-        self._build_info = BuildInfo.load_from_directory(project_dir, build_env)
+        self._project_dir = project_dir
+        self._build_env = build_env
+        self._build_info: BuildInfo
+        try:
+            self._build_info = BuildInfo.load_from_directory(project_dir, build_env)
+        except FileNotFoundError:
+            self._build_info = BuildInfo.rebuild(project_dir, build_env)
 
     def list(self) -> ModuleList:
-        raise NotImplementedError
-
-    def retrieve_resource(self, kind: str, identifier: T_ID) -> ResourceInfo[T_ID]:
-        raise NotImplementedError
-
-    def retrieve_resources(self, kind: str) -> ResourceList:
-        raise NotImplementedError
+        current_modules = ModuleDirectories.load(self._project_dir, set())
+        # Check if the build info is up-to-date
+        if needs_rebuild := self._build_info.compare_modules(current_modules):
+            self._build_info = BuildInfo.rebuild(self._project_dir, self._build_env, needs_rebuild)
+        return self._build_info.modules.modules
