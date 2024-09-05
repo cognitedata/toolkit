1cded8c5f399903b835ea74b323385c0e353f850
anders-albert <anders0albert@gmail.com>
2024-09-05 07:23:03 +0200

    refactor: remove old run locally

--- cognite_toolkit/_cdf_tk/commands/run.py
+++ cognite_toolkit/_cdf_tk/commands/run.py
@@ -7,8 +7,6 @@ import os
 import platform
 import re
 import shutil
-import subprocess
-import tempfile
 import time
 from pathlib import Path
 from typing import Any, cast
@@ -18,8 +16,6 @@ from cognite.client._api.functions import ALLOWED_HANDLE_ARGS
 from cognite.client.credentials import OAuthClientCredentials, OAuthInteractive, Token
 from cognite.client.data_classes import (
     FunctionCall,
-    FunctionScheduleWriteList,
-    FunctionWriteList,
 )
 from cognite.client.data_classes.transformations import TransformationList
 from cognite.client.data_classes.transformations.common import NonceCredentials
@@ -28,10 +24,8 @@ from rich import print
 from rich.progress import Progress
 from rich.table import Table
 
-from cognite_toolkit._cdf_tk.cdf_toml import CDFToml
-from cognite_toolkit._cdf_tk.commands.build import BuildCommand
 from cognite_toolkit._cdf_tk.constants import _RUNNING_IN_BROWSER
-from cognite_toolkit._cdf_tk.data_classes import BuildConfigYAML, ModuleResources, ResourceBuildInfoFull
+from cognite_toolkit._cdf_tk.data_classes import ModuleResources, ResourceBuildInfoFull
 from cognite_toolkit._cdf_tk.exceptions import (
     ToolkitFileNotFoundError,
     ToolkitInvalidFunctionError,
@@ -42,7 +36,7 @@ from cognite_toolkit._cdf_tk.exceptions import (
 from cognite_toolkit._cdf_tk.loaders import FunctionLoader, FunctionScheduleLoader
 from cognite_toolkit._cdf_tk.loaders.data_classes import FunctionScheduleID
 from cognite_toolkit._cdf_tk.tk_warnings import MediumSeverityWarning
-from cognite_toolkit._cdf_tk.utils import CDFToolConfig, get_oneshot_session, module_from_path, safe_read
+from cognite_toolkit._cdf_tk.utils import CDFToolConfig, get_oneshot_session
 
 from ._base import ToolkitCommand
 
@@ -530,276 +524,6 @@ if __name__ == "__main__":
             )
             table.add_row("Logs", str(logs))
             print(table)
-        return True
-
-    def run_local_function(
-        self,
-        ToolGlobals: CDFToolConfig,
-        source_path: Path,
-        external_id: str,
-        payload: str,
-        build_env_name: str,
-        schedule: str | None = None,
-        rebuild_env: bool = False,
-        verbose: bool = False,
-        no_cleanup: bool = False,
-    ) -> bool:
-        try:
-            import venv
-        except ImportError:
-            if _RUNNING_IN_BROWSER:
-                print("  [bold red]ERROR:[/] This functionality is not supported in a browser environment.")
-                return False
-            raise
-
-        cdf_toml = CDFToml.load(source_path)
-        config = BuildConfigYAML.load_from_directory(source_path, build_env_name)
-        print(f"[bold]Building for environment {build_env_name} using {source_path!s} as sources...[/bold]")
-        config.set_environment_variables()
-        build_dir = Path(tempfile.mkdtemp(prefix="build.", suffix=".tmp", dir=Path.cwd()))
-
-        found = False
-        for function_dir in source_path.glob("**/functions"):
-            if not function_dir.is_dir():
-                continue
-            for path in function_dir.iterdir():
-                if path.is_dir() and path.name == external_id:
-                    config.environment.selected = [module_from_path(path)]
-                    found = True
-                    break
-
-        if not found:
-            print(f"  [bold red]ERROR:[/] Could not find function with external id {external_id}, exiting.")
-            return False
-        BuildCommand().build_config(
-            build_dir=build_dir,
-            organization_dir=source_path,
-            config=config,
-            packages=cdf_toml.modules.packages,
-            clean=True,
-            verbose=False,
-        )
-        virtual_env_dir = Path(source_path / f".venv.{external_id}")
-        if not virtual_env_dir.exists() or rebuild_env:
-            print(f"  Creating virtual environment in {virtual_env_dir}...")
-            venv.create(env_dir=virtual_env_dir.as_posix(), with_pip=True, system_site_packages=False)
-            req_file = build_dir / "functions" / external_id / "requirements.txt"
-            if req_file.exists():
-                if platform.system() == "Windows":
-                    process = subprocess.run(
-                        [
-                            str(virtual_env_dir / "Scripts" / "pip"),
-                            "install",
-                            "-r",
-                            str(req_file),
-                        ],
-                        capture_output=True,
-                        shell=True,
-                    )
-                else:
-                    process = subprocess.run(
-                        [
-                            f"{virtual_env_dir}/bin/pip",
-                            "--python",
-                            f"{virtual_env_dir}/bin/python",
-                            "install",
-                            "--disable-pip-version-check",
-                            "-r",
-                            f"{req_file}",
-                        ],
-                        capture_output=True,
-                    )
-                if process.returncode != 0:
-                    print(
-                        "  [bold red]ERROR:[/] Failed to install requirements in virtual environment: ",
-                        process.stderr.decode("utf-8"),
-                    )
-                    if not no_cleanup:
-                        shutil.rmtree(build_dir)
-                    return False
-                if verbose:
-                    print(process.stdout.decode("utf-8"))
-
-        if verbose:
-            print(f"  [bold]Loading function from {build_dir}...[/]")
-        function_loader: FunctionLoader = FunctionLoader.create_loader(ToolGlobals, build_dir)
-        function = None
-        for filepath in function_loader.find_files():
-            functions = function_loader.load_resource(
-                Path(filepath), ToolGlobals=ToolGlobals, skip_validation=True
-            ) or FunctionWriteList([])
-            if not isinstance(functions, FunctionWriteList):
-                functions = FunctionWriteList([functions])
-            for func in functions:
-                if func.external_id == external_id:
-                    function = func
-        if not function:
-            print(
-                f"  [bold red]ERROR:[/] Could not find function with external id {external_id} in the build directory, exiting."
-            )
-            if not no_cleanup:
-                shutil.rmtree(build_dir)
-            return False
-        handler_file = Path(build_dir) / "functions" / external_id / func.function_path  # type: ignore[operator]
-        if not handler_file.exists():
-            print(f"  [bold red]ERROR:[/] Could not find handler file {handler_file}, exiting.")
-            if not no_cleanup:
-                shutil.rmtree(build_dir)
-            return False
-        if verbose:
-            print("  [bold]Creating environment and tmpmain.py to run...[/]")
-        # Create environment to transfer to sub-process
-        environ = {
-            "CDF_CLUSTER": ToolGlobals.environ("CDF_CLUSTER"),
-            "CDF_PROJECT": ToolGlobals.environ("CDF_PROJECT"),
-        }
-        if ToolGlobals.environ("CDF_TOKEN", fail=False):
-            environ["CDF_TOKEN"] = ToolGlobals.environ("CDF_TOKEN")
-        else:
-            schedule_loader = FunctionScheduleLoader.create_loader(ToolGlobals, build_dir)
-            for filepath in schedule_loader.find_files():
-                schedule_loader.load_resource(
-                    Path(filepath), ToolGlobals=ToolGlobals, skip_validation=False
-                ) or FunctionScheduleWriteList([])
-            if schedule_loader.extra_configs.get(f"{external_id}:{schedule}", {}).get("authentication"):
-                environ.update(
-                    {
-                        "IDP_CLIENT_ID": schedule_loader.extra_configs[f"{external_id}:{schedule}"][
-                            "authentication"
-                        ].get("clientId", ""),
-                        "IDP_CLIENT_SECRET": schedule_loader.extra_configs[f"{external_id}:{schedule}"][
-                            "authentication"
-                        ].get("clientSecret", ""),
-                    },
-                )
-            else:
-                environ.update(
-                    {
-                        "IDP_CLIENT_ID": ToolGlobals.environ("IDP_CLIENT_ID"),
-                        "IDP_CLIENT_SECRET": ToolGlobals.environ("IDP_CLIENT_SECRET"),
-                    }
-                )
-            if ToolGlobals.environ("IDP_TOKEN_URL", fail=False):
-                environ["IDP_TOKEN_URL"] = ToolGlobals.environ("IDP_TOKEN_URL")
-            if ToolGlobals.environ("CDF_URL", fail=False):
-                environ["CDF_URL"] = ToolGlobals.environ("CDF_URL")
-            if ToolGlobals.environ("IDP_TENANT_ID", fail=False):
-                environ["IDP_TENANT_ID"] = ToolGlobals.environ("IDP_TENANT_ID")
-            if ToolGlobals.environ("IDP_AUDIENCE", fail=False):
-                environ["IDP_AUDIENCE"] = ToolGlobals.environ("IDP_AUDIENCE")
-            if ToolGlobals.environ("IDP_SCOPES", fail=False):
-                environ["IDP_SCOPES"] = ToolGlobals.environ("IDP_SCOPES")
-        if function.env_vars is not None and len(function.env_vars) > 0:
-            for var, value in function.env_vars.items():
-                environ[var] = value
-        # Create temporary main file to execute
-        (handler_file.parent / "tmpmain.py").write_text(
-            """
-from pathlib import Path
-from handler import handle
-import json
-import inspect
-import os
-from collections import OrderedDict
-
-from cognite.client import CogniteClient
-
-
-def get_args(fn, handle_args):
-    params = inspect.signature(fn).parameters
-    kwargs = OrderedDict()
-    for p in params.values():
-        if p.name in handle_args:
-            kwargs[p.name] = handle_args[p.name]
-    return kwargs
-
-if __name__ == "__main__":
-    client = CogniteClient.default_oauth_client_credentials(
-        client_id=os.getenv('IDP_CLIENT_ID'),
-        client_secret=os.getenv('IDP_CLIENT_SECRET'),
-        project=os.getenv('CDF_PROJECT'),
-        cdf_cluster=os.getenv('CDF_CLUSTER'),
-        tenant_id=os.getenv('IDP_TENANT_ID'),
-        client_name="cognite-toolkit",
-    )
-    data = json.loads(Path("./in.json").read_text())
-    args = get_args(handle, {
-        "client": client,
-        "data": data,
-        "secrets": {},
-        "function_call_info": {"local": True}
-    })
-    out = handle(**args)
-    Path("./out.json").write_text(json.dumps(out))
-
-    """
-        )
-        try:
-            (Path(build_dir) / "functions" / external_id / "in.json").write_text(json.dumps(json.loads(payload)))
-        except Exception:
-            print(f"  [bold red]ERROR:[/] Could not decode your payload as json: {payload}")
-            print('Remember to escape, example: --payload={\\"name\\": \\"test\\"}')
-            if not no_cleanup:
-                shutil.rmtree(build_dir)
-            return False
-        print("[bold]Running function locally...[/]")
-        print("-------------------------------")
-        if platform.system() == "Windows":
-            python_exe = Path(virtual_env_dir / "Scripts" / "python.exe").absolute()
-        else:
-            python_exe = Path(virtual_env_dir / "bin" / "python").absolute()
-        if verbose:
-            print(f"  [bold]Running function with {python_exe}...[/]")
-
-        if platform.system() == "Windows" and (system_root := os.environ.get("SYSTEMROOT")):
-            # This is needed to run python on Windows.
-            environ["SYSTEMROOT"] = system_root
-        if platform.system() == "Windows":
-            environ = {k: str(v) for k, v in environ.items()}
-
-        process_run = subprocess.run(
-            [
-                str(python_exe),
-                "-Xfrozen_modules=off",
-                "tmpmain.py",
-            ],
-            capture_output=True,
-            shell=True if platform.system() == "Windows" else False,
-            env=environ,
-            cwd=Path(build_dir) / "functions" / external_id,
-        )
-
-        out, err = process_run.stdout.decode("utf-8"), process_run.stderr.decode("utf-8")
-
-        if process_run.returncode != 0:
-            if "ModuleNotFoundError: No module named 'cognite'" in err:
-                print(
-                    "  [bold red]ERROR:[/] Could not find the Cognite SDK available in your function, check requirements.txt and try , try --rebuild-env:",
-                    err,
-                )
-            else:
-                print(
-                    "  [bold red]ERROR:[/] Failed to run function: ",
-                    err,
-                    out,
-                )
-            if not no_cleanup:
-                shutil.rmtree(build_dir)
-            return False
-        if (outfile := Path(build_dir) / "functions" / external_id / "out.json").exists():
-            out_data = json.loads(safe_read(outfile))
-            print("  [bold]Function output:[/]")
-            print(json.dumps(out_data, indent=2, sort_keys=True))
-        else:
-            print(
-                "  [bold red]ERROR:[/] Could not get output from function.",
-            )
-        print("-------------------------------")
-        print(f"[bold]Function {external_id} run completed with the following log: [/bold]")
-        print("------------")
-        print(out)
-        if not no_cleanup:
-            shutil.rmtree(build_dir)
         return True
 
 
