598591956c571a800c69ca9353793f2717df799e
anders-albert <anders0albert@gmail.com>
2024-09-06 08:54:04 +0200

    refactor: color

--- cognite_toolkit/_cdf_tk/commands/modules.py
+++ cognite_toolkit/_cdf_tk/commands/modules.py
@@ -379,12 +379,21 @@ class ModulesCommand(ToolkitCommand):
         table.add_column("Location", style="bold")
 
         for module in modules.list():
+            if module.status == "Success":
+                status = f"[green]{module.status}[/]"
+            else:
+                status = f"[red]{module.status}[/]"
+            if module.warning_count > 0:
+                warning_count = f"[yellow]{module.warning_count:,}[/]"
+            else:
+                warning_count = f"{module.warning_count:,}"
+
             table.add_row(
                 module.name,
                 f"{len(module.resources):,}",
                 f"{sum(len(resources) for resources in module.resources.values()):,}",
-                f"{module.warning_count:,}",
-                module.status,
+                warning_count,
+                status,
                 module.location.path.as_posix(),
             )
 
