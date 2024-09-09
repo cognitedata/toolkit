f8ef5c9c62c16e1cb798e9ae530c3f1c24a48612
anders-albert <anders0albert@gmail.com>
2024-09-09 17:58:44 +0200

    tests: extended test script

--- tests/trigger_mixpanel_event.py
+++ tests/trigger_mixpanel_event.py
@@ -1,23 +1,68 @@
 """This script is for triggering a Mixpanel event. It is used for testing purposes only."""
 
 import sys
+from pathlib import Path
 
 from cognite_toolkit._cdf_tk.cdf_toml import CDFToml
+from cognite_toolkit._cdf_tk.data_classes import (
+    BuildLocationEager,
+    BuildVariables,
+    BuiltModule,
+    ResourceBuildInfo,
+    ResourceBuiltList,
+)
 from cognite_toolkit._cdf_tk.tk_warnings import WarningList
 from cognite_toolkit._cdf_tk.tracker import Tracker
 from tests.constants import REPO_ROOT, chdir
 
 
-def main() -> None:
+def track_cli_command() -> None:
     sys.argv = ["commandTest", "pos0", "pos1", "--opt1", "opt1", "--opt2", "opt2", "--flag"]
 
     with chdir(REPO_ROOT):
         # To ensure that cdf.toml is loaded correctly
         _ = CDFToml.load()
-        tracker = Tracker(" ".join(sys.argv))
+        tracker = Tracker()
         tracker.track_cli_command(WarningList([]), "Success", "test")
         print("Event sent")
 
 
+def track_module_build() -> None:
+    with chdir(REPO_ROOT):
+        # To ensure that cdf.toml is loaded correctly
+        tracker = Tracker()
+        tracker.track_module_build(
+            BuiltModule(
+                "cdf_module_test",
+                location=BuildLocationEager(
+                    path=Path("modules/cdf_module_test"),
+                    _hash="hash",
+                ),
+                build_variables=BuildVariables([]),
+                resources={
+                    "files": ResourceBuiltList(
+                        [
+                            ResourceBuildInfo(
+                                "file1", BuildLocationEager(Path("files/file1.yaml"), "hash"), kind="File"
+                            ),
+                        ]
+                    ),
+                    "functions": ResourceBuiltList(
+                        [
+                            ResourceBuildInfo(
+                                "function1",
+                                BuildLocationEager(Path("functions/function1.yaml"), "hash"),
+                                kind="Function",
+                            )
+                        ]
+                    ),
+                },
+                warning_count=4,
+                status="success",
+            )
+        )
+        print("Event sent")
+
+
 if __name__ == "__main__":
-    main()
+    track_module_build()
