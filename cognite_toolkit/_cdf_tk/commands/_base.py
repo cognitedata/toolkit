7937ea0b91f7e163f3fa4fae59ae7c66345626bb
anders-albert <anders0albert@gmail.com>
2024-09-09 17:43:51 +0200

    refactor; review feedback

--- cognite_toolkit/_cdf_tk/commands/_base.py
+++ cognite_toolkit/_cdf_tk/commands/_base.py
@@ -39,13 +39,14 @@ class ToolkitCommand:
         self.tracker.track_cli_command(self.warning_list, result, type(self).__name__.removesuffix("Command"))
 
     def run(self, execute: Callable[..., Any], *args: Any, **kwargs: Any) -> Any:
-        if not self.tracker.opted_in and not self.tracker.opted_out and "collect" not in sys.argv:
+        is_collect_command = len(sys.argv) >= 2 and "collect" == sys.argv[1]
+        if not self.tracker.opted_in and not self.tracker.opted_out and not is_collect_command:
             print(
                 "You acknowledge and agree that the CLI tool may collect usage information, user environment, "
                 "and crash reports for the purposes of providing services of functions that are relevant "
                 "to use of the CLI tool and product improvements. "
-                "To remove this message run 'cdf-tk collect opt-in', "
-                "or to stop collecting usage information run 'cdf-tk collect opt-out'."
+                "To remove this message run 'cdf collect opt-in', "
+                "or to stop collecting usage information run 'cdf collect opt-out'."
             )
 
         try:
