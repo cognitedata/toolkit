4fe1b0eea13c0f1d109e256fd306648d9b9f145c
anders-albert <anders0albert@gmail.com>
2024-09-09 16:00:53 +0200

    refactor; created init

--- cognite_toolkit/_cdf_tk/commands/repo.py
+++ cognite_toolkit/_cdf_tk/commands/repo.py
@@ -4,5 +4,8 @@ from ._base import ToolkitCommand
 
 
 class RepoCommand(ToolkitCommand):
+    def __init__(self, print_warning: bool = True, skip_tracking: bool = False, silent: bool = False) -> None:
+        super().__init__(print_warning=print_warning, skip_tracking=skip_tracking, silent=silent)
+
     def init(self, cwd: Path) -> None:
         print("Initializing git repository...")
