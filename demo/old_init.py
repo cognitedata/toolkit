fa3990129a018341805b855bdc64444684f5f672
anders-albert <anders0albert@gmail.com>
2024-09-09 16:48:51 +0200

    fix:

--- demo/old_init.py
+++ demo/old_init.py
@@ -1,14 +1,13 @@
 from pathlib import Path
 
 from cognite_toolkit._cdf_tk.commands import ModulesCommand, RepoCommand
-from cognite_toolkit._cdf_tk.constants import ROOT_PATH
 
 THIS_FOLDER = Path(__file__).parent.absolute()
 DEMO_PROJECT = THIS_FOLDER.parent / "demo_project"
 
 if __name__ == "__main__":
     RepoCommand(skip_tracking=True).init(
-        ROOT_PATH,
+        THIS_FOLDER.parent,
         verbose=True,
     )
 
