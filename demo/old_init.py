530bd2506b48f7056e15ecdc7ceba6b842e1a318
anders-albert <anders0albert@gmail.com>
2024-09-09 16:46:32 +0200

    refactor: update demo project

--- demo/old_init.py
+++ demo/old_init.py
@@ -1,21 +1,15 @@
-import shutil
 from pathlib import Path
 
+from cognite_toolkit._cdf_tk.commands import ModulesCommand, RepoCommand
 from cognite_toolkit._cdf_tk.constants import ROOT_PATH
-from cognite_toolkit._cdf_tk.data_classes import ModuleDirectories
 
 THIS_FOLDER = Path(__file__).parent.absolute()
 DEMO_PROJECT = THIS_FOLDER.parent / "demo_project"
 
 if __name__ == "__main__":
-    modules_directories = ModuleDirectories.load(ROOT_PATH, {Path("")})
-    DEMO_PROJECT.mkdir(exist_ok=True, parents=True)
+    RepoCommand(skip_tracking=True).init(
+        ROOT_PATH,
+        verbose=True,
+    )
 
-    modules_directories.dump(DEMO_PROJECT)
-
-    for file_name in [
-        "README.md",
-        ".gitignore",
-        ".env.tmpl",
-    ]:
-        shutil.copy(ROOT_PATH / file_name, DEMO_PROJECT / file_name)
+    ModulesCommand(skip_tracking=True).init(DEMO_PROJECT)
