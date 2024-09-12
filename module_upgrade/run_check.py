a976a293e1a3cf8b73eab0febf307b59f2a2ef02
anders-albert <anders0albert@gmail.com>
2024-09-12 13:26:14 +0200

    refactor; update

--- module_upgrade/run_check.py
+++ module_upgrade/run_check.py
@@ -143,6 +143,10 @@ def create_project_init(version: str) -> None:
             shell=True if platform.system() == "Windows" else False,
             env=modified_env_variables,
         )
+        if output.returncode != 0:
+            print(output.stderr.decode())
+            raise ValueError(f"Failed to create project init for version {version}.")
+
         if parse_version("0.3.0a3") > version_parsed >= parse_version("0.3.0a1"):
             cmd = [
                 str(old_version_script_dir / "cdf"),
@@ -156,14 +160,13 @@ def create_project_init(version: str) -> None:
                 shell=True if platform.system() == "Windows" else False,
                 env=modified_env_variables,
             )
+            if output.returncode != 0:
+                print(output.stderr.decode())
+                raise ValueError(f"Failed to create cdf.toml for version {version}.")
 
         cdf_toml_path = TEST_DIR_ROOT / CDFToml.file_name
         if cdf_toml_path.exists():
             shutil.move(cdf_toml_path, project_init / CDFToml.file_name)
-
-        if output.returncode != 0:
-            print(output.stderr.decode())
-            raise ValueError(f"Failed to create project init for version {version}.")
 
     print(f"Project init for version {version} created.")
     with chdir(TEST_DIR_ROOT):
