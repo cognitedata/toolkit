576ee7a77272d04c43807b199c6d791569006af0
anders-albert <anders0albert@gmail.com>
2024-09-16 17:44:59 +0200

    refactor: setup shell for new command

--- cognite_toolkit/_cdf.py
+++ cognite_toolkit/_cdf.py
@@ -22,7 +22,7 @@ from dotenv import load_dotenv
 from rich import print
 from rich.panel import Panel
 
-from cognite_toolkit._cdf_tk.apps import LandingApp, ModulesApp, RepoApp, RunApp
+from cognite_toolkit._cdf_tk.apps import AuthApp, LandingApp, ModulesApp, RepoApp, RunApp
 from cognite_toolkit._cdf_tk.cdf_toml import CDFToml
 from cognite_toolkit._cdf_tk.commands import (
     AuthCommand,
@@ -101,7 +101,7 @@ user_app = typer.Typer(**default_typer_kws, hidden=True)  # type: ignore [arg-ty
 modules_app = ModulesApp(**default_typer_kws)  # type: ignore [arg-type]
 landing_app = LandingApp(**default_typer_kws)  # type: ignore [arg-type]
 
-_app.add_typer(auth_app, name="auth")
+_app.add_typer(AuthApp(**default_typer_kws), name="auth")
 _app.add_typer(describe_app, name="describe")
 _app.add_typer(RunApp(**default_typer_kws), name="run")
 _app.add_typer(RepoApp(**default_typer_kws), name="repo")
---
+++ cognite_toolkit/_cdf_tk/apps/_auth_app.py
@@ -0,0 +1,35 @@
+from typing import Any
+
+import typer
+from rich import print
+
+
+class AuthApp(typer.Typer):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.callback(invoke_without_command=True)(self.main)
+        self.command()(self.init)
+        self.command()(self.verify)
+
+    def main(self, ctx: typer.Context) -> None:
+        """Commands to auth setup"""
+        if ctx.invoked_subcommand is None:
+            print("Use [bold yellow]cdf auth --help[/] for more information.")
+
+    def init(
+        self,
+        verbose: bool = typer.Option(False, "-v", "--verbose", help="Verbose output"),
+    ) -> None:
+        """Creates the authorization for a user/service principal to run the CDF Toolkit commands.
+
+        Needed capabilities for bootstrapping:
+        "projectsAcl": ["LIST", "READ"],
+        "groupsAcl": ["LIST", "READ", "CREATE", "UPDATE", "DELETE"]
+        """
+        print("Auth setup")
+
+    def verify(
+        self,
+    ) -> None:
+        """Verify that the current user/service principal has the required capabilities to run the CDF Toolkit commands."""
+        print("Auth verify")
