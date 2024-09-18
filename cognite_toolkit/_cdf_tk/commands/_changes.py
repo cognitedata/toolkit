dee589ef794141e3b36e825c2944ea098840398a
anders-albert <anders0albert@gmail.com>
2024-09-18 08:45:04 +0200

    refactor: added changes

--- cognite_toolkit/_cdf_tk/commands/_changes.py
+++ cognite_toolkit/_cdf_tk/commands/_changes.py
@@ -40,6 +40,35 @@ class ManualChange(Change):
         return ""
 
 
+class DeployCleanInteractiveFlagRemoved(AutomaticChange):
+    """The `--interactive` flag has been removed from the `cdf deploy` and `cdf clean` commands.
+    """
+
+    deprecated_from = Version("0.3.0a4")
+    required_from = Version("0.3.0a4")
+    has_file_changes = False
+
+
+class SharedVerboseFlagRemoved(AutomaticChange):
+    """The shared `--verbose` flag been removed. Now each command has its own `--verbose` flag.
+
+For example, before:
+```bash
+    cdf --verbose deploy
+```
+
+After:
+```bash
+    cdf deploy --verbose
+```
+    """
+
+    deprecated_from = Version("0.3.0a4")
+    required_from = Version("0.3.0a4")
+    has_file_changes = False
+
+
+
 class RenamedOrganizationDirInCDFToml(AutomaticChange):
     """In the cdf.toml file, the 'organization_dir' field in the 'cdf' section has been renamed to
 'default_organization_dir'.
