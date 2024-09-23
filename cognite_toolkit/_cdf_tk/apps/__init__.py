95ed88ccc89d5db6a166c998fc226d883bd27b0e
Anders Albert <60234212+doctrino@users.noreply.github.com>
2024-09-23 13:36:30 +0200

    [CDF-22589] 🎚Rework Pull Command (#985)
    * refactor: removed unused
    
    * refactor: minor cleanups
    
    * refactor: minor cleanup
    
    * tests: upgrade test
    
    * build: remove obsolete dependencies
    
    * refactor: remove
    
    * build changelog
    
    * refactor: remove timeshift feature
    
    * build: changelog
    
    * refactor; introduce destination
    
    * refactor: move to avoid circular import
    
    * refactor; reorg data classes
    
    * refactor: reuse built resources
    
    * refactor: improved building
    
    * refactor; introduce deploy state
    
    * docs: fix language
    
    * refactor: implemented new setup for raw file loader
    
    * refactor: change do deploy state in deploy command
    
    * refactor: renaming
    
    * refactor: timeseries from source
    
    * Ãrefactor: file upload from source
    
    * refactor: cleaunp build
    
    * fix: circular
    
    * fix: correct hash
    
    * tests: updated tests
    
    * refactor; structure
    
    * fix: unique display names
    
    * fix: simplify metadata
    
    * fix: introduced bugs
    
    * fix; in function
    
    * fix: attempt fix RawTableLoader
    
    * refactor: fix
    
    * build: changelog
    
    * fix: database
    
    * tests: regen
    
    * refactor: fixed
    
    * refactor; implemented change
    
    * fix: typo
    
    * style
    
    * refactor: cleanup unused
    
    * refactor; setup pull app
    
    * refactor; selecte resource
    
    * refactor: in-progress
    
    * style: improved interactive
    
    * refactor: one more step
    
    * build: poetry update
    
    * refactor: simplification
    
    * tests: updated tests
    
    * refactor; setup pull app
    
    * refactor; selecte resource
    
    * refactor: in-progress
    
    * style: improved interactive
    
    * refactor: one more step
    
    * Ãtests: updated test
    
    * fix: hacky solution to rewrite problem
    
    * fix: 3.9 happy

--- cognite_toolkit/_cdf_tk/apps/__init__.py
+++ cognite_toolkit/_cdf_tk/apps/__init__.py
@@ -2,7 +2,8 @@ from ._auth_app import AuthApp
 from ._core_app import CoreApp
 from ._landing_app import LandingApp
 from ._modules_app import ModulesApp
+from ._pull_app import PullApp
 from ._repo_app import RepoApp
 from ._run import RunApp
 
-__all__ = ["CoreApp", "AuthApp", "LandingApp", "ModulesApp", "RunApp", "RepoApp"]
+__all__ = ["CoreApp", "AuthApp", "LandingApp", "ModulesApp", "RunApp", "RepoApp", "PullApp"]
