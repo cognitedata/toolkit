7cbe0d60a1ac10e14342fef6ed6d7782bbcdb524
Anders Albert <60234212+doctrino@users.noreply.github.com>
2024-09-23 14:03:51 +0200

    [CDF-22346]🗒 Support CogniteFile (#982)
    * refactor: removed unused
    
    * refactor: minor cleanups
    
    * refactor: minor cleanup
    
    * tests: upgrade test
    
    * build: remove obsolete dependencies
    
    * refactor: remove
    
    * build changelog
    
    * refactor: remove timeshift feature
    
    * build: changelog
    
    * refactor; introduce destination
    
    * refactor: move to avoid circular import
    
    * refactor; reorg data classes
    
    * refactor: reuse built resources
    
    * refactor: improved building
    
    * refactor; introduce deploy state
    
    * docs: fix language
    
    * refactor: implemented new setup for raw file loader
    
    * refactor: change do deploy state in deploy command
    
    * refactor: renaming
    
    * refactor: timeseries from source
    
    * Ãrefactor: file upload from source
    
    * refactor: cleaunp build
    
    * fix: circular
    
    * fix: correct hash
    
    * tests: updated tests
    
    * refactor; structure
    
    * fix: unique display names
    
    * fix: simplify metadata
    
    * fix: introduced bugs
    
    * fix; in function
    
    * fix: attempt fix RawTableLoader
    
    * refactor: fix
    
    * build: changelog
    
    * fix: database
    
    * tests: regen
    
    * refactor: fixed
    
    * refactor; implemented change
    
    * fix: typo
    
    * style
    
    * refactor: cleanup unused
    
    * refactor: start on CogniteFileLoader
    
    * tests: Added test cases
    
    * refactor: first pass
    
    * fix: Handle base case (no extension)
    
    * fix: handle different context in dump
    
    * fix: loading of custom
    
    * refactor: done with first pass
    
    * fix: update fake gen
    
    * fix: spec and disambiguation between FileMetadata and CogniteFile
    
    * tests: regen
    
    * fix: bad regex
    
    * build: poetry update
    
    * refactor: simplification
    
    * tests: updated tests
    
    * tests: wording

--- cognite_toolkit/_cdf_tk/loaders/_resource_loaders/file_loader.py
+++ cognite_toolkit/_cdf_tk/loaders/_resource_loaders/file_loader.py
@@ -26,25 +26,35 @@ from cognite.client.data_classes import (
 )
 from cognite.client.data_classes.capabilities import (
     Capability,
+    DataModelInstancesAcl,
     FilesAcl,
 )
+from cognite.client.data_classes.data_modeling import NodeApplyResultList, NodeId, ViewId
 from cognite.client.exceptions import CogniteAPIError
 from cognite.client.utils.useful_types import SequenceNotStr
 from rich import print
 
-from cognite_toolkit._cdf_tk._parameters import ANY_INT, ANY_STR, ParameterSpec, ParameterSpecSet
+from cognite_toolkit._cdf_tk._parameters import ANY_INT, ANY_STR, ANYTHING, ParameterSpec, ParameterSpecSet
 from cognite_toolkit._cdf_tk.exceptions import (
     ToolkitRequiredValueError,
 )
 from cognite_toolkit._cdf_tk.loaders._base_loaders import ResourceContainerLoader, ResourceLoader
+from cognite_toolkit._cdf_tk.loaders.data_classes import (
+    ExtendableCogniteFile,
+    ExtendableCogniteFileApply,
+    ExtendableCogniteFileApplyList,
+    ExtendableCogniteFileList,
+)
 from cognite_toolkit._cdf_tk.utils import (
     CDFToolConfig,
+    in_dict,
     load_yaml_inject_variables,
 )
 
 from .auth_loaders import GroupAllScopedLoader, SecurityCategoryLoader
 from .classic_loaders import AssetLoader
 from .data_organization_loaders import DataSetsLoader, LabelLoader
+from .datamodel_loaders import SpaceLoader, ViewLoader
 
 
 @final
@@ -54,6 +64,10 @@ class FileMetadataLoader(
     template_pattern = "$FILENAME"
     item_name = "file contents"
     folder_name = "files"
+    filename_pattern = (
+        # Matches all yaml files except file names whose stem ends with `.CogniteFile`.
+        r"^(?!.*\.CogniteFile$).*"
+    )
     resource_cls = FileMetadata
     resource_write_cls = FileMetadataWrite
     list_cls = FileMetadataList
@@ -213,3 +227,135 @@ class FileMetadataLoader(
         spec.discard(ParameterSpec(("assetIds", ANY_INT), frozenset({"str"}), is_required=False, _is_nullable=False))
 
         return spec
+
+
+@final
+class CogniteFileLoader(
+    ResourceContainerLoader[
+        NodeId,
+        ExtendableCogniteFileApply,
+        ExtendableCogniteFile,
+        ExtendableCogniteFileApplyList,
+        ExtendableCogniteFileList,
+    ]
+):
+    template_pattern = "$FILENAME"
+    item_name = "file contents"
+    folder_name = "files"
+    filename_pattern = r"^.*\.CogniteFile"  # Matches all yaml files whose stem ends with '.CogniteFile'.
+    kind = "CogniteFile"
+    resource_cls = ExtendableCogniteFile
+    resource_write_cls = ExtendableCogniteFileApply
+    list_cls = ExtendableCogniteFileList
+    list_write_cls = ExtendableCogniteFileApplyList
+    dependencies = frozenset({GroupAllScopedLoader, SpaceLoader, ViewLoader})
+
+    _doc_url = "Files/operation/initFileUpload"
+
+    @property
+    def display_name(self) -> str:
+        return "cognite_file"
+
+    @classmethod
+    def get_id(cls, item: ExtendableCogniteFile | ExtendableCogniteFileApply | dict) -> NodeId:
+        if isinstance(item, dict):
+            if missing := tuple(k for k in {"space", "externalId"} if k not in item):
+                # We need to raise a KeyError with all missing keys to get the correct error message.
+                raise KeyError(*missing)
+            return NodeId(space=item["space"], external_id=item["externalId"])
+        return item.as_id()
+
+    @classmethod
+    def dump_id(cls, id: NodeId) -> dict[str, Any]:
+        return id.dump(include_instance_type=False)
+
+    @classmethod
+    def get_required_capability(cls, items: ExtendableCogniteFileApplyList | None) -> list[Capability]:
+        if not items and items is not None:
+            return []
+        scope: DataModelInstancesAcl.Scope.All | DataModelInstancesAcl.Scope.SpaceID = DataModelInstancesAcl.Scope.All()  # type: ignore[valid-type]
+        if items:
+            if spaces := {item.space for item in items}:
+                scope = DataModelInstancesAcl.Scope.SpaceID(list(spaces))
+        return [
+            FilesAcl([FilesAcl.Action.Read, FilesAcl.Action.Write], FilesAcl.Scope.All()),
+            DataModelInstancesAcl(
+                [DataModelInstancesAcl.Action.Read, DataModelInstancesAcl.Action.Write],  # type: ignore[valid-type]
+                scope,  # type: ignore[arg-type]
+            ),
+        ]
+
+    def create(self, items: ExtendableCogniteFileApplyList) -> NodeApplyResultList:
+        created = self.client.data_modeling.instances.apply(
+            nodes=items, replace=False, skip_on_version_conflict=True, auto_create_direct_relations=True
+        )
+        return created.nodes
+
+    def retrieve(self, ids: SequenceNotStr[NodeId]) -> ExtendableCogniteFileList:
+        # Todo: Problem, if you extend the CogniteFiles with a custom view, we need to know
+        #   the ID of the custom view to retrieve data from it. This is not possible with the current
+        #   structure. Need to reconsider how to handle this.
+        items = self.client.data_modeling.instances.retrieve_nodes(  # type: ignore[call-overload]
+            nodes=ids,
+            node_cls=ExtendableCogniteFile,
+        )
+        return ExtendableCogniteFileList(items)
+
+    def update(self, items: ExtendableCogniteFileApplyList) -> NodeApplyResultList:
+        updated = self.client.data_modeling.instances.apply(nodes=items, replace=True)
+        return updated.nodes
+
+    def delete(self, ids: SequenceNotStr[NodeId]) -> int:
+        try:
+            deleted = self.client.data_modeling.instances.delete(nodes=list(ids))
+        except CogniteAPIError as e:
+            if "not exist" in e.message and "space" in e.message.lower():
+                return 0
+            raise e
+        return len(deleted.nodes)
+
+    def iterate(self) -> Iterable[ExtendableCogniteFile]:
+        raise NotImplementedError("")
+        # return iter(self.client.data_modeling.instances)
+
+    def count(self, ids: SequenceNotStr[NodeId]) -> int:
+        return sum(
+            [
+                bool(n.is_uploaded or False)
+                for n in self.client.data_modeling.instances.retrieve_nodes(nodes=ids, node_cls=ExtendableCogniteFile)  # type: ignore[call-overload]
+            ]
+        )
+
+    def drop_data(self, ids: SequenceNotStr[NodeId]) -> int:
+        existing_meta = self.client.files.retrieve_multiple(instance_ids=list(ids), ignore_unknown_ids=True)
+        existing_node = self.retrieve(ids)
+
+        # File and FileMetadata is tightly coupled, so we need to delete the metadata and recreate it
+        # without the source set to delete the file.
+        self.client.files.delete(id=existing_meta.as_ids())
+        self.create(existing_node.as_write())
+        return len(existing_meta)
+
+    @classmethod
+    @lru_cache(maxsize=1)
+    def get_write_cls_parameter_spec(cls) -> ParameterSpecSet:
+        spec = super().get_write_cls_parameter_spec()
+        # Removed by the SDK
+        spec.add(ParameterSpec(("instanceType",), frozenset({"str"}), is_required=False, _is_nullable=False))
+        # Sources are used when writing to the API.
+        spec.add(ParameterSpec(("sources",), frozenset({"list"}), is_required=False, _is_nullable=False))
+        spec.add(ParameterSpec(("sources", ANYTHING), frozenset({"list"}), is_required=False, _is_nullable=False))
+        return spec
+
+    @classmethod
+    def get_dependent_items(cls, item: dict) -> Iterable[tuple[type[ResourceLoader], Hashable]]:
+        """Returns all items that this item requires.
+
+        For example, a TimeSeries requires a DataSet, so this method would return the
+        DatasetLoader and identifier of that dataset.
+        """
+        if "space" in item:
+            yield SpaceLoader, item["space"]
+        if "nodeSource" in item:
+            if in_dict(("space", "externalId", "type"), item["nodeSource"]):
+                yield ViewLoader, ViewId.load(item["nodeSource"])
