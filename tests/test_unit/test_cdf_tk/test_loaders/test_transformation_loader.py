f82fe0dc49d1cd02e58dd7d97e2b06503fb370b4
anders-albert <anders0albert@gmail.com>
2024-09-24 15:10:04 +0200

    Ãtests: started updating test

--- tests/test_unit/test_cdf_tk/test_loaders/test_transformation_loader.py
+++ tests/test_unit/test_cdf_tk/test_loaders/test_transformation_loader.py
@@ -1,7 +1,7 @@
 import pathlib
 from collections.abc import Hashable
 from pathlib import Path
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 import pytest
 import yaml
@@ -48,8 +48,12 @@ conflictMode: upsert
         monkeypatch: MonkeyPatch,
     ) -> None:
         loader = TransformationLoader(toolkit_client_approval.mock_client, None)
-        mock_read_yaml_file({"transformation.yaml": yaml.CSafeLoader(self.trafo_yaml).get_data()}, monkeypatch)
-        loaded = loader.load_resource(Path("transformation.yaml"), cdf_tool_real, skip_validation=False)
+        filepath = MagicMock(spec=Path)
+        filepath.read_text.return_value = self.trafo_yaml
+        filepath.name = "transformation.yaml"
+        filepath.stem = "transformation"
+
+        loaded = loader.load_resource(filepath, cdf_tool_real, skip_validation=False)
         assert loaded.destination_oidc_credentials is None
         assert loaded.source_oidc_credentials is None
 
