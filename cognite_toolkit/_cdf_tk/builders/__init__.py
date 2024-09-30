7cf608d39b3d1001494beb7f6b5de88b37aeca8a
anders-albert <anders0albert@gmail.com>
2024-09-30 19:06:34 +0200

    refactor: moved all code

--- cognite_toolkit/_cdf_tk/builders/__init__.py
+++ cognite_toolkit/_cdf_tk/builders/__init__.py
@@ -1,3 +1,10 @@
 from ._base import Builder
 
+
+def create_builder(
+    resource_folder: str, module_names_by_variable_key: dict[str, list[str]], silent: bool, verbose: bool
+) -> Builder:
+    return Builder(module_names_by_variable_key, silent, resource_folder, verbose)
+
+
 __all__ = ["Builder"]
