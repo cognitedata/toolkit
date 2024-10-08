df790210414a041884d09b28cc7ff1b6d8f8f3b8
Pål Rønning <ronpal@users.noreply.github.com>
2024-10-08 11:45:46 +0200

    Module Dependencies and APM files for InRobot (#1060)
    * revived APM files for InRobot
    
    * reviving APM module
    
    * dependencies in toml
    
    * Module dependency and default/disabled
    
    * test update
    
    * added rewrite-all to examples
    
    * Apply suggestions from code review
    
    Co-authored-by: Anders Albert <60234212+doctrino@users.noreply.github.com>
    
    * changed var name
    
    * fix: added missing default config
    
    * refactor: regen
    
    * tests: mock questionary
    
    ---------
    
    Co-authored-by: Anders Albert <60234212+doctrino@users.noreply.github.com>
    Co-authored-by: anders-albert <anders0albert@gmail.com>

--- cognite_toolkit/_cdf_tk/commands/modules.py
+++ cognite_toolkit/_cdf_tk/commands/modules.py
@@ -258,6 +258,34 @@ default_organization_dir = "{organization_dir.name}"''',
 
         raise typer.Exit()
 
+    def _select_modules_in_package(self, package: Package) -> list[ModuleLocation]:
+        dependencies: set[str] = set()
+        for module in package.modules:
+            for dependency in module.dependencies:
+                dependencies.add(dependency)
+
+        choices = sorted(
+            [
+                questionary.Choice(
+                    title=module.title,
+                    value=module,
+                    checked=module.is_selected_by_default,
+                    disabled="required" if module.name in dependencies else None,
+                )
+                for module in package.modules
+            ],
+            key=lambda choice: not choice.checked,
+        )
+
+        return questionary.checkbox(
+            f"Which modules in {package.name} would you like to add?",
+            instruction="Use arrow up/down, press space to select item(s) and enter to save",
+            choices=choices,
+            qmark=INDENT,
+            pointer=POINTER,
+            style=custom_style_fancy,
+        ).ask()
+
     def _select_packages(self, packages: Packages, existing_module_names: list[str] | None = None) -> Packages:
         adding_to_existing = False
         if existing_module_names is not None:
@@ -301,20 +329,7 @@ default_organization_dir = "{organization_dir.name}"''',
             if package.name != "bootcamp" and (
                 len(package.modules) > 1 or (adding_to_existing and len(package.modules) > 0)
             ):
-                selection = questionary.checkbox(
-                    f"Which modules in {package.name} would you like to add?",
-                    instruction="Use arrow up/down, press space to select item(s) and enter to save",
-                    choices=[
-                        questionary.Choice(
-                            title=selectable_module.title,
-                            value=selectable_module,
-                        )
-                        for selectable_module in package.modules
-                    ],
-                    qmark=INDENT,
-                    pointer=POINTER,
-                    style=custom_style_fancy,
-                ).ask()
+                selection = self._select_modules_in_package(package)
             else:
                 selection = package.modules
 
