9de6b95119c87cfe945d3656ee166a9f4adde652
Pål Rønning <ronpal@users.noreply.github.com>
2024-10-11 15:02:57 +0200

    [CDF-22859] ☕  Create boilerplate Azure Devops pipelines with repo init command (#1075)
    * Azure Devops starter pack
    
    * ADO support
    
    * retest
    
    * file location and naming
    
    * File rename to ADO standard and changelog entry
    
    * cleanup
    
    * cleanup
    
    * reinstated lost choice
    
    * Update cognite_toolkit/_cdf_tk/commands/repo.py
    
    Co-authored-by: Anders Albert <60234212+doctrino@users.noreply.github.com>
    
    * Put ado behind a feature flag
    
    * Suggestion
    
    * Made env vars available to build step
    
    ---------
    
    Co-authored-by: Anders Albert <60234212+doctrino@users.noreply.github.com>
    Co-authored-by: anders-albert <anders0albert@gmail.com>

--- cognite_toolkit/_cdf_tk/commands/repo.py
+++ cognite_toolkit/_cdf_tk/commands/repo.py
@@ -7,6 +7,7 @@ import questionary
 import cognite_toolkit
 from cognite_toolkit._cdf_tk.constants import REPO_FILES_DIR
 from cognite_toolkit._cdf_tk.exceptions import ToolkitValueError
+from cognite_toolkit._cdf_tk.feature_flags import FeatureFlag, Flags
 from cognite_toolkit._cdf_tk.tk_warnings import LowSeverityWarning, MediumSeverityWarning
 
 from . import _cli_commands
@@ -17,6 +18,10 @@ REPOSITORY_HOSTING = [
     "Other",
     "None",
 ]
+
+
+if FeatureFlag.is_enabled(Flags.ADO_PIPELINES):
+    REPOSITORY_HOSTING.insert(1, "Azure DevOps")
 
 
 class RepoCommand(ToolkitCommand):
@@ -56,17 +61,20 @@ class RepoCommand(ToolkitCommand):
             repo_host = host
         if repo_host == "GitHub":
             self.console("The repository will be hosted on GitHub.")
+        elif repo_host == "Azure DevOps":
+            self.console("The repository will be hosted on Azure DevOps.")
         elif repo_host == "Other":
             self.console("No template for CI/CD available for other hosting services yet.")
         elif repo_host == "None":
-            self.console("It is recommended to host your repository on a platform like GitHub.")
+            self.console("It is recommended to use a hosted version control service like GitHub or Azure DevOps.")
 
         if verbose:
             self.console("Initializing toolkit repository...")
 
         iterables = [(self._repo_files, self._repo_files.glob("*"))]
-        if repo_host == "GitHub":
-            iterables.append((self._repo_files / repo_host, self._repo_files.rglob(f"{repo_host}/**/*.yaml")))
+        if repo_host in ["GitHub", "Azure DevOps"]:
+            repo_host = repo_host.replace(" ", "")
+            iterables.append((self._repo_files / repo_host, self._repo_files.rglob(f"{repo_host}/**/*.*")))
 
         for root, iterable in iterables:
             for file in iterable:
