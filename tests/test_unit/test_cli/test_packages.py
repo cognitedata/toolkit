9cd2b5bc0b3062ebd6da9103091216a98bfcd37d
anders-albert <anders0albert@gmail.com>
2024-10-13 17:01:37 +0200

    tests: improved tests

--- tests/test_unit/test_cli/test_packages.py
+++ tests/test_unit/test_cli/test_packages.py
@@ -9,8 +9,8 @@ from questionary import Choice
 
 from cognite_toolkit._cdf_tk.commands import BuildCommand, ModulesCommand
 from cognite_toolkit._cdf_tk.constants import BUILTIN_MODULES_PATH
-from cognite_toolkit._cdf_tk.data_classes import BuildConfigYAML, BuildVariables, ModuleDirectories, Packages
-from cognite_toolkit._cdf_tk.tk_warnings import TemplateVariableWarning
+from cognite_toolkit._cdf_tk.data_classes import Packages
+from cognite_toolkit._cdf_tk.tk_warnings import MediumSeverityWarning, TemplateVariableWarning
 
 
 class MockQuestion:
@@ -88,13 +88,28 @@ def test_build_packages_without_warnings(
 
     build_cmd = BuildCommand(silent=True, skip_tracking=True)
 
-    modules = ModuleDirectories.load(organization_dir)
-    config = BuildConfigYAML.load_default(organization_dir)
-    variables = BuildVariables.load_raw(config.variables, modules.available_paths, modules.selected.available_paths)
-    build_cmd.build_modules(modules, build_tmp_path, variables, verbose=False, progress_bar=False)
+    build_cmd.execute(
+        verbose=False,
+        build_dir=build_tmp_path,
+        organization_dir=organization_dir,
+        build_env_name="dev",
+        no_clean=False,
+        ToolGlobals=None,
+        selected=None,
+    )
 
     # TemplateVariableWarning is when <change_me> is not replaced in the config file.
     # This is expected to be replaced by the users, and will thus raise when we run a fully automated test.
-    warnings = [warning for warning in build_cmd.warning_list if not isinstance(warning, TemplateVariableWarning)]
+    warnings = [
+        warning
+        for warning in build_cmd.warning_list
+        if not isinstance(warning, TemplateVariableWarning)
+        and not (
+            # This warning is expected to be raised when the 'CDF_PROJECT' environment variable is not set.
+            isinstance(warning, MediumSeverityWarning)
+            and warning.message_raw
+            and warning.message_raw.startswith("No 'CDF_PROJECT' environment variable set.")
+        )
+    ]
 
     assert not warnings, f"{len(warnings)} warnings found: {warnings}"
