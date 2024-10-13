75273080e077cade46b68ac512b457cab0ae22ea
anders-albert <anders0albert@gmail.com>
2024-10-13 16:19:26 +0200

    refactor: Setup shell for mocking questionary

--- tests/test_unit/test_cli/test_packages.py
+++ tests/test_unit/test_cli/test_packages.py
@@ -1,11 +1,47 @@
+from collections.abc import Sequence
 from pathlib import Path
+from typing import Any
 
 import pytest
+from _pytest.monkeypatch import MonkeyPatch
 
 from cognite_toolkit._cdf_tk.commands import BuildCommand, ModulesCommand
 from cognite_toolkit._cdf_tk.constants import BUILTIN_MODULES_PATH
 from cognite_toolkit._cdf_tk.data_classes import BuildConfigYAML, BuildVariables, ModuleDirectories, Packages
 from cognite_toolkit._cdf_tk.tk_warnings import TemplateVariableWarning
+
+
+class MockQuestion:
+    def __init__(self, answer: Any) -> None:
+        self.answer = answer
+
+    def ask(self) -> Any:
+        return self.answer
+
+
+class MockQuestionary:
+    def __init__(self, module_target: str, monkeypatch: MonkeyPatch, answers: Sequence[Any]) -> None:
+        self.answers = answers
+        for method in [self.select, self.confirm, self.checkbox, self.text]:
+            monkeypatch.setattr(f"{module_target}.questionary.{method.__name__}", method)
+
+    def select(self, *args, **kwargs) -> MockQuestion:
+        raise NotImplementedError("select not implemented in MockQuestionary")
+
+    def confirm(self, *args, **kwargs) -> MockQuestion:
+        raise NotImplementedError("confirm not implemented in MockQuestionary")
+
+    def checkbox(self, *args, **kwargs) -> MockQuestion:
+        raise NotImplementedError("checkbox not implemented in MockQuestionary")
+
+    def text(self, *args, **kwargs) -> MockQuestion:
+        raise NotImplementedError("text not implemented in MockQuestionary")
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        raise NotImplementedError("exit not implemented in MockQuestionary")
 
 
 def get_packages() -> list[str]:
@@ -14,12 +50,17 @@ def get_packages() -> list[str]:
 
 
 @pytest.mark.parametrize("package", get_packages())
-def test_build_packages_without_warnings(package: str, tmp_path: Path, build_tmp_path: Path) -> None:
+def test_build_packages_without_warnings(
+    package: str, tmp_path: Path, build_tmp_path: Path, monkeypatch: MonkeyPatch
+) -> None:
     assert package
+
     organization_dir = tmp_path
 
     module_cmd = ModulesCommand(silent=True, skip_tracking=True)
-    module_cmd.init(organization_dir, clean=True)
+
+    with MockQuestionary(ModulesCommand.__module__, monkeypatch, ["y"]):
+        module_cmd.init(organization_dir, clean=True)
 
     build_cmd = BuildCommand(silent=True, skip_tracking=True)
 
