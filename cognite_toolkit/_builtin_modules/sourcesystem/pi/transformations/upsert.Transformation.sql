e8923f5e236a74d3316ebe2738fafd3ca30b2628
Pål Rønning <ronpal@users.noreply.github.com>
2024-10-15 17:48:13 +0200

    Still WIP

---
+++ cognite_toolkit/_builtin_modules/sourcesystem/pi/transformations/upsert.Transformation.sql
@@ -0,0 +1,9 @@
+select 
+  external_id as externalId,
+  name as name,
+  'numeric' as type,
+  false as isStep,
+  if(try_get_unit(`unit`) IS NOT NULL, node_reference('cdf_cdm_units', try_get_unit(`unit`)), NULL) as unit,
+  `unit` as sourceUnit
+  
+from `{{location}}_pi_staging`.`timeseries_metadata`
