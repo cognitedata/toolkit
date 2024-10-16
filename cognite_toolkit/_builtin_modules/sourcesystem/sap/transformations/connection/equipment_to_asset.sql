5efe7654a38c3e80f8d9b7ae0df20a51e4ecb56f
anders-albert <anders0albert@gmail.com>
2024-10-16 18:03:23 +0200

    refactor: regen

--- cognite_toolkit/_builtin_modules/sourcesystem/sap/transformations/connection/equipment_to_asset.sql
+++ cognite_toolkit/_builtin_modules/sourcesystem/sap/transformations/connection/equipment_to_asset.sql
@@ -2,7 +2,7 @@ select
   concat('WMT:', cast(d1.`WMT_TAG_NAME` as STRING)) as externalId,
   node_reference('{{ instanceSpace }}',  concat('WMT', cast(d2.`WMT_TAG_NAME` as STRING))) as asset
 from
-    {{ rawDb }}.`dump` d1
+    {{ rawDatabase }}.`dump` d1
 join
   {{ rawDatabase }}.`dump` d2
 on
