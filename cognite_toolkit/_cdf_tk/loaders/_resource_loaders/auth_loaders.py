e0988ee20359e801ad781b2715ac241c738dde13
anders-albert <anders0albert@gmail.com>
2024-10-17 14:46:48 +0200

    fix: verify security category

--- cognite_toolkit/_cdf_tk/loaders/_resource_loaders/auth_loaders.py
+++ cognite_toolkit/_cdf_tk/loaders/_resource_loaders/auth_loaders.py
@@ -229,6 +229,11 @@ class GroupLoader(ResourceLoader[str, GroupWrite, Group, GroupWriteList, GroupLi
                 operation="replace extractionPipelineExternalId with extractionPipelineId in group",
                 id_name="ids",
             ),
+            (cap.SecurityCategoriesAcl, cap.SecurityCategoriesAcl.Scope.ID): _ReplaceMethod(
+                ToolGlobals.verify_security_categories,
+                operation="replace securityCategoryExternalId with securityCategoryId in group",
+                id_name="ids",
+            ),
             cap.DataSetScope: _ReplaceMethod(
                 ToolGlobals.verify_dataset,
                 operation="replace datasetExternalId with dataSetId in group",
