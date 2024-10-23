43c51e6f8a81f93d4532ef744ddda3b17a91a751
anders-albert <anders0albert@gmail.com>
2024-10-23 14:11:14 +0200

    feat: added feature flag

--- cognite_toolkit/_cdf_tk/feature_flags.py
+++ cognite_toolkit/_cdf_tk/feature_flags.py
@@ -19,6 +19,10 @@ class Flags(Enum):
         "visible": True,
         "description": "Enables creation of Azure DevOps pipelines in repo init command",
     }
+    REQUIRE_KIND: ClassVar[dict[str, Any]] = {
+        "visible": True,
+        "description": "Require kind in all config file names. For example, `my.FileMetadata.yaml`",
+    }
 
 
 class FeatureFlag:
