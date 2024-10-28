7a805bf4d5d8921b9e4bf4a72f5bc6e599e5704f
Anders Albert <60234212+doctrino@users.noreply.github.com>
2024-10-28 12:50:40 +0100

    Typo in connection transformation (#1139)
    * fix: typo in conection transformation
    
    * refactor; gitignote
    
    * refactor: delete unused
    
    * tests: regen
    
    * [CDF-22944] Asset.parent to asset (#1142)
    
    * refactor: shell for new SQL
    
    * refactor: updated SQL
    
    * tests: regen

--- cognite_toolkit/_builtin_modules/sourcesystem/cdf_sap_assets/transformations/connection/equipment_to_asset.Transformation.sql
+++ cognite_toolkit/_builtin_modules/sourcesystem/cdf_sap_assets/transformations/connection/equipment_to_asset.Transformation.sql
@@ -1,6 +1,6 @@
 select
   concat('WMT:', cast(d1.`WMT_TAG_NAME` as STRING)) as externalId,
-  node_reference('{{ instanceSpace }}',  concat('WMT', cast(d2.`WMT_TAG_NAME` as STRING))) as asset
+  node_reference('{{ instanceSpace }}',  concat('WMT:', cast(d2.`WMT_TAG_NAME` as STRING))) as asset
 from
     {{ rawDatabase }}.`dump` d1
 join
