6d1e63efd350169e1f250499c0db2ff8b660b74b
anders-albert <anders0albert@gmail.com>
2024-10-29 15:19:52 +0100

    refactor: handle duplicates

--- cognite_toolkit/_builtin_modules/sourcesystem/cdf_sap_events/transformations/population/operation.Transformation.sql
+++ cognite_toolkit/_builtin_modules/sourcesystem/cdf_sap_events/transformations/population/operation.Transformation.sql
@@ -1,9 +1,18 @@
+-- The source data has duplicates this filters them out.
+with unique_workitem as (
+  select
+    *,
+    row_number() over (partition by `sourceId` order by `sourceId`) as rn
+  from
+    `{{ rawDatabase }}`.`workitem`
+)
 select
   cast(`sourceId` as STRING) as externalId,
   cast(`WORKORDER_TASKNAME` as STRING) as name,
   cast(`WORKORDER_STATUS` as STRING) as status,
   array(cast(`WORKORDER_ITEMNAME` as STRING)) as tag
 from
-  `{{ rawDatabase }}`.`workitem`
+  unique_workitem
 where
-  isnotnull(`sourceId`)
+  isnotnull(`sourceId`) and
+  rn = 1
