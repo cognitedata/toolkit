26e265e9b3dfa235960bdd9f3694c8f9bd613dcb
anders-albert <anders0albert@gmail.com>
2024-10-29 20:06:10 +0100

    refactor; update connection

--- cognite_toolkit/_builtin_modules/sourcesystem/cdf_sap_events/transformations/connection/operation_to_maintenance_order.Transformation.sql
+++ cognite_toolkit/_builtin_modules/sourcesystem/cdf_sap_events/transformations/connection/operation_to_maintenance_order.Transformation.sql
@@ -1,6 +1,13 @@
 /* The WORKORDER_NUMBER is not unique in the workorder table,
 so use the following query to select the first match. */
-with worder_unique as (
+with unique_workitem as (
+  select
+    *,
+    row_number() over (partition by `sourceId` order by `sourceId`) as rn
+  from
+    {{ rawDatabase }}.`workitem`
+),
+worder_unique as (
   select
     *,
     row_number() over (partition by `WORKORDER_NUMBER` order by `sourceId`) as rn
@@ -11,7 +18,7 @@ select
   cast(task.`sourceId` as STRING) as externalId,
   node_reference('{{ instanceSpace }}', cast(worder.`sourceId` as STRING)) as maintenanceOrder
 from
-  {{ rawDatabase }}.`workitem` as task
+  unique_workitem as task
 join
   worder_unique as worder
 on
@@ -21,4 +28,5 @@ where
   isnotnull(task.`WORKORDER_NUMBER`) AND
   isnotnull(worder.`WORKORDER_NUMBER`) AND
   isnotnull(worder.`sourceId`) AND
-  worder.rn = 1
+  worder.rn = 1 AND
+  task.rn = 1
