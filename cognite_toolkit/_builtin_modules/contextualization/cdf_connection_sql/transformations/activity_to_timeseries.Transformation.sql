6bd27af902e0667a0dad91bfdd7c3d9f6d82d30c
anders-albert <anders0albert@gmail.com>
2024-10-29 20:47:28 +0100

    refactor: fixed activity to timeseries

--- cognite_toolkit/_builtin_modules/contextualization/cdf_connection_sql/transformations/activity_to_timeseries.Transformation.sql
+++ cognite_toolkit/_builtin_modules/contextualization/cdf_connection_sql/transformations/activity_to_timeseries.Transformation.sql
@@ -1,29 +1,33 @@
 select
   /* three first properties are required */
-  cast(timeseries.`externalId` as STRING) as externalId, 
-  cast(timeseries.`isStep` as BOOLEAN) as isStep,
-  cast(timeseries.`type` as STRING) as type,
+  cast(activity.`externalId` as STRING) as externalId,
   /* direct relation */
-  array(
+  collect_list(
     node_reference(
       '{{ instanceSpace }}',
-      equipment_lookup.`externalId`
+      timeseries_lookup.`externalId`
     )
-  ) as equipment
+  ) as timeSeries
 from
   cdf_data_models(
-    "cdf_idm",
-    "CogniteProcessIndustries",
+    "cdf_cdm",
+    "CogniteCore",
+    "v1",
+    "CogniteActivity"
+  ) as activity
+left join cdf_data_models(
+    "cdf_cdm",
+    "CogniteCore",
     "v1",
     "CogniteTimeSeries"
-  ) as timeseries
-left join cdf_data_models(
-    "cdf_idm",
-    "CogniteProcessIndustries",
-    "v1",
-    "CogniteEquipment"
-  ) as equipment_lookup 
+  ) as timeseries_lookup
   /* update to the correct matching criteria for your data */
-  on substring_index(replace(timeseries.`name`, 'VAL_', ''), ':', 1) == equipment_lookup.`name`
+on
+  activity.`tags`[0]  = substring_index(replace(timeseries_lookup.`name`, 'VAL_', ''), ':', 1)
 where
-  timeseries.space == '{{ instanceSpace }}'
+  activity.space == '{{ instanceSpace }}' and
+  isnotnull(activity.tags) and
+  isnotnull(activity.`externalId`) and
+  isnotnull(timeseries_lookup.`name`)
+group by
+  activity.`externalId`
