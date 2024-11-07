5d4f37a9ad5ef180a06d29ea099975d5dd10934c
anders-albert <anders0albert@gmail.com>
2024-11-07 19:06:05 +0100

    refactor: extended module toml

--- cognite_toolkit/_cdf_tk/data_classes/_module_toml.py
+++ cognite_toolkit/_cdf_tk/data_classes/_module_toml.py
@@ -12,12 +12,28 @@ else:
 
 
 @dataclass(frozen=True)
+class ExampleData:
+    repo: str
+    source: str
+    description: str
+
+    @classmethod
+    def load(cls, data: dict[str, Any]) -> ExampleData:
+        return cls(
+            repo=data["repo"],
+            source=data["source"],
+            description=data["description"],
+        )
+
+
+@dataclass(frozen=True)
 class ModuleToml:
     filename: ClassVar[str] = "module.toml"
     title: str | None
     tags: frozenset[str] = field(default_factory=frozenset)
     dependencies: frozenset[str] = field(default_factory=frozenset)
     is_selected_by_default: bool = False
+    data: list[ExampleData] = field(default_factory=list)
 
     @classmethod
     def load(cls, data: dict[str, Any] | Path) -> ModuleToml:
@@ -29,9 +45,14 @@ class ModuleToml:
         else:
             dependencies = frozenset()
 
+        example_data: list[ExampleData] = []
+        if "data" in data and isinstance(data["data"], list):
+            example_data = [ExampleData.load(d) for d in data["data"]]
+
         return cls(
             title=data["module"].get("title"),
             tags=frozenset(data["packages"].get("tags", set())),
             dependencies=dependencies,
             is_selected_by_default=data["module"].get("is_selected_by_default", False),
+            data=example_data,
         )
