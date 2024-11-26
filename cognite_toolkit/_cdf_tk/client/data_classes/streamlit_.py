0f4d82b7c2501898e9ced638730865e6cd228a90
Pål Rønning <ronpal@users.noreply.github.com>
2024-11-26 12:55:28 +0100

    test saved me from stupid mistake

--- cognite_toolkit/_cdf_tk/client/data_classes/streamlit_.py
+++ cognite_toolkit/_cdf_tk/client/data_classes/streamlit_.py
@@ -118,7 +118,10 @@ class Streamlit(_StreamlitCore):
             if key in resource:
                 args[key] = resource[key]
         if "published" in resource:
-            args["published"] = resource[key].strip().lower() == "true"
+            if isinstance(resource["published"], str):
+                args["published"] = resource["published"].strip().lower() == "true"
+            else:
+                args["published"] = resource["published"]
         return cls(**args)
 
     @classmethod
