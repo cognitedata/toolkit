9db3854af2dd324335b1bc69463b6d7f22d9436e
anders-albert <anders0albert@gmail.com>
2024-12-15 07:12:35 +0100

    tests: added test cases

---
+++ tests/test_unit/test_cdf_tk/test_data_classes/test_yaml_comments.py
@@ -0,0 +1,26 @@
+from collections.abc import Iterable
+
+import pytest
+import yaml
+
+from cognite_toolkit._cdf_tk.data_classes import YAMLComments
+
+
+def load_dump_test_cases() -> Iterable:
+    yield pytest.param("""key: value  # comment""", id="simple")
+    yield pytest.param(
+        """- item: 23 # comment
+  # comment
+  - item: 24
+""",
+        id="list",
+    )
+
+
+class TestYAMLComments:
+    @pytest.mark.parametrize("yaml_str", list(load_dump_test_cases()))
+    def test_load_dump(self, yaml_str: str) -> None:
+        comments = YAMLComments.load(yaml_str)
+        data = yaml.safe_load(yaml_str)
+        dumped = yaml.safe_dump(data, sort_keys=False)
+        assert comments.dump(yaml_str) == dumped
