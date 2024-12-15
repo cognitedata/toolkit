8d36a92255edb556ea23dc63750832f4017ed100
anders-albert <anders0albert@gmail.com>
2024-12-15 08:55:16 +0100

    fix: handle comments

--- tests/test_unit/test_cdf_tk/test_data_classes/test_yaml_comments.py
+++ tests/test_unit/test_cdf_tk/test_data_classes/test_yaml_comments.py
@@ -1,4 +1,3 @@
-import textwrap
 from collections.abc import Iterable
 
 import pytest
@@ -8,31 +7,31 @@ from cognite_toolkit._cdf_tk.data_classes import YAMLComments
 
 
 def load_dump_test_cases() -> Iterable:
-    yield pytest.param("""key: value # comment""", id="simple")
+    yield pytest.param("""key: value # comment""", 2, id="simple")
     yield pytest.param(
         """- item: 23 # comment
   # other comment
 - item: 24""",
+        2,
         id="list",
     )
     yield pytest.param(
-        textwrap.dedent("""
-            first: value
-            second:
-                key: value
-                # above comment
-                third:
-                 - item: 23
-                 - item: 24 # with comment
-        """),
+        """first: value
+second:
+    key: value
+    # above comment
+    third:
+    -   item: 23
+    -   item: 24 # with comment""",
+        4,
         id="nested with indent 4",
     )
 
 
 class TestYAMLComments:
-    @pytest.mark.parametrize("yaml_str", list(load_dump_test_cases()))
-    def test_load_dump(self, yaml_str: str) -> None:
+    @pytest.mark.parametrize("yaml_str, indent", list(load_dump_test_cases()))
+    def test_load_dump(self, yaml_str: str, indent: int) -> None:
         comments = YAMLComments.load(yaml_str)
         data = yaml.safe_load(yaml_str)
-        dumped = yaml.safe_dump(data, sort_keys=False)
+        dumped = yaml.safe_dump(data, sort_keys=False, indent=indent)
         assert comments.dump(dumped) == yaml_str
