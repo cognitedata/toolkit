0b7c3aed1ca1fd0e7b4778eb0ae33339ebfcfec4
anders-albert <anders0albert@gmail.com>
2024-12-20 08:19:04 +0100

    feat: implemented first pass of lookup APIs

---
+++ cognite_toolkit/_cdf_tk/client/_toolkit_client.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from cognite.client import ClientConfig, CogniteClient
+
+from .api.dml import DMLAPI
+from .api.location_filters import LocationFiltersAPI
+from .api.lookup import LookUpGroup
+from .api.robotics import RoboticsAPI
+
+
+class ToolkitClient(CogniteClient):
+    def __init__(self, config: ClientConfig | None = None) -> None:
+        super().__init__(config=config)
+        self.location_filters = LocationFiltersAPI(self._config, self._API_VERSION, self)
+        self.robotics = RoboticsAPI(self._config, self._API_VERSION, self)
+        self.dml = DMLAPI(self._config, self._API_VERSION, self)
+        self.lookup = LookUpGroup(self._config, self._API_VERSION, self)
--- cognite_toolkit/_cdf_tk/client/api/lookup.py
+++ cognite_toolkit/_cdf_tk/client/api/lookup.py
@@ -1,19 +1,28 @@
 from abc import ABC, abstractmethod
 from collections.abc import Sequence
-from typing import overload
+from typing import TYPE_CHECKING, overload
 
-from cognite.client import ClientConfig, CogniteClient
-from cognite.client._api_client import APIClient
+from cognite.client import ClientConfig
 from cognite.client.utils.useful_types import SequenceNotStr
 
+from cognite_toolkit._cdf_tk.client.api_client import ToolkitAPI
+from cognite_toolkit._cdf_tk.exceptions import ResourceRetrievalError
 
-class LookUpAPI(APIClient, ABC):
+if TYPE_CHECKING:
+    from cognite_toolkit._cdf_tk.client._toolkit_client import ToolkitClient
+
+
+class LookUpAPI(ToolkitAPI, ABC):
     dry_run: int = -1
 
-    def __init__(self, config: ClientConfig, api_version: str | None, cognite_client: CogniteClient) -> None:
+    def __init__(self, config: ClientConfig, api_version: str | None, cognite_client: ToolkitClient) -> None:
         super().__init__(config, api_version, cognite_client)
         self._cache: dict[str, int] = {}
         self._reverse_cache: dict[int, str] = {}
+
+    @property
+    def resource_name(self) -> str:
+        return type(self).__name__.removesuffix("LookUpAPI")
 
     @overload
     def id(self, external_id: str) -> int: ...
@@ -22,11 +31,15 @@ class LookUpAPI(APIClient, ABC):
     def id(self, external_id: SequenceNotStr[str]) -> list[int]: ...
 
     def id(self, external_id: str | SequenceNotStr[str]) -> int | list[int]:
-        return self._id(external_id)
-
-    @abstractmethod
-    def _id(self, external_id: str | SequenceNotStr[str]) -> int | list[int]:
-        raise NotImplementedError
+        ids = [external_id] if isinstance(external_id, str) else external_id
+        missing = [id for id in ids if id not in self._cache]
+        if missing:
+            lookup = self._id(missing)
+            self._cache.update(lookup)
+            self._reverse_cache.update({v: k for k, v in lookup.items()})
+            if len(missing) != len(lookup):
+                raise ResourceRetrievalError(f"Failed to retrieve {self.resource_name} with external_id {missing}")
+        return self._cache[external_id] if isinstance(external_id, str) else [self._cache[id] for id in ids]
 
     @overload
     def external_id(self, id: int) -> str: ...
@@ -35,76 +48,119 @@ class LookUpAPI(APIClient, ABC):
     def external_id(self, id: Sequence[int]) -> list[str]: ...
 
     def external_id(self, id: int | Sequence[int]) -> str | list[str]:
-        return self._external_id(id)
+        ids = [id] if isinstance(id, int) else id
+        missing = [id_ for id_ in ids if id not in self._reverse_cache]
+        if missing:
+            lookup = self._external_id(missing)
+            self._reverse_cache.update(lookup)
+            self._cache.update({v: k for k, v in lookup.items()})
+            if len(missing) != len(lookup):
+                raise ResourceRetrievalError(f"Failed to retrieve {self.resource_name} with id {missing}")
+        return self._reverse_cache[id] if isinstance(id, int) else [self._reverse_cache[id] for id in ids]
 
     @abstractmethod
-    def _external_id(self, id: int | Sequence[int]) -> str | list[str]:
+    def _id(self, external_id: SequenceNotStr[str]) -> dict[str, int]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _external_id(self, id: Sequence[int]) -> dict[int, str]:
         raise NotImplementedError
 
 
 class DataSetLookUpAPI(LookUpAPI):
-    def _id(self, external_id: str | SequenceNotStr[str]) -> int | list[int]:
-        return [
-            data_set.id
+    def _id(self, external_id: SequenceNotStr[str]) -> dict[str, int]:
+        return {
+            data_set.external_id: data_set.id
             for data_set in self._cognite_client.data_sets.retrieve_multiple(
-                external_id=external_id, ignore_unknown_ids=True
+                external_ids=external_id, ignore_unknown_ids=True
             )
-        ]
+            if data_set.external_id and data_set.id
+        }
 
-    def _external_id(self, id: int | Sequence[int]) -> str | list[str]:
-        return [
-            data_set.external_id
-            for data_set in self._cognite_client.data_sets.retrieve_multiple(id=id, ignore_unknown_ids=True)
-        ]
+    def _external_id(self, id: Sequence[int]) -> dict[int, str]:
+        return {
+            data_set.id: data_set.external_id
+            for data_set in self._cognite_client.data_sets.retrieve_multiple(ids=id, ignore_unknown_ids=True)
+            if data_set.external_id and data_set.id
+        }
 
 
 class AssetLookUpAPI(LookUpAPI):
-    def _id(self, external_id: str | SequenceNotStr[str]) -> int | list[int]:
-        return [
-            asset.id
-            for asset in self._cognite_client.assets.retrieve_multiple(external_id=external_id, ignore_unknown_ids=True)
-        ]
+    def _id(self, external_id: SequenceNotStr[str]) -> dict[str, int]:
+        return {
+            asset.external_id: asset.id
+            for asset in self._cognite_client.assets.retrieve_multiple(
+                external_ids=external_id, ignore_unknown_ids=True
+            )
+            if asset.external_id and asset.id
+        }
 
-    def _external_id(self, id: int | Sequence[int]) -> str | list[str]:
-        return [
-            asset.external_id for asset in self._cognite_client.assets.retrieve_multiple(id=id, ignore_unknown_ids=True)
-        ]
+    def _external_id(self, id: Sequence[int]) -> dict[int, str]:
+        return {
+            asset.id: asset.external_id
+            for asset in self._cognite_client.assets.retrieve_multiple(ids=id, ignore_unknown_ids=True)
+            if asset.external_id and asset.id
+        }
 
 
 class TimeSeriesLookUpAPI(LookUpAPI):
-    def _id(self, external_id: str | SequenceNotStr[str]) -> int | list[int]:
-        return [
-            time_series.id
-            for time_series in self._cognite_client.time_series.retrieve_multiple(
-                external_id=external_id, ignore_unknown_ids=True
+    def _id(self, external_id: SequenceNotStr[str]) -> dict[str, int]:
+        return {
+            ts.external_id: ts.id
+            for ts in self._cognite_client.time_series.retrieve_multiple(
+                external_ids=external_id, ignore_unknown_ids=True
             )
-        ]
+            if ts.external_id and ts.id
+        }
 
-    def _external_id(self, id: int | Sequence[int]) -> str | list[str]:
-        return [
-            time_series.external_id
-            for time_series in self._cognite_client.time_series.retrieve_multiple(id=id, ignore_unknown_ids=True)
-        ]
+    def _external_id(self, id: Sequence[int]) -> dict[int, str]:
+        return {
+            ts.id: ts.external_id
+            for ts in self._cognite_client.time_series.retrieve_multiple(ids=id, ignore_unknown_ids=True)
+            if ts.external_id and ts.id
+        }
 
 
-class SecurityCategoriesLookUpAPI(LookUpAPI):
-    def _id(self, external_id: str | SequenceNotStr[str]) -> int | list[int]:
+class AllLookUpAPI(LookUpAPI, ABC):
+    def __init__(self, config: ClientConfig, api_version: str | None, cognite_client: ToolkitClient) -> None:
+        super().__init__(config, api_version, cognite_client)
+        self._has_looked_up = False
+
+    @abstractmethod
+    def _lookup(self) -> None:
         raise NotImplementedError
 
-    def _external_id(self, id: int | Sequence[int]) -> str | list[str]:
-        raise NotImplementedError
+    def _id(self, external_id: SequenceNotStr[str]) -> dict[str, int]:
+        if not self._has_looked_up:
+            self._lookup()
+        return {external_id: self._cache[external_id] for external_id in external_id}
+
+    def _external_id(self, id: Sequence[int]) -> dict[int, str]:
+        if not self._has_looked_up:
+            self._lookup()
+            self._has_looked_up = True
+        return {id: self._reverse_cache[id] for id in id}
 
 
-class LocationFiltersLookUpAPI(LookUpAPI):
-    def _id(self, external_id: str | SequenceNotStr[str]) -> int | list[int]:
-        raise NotImplementedError
+class SecurityCategoriesLookUpAPI(AllLookUpAPI):
+    def _lookup(self) -> None:
+        categories = self._cognite_client.iam.security_categories.list(limit=-1)
+        self._cache = {category.name: category.id for category in categories if category.name and category.id}
+        self._reverse_cache = {category.id: category.name for category in categories if category.name and category.id}
 
-    def _external_id(self, id: int | Sequence[int]) -> str | list[str]:
-        raise NotImplementedError
+    def name(self, id: int | Sequence[int]) -> str | list[str]:
+        return self.external_id(id)
 
 
-class LookUpGroup(APIClient):
-    def __init__(self, config: ClientConfig, api_version: str | None, cognite_client: CogniteClient) -> None:
+class LocationFiltersLookUpAPI(AllLookUpAPI):
+    def _lookup(self) -> None:
+        location_filters = self._toolkit_client.location_filters.list()
+        self._cache = {location_filter.name: location_filter.id for location_filter in location_filters}
+        self._reverse_cache = {location_filter.id: location_filter.name for location_filter in location_filters}
+
+
+class LookUpGroup(ToolkitAPI):
+    def __init__(self, config: ClientConfig, api_version: str | None, cognite_client: ToolkitClient) -> None:
         super().__init__(config, api_version, cognite_client)
         self.data_sets = DataSetLookUpAPI(config, api_version, cognite_client)
         self.assets = AssetLookUpAPI(config, api_version, cognite_client)
