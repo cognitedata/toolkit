54a5053d9ec1d677c87bcd34fbf8e71d28530194
anders-albert <anders0albert@gmail.com>
2024-12-20 15:53:28 +0100

    tests: updated tests

--- tests/test_unit/test_cdf_tk/test_loaders/test_file_metadata_loader.py
+++ tests/test_unit/test_cdf_tk/test_loaders/test_file_metadata_loader.py
@@ -10,10 +10,10 @@ from cognite.client.data_classes import FileMetadataWrite, FileMetadataWriteList
 from cognite_toolkit._cdf_tk.loaders import FileMetadataLoader
 from cognite_toolkit._cdf_tk.utils import CDFToolConfig
 from tests.test_unit.approval_client import ApprovalToolkitClient
+from tests.test_unit.approval_client.client import LookUpAPIMock
 
 
 def file_metadata_config_cases() -> Iterable[ParameterSet]:
-    data_set_mapping = {"ds_files": 42}
     yield pytest.param(
         """externalId: sharepointABC
 name: A file.txt
@@ -21,14 +21,13 @@ dataSetExternalId: ds_files
 source: sharepointABC
 """,
         ["1.A file.txt"],
-        data_set_mapping,
         FileMetadataWriteList(
             [
                 FileMetadataWrite(
                     external_id="sharepointABC",
                     source="sharepointABC",
                     name="A file.txt",
-                    data_set_id=42,
+                    data_set_id=LookUpAPIMock._create_id("ds_files"),
                 )
             ]
         ),
@@ -45,20 +44,19 @@ source: sharepointABC
   source: sharepointABC
 """,
         ["1.A file.txt", "1.Another file.txt"],
-        data_set_mapping,
         FileMetadataWriteList(
             [
                 FileMetadataWrite(
                     external_id="sharepointABC",
                     source="sharepointABC",
                     name="A file.txt",
-                    data_set_id=42,
+                    data_set_id=LookUpAPIMock._create_id("ds_files"),
                 ),
                 FileMetadataWrite(
                     external_id="sharepointABC2",
                     source="sharepointABC",
                     name="Another file.txt",
-                    data_set_id=42,
+                    data_set_id=LookUpAPIMock._create_id("ds_files"),
                 ),
             ]
         ),
@@ -67,12 +65,11 @@ source: sharepointABC
 
 
 class TestLoadResources:
-    @pytest.mark.parametrize("yaml_content, files, data_set_mapping, expected", list(file_metadata_config_cases()))
+    @pytest.mark.parametrize("yaml_content, files, expected", list(file_metadata_config_cases()))
     def test_load_resources(
         self,
         yaml_content: str,
         files: list[str],
-        data_set_mapping: dict[str, int],
         expected: FileMetadataWriteList,
         toolkit_client_approval: ApprovalToolkitClient,
         monkeypatch: MonkeyPatch,
