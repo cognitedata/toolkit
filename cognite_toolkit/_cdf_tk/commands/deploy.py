50df2bd1314b26a570386533313edfffa4f8ed95
anders-albert <anders0albert@gmail.com>
2024-12-21 22:28:35 +0100

    refactor: cleanup

--- cognite_toolkit/_cdf_tk/commands/deploy.py
+++ cognite_toolkit/_cdf_tk/commands/deploy.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import traceback
 from graphlib import TopologicalSorter
 from pathlib import Path
-from typing import Any
 
 from cognite.client.data_classes._base import T_CogniteResourceList, T_WritableCogniteResource, T_WriteClass
 from cognite.client.exceptions import CogniteAPIError, CogniteDuplicatedError
@@ -45,13 +44,11 @@ from cognite_toolkit._cdf_tk.loaders import (
 from cognite_toolkit._cdf_tk.loaders._base_loaders import T_WritableCogniteResourceList
 from cognite_toolkit._cdf_tk.tk_warnings.other import (
     LowSeverityWarning,
-    MediumSeverityWarning,
     ToolkitDependenciesIncludedWarning,
 )
 from cognite_toolkit._cdf_tk.utils import (
     CDFToolConfig,
     read_yaml_file,
-    to_diff,
 )
 
 from ._utils import _print_ids_or_length
@@ -288,66 +285,6 @@ class DeployCommand(ToolkitCommand):
                 unchanged=nr_of_unchanged,
                 total=nr_of_items,
             )
-
-    def to_create_changed_unchanged_triple(
-        self,
-        resources: T_CogniteResourceList,
-        loader: ResourceLoader,
-        verbose: bool = False,
-    ) -> tuple[T_CogniteResourceList, T_CogniteResourceList, T_CogniteResourceList]:
-        """Returns a triple of lists of resources that should be created, updated, and are unchanged."""
-        resource_ids = loader.get_ids(resources)
-        to_create, to_update, unchanged = (
-            loader.list_write_cls([]),
-            loader.list_write_cls([]),
-            loader.list_write_cls([]),
-        )
-        try:
-            cdf_resources = loader.retrieve(resource_ids)
-        except CogniteAPIError as e:
-            self.warn(
-                MediumSeverityWarning(
-                    f"Failed to retrieve {len(resource_ids)} of {loader.display_name}. Proceeding assuming not data in CDF. Error {e}."
-                )
-            )
-            print(Panel(traceback.format_exc()))
-            cdf_resource_by_id = {}
-        else:
-            cdf_resource_by_id = {loader.get_id(resource): resource for resource in cdf_resources}
-
-        for item in resources:
-            identifier = loader.get_id(item)
-            cdf_resource = cdf_resource_by_id.get(identifier)
-            local_dumped: dict[str, Any] = {}
-            cdf_dumped: dict[str, Any] = {}
-            are_equal = False
-            if cdf_resource:
-                try:
-                    are_equal, local_dumped, cdf_dumped = loader.are_equal(item, cdf_resource, return_dumped=True)
-                except CogniteAPIError as e:
-                    self.warn(
-                        MediumSeverityWarning(
-                            f"Failed to compare {loader.display_name} {loader.get_id(item)} for equality. Proceeding assuming not data in CDF. Error {e}."
-                        )
-                    )
-                    print(Panel(traceback.format_exc()))
-
-            if are_equal:
-                unchanged.append(item)
-            elif cdf_resource:
-                if verbose:
-                    print(
-                        Panel(
-                            "\n".join(to_diff(cdf_dumped, local_dumped)),
-                            title=f"{loader.display_name}: {identifier}",
-                            expand=False,
-                        )
-                    )
-                to_update.append(item)
-            else:
-                to_create.append(item)
-
-        return to_create, to_update, unchanged
 
     def _verbose_print(
         self,
