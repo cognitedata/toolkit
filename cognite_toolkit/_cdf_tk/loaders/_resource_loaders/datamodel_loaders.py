fda261164b4e7e484ea8b57a22c00932286c6c66
anders-albert <anders0albert@gmail.com>
2024-12-22 21:20:29 +0100

    fix: introduced bug

--- cognite_toolkit/_cdf_tk/loaders/_resource_loaders/datamodel_loaders.py
+++ cognite_toolkit/_cdf_tk/loaders/_resource_loaders/datamodel_loaders.py
@@ -1062,7 +1062,7 @@ class GraphQLLoader(
         self, filepath: Path, environment_variables: dict[str, str | None] | None = None
     ) -> list[dict[str, Any]]:
         raw_yaml = load_yaml_inject_variables(
-            safe_read(filepath), environment_variables or {} if self.do_environment_variable_injection else {}
+            self.safe_read(filepath), environment_variables or {} if self.do_environment_variable_injection else {}
         )
         raw_list = raw_yaml if isinstance(raw_yaml, list) else [raw_yaml]
 
--- cognite_toolkit/_cdf_tk/loaders/_resource_loaders/transformation_loaders.py
+++ cognite_toolkit/_cdf_tk/loaders/_resource_loaders/transformation_loaders.py
@@ -191,7 +191,7 @@ class TransformationLoader(
         self, filepath: Path, environment_variables: dict[str, str | None] | None = None
     ) -> list[dict[str, Any]]:
         resources = load_yaml_inject_variables(
-            safe_read(filepath), environment_variables or {} if self.do_environment_variable_injection else {}
+            self.safe_read(filepath), environment_variables or {} if self.do_environment_variable_injection else {}
         )
 
         raw_list = resources if isinstance(resources, list) else [resources]
