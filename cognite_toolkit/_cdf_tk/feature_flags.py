9993693cb052986eafa6d7b5ec85a24519c07742
anders-albert <anders0albert@gmail.com>
2024-12-30 22:14:14 +0100

    style: happy mypy

--- cognite_toolkit/_cdf_tk/feature_flags.py
+++ cognite_toolkit/_cdf_tk/feature_flags.py
@@ -9,53 +9,54 @@ from cognite_toolkit._cdf_tk.constants import clean_name
 
 
 class Flags(Enum):
-    INTERNAL: ClassVar[dict[str, Any]] = {"visible": False, "description": "Does nothing"}
-    IMPORT_CMD: ClassVar[dict[str, Any]] = {"visible": True, "description": "Enables the import sub application"}
-    GRAPHQL: ClassVar[dict[str, Any]] = {
+    # Ruff requires annotations while mypy requires no annotations
+    INTERNAL: ClassVar[dict[str, Any]] = {"visible": False, "description": "Does nothing"}  # type: ignore[misc]
+    IMPORT_CMD: ClassVar[dict[str, Any]] = {"visible": True, "description": "Enables the import sub application"}  # type: ignore[misc]
+    GRAPHQL: ClassVar[dict[str, Any]] = {  # type: ignore[misc]
         "visible": True,
         "description": "Enables the support for deploying data models as GraphQL schemas",
     }
-    ADO_PIPELINES: ClassVar[dict[str, Any]] = {
+    ADO_PIPELINES: ClassVar[dict[str, Any]] = {  # type: ignore[misc]
         "visible": True,
         "description": "Enables creation of Azure DevOps pipelines in repo init command",
     }
-    REQUIRE_KIND: ClassVar[dict[str, Any]] = {
+    REQUIRE_KIND: ClassVar[dict[str, Any]] = {  # type: ignore[misc]
         "visible": True,
         "description": "Require kind in all config file names. For example, `my.FileMetadata.yaml`",
     }
-    RUN_WORKFLOW: ClassVar[dict[str, Any]] = {
+    RUN_WORKFLOW: ClassVar[dict[str, Any]] = {  # type: ignore[misc]
         "visible": True,
         "description": "Enables the support for running workflows",
     }
-    STREAMLIT: ClassVar[dict[str, Any]] = {
+    STREAMLIT: ClassVar[dict[str, Any]] = {  # type: ignore[misc]
         "visible": True,
         "description": "Enables the support for deploying Streamlit apps",
     }
-    FORCE_UPDATE: ClassVar[dict[str, Any]] = {
+    FORCE_UPDATE: ClassVar[dict[str, Any]] = {  # type: ignore[misc]
         "visible": False,
         "description": "Adding the --force-update flag to the deploy command",
     }
-    MODULE_REPEAT: ClassVar[dict[str, Any]] = {
+    MODULE_REPEAT: ClassVar[dict[str, Any]] = {  # type: ignore[misc]
         "visible": True,
         "description": "Enables the support for repeating modules in the config file",
     }
-    EDGES: ClassVar[dict[str, Any]] = {
+    EDGES: ClassVar[dict[str, Any]] = {  # type: ignore[misc]
         "visible": True,
         "description": "Enables the support for creating edges between nodes",
     }
-    CLASSIC: ClassVar[dict[str, Any]] = {
+    CLASSIC: ClassVar[dict[str, Any]] = {  # type: ignore[misc]
         "visible": True,
         "description": "Enables the support for events and relationships",
     }
-    MODULE_PULL: ClassVar[dict[str, Any]] = {
+    MODULE_PULL: ClassVar[dict[str, Any]] = {  # type: ignore[misc]
         "visible": True,
         "description": "Enables the support for pulling modules from the CDF",
     }
-    PURGE: ClassVar[dict[str, Any]] = {
+    PURGE: ClassVar[dict[str, Any]] = {  # type: ignore[misc]
         "visible": True,
         "description": "Enables the support for purging datasets and spaces",
     }
-    BUILD_OFFLINE: ClassVar[dict[str, Any]] = {
+    BUILD_OFFLINE: ClassVar[dict[str, Any]] = {  # type: ignore[misc]
         "visible": True,
         "description": "Enables builds without comparing against CDF project for missing dependencies.",
     }
