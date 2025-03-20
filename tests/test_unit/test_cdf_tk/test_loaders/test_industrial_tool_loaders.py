b4d0513f6bd19fe64c38cd562c7d4ef932565e91
Pål Rønning <ronpal@users.noreply.github.com>
2025-03-20 11:05:36 +0100

    Removed tests of questionable value

--- tests/test_unit/test_cdf_tk/test_loaders/test_industrial_tool_loaders.py
+++ tests/test_unit/test_cdf_tk/test_loaders/test_industrial_tool_loaders.py
@@ -1,14 +1,9 @@
-from pathlib import Path
-
 import pytest
 from cognite.client import _version as CogniteSDKVersion
 from packaging.requirements import Requirement
 
-from cognite_toolkit._cdf_tk.commands.build import BuildCommand
 from cognite_toolkit._cdf_tk.loaders._resource_loaders.industrial_tool_loaders import StreamlitLoader
 from cognite_toolkit._cdf_tk.tk_warnings import StreamlitRequirementsWarning
-from cognite_toolkit._cdf_tk.utils.file import safe_read
-from tests.data import COMPLETE_ORG
 
 
 class TestStreamlitLoader:
@@ -42,37 +37,3 @@ class TestStreamlitLoader:
     def test_requirements_file_missing_packages(self, mock_content, expected_result) -> None:
         actual = StreamlitLoader._missing_recommended_requirements(mock_content)
         assert set(actual) == set(expected_result)
-
-    @staticmethod
-    def mock_safe_read(file_path, mock_content):
-        if file_path.name == "requirements.txt":
-            return mock_content  # Your mocked content
-        else:
-            # If it's not requirements.txt, call the original safe_read
-            return safe_read(file_path)
-
-    @pytest.mark.parametrize(
-        "mock_content, expected_result",
-        user_requirements_txt(),
-    )
-    def test_requirements_file_missing_packages_warnings(
-        self, monkeypatch, tmp_path: Path, mock_content, expected_result
-    ) -> None:
-        monkeypatch.setattr(
-            "cognite_toolkit._cdf_tk.builders._streamlit.safe_read",
-            lambda file_path: self.mock_safe_read(file_path, "\n".join(mock_content)),
-        )
-
-        cmd = BuildCommand(print_warning=False)
-        cmd.execute(
-            verbose=False,
-            build_dir=tmp_path,
-            organization_dir=COMPLETE_ORG,
-            selected=None,
-            build_env_name="dev",
-            no_clean=False,
-        )
-
-        actual_warning = next((w for w in cmd.warning_list if isinstance(w, StreamlitRequirementsWarning)), None)
-        expected_warning = StreamlitRequirementsWarning(expected_result) if len(expected_result) > 0 else None
-        assert actual_warning == expected_warning
