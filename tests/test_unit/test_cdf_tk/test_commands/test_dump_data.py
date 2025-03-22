492c37d052d3887f457c8b7d9c1c8d40fea06fec
anders-albert <anders0albert@gmail.com>
2025-03-22 17:35:43 +0100

    tests: updated test

--- tests/test_unit/test_cdf_tk/test_commands/test_dump_data.py
+++ tests/test_unit/test_cdf_tk/test_commands/test_dump_data.py
@@ -12,7 +12,8 @@ from cognite.client.data_classes import (
 from questionary import Choice
 
 from cognite_toolkit._cdf_tk.client.testing import monkeypatch_toolkit_client
-from cognite_toolkit._cdf_tk.commands import DumpAssetsCommand, DumpTimeSeriesCommand
+from cognite_toolkit._cdf_tk.commands import DumpDataCommand
+from cognite_toolkit._cdf_tk.commands.dump_data import AssetFinder, TimeSeriesFinder
 from cognite_toolkit._cdf_tk.utils.file import read_yaml_file
 from tests.test_unit.utils import MockQuestionary
 
@@ -31,16 +32,27 @@ class TestDumpData:
             source="MySource",
             labels=[my_label.external_id],
         )
-        cmd = DumpAssetsCommand(skip_tracking=False, print_warning=False)
+        cmd = DumpDataCommand(skip_tracking=False, print_warning=False)
         output_dir = tmp_path / "asset_dump"
         with monkeypatch_toolkit_client() as client:
             client.assets.return_value = [AssetList([my_asset])]
             client.assets.aggregate_count.return_value = 1
             client.assets.retrieve.return_value = Asset(external_id="rootAsset")
             client.labels.retrieve.return_value = LabelDefinitionList([my_label])
-            client.data_sets.retrieve.return_value = dataset
+            client.data_sets.retrieve_multiple.return_value = [dataset]
+            client.lookup.assets.external_id.return_value = "rootAsset"
+            client.lookup.data_sets.external_id.return_value = dataset.external_id
 
-            cmd.execute(client, ["my_asset"], None, output_dir, clean=True, limit=None, format_="csv", verbose=False)
+            cmd.dump_table(
+                AssetFinder(client),
+                ["rootAsset"],
+                [],
+                output_dir,
+                clean=True,
+                limit=None,
+                format_="csv",
+                verbose=False,
+            )
 
         output_csv = next(output_dir.rglob("*.csv"))
         assert output_csv.read_text().splitlines() == [
@@ -55,7 +67,7 @@ class TestDumpData:
         assert read_yaml_file(label_yaml) == [my_label.as_write().dump()]
 
     def test_interactive_select_assets(self, monkeypatch) -> None:
-        cmd = DumpAssetsCommand(skip_tracking=False, print_warning=False)
+        cmd = DumpDataCommand(skip_tracking=False, print_warning=False)
 
         def select_hierarchy(choices: list[Choice]) -> list[str]:
             assert len(choices) == 2
@@ -68,19 +80,17 @@ class TestDumpData:
         answers = ["Hierarchy", select_hierarchy, "Data Set", select_data_set, "Done"]
         with (
             monkeypatch_toolkit_client() as client,
-            MockQuestionary(DumpAssetsCommand.__module__, monkeypatch, answers),
+            MockQuestionary(DumpDataCommand.__module__, monkeypatch, answers),
         ):
-            client.assets.return_value = [Asset(id=1, external_id="Root1"), Asset(id=2, external_id="Root2")]
+            client.assets.list.return_value = [Asset(id=1, external_id="Root1"), Asset(id=2, external_id="Root2")]
             client.assets.aggregate_count.return_value = 100
-            client.data_sets = [
+            client.data_sets.list.return_value = [
                 DataSet(id=1, external_id="dataset1"),
                 DataSet(id=2, external_id="dataset2"),
                 DataSet(id=3, external_id="dataset3"),
             ]
 
-            selected_hierarchy, selected_dataset = cmd._select_hierarchy_and_data_set(
-                client, None, None, interactive=True
-            )
+            selected_hierarchy, selected_dataset = cmd.interactive_select_hierarchy_datasets(AssetFinder(client))
 
         assert selected_hierarchy == ["Root2"]
         assert selected_dataset == ["dataset3"]
@@ -96,15 +106,23 @@ class TestDumpData:
             is_step=False,
             data_set_id=dataset.id,
         )
-        cmd = DumpTimeSeriesCommand(skip_tracking=False, print_warning=False)
+        cmd = DumpDataCommand(skip_tracking=False, print_warning=False)
         output_dir = tmp_path / "timeseries_dump"
         with monkeypatch_toolkit_client() as client:
             client.time_series.return_value = [TimeSeriesList([my_timeseries])]
             client.time_series.aggregate_count.return_value = 1
-            client.data_sets.retrieve.return_value = dataset
+            client.data_sets.retrieve_multiple.return_value = [dataset]
+            client.lookup.data_sets.external_id.return_value = dataset.external_id
 
-            cmd.execute(
-                client, [dataset.external_id], None, output_dir, clean=True, limit=None, format_="csv", verbose=False
+            cmd.dump_table(
+                TimeSeriesFinder(client),
+                [],
+                [dataset.external_id],
+                output_dir,
+                clean=True,
+                limit=None,
+                format_="csv",
+                verbose=False,
             )
 
         output_csv = next(output_dir.rglob("*.csv"))
@@ -117,7 +135,7 @@ class TestDumpData:
         assert read_yaml_file(dataset_yaml) == [dataset.as_write().dump()]
 
     def test_interactive_select_timeseries(self, monkeypatch) -> None:
-        cmd = DumpTimeSeriesCommand(skip_tracking=False, print_warning=False)
+        cmd = DumpDataCommand(skip_tracking=False, print_warning=False)
 
         def select_data_set(choices: list[Choice]) -> list[str]:
             assert len(choices) == 3
@@ -130,19 +148,19 @@ class TestDumpData:
         answers = ["Data Set", select_data_set, "Hierarchy", select_hierarchy, "Done"]
         with (
             monkeypatch_toolkit_client() as client,
-            MockQuestionary(DumpTimeSeriesCommand.__module__, monkeypatch, answers),
+            MockQuestionary(DumpDataCommand.__module__, monkeypatch, answers),
         ):
-            client.data_sets = [
+            client.data_sets.list.return_value = [
                 DataSet(id=1, external_id="dataset1", name="Dataset 1"),
                 DataSet(id=2, external_id="dataset2", name="Dataset 2"),
                 DataSet(id=3, external_id="dataset3", name="Dataset 3"),
             ]
-            client.assets.return_value = [
+            client.assets.list.return_value = [
                 Asset(id=1, external_id="Root1", name="Root 1"),
                 Asset(id=2, external_id="Root2", name="Root 2"),
             ]
             client.time_series.aggregate_count.return_value = 100
-            selected_hierarchy, selected_dataset = cmd._select_data_set(client, None, None, interactive=True)
+            selected_hierarchy, selected_dataset = cmd.interactive_select_hierarchy_datasets(TimeSeriesFinder(client))
 
         assert selected_hierarchy == ["Root2"]
         assert selected_dataset == ["dataset3"]
