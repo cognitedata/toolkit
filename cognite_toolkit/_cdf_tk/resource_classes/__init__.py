a2c6db4fb427c8c1264c7eeaf8739152b167166f
anders-albert <anders0albert@gmail.com>
2025-04-23 08:13:09 +0200

    refactor; expose new tk class

--- cognite_toolkit/_cdf_tk/resource_classes/__init__.py
+++ cognite_toolkit/_cdf_tk/resource_classes/__init__.py
@@ -7,24 +7,10 @@ This is means that we have three set of resource classes we use in Toolkit:
 3. Read resource classes (from cognite-sdk): Represent the read/response format the Cognite resources.
 """
 
-from typing import ClassVar
+from .base import ToolkitResource
+from .timeseries import TimeSeriesTK
 
-from cognite.client.data_classes._base import CogniteResource
-from cognite.client.utils._text import to_camel_case
-from pydantic import BaseModel
-
-
-class BaseModelResource(BaseModel, alias_generator=to_camel_case, extra="ignore"):
-    """Base class for all Toolkit resources class and the nested classes."""
-
-    pass
-
-
-class ToolkitResource(BaseModelResource):
-    """The base class for all Toolkit resources, i.e., resource that has a YAML file format."""
-
-    _cdf_resource: ClassVar[type[CogniteResource]]
-
-    def as_cognite_sdk_resource(self) -> CogniteResource:
-        """Convert the model to a CDF resource."""
-        return self._cdf_resource.load(self.model_dump(exclude_unset=True))
+__all__ = [
+    "TimeSeriesTK",
+    "ToolkitResource",
+]
