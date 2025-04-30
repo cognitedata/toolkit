0326ae2945dc528cf689f56ffcbe21632a9ed011
anders-albert <anders0albert@gmail.com>
2025-04-30 07:57:23 +0200

    tests: added failing test

--- tests/test_unit/test_cdf_tk/test_validation/test_validate_resource_pydantic.py
+++ tests/test_unit/test_cdf_tk/test_validation/test_validate_resource_pydantic.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import pytest
 
-from cognite_toolkit._cdf_tk.resource_classes import TimeSeriesYAML
+from cognite_toolkit._cdf_tk.resource_classes import GroupYAML, TimeSeriesYAML
 from cognite_toolkit._cdf_tk.tk_warnings.fileread import ResourceFormatWarning
 from cognite_toolkit._cdf_tk.validation import as_json_path, validate_resource_yaml_pydantic
 
@@ -49,6 +49,42 @@ def timeseries_yaml_test_cases() -> Iterable:
     )
 
 
+def group_yaml_test_cases() -> Iterable:
+    yield pytest.param(
+        {"sourceId": "123-345"},
+        {"Missing required field: 'name'"},
+        id="Missing name",
+    )
+    yield pytest.param(
+        {
+            "name": "invalid-group",
+            "capabilities": [{"dataModelInstancesAcl": {"actions": ["INVALID_ACTION"]}}],
+            "members": "allUserAccounts",
+        },
+        {
+            "In capabilities[0].actions input should be 'READ', 'WRITE' or 'WRITE_PROPERTIES'",
+            "In capabilities[0] missing required field: 'scope'",
+        },
+        id="Invalid action and missing scope",
+    )
+
+    yield pytest.param(
+        [
+            {
+                "name": "invalid-group",
+                "capabilities": [
+                    {"dataModelsAcl": {"actions": ["WRITE"], "scope": {"notExisting": {"spaceIds": {"my_space"}}}}}
+                ],
+                "members": "allUserAccounts",
+            }
+        ],
+        {
+            "In [0].capabilities[0] value error, Invalid scope name 'notExisting'. Expected one of all or spaceIdScope",
+        },
+        id="Invalid scope name",
+    )
+
+
 class TestValidateResourceYAML:
     @pytest.mark.parametrize("data, expected_errors", list(timeseries_yaml_test_cases()))
     def test_validate_timeseries_resource_yaml(self, data: dict | list, expected_errors: list[str]) -> None:
@@ -59,6 +95,16 @@ class TestValidateResourceYAML:
         assert isinstance(format_warning, ResourceFormatWarning)
 
         assert format_warning.errors == tuple(expected_errors)
+
+    @pytest.mark.parametrize("data, expected_errors", list(group_yaml_test_cases()))
+    def test_validate_group_resource_yaml(self, data: dict | list, expected_errors: set[str]) -> None:
+        """Test the validate_resource_yaml function for GroupYAML."""
+        warning_list = validate_resource_yaml_pydantic(data, GroupYAML, Path("some_file.yaml"))
+        assert len(warning_list) == 1
+        format_warning = warning_list[0]
+        assert isinstance(format_warning, ResourceFormatWarning)
+
+        assert set(format_warning.errors) == expected_errors
 
 
 class TestAsJsonPath:
