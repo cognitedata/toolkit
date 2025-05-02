7dd64d2bd06d7ebaf97cb6c651e9011863dbf757
Aashutosh-cognite <aashutosh.chaubey@cognite.com>
2025-05-02 12:24:12 +0530

    [CDF-24572] Pydantic Validation for Assets

---
+++ cognite_toolkit/_cdf_tk/resource_classes/asset.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+from pydantic import Field
+
+from .base import ToolkitResource
+
+
+class AssetYAML(ToolkitResource):
+    external_id: str = Field(description="External ID provided by the client.", max_length=255)
+    name: str | None = Field(None, description="The name of the asset.", min_length=1, max_length=140)
+    parent_id: str | None = Field(None, description="The parent of the node.")
+    external_parent_id: str | None = Field(
+        None, description="The external ID of the parent of the node.", max_length=255
+    )
+    description: str | None = Field(None, description="The description of the asset.", max_length=500)
+    metadata: dict[str, str] | None = Field(None, description="Custom, application-specific metadata.", max_length=256)
+    data_set_external_id: str | None = Field(None, description="The external id of the dataset this asset belongs to.")
+    source: str | None = Field(None, description="The source of the asset.", max_length=128)
+    labels: list[str | dict] | None = Field(
+        None, description="A list of labels associated with this resource.", max_length=10
+    )  # TODO: Add Label pydantic model here
+    geo_location: dict[str, Any] | None = Field(None, description="The geographic metadata of the asset")
---
+++ tests/test_unit/test_cdf_tk/test_resource_classes/test_asset.py
@@ -0,0 +1,30 @@
+import pytest
+
+from cognite_toolkit._cdf_tk.constants import MODULES
+from cognite_toolkit._cdf_tk.resource_classes.asset import AssetYAML
+from cognite_toolkit._cdf_tk.utils.file import read_yaml_file
+from tests.data import COMPLETE_ORG
+
+
+def find_resources(resource: str):
+    base = COMPLETE_ORG / MODULES
+    for path in base.rglob(f"*{resource}.yaml"):
+        data = read_yaml_file(path)
+        if isinstance(data, dict):
+            yield pytest.param(data, id=path.relative_to(base).as_posix())
+        elif isinstance(data, list):
+            for no, item in enumerate(data):
+                if isinstance(item, dict):
+                    yield pytest.param(item, id=f"{path.relative_to(base).as_posix()} - Item: {no}")
+                else:
+                    raise ValueError(f"Invalid data format in {path}: {item}")
+        else:
+            raise ValueError(f"Invalid data format in {path}: {data}")
+
+
+class TestAssetYAML:
+    @pytest.mark.parametrize("data", list(find_resources("Asset")))
+    def test_load_valid_dataset(self, data: dict[str, object]) -> None:
+        loaded = AssetYAML.model_validate(data)
+
+        assert loaded.model_dump(exclude_unset=True, by_alias=True) == data
