3e8e817ce253361d9d9d83dd788aa723a1f0a580
Aashutosh-cognite <aashutosh.chaubey@cognite.com>
2025-05-02 14:28:56 +0530

    test space method name update

--- tests/test_unit/test_cdf_tk/test_resource_classes/test_space.py
+++ tests/test_unit/test_cdf_tk/test_resource_classes/test_space.py
@@ -6,7 +6,7 @@ from tests.test_unit.utils import find_resources
 
 class TestSpaceYAML:
     @pytest.mark.parametrize("data", list(find_resources("Space")))
-    def test_load_valid_timeseries(self, data: dict[str, object]) -> None:
+    def test_load_valid_space(self, data: dict[str, object]) -> None:
         loaded = SpaceYAML.model_validate(data)
 
         assert loaded.model_dump(exclude_unset=True, by_alias=True) == data
