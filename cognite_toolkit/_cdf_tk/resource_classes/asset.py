ef89fe29dae4dd6fb35176abd14cdeed572d942b
Aashutosh-cognite <aashutosh.chaubey@cognite.com>
2025-05-05 11:22:02 +0530

    refactor: labels field type update

--- cognite_toolkit/_cdf_tk/resource_classes/asset.py
+++ cognite_toolkit/_cdf_tk/resource_classes/asset.py
@@ -1,12 +1,8 @@
-from typing import Any
+from typing import Any, Literal
 
 from pydantic import Field
 
 from .base import ToolkitResource
-
-
-class AssetLabels(ToolkitResource):
-    external_id: str = Field(description="External ID provided by the client.", max_length=255)
 
 
 class AssetYAML(ToolkitResource):
@@ -19,7 +15,7 @@ class AssetYAML(ToolkitResource):
     metadata: dict[str, str] | None = Field(None, description="Custom, application-specific metadata.", max_length=256)
     data_set_external_id: str | None = Field(None, description="The external id of the dataset this asset belongs to.")
     source: str | None = Field(None, description="The source of the asset.", max_length=128)
-    labels: list[str | AssetLabels] | None = Field(
+    labels: list[str | dict[Literal["externalId"], str]] | None = Field(
         None, description="A list of labels associated with this resource.", max_length=10
     )
     geo_location: dict[str, Any] | None = Field(None, description="The geographic metadata of the asset")
