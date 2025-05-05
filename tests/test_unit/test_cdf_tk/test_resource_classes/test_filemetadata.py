d017e6406101b27712fb2135c77786e0b35c64c5
Aashutosh-cognite <aashutosh.chaubey@cognite.com>
2025-05-05 15:09:05 +0530

    [CDF-24578] Pydantic Validation for FileMetadata

---
+++ tests/test_unit/test_cdf_tk/test_resource_classes/test_filemetadata.py
@@ -0,0 +1,12 @@
+import pytest
+
+from cognite_toolkit._cdf_tk.resource_classes.filemetadata import FileMetadataYAML
+from tests.test_unit.utils import find_resources
+
+
+class TestFileMetadataYAML:
+    @pytest.mark.parametrize("data", list(find_resources("FileMetadata")))
+    def test_load_valid_file_metadata(self, data: dict[str, object]) -> None:
+        loaded = FileMetadataYAML.model_validate(data)
+
+        assert loaded.model_dump(exclude_unset=True, by_alias=True) == data
