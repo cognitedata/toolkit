7c97522782ad9ed99dc939d065d665a72181516d
Pål Rønning <ronpal@users.noreply.github.com>
2025-05-08 10:19:30 +0200

    Removed unused function

--- cognite_toolkit/_cdf_tk/builders/_location.py
+++ cognite_toolkit/_cdf_tk/builders/_location.py
@@ -1,10 +1,8 @@
 from collections.abc import Callable, Iterable, Sequence
 from graphlib import TopologicalSorter
-from pathlib import Path
 from typing import Any
 
 from cognite_toolkit._cdf_tk.builders._base import Builder
-from cognite_toolkit._cdf_tk.constants import INDEX_PATTERN
 from cognite_toolkit._cdf_tk.data_classes._build_files import BuildDestinationFile, BuildSourceFile
 from cognite_toolkit._cdf_tk.data_classes._module_directories import ModuleLocation
 from cognite_toolkit._cdf_tk.loaders._resource_loaders.location_loaders import LocationFilterLoader
@@ -68,24 +66,3 @@ class LocationBuilder(Builder):
                 extra_sources=None,
                 warnings=warnings,
             )
-
-    def _create_file_path(self, source_path: Path, index: int, kind: str) -> Path:
-        """Creates the filepath in the build directory for the given source path.
-
-        Note that we are splitting location filter into single files to ensure deployment dependencies.
-        """
-        filestem = source_path.stem
-        # Get rid of the local index
-        filestem = INDEX_PATTERN.sub("", filestem)
-
-        # Increment to ensure we do not get duplicate filenames when we flatten the file
-        # structure from the module to the build directory.
-        self.resource_counter = index
-
-        filename = f"{self.resource_counter}.{filestem}"
-        if not filename.casefold().endswith(kind.casefold()):
-            filename = f"{filename}.{kind}"
-        filename = f"{filename}{source_path.suffix}"
-        destination_path = self.build_dir / self.resource_folder / filename
-        destination_path.parent.mkdir(parents=True, exist_ok=True)
-        return destination_path
