f9fd8c5197385c9a8e1daa053b357e61eecf12a9
anders-albert <anders0albert@gmail.com>
2025-05-16 09:53:53 +0200

    refactor: added interface for app

--- cognite_toolkit/_cdf_tk/apps/_profile_app.py
+++ cognite_toolkit/_cdf_tk/apps/_profile_app.py
@@ -8,8 +8,17 @@ class ProfileApp(typer.Typer):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self.callback(invoke_without_command=True)(self.main)
+        self.command("asset-centric")(self.asset_centric)
 
     def main(self, ctx: typer.Context) -> None:
         """Commands populate functionality"""
         if ctx.invoked_subcommand is None:
             print("Use [bold yellow]cdf profile --help[/] for more information.")
+
+    @staticmethod
+    def asset_centric(
+        ctx: typer.Context,
+        verbose: bool = False,
+    ) -> None:
+        """Asset centric profile"""
+        raise NotImplementedError()
