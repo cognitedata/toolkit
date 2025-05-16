cognitedata/toolkit

commit 9405d74860611107986da480070269d007d047bc

Updates to GitHub Action support (#1568)

dev.py
@@ -37,7 +37,10 @@ VERSION_FILES = [
     *(REPO_ROOT / "tests" / "data").rglob("cdf.toml"),
     *(REPO_ROOT / "tests" / "data").rglob("_build_environment.yaml"),
     *(REPO_ROOT / "cognite_toolkit").rglob("cdf.toml"),
+    REPO_ROOT / "cognite_toolkit" / "_repo_files" / "GitHub" / ".github" / "workflows" / "build.yaml",
+    REPO_ROOT / "cognite_toolkit" / "_repo_files" / "GitHub" / ".github" / "workflows" / "deploy.yaml",
 ]
+
 DOCKER_IMAGE_FILES = [
     *(REPO_ROOT / "cognite_toolkit" / "_repo_files").rglob("*.yml"),
     *(REPO_ROOT / "cognite_toolkit" / "_repo_files").rglob("*.yaml"),

commit 112e14efecb912e24f389f5664c4c44d7030d057

Outdated path in dev.py script (#1577)
# Description

This is a fix to wrong path introduced in #1568

## Changelog

- [x] Patch
- [ ] Minor
- [ ] Skip

## cdf

### Fixed

- GitHub Action examples now get current Toolkit version
- Upgraded pydantic version lower bound.

## templates

No changes.

dev.py
@@ -37,7 +37,7 @@ VERSION_FILES = [
     *(REPO_ROOT / "tests" / "data").rglob("cdf.toml"),
     *(REPO_ROOT / "tests" / "data").rglob("_build_environment.yaml"),
     *(REPO_ROOT / "cognite_toolkit").rglob("cdf.toml"),
-    REPO_ROOT / "cognite_toolkit" / "_repo_files" / "GitHub" / ".github" / "workflows" / "build.yaml",
+    REPO_ROOT / "cognite_toolkit" / "_repo_files" / "GitHub" / ".github" / "workflows" / "dry-run.yaml",
     REPO_ROOT / "cognite_toolkit" / "_repo_files" / "GitHub" / ".github" / "workflows" / "deploy.yaml",
 ]
 