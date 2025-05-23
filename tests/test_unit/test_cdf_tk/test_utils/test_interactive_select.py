49ba537fcf516c9a5e2b7483063f80c3e6a99616
anders-albert <anders0albert@gmail.com>
2025-05-23 22:03:15 +0200

    feat: Interactive Select files

--- tests/test_unit/test_cdf_tk/test_utils/test_interactive_select.py
+++ tests/test_unit/test_cdf_tk/test_utils/test_interactive_select.py
@@ -1,12 +1,13 @@
 from cognite.client.data_classes import (
     Asset,
+    CountAggregate,
     DataSet,
 )
 from questionary import Choice
 
 from cognite_toolkit._cdf_tk.client.testing import monkeypatch_toolkit_client
 from cognite_toolkit._cdf_tk.utils.interactive_select import (
-    AssetInteractiveSelect,
+    FileMetadataInteractiveSelect,
     TimeSeriesInteractiveSelect,
 )
 from tests.test_unit.utils import MockQuestionary
@@ -25,17 +26,47 @@ class TestInteractiveSelect:
         answers = ["Hierarchy", select_hierarchy, "Data Set", select_data_set, "Done"]
         with (
             monkeypatch_toolkit_client() as client,
-            MockQuestionary(AssetInteractiveSelect.__module__, monkeypatch, answers),
+            MockQuestionary(FileMetadataInteractiveSelect.__module__, monkeypatch, answers),
         ):
             client.assets.list.return_value = [Asset(id=1, external_id="Root1"), Asset(id=2, external_id="Root2")]
-            client.assets.aggregate_count.return_value = 100
+            client.files.aggregate.return_value = [CountAggregate(count=100)]
             client.data_sets.list.return_value = [
                 DataSet(id=1, external_id="dataset1"),
                 DataSet(id=2, external_id="dataset2"),
                 DataSet(id=3, external_id="dataset3"),
             ]
 
-            selector = AssetInteractiveSelect(client)
+            selector = FileMetadataInteractiveSelect(client)
+            selected_hierarchy, selected_dataset = selector.interactive_select_hierarchy_datasets()
+
+        assert selected_hierarchy == ["Root2"]
+        assert selected_dataset == ["dataset3"]
+
+    def test_interactive_select_filemetadata(self, monkeypatch) -> None:
+        def select_data_set(choices: list[Choice]) -> list[str]:
+            assert len(choices) == 3
+            return [choices[2].value]
+
+        def select_hierarchy(choices: list[Choice]) -> list[str]:
+            assert len(choices) == 2
+            return [choices[1].value]
+
+        answers = ["Data Set", select_data_set, "Hierarchy", select_hierarchy, "Done"]
+        with (
+            monkeypatch_toolkit_client() as client,
+            MockQuestionary(TimeSeriesInteractiveSelect.__module__, monkeypatch, answers),
+        ):
+            client.data_sets.list.return_value = [
+                DataSet(id=1, external_id="dataset1", name="Dataset 1"),
+                DataSet(id=2, external_id="dataset2", name="Dataset 2"),
+                DataSet(id=3, external_id="dataset3", name="Dataset 3"),
+            ]
+            client.assets.list.return_value = [
+                Asset(id=1, external_id="Root1", name="Root 1"),
+                Asset(id=2, external_id="Root2", name="Root 2"),
+            ]
+            client.time_series.aggregate_count.return_value = 100
+            selector = TimeSeriesInteractiveSelect(client)
             selected_hierarchy, selected_dataset = selector.interactive_select_hierarchy_datasets()
 
         assert selected_hierarchy == ["Root2"]
