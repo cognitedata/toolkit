545e342f97bb13f42c3738e1ddd447a8e47580f4
anders-albert <anders0albert@gmail.com>
2025-05-23 09:17:08 +0200

    refactor: add back in parallelism

--- cognite_toolkit/_cdf_tk/commands/dump_data.py
+++ cognite_toolkit/_cdf_tk/commands/dump_data.py
@@ -1,4 +1,6 @@
 import csv
+import queue
+import threading
 from abc import ABC, abstractmethod
 from collections.abc import Iterable, Iterator
 from dataclasses import dataclass
@@ -21,7 +23,7 @@ from cognite.client.data_classes import (
 )
 from cognite.client.data_classes._base import T_CogniteResource
 from rich.console import Console
-from rich.progress import track
+from rich.progress import BarColumn, Progress, TaskID, TaskProgressColumn, TextColumn, TimeRemainingColumn, track
 
 from cognite_toolkit._cdf_tk.client import ToolkitClient
 from cognite_toolkit._cdf_tk.commands._base import ToolkitCommand
@@ -466,6 +468,118 @@ class YAMLWriter(FileWriter):
                     f.write(yaml_safe_dump(group_rows))
 
 
+class ProducerWorkerExecutor:
+    def __init__(
+        self,
+        download_iterable: Iterable,
+        process: Callable,
+        write_to_file: Callable[[Any], None],
+        total_items: int,
+        max_queue_size: int,
+    ) -> None:
+        self._download_iterable = download_iterable
+        self.download_complete = False
+        self._write_to_file = write_to_file
+        self._process = process
+        self.console = Console()
+        self.process_queue = queue.Queue(maxsize=max_queue_size)
+        self.file_queue = queue.Queue(maxsize=max_queue_size)
+
+        self.total_items = total_items
+        self.error_occurred = False
+        self.error_message = ""
+
+    def run(self) -> None:
+        with Progress(
+            TextColumn("[bold blue]{task.description}"),
+            BarColumn(),
+            TaskProgressColumn(),
+            TimeRemainingColumn(),
+            console=self.console,
+        ) as progress:
+            download_task = progress.add_task("Downloading", total=self.total_items)
+            download_thread = threading.Thread(target=self._download_worker, args=(progress, download_task))
+            process_thread: threading.Thread | None = None
+            if self.process_queue:
+                process_task = progress.add_task("Processing", total=self.total_items)
+                process_thread = threading.Thread(target=self._process_worker, args=(progress, process_task))
+
+            write_task = progress.add_task("Writing to file", total=self.total_items)
+            write_thread = threading.Thread(target=self._write_worker, args=(progress, write_task))
+
+            download_thread.start()
+            if process_thread:
+                process_thread.start()
+            write_thread.start()
+
+            # Wait for all threads to finish
+            download_thread.join()
+            if process_thread:
+                process_thread.join()
+            write_thread.join()
+
+    def _download_worker(self, progress: Progress, download_task: TaskID) -> None:
+        """Worker thread for downloading data."""
+        iterable = iter(self._download_iterable)
+        while True:
+            try:
+                items = next(iterable)
+                while True:
+                    try:
+                        self.process_queue.put(items, timeout=0.5)
+                        progress.update(download_task, advance=len(items))
+                        break  # Exit the loop once the item is successfully added
+                    except queue.Full:
+                        # Retry until the queue has space
+                        continue
+            except StopIteration:
+                self.download_complete = True
+                break
+            except Exception as e:
+                self.error_occurred = True
+                self.error_message = str(e)
+                self.console.print(f"[red]Error[/red] occurred while downloading: {self.error_message}")
+                break
+
+    def _process_worker(self, progress: Progress, process_task: TaskID) -> None:
+        """Worker thread for processing data."""
+        if self._process is None or self.process_queue is None:
+            return
+        while not self.download_complete or not self.process_queue.empty():
+            try:
+                items = self.process_queue.get(timeout=0.5)
+                processed_items = self._process(items)
+                self.file_queue.put(processed_items)
+                progress.update(process_task, advance=len(items))
+            except queue.Empty:
+                continue
+            except Exception as e:
+                self.error_occurred = True
+                self.error_message = str(e)
+                self.console.print(f"[red]ErrorError[/red] occurred while processing: {self.error_message}")
+                break
+            finally:
+                self.process_queue.task_done()
+
+    def _write_worker(self, progress: Progress, write_task: TaskID) -> None:
+        """Worker thread for writing data to file."""
+        # Simulate writing data to file
+        while not self.download_complete or not self.file_queue.empty():
+            try:
+                items = self.file_queue.get(timeout=0.5)
+                self._write_to_file(items)
+                progress.update(write_task, advance=len(items))
+            except queue.Empty:
+                continue
+            except Exception as e:
+                self.error_occurred = True
+                self.error_message = str(e)
+                self.console.print(f"[red]Error[/red] occurred while writing: {self.error_message}")
+                break
+            finally:
+                self.file_queue.task_done()
+
+
 class DumpDataCommand(ToolkitCommand):
     def dump_table(
         self,
@@ -475,6 +589,8 @@ class DumpDataCommand(ToolkitCommand):
         limit: int | None = None,
         format_: FileFormat = "csv",
         verbose: bool = False,
+        parallel_threshold: int = 10_000,
+        max_queue_size: int = 10,
     ) -> None:
         """Dumps data from CDF to files(s).
 
@@ -494,9 +610,23 @@ class DumpDataCommand(ToolkitCommand):
         console = Console()
         for schema, total_items, resource_iterator, resource_processor in finder.create_iterators(format_, limit):
             file_writer = FileWriter.load(schema, output_dir)
-            for resources in track(resource_iterator, total=total_items, description=f"Dumping {schema.display_name}"):
-                processed = resource_processor(resources)
-                file_writer.write_rows(processed)
+            if total_items > parallel_threshold:
+                executor = ProducerWorkerExecutor(
+                    resource_iterator,
+                    file_writer.write_rows,
+                    resource_processor,
+                    total_items,
+                    max_queue_size,
+                )
+                executor.run()
+                if executor.error_occurred:
+                    raise ToolkitValueError(executor.error_message)
+            else:
+                for resources in track(
+                    resource_iterator, total=total_items, description=f"Dumping {schema.display_name}"
+                ):
+                    processed = resource_processor(resources)
+                    file_writer.write_rows(processed)
             console.print(f"Dumped {total_items:,} rows to {output_dir}")
 
     @staticmethod
