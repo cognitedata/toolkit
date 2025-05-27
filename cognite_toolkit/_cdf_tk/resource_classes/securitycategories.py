48c707556f656fb5556c82bd7c7e923880cca7a2
anders-albert <anders0albert@gmail.com>
2025-05-27 12:56:35 +0200

    refactor; expose new yaml classe in module

--- cognite_toolkit/_cdf_tk/resource_classes/securitycategories.py
+++ cognite_toolkit/_cdf_tk/resource_classes/securitycategories.py
@@ -3,5 +3,5 @@ from pydantic import Field
 from .base import ToolkitResource
 
 
-class SecuritytCategoriesYAML(ToolkitResource):
+class SecurityCategoriesYAML(ToolkitResource):
     name: str = Field(description="The name of the data set.")
