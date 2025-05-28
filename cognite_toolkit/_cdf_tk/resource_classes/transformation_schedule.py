d6a1cb28b7d3ada24960b4cc880db87be7ed7854
Aashutosh-cognite <aashutosh.chaubey@cognite.com>
2025-05-28 12:11:11 +0530

    PR comment update

--- cognite_toolkit/_cdf_tk/resource_classes/transformation_schedule.py
+++ cognite_toolkit/_cdf_tk/resource_classes/transformation_schedule.py
@@ -7,4 +7,3 @@ class TransformationScheduleYAML(ToolkitResource):
     external_id: str = Field(description="External ID of the scheduled transformation.")
     interval: str = Field(description="Cron expression describes when the job should run.")
     is_paused: bool | None = Field(default=None, description="If true, the transformation is not scheduled.")
-    id: str | None = Field(default=None, description="ID of the scheduled transformation.")
