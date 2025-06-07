5737c3569c07f3d7c906d84766e671e666f74696
anders-albert <anders0albert@gmail.com>
2025-06-07 23:06:53 +0200

    feat; raw profiler

--- cognite_toolkit/_cdf_tk/commands/_profile.py
+++ cognite_toolkit/_cdf_tk/commands/_profile.py
@@ -2,8 +2,17 @@ from abc import ABC, abstractmethod
 from collections.abc import Callable
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from typing import Literal
+from collections import defaultdict
+from collections.abc import Callable, Mapping
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from typing import Any, Literal
 
 from cognite.client.exceptions import CogniteException
+from rich.live import Live
+from rich.spinner import Spinner
+from cognite.client.data_classes import Transformation
+from cognite.client.exceptions import CogniteException
+from rich.console import Console
 from rich.live import Live
 from rich.spinner import Spinner
 from rich.table import Table
@@ -15,6 +24,8 @@ from cognite_toolkit._cdf_tk.utils.cdf import (
     metadata_key_counts,
     relationship_aggregate_count,
 )
+from cognite_toolkit._cdf_tk.client.data_classes.raw import RawTable
+from cognite_toolkit._cdf_tk.utils.cdf import label_count, metadata_key_counts
 
 from ._base import ToolkitCommand
 
@@ -230,3 +241,107 @@ class ProfileCommand(ToolkitCommand):
                 return f"{value:,}"
 
         return styled_callable
+
+
+class ProfileRawCommand(ToolkitCommand):
+    class Columns:
+        RAW = "Raw"
+        Columns = "Columns"
+        Rows = "Rows"
+        Transformation = "Transformation"
+        Destination = "Destination"
+        Operation = "Operation"
+        UseAll = "Use All"
+
+    columns = (
+        Columns.RAW,
+        Columns.Columns,
+        Columns.Rows,
+        Columns.Transformation,
+        Columns.Destination,
+        Columns.Operation,
+        Columns.UseAll,
+    )
+    spinner_args: Mapping[str, Any] = dict(name="arc", text="loading...", style="bold green", speed=1.0)
+
+    @classmethod
+    def raw(
+        cls,
+        client: ToolkitClient,
+        destination_type: str,
+        verbose: bool = False,
+    ) -> list[dict[str, str]]:
+        transformations = client.transformations.list(destination_type=destination_type)
+        transformations_by_raw_table: dict[RawTable, list[Transformation]] = defaultdict(list)
+        for transformation in transformations:
+            if raw_table := get_raw_table(transformation.query):
+                transformations_by_raw_table[raw_table].append(transformation)
+
+        table_content, api_calls, indices_by_raw_table = cls._setup_table_and_api_calls(
+            client, transformations_by_raw_table
+        )
+        with Live(cls.draw_table(table_content, destination_type), refresh_per_second=4) as live:
+            with ThreadPoolExecutor(max_workers=8) as executor:
+                future_to_cell = {
+                    executor.submit(api_calls[raw_id]): raw_id for raw_id in transformations_by_raw_table.keys()
+                }
+                for future in as_completed(future_to_cell):
+                    raw_id = future_to_cell[future]
+                    column_count, row_count = future.result()
+                    index = indices_by_raw_table[raw_id]
+                    table_content[index][cls.Columns.Columns] = column_count
+                    table_content[index][cls.Columns.Rows] = row_count
+                    live.update(cls.draw_table(table_content, destination_type))
+        return [{col: str(value) for col, value in row.items()} for row in table_content]
+
+    @classmethod
+    def _setup_table_and_api_calls(
+        cls, client: ToolkitClient, transformations_by_raw_table: dict[RawTable, list[Transformation]]
+    ) -> tuple[list[dict[str, str | Spinner]], dict[RawTable, Callable[[], tuple[str, str]]], dict[RawTable, int]]:
+        rows: list[dict[str, str | Spinner]] = []
+        api_calls: dict[RawTable, Callable[[], tuple[str, str]]] = {}
+        index_by_raw_id: dict[RawTable, int] = {}
+        index = 0
+        for raw_id, transformations in transformations_by_raw_table.items():
+            api_calls[raw_id] = cls.call_api(client, raw_id)
+            for no, transformation in enumerate(transformations):
+                is_first = no == 0
+                row: dict[str, str | Spinner] = {
+                    cls.Columns.RAW: f"{raw_id.db_name}.{raw_id.table_name}" if is_first else "",
+                    cls.Columns.Columns: Spinner(**cls.spinner_args) if is_first else "",
+                    cls.Columns.Rows: Spinner(**cls.spinner_args) if is_first else "",
+                    cls.Columns.Transformation: transformation.name or transformation.external_id or "Unknown",
+                    cls.Columns.Destination: transformation.destination.type if transformation.destination else "Unknown",
+                    cls.Columns.Operation: transformation.conflict_mode or "Unknown",
+                    cls.Columns.UseAll: str("where" in (transformation.query or "unknown").casefold()),
+                }
+                rows.append(row)
+                if is_first:
+                    index_by_raw_id[raw_id] = index
+                index += 1
+        return rows, api_calls, index_by_raw_id
+
+    @classmethod
+    def draw_table(cls, rows: list[dict[str, str | Spinner]], destination: str) -> Table:
+        table = Table(
+            title=f"RAW Profile destination: {destination}",
+            title_justify="left",
+            show_header=True,
+            header_style="bold magenta",
+        )
+        for col in cls.columns:
+            table.add_column(col)
+        for row in rows:
+            table.add_row(*row.values())
+        return table
+
+    @classmethod
+    def call_api(cls, client: ToolkitClient, raw_table: RawTable) -> Callable[[], tuple[str, str]]:
+        def api_call() -> tuple[str, str]:
+            try:
+                response = client.raw.profile(raw_table, limit=1_000_000)
+                return f"{response.column_count:,}", f"{(response.row_count,)}"
+            except CogniteException as e:
+                return type(e).__name__, type(e).__name__
+
+        return api_call
