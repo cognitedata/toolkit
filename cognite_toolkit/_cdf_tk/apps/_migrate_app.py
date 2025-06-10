55d85fa6efb409040af8f1dff3f3784556029f88
anders-albert <anders0albert@gmail.com>
2025-06-10 07:58:58 +0200

    docs: documenation

--- cognite_toolkit/_cdf_tk/apps/_migrate_app.py
+++ cognite_toolkit/_cdf_tk/apps/_migrate_app.py
@@ -6,8 +6,9 @@ import typer
 class MigrateApp(typer.Typer):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
+        self.callback(invoke_without_command=True)(self.main)
 
     def main(self, ctx: typer.Context) -> None:
-        """Commands migrate functionality"""
+        """Migrate resources from Asset-Centric to data modeling in CDF."""
         if ctx.invoked_subcommand is None:
             print("Use [bold yellow]cdf migrate --help[/] for more information.")
