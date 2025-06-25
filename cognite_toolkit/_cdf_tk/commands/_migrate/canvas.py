6bd822f398f60f8a0240a69a58b74ea3e93459ea
anders-albert <anders0albert@gmail.com>
2025-06-25 14:07:20 +0200

    refactor; stipulate command and interactive select

---
+++ cognite_toolkit/_cdf_tk/commands/_migrate/canvas.py
@@ -0,0 +1,18 @@
+from rich import print
+
+from cognite_toolkit._cdf_tk.client import ToolkitClient
+from cognite_toolkit._cdf_tk.commands._base import ToolkitCommand
+from cognite_toolkit._cdf_tk.utils import humanize_collection
+from cognite_toolkit._cdf_tk.utils.interactive_select import InteractiveCanvasSelection
+
+
+class MigrationCanvasCommand(ToolkitCommand):
+    def migrate_canvas(
+        self,
+        client: ToolkitClient,
+        names: list[str] | None = None,
+        dry_run: bool = False,
+        verbose: bool = False,
+    ) -> None:
+        names = names or InteractiveCanvasSelection(client).select_names()
+        print(f"Would migrate {len(names)} canvs: {humanize_collection(names)}")
