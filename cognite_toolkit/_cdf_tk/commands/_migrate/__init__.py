bb9c73774af70240f9311417ce79499ac0d929b6
anders-albert <anders0albert@gmail.com>
2025-06-25 14:20:48 +0200

    refactor; expose canvas command

--- cognite_toolkit/_cdf_tk/commands/_migrate/__init__.py
+++ cognite_toolkit/_cdf_tk/commands/_migrate/__init__.py
@@ -1,4 +1,5 @@
+from .canvas import MigrationCanvasCommand
 from .prepare import MigrationPrepareCommand
 from .timeseries import MigrateTimeseriesCommand
 
-__all__ = ["MigrateTimeseriesCommand", "MigrationPrepareCommand"]
+__all__ = ["MigrateTimeseriesCommand", "MigrationCanvasCommand", "MigrationPrepareCommand"]
