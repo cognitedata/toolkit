16a211ac288cd75aaf7b879ce894ded54e0f78e6
anders-albert <anders0albert@gmail.com>
2025-06-26 12:01:31 +0200

    refactor: rename

--- cognite_toolkit/_cdf_tk/commands/_migrate/canvas.py
+++ cognite_toolkit/_cdf_tk/commands/_migrate/canvas.py
@@ -3,7 +3,7 @@ from rich import print
 from cognite_toolkit._cdf_tk.client import ToolkitClient
 from cognite_toolkit._cdf_tk.commands._base import ToolkitCommand
 from cognite_toolkit._cdf_tk.utils import humanize_collection
-from cognite_toolkit._cdf_tk.utils.interactive_select import InteractiveCanvasSelection
+from cognite_toolkit._cdf_tk.utils.interactive_select import InteractiveCanvasSelect
 
 
 class MigrationCanvasCommand(ToolkitCommand):
@@ -14,5 +14,5 @@ class MigrationCanvasCommand(ToolkitCommand):
         dry_run: bool = False,
         verbose: bool = False,
     ) -> None:
-        external_ids = external_ids or InteractiveCanvasSelection(client).select_external_ids()
+        external_ids = external_ids or InteractiveCanvasSelect(client).select_external_ids()
         print(f"Would migrate {len(external_ids)} canvases: {humanize_collection(external_ids)}")
