ab0c4bdbba6eb239ea38ee57ae4479dfe8aaa242
anders-albert <anders0albert@gmail.com>
2025-06-26 15:20:23 +0200

    tests: finished testing

--- tests/test_unit/test_cdf_tk/test_utils/test_interactive_select.py
+++ tests/test_unit/test_cdf_tk/test_utils/test_interactive_select.py
@@ -5,12 +5,15 @@ from cognite.client.data_classes import (
     Asset,
     CountAggregate,
     DataSet,
+    UserProfile,
+    UserProfileList,
 )
 from cognite.client.data_classes.data_modeling import NodeList
 from questionary import Choice
 
 from cognite_toolkit._cdf_tk.client.data_classes.canvas import CANVAS_INSTANCE_SPACE, Canvas
 from cognite_toolkit._cdf_tk.client.testing import monkeypatch_toolkit_client
+from cognite_toolkit._cdf_tk.exceptions import ToolkitValueError
 from cognite_toolkit._cdf_tk.utils.interactive_select import (
     AssetInteractiveSelect,
     EventInteractiveSelect,
@@ -166,25 +169,53 @@ class TestInteractiveSelect:
 
 class TestInteractiveCanvasSelect:
     @pytest.mark.parametrize(
-        "selected_cdf, answers, expected_selected",
+        "selected_cdf, answers, expected_selected, expected_options",
         [
-            pytest.param({}, ["All Canvases"], [], id="No canvases in CDF"),
+            pytest.param({}, ["All Canvases"], [], None, id="No canvases in CDF"),
             pytest.param(
                 {"Public1", "Public2", "Private1", "Private2"},
                 ["All Canvases"],
                 ["Public1", "Public2", "Private1", "Private2"],
+                None,
                 id="All canvases selected",
             ),
             pytest.param(
                 {"Public1", "Public2"},
                 ["All public Canvases"],
                 ["Public1", "Public2"],
+                None,
                 id="All public canvases selected",
+            ),
+            pytest.param(
+                {"Public1", "Public2"},
+                ["Selected public Canvases", {"Public1"}],
+                ["Public1"],
+                2,
+                id="Selected public canvases",
+            ),
+            pytest.param(
+                {"Public1", "Public2", "Private1", "Private2"},
+                ["All by given user", "Marge Simpson (marge)"],
+                ["Public2", "Private1"],
+                None,
+                id="All by given user",
+            ),
+            pytest.param(
+                {"Public1", "Public2", "Private1", "Private2"},
+                ["Selected by given user", "Marge Simpson (marge)", {"Public2"}],
+                ["Public2"],
+                2,
+                id="Selected by given user",
             ),
         ],
     )
     def test_interactive_selection(
-        self, selected_cdf: set[str], answers: list, expected_selected: list[str], monkeypatch
+        self,
+        selected_cdf: set[str],
+        answers: list,
+        expected_selected: list[str],
+        expected_options: int | None,
+        monkeypatch,
     ) -> None:
         default_args = dict(
             space=CANVAS_INSTANCE_SPACE,
@@ -195,14 +226,37 @@ class TestInteractiveCanvasSelect:
             updated_at=datetime.now(),
         )
         cdf_canvases = [
-            Canvas(external_id="Public1", name="Canvas 1", visibility="public", created_by="Homer", **default_args),
-            Canvas(external_id="Public2", name="Canvas 2", visibility="public", created_by="Marge", **default_args),
-            Canvas(external_id="Private1", name="Private 1", visibility="private", created_by="Marge", **default_args),
-            Canvas(external_id="Private2", name="Private 2", visibility="private", created_by="Homer", **default_args),
+            Canvas(external_id="Public1", name="Canvas 1", visibility="public", created_by="homer", **default_args),
+            Canvas(external_id="Public2", name="Canvas 2", visibility="public", created_by="marge", **default_args),
+            Canvas(external_id="Private1", name="Private 1", visibility="private", created_by="marge", **default_args),
+            Canvas(external_id="Private2", name="Private 2", visibility="private", created_by="homer", **default_args),
         ]
-        first_answer_by_title = {c.title: c.value for c in InteractiveCanvasSelect.opening_choices}
+        first_answer_by_choice_title = {c.title: c.value for c in InteractiveCanvasSelect.opening_choices}
         assert len(answers) >= 1, "At least one answer is required to select a canvas"
-        answers = [first_answer_by_title[answers[0]], *answers[1:]]
+        assert answers[0] in first_answer_by_choice_title, "Bug in test data. First answer must be a choice title"
+        if "user" in answers[0] and len(answers) >= 2:
+            user_title = answers[1]
+
+            def select_user(choices: list[Choice]) -> str:
+                assert len(choices) == 2
+                user_choice = next((c for c in choices if c.title == user_title), None)
+                assert user_choice is not None, f"Bug in test data. User choice '{user_title}' not found in choices"
+                return user_choice.value
+
+            answers[1] = select_user
+        answers[0] = first_answer_by_choice_title[answers[0]]
+
+        if expected_options is not None:
+            # Last question is which canvases to select.
+            last_selection = answers[-1]
+
+            def select_canvases(choices: list[Choice]) -> list[str]:
+                assert len(choices) == expected_options, (
+                    f"Expected {expected_options} choices, but got {len(choices)} choices: {choices}"
+                )
+                return [choice.value for choice in choices if choice.value in last_selection]
+
+            answers[-1] = select_canvases
 
         with (
             monkeypatch_toolkit_client() as client,
@@ -211,7 +265,24 @@ class TestInteractiveCanvasSelect:
             client.canvas.list.return_value = NodeList[Canvas](
                 [canvas for canvas in cdf_canvases if canvas.external_id in selected_cdf]
             )
+            client.iam.user_profiles.list.return_value = UserProfileList(
+                [
+                    UserProfile(user_identifier="homer", display_name="Homer Simpson", last_updated_time=1),
+                    UserProfile(user_identifier="marge", display_name="Marge Simpson", last_updated_time=1),
+                ]
+            )
             selector = InteractiveCanvasSelect(client)
             selected_external_ids = selector.select_external_ids()
 
         assert selected_external_ids == expected_selected
+
+    def test_interactive_abort_selection(self, monkeypatch) -> None:
+        answers = [None]
+        with (
+            monkeypatch_toolkit_client() as client,
+            MockQuestionary(InteractiveCanvasSelect.__module__, monkeypatch, answers),
+        ):
+            selector = InteractiveCanvasSelect(client)
+            with pytest.raises(ToolkitValueError) as exc_info:
+                selector.select_external_ids()
+        assert str(exc_info.value) == "No Canvas selection made. Aborting."
