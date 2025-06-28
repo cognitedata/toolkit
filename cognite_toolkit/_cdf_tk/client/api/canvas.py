063442b16f90874551246688c40ac27d3918d991
anders-albert <anders0albert@gmail.com>
2025-06-28 11:00:42 +0200

    refactor: stipulate new solution

--- cognite_toolkit/_cdf_tk/client/api/canvas.py
+++ cognite_toolkit/_cdf_tk/client/api/canvas.py
@@ -1,12 +1,16 @@
-from collections.abc import Iterable, Sequence
-from typing import Any, Literal, overload
+from collections.abc import Sequence
+from typing import Any, Literal
 
 from cognite.client._constants import DEFAULT_LIMIT_READ
 from cognite.client.data_classes.data_modeling import InstanceSort, NodeApplyResultList, NodeId, NodeList
 from cognite.client.data_classes.filters import Filter
 from cognite.client.utils.useful_types import SequenceNotStr
 
-from cognite_toolkit._cdf_tk.client.data_classes.canvas import CANVAS_INSTANCE_SPACE, Canvas, CanvasApply
+from cognite_toolkit._cdf_tk.client.data_classes.canvas import (
+    CANVAS_INSTANCE_SPACE,
+    Canvas,
+    CanvasApply,
+)
 
 from .extended_data_modeling import ExtendedInstancesAPI
 
@@ -24,41 +28,43 @@ class CanvasAPI:
         result = self._instance_api.delete([NodeId(self.instance_space, item) for item in external_ids])
         return [item.external_id for item in result.nodes]
 
-    @overload
     def retrieve(
         self, external_id: str | None = None, retrieve_connections: Literal["skip", "full"] = "skip"
-    ) -> Canvas | None: ...
+    ) -> Canvas | None:
+        """Retrieve a canvas with a given external ID.
 
-    @overload
-    def retrieve(
-        self, external_id: SequenceNotStr[str], retrieve_connections: Literal["skip", "full"] = "skip"
-    ) -> NodeList[Canvas]: ...
-
-    def retrieve(
-        self,
-        external_id: str | SequenceNotStr[str] | None = None,
-        retrieve_connections: Literal["skip", "full"] = "skip",
-    ) -> Canvas | NodeList[Canvas] | None:
-        """Retrieve a canvas or a list of canvases by their external IDs.
-
-        If the retrieve connections parameter is set to 'full', it will retrieve all connections associated with the canvases.
+        If the retrieve connections parameter is set to 'full', it will retrieve all connections associated with the Canvas will
+        be returned.
 
         Args:
             external_id (str | SequenceNotStr[str] | None): The external ID of the canvas or a sequence of external IDs.
             retrieve_connections (Literal["skip", "full"]): Whether to skip or retrieve full connections. Defaults to 'skip'.
 
         Returns:
-            Canvas | NodeList[Canvas] | None: A single Canvas object if a single external ID is provided, a NodeList of Canvas objects if multiple external IDs are provided, or None if no external ID is provided.
+            Canvas | NodeList[Canvas] | None: A single Canvas object if a single external ID is provided.
 
         """
-        if isinstance(external_id, str):
+        if retrieve_connections == "skip":
             return self._instance_api.retrieve_nodes(NodeId(self.instance_space, external_id), node_cls=Canvas)
-        elif isinstance(external_id, Iterable):
-            return self._instance_api.retrieve_nodes(
-                [NodeId(self.instance_space, item) for item in external_id], node_cls=Canvas
-            )
         else:
-            raise TypeError(f"Expected str or SequenceNotStr[str], got {type(external_id)}")
+            canvas_query = self._canvas_with_connections_query(external_id)
+            result = self._instance_api.query(canvas_query)
+            return Canvas._load_query(result)
+
+    def retrieve_multiple(self, external_ids: SequenceNotStr[str]) -> NodeList[Canvas]:
+        """Retrieve a list of canvases by their external IDs.
+
+
+        Args:
+            external_ids (SequenceNotStr[str]): The external IDs of the Canvases to retrieve.
+
+        Returns:
+            NodeList[Canvas]: A list of Canvas objects corresponding to the provided external IDs.
+
+        """
+        return self._instance_api.retrieve_nodes(
+            [NodeId(self.instance_space, external_id) for external_id in external_ids], node_cls=Canvas
+        )
 
     def list(
         self,
