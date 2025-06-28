844683fb39a49fbcecb41356cfbfae2dea28104f
anders-albert <anders0albert@gmail.com>
2025-06-28 12:14:07 +0200

    refactor: setup API

--- cognite_toolkit/_cdf_tk/client/api/canvas.py
+++ cognite_toolkit/_cdf_tk/client/api/canvas.py
@@ -2,11 +2,24 @@ from collections.abc import Iterable, Sequence
 from typing import Any, overload
 
 from cognite.client._constants import DEFAULT_LIMIT_READ
-from cognite.client.data_classes.data_modeling import InstanceSort, NodeApplyResultList, NodeId, NodeList
+from cognite.client.data_classes.data_modeling import (
+    InstancesDeleteResult,
+    InstanceSort,
+    NodeApplyResultList,
+    NodeId,
+    NodeList,
+)
 from cognite.client.data_classes.filters import Filter
 from cognite.client.utils.useful_types import SequenceNotStr
 
-from cognite_toolkit._cdf_tk.client.data_classes.canvas import CANVAS_INSTANCE_SPACE, Canvas, CanvasApply
+from cognite_toolkit._cdf_tk.client.data_classes.canvas import (
+    CANVAS_INSTANCE_SPACE,
+    Canvas,
+    CanvasApply,
+    IndustrialCanvas,
+    IndustrialCanvasApply,
+)
+from cognite_toolkit._cdf_tk.client.data_classes.instances import InstancesApplyResultList
 
 from .extended_data_modeling import ExtendedInstancesAPI
 
@@ -15,6 +28,7 @@ class CanvasAPI:
     def __init__(self, instance_api: ExtendedInstancesAPI) -> None:
         self._instance_api = instance_api
         self.instance_space = CANVAS_INSTANCE_SPACE
+        self.industrial = IndustrialCanvasAPI(instance_api)
 
     def upsert(self, canvas: CanvasApply | Sequence[CanvasApply]) -> NodeApplyResultList:
         return self._instance_api.apply(canvas).nodes
@@ -49,3 +63,17 @@ class CanvasAPI:
         return self._instance_api.list(
             instance_type=Canvas, space=self.instance_space, limit=limit, sort=sort, filter=filter
         )
+
+
+class IndustrialCanvasAPI:
+    def __init__(self, instance_api: ExtendedInstancesAPI) -> None:
+        self._instance_api = instance_api
+
+    def retrieve(self, external_id: str) -> IndustrialCanvas:
+        raise NotImplementedError()
+
+    def upsert(self, canvas: IndustrialCanvasApply) -> InstancesApplyResultList:
+        raise NotImplementedError()
+
+    def delete(self, canvas: IndustrialCanvasApply | IndustrialCanvas) -> InstancesDeleteResult:
+        raise NotImplementedError()
