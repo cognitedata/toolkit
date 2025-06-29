439536b8503492fc041352266a88dc21279984bb
anders-albert <anders0albert@gmail.com>
2025-06-29 17:30:06 +0200

    feat: activate command

--- cognite_toolkit/_cdf_tk/apps/_migrate_app.py
+++ cognite_toolkit/_cdf_tk/apps/_migrate_app.py
@@ -13,8 +13,7 @@ class MigrateApp(typer.Typer):
         self.callback(invoke_without_command=True)(self.main)
         self.command("prepare")(self.prepare)
         self.command("timeseries")(self.timeseries)
-        # Uncomment when the Canvas migration command is ready
-        # self.command("canvas")(self.canvas)
+        self.command("canvas")(self.canvas)
 
     def main(self, ctx: typer.Context) -> None:
         """Migrate resources from Asset-Centric to data modeling in CDF."""
