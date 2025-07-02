9571f62effc3f36076415d38f49475d9ea24c17e
anders-albert <anders0albert@gmail.com>
2025-07-02 14:55:07 +0200

    refactor: reuse base

--- cognite_toolkit/_cdf_tk/commands/_migrate/assets.py
+++ cognite_toolkit/_cdf_tk/commands/_migrate/assets.py
@@ -3,43 +3,41 @@ from pathlib import Path
 
 from cognite.client.data_classes import Asset, Label, LabelDefinition
 from cognite.client.data_classes.capabilities import (
+    AssetsAcl,
     Capability,
-    DataModelInstancesAcl,
-    DataModelsAcl,
     DataSetScope,
-    SpaceIDScope,
-    TimeSeriesAcl,
 )
 from cognite.client.data_classes.data_modeling import NodeApply, NodeOrEdgeData, ViewId
-from cognite.client.exceptions import CogniteAPIError, CogniteException
+from cognite.client.exceptions import CogniteException
 from rich import print
 
 from cognite_toolkit._cdf_tk.client import ToolkitClient
 from cognite_toolkit._cdf_tk.client._constants import DATA_MODELING_MAX_WRITE_WORKERS
-from cognite_toolkit._cdf_tk.commands._base import ToolkitCommand
-from cognite_toolkit._cdf_tk.constants import DMS_INSTANCE_LIMIT_MARGIN
 from cognite_toolkit._cdf_tk.exceptions import (
-    AuthenticationError,
     ResourceCreationError,
     ResourceRetrievalError,
-    ToolkitMigrationError,
-    ToolkitValueError,
 )
-from cognite_toolkit._cdf_tk.tk_warnings import HighSeverityWarning
-from cognite_toolkit._cdf_tk.utils import humanize_collection
 from cognite_toolkit._cdf_tk.utils.collection import chunker_sequence
 from cognite_toolkit._cdf_tk.utils.producer_worker import ProducerWorkerExecutor
 
+from .base import BaseMigrateCommand
 from .data_classes import MigrationMapping, MigrationMappingList
 from .data_model import MAPPING_VIEW_ID
 
 
-class MigrateAssetsCommand(ToolkitCommand):
+class MigrateAssetsCommand(BaseMigrateCommand):
     cdf_cdm = "cdf_cdm"
     asset_id = ViewId(cdf_cdm, "CogniteAsset", "v1")
 
     # This is the number of timeseries that can be written in parallel.
     chunk_size = 1000 * DATA_MODELING_MAX_WRITE_WORKERS
+
+    @property
+    def schema_spaces(self) -> list[str]:
+        return [f"{self.cdf_cdm}", MAPPING_VIEW_ID.space]
+
+    def source_acl(self, data_set_id: list[int]) -> Capability:
+        return AssetsAcl(actions=[AssetsAcl.Action.Read], scope=DataSetScope(data_set_id))
 
     def migrate_assets(
         self,
@@ -50,9 +48,9 @@ class MigrateAssetsCommand(ToolkitCommand):
     ) -> None:
         """Migrate resources from Asset-Centric to data modeling in CDF."""
         mappings = MigrationMappingList.read_mapping_file(mapping_file)
-        self._validate_access(client, mappings)
-        self._validate_migration_mappings_exists(client)
-        self._validate_available_capacity(client, mappings)
+        self._validate_access(client, list(mappings.spaces()), list(mappings.get_data_set_ids()))
+        self._validate_instance_source_exists(client)
+        self._validate_available_capacity(client, len(mappings))
         iteration_count = len(mappings) // self.chunk_size + 1
         executor = ProducerWorkerExecutor[list[tuple[Asset, MigrationMapping]], list[NodeApply]](
             download_iterable=self._download_assets(client, mappings),
@@ -70,61 +68,6 @@ class MigrateAssetsCommand(ToolkitCommand):
 
         prefix = "Would have" if dry_run else "Successfully"
         self.console(f"{prefix} migrated {executor.total_items:,} assets to CogniteAssets.")
-
-    def _validate_access(self, client: ToolkitClient, mappings: MigrationMappingList) -> None:
-        required_capabilities: list[Capability] = [
-            DataModelsAcl(
-                actions=[DataModelsAcl.Action.Read], scope=SpaceIDScope([self.cdf_cdm, MAPPING_VIEW_ID.space])
-            ),
-            DataModelInstancesAcl(
-                actions=[
-                    DataModelInstancesAcl.Action.Read,
-                    DataModelInstancesAcl.Action.Write,
-                    DataModelInstancesAcl.Action.Write_Properties,
-                ],
-                scope=SpaceIDScope(list(mappings.spaces())),
-            ),
-        ]
-        if data_set_ids := mappings.get_data_set_ids():
-            required_capabilities.append(
-                TimeSeriesAcl(
-                    actions=[TimeSeriesAcl.Action.Read, TimeSeriesAcl.Action.Write],
-                    scope=DataSetScope(list(data_set_ids)),
-                )
-            )
-        if missing := client.iam.verify_capabilities(required_capabilities):
-            raise AuthenticationError(f"Missing required capabilities: {humanize_collection(missing)}.")
-
-    def _validate_migration_mappings_exists(self, client: ToolkitClient) -> None:
-        view = client.data_modeling.views.retrieve(MAPPING_VIEW_ID)
-        if not view:
-            raise ToolkitMigrationError(
-                f"The migration mapping view {MAPPING_VIEW_ID} does not exist. "
-                f"Please run the `cdf migrate prepare` command to deploy the migration data model."
-            )
-
-    def _validate_available_capacity(self, client: ToolkitClient, mappings: MigrationMappingList) -> None:
-        """Validate that the project has enough capacity to accommodate the migration."""
-        try:
-            stats = client.data_modeling.statistics.project()
-        except CogniteAPIError:
-            # This endpoint is not yet in alpha, it may change or not be available.
-            self.warn(HighSeverityWarning("Cannot check the instances capacity proceeding with migration anyway."))
-            return
-        available_capacity = stats.instances.instances_limit - stats.instances.instances
-        available_capacity_after = available_capacity - len(mappings)
-
-        if available_capacity_after < DMS_INSTANCE_LIMIT_MARGIN:
-            raise ToolkitValueError(
-                "Cannot proceed with migration, not enough instance capacity available. Total capacity after migration"
-                f"would be {available_capacity_after:,} instances, which is less than the required margin of"
-                f"{DMS_INSTANCE_LIMIT_MARGIN:,} instances. Please increase the instance capacity in your CDF project"
-                f" or delete some existing instances before proceeding with the migration of {len(mappings):,} assets."
-            )
-        total_instances = stats.instances.instances + len(mappings)
-        self.console(
-            f"Project has enough capacity for migration. Total instances after migration: {total_instances:,}."
-        )
 
     def _download_assets(
         self, client: ToolkitClient, mappings: MigrationMappingList
