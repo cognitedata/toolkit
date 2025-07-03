d0fec92d5469dd037a640976f24544c1d4b6d566
Aashutosh-cognite <aashutosh.chaubey@cognite.com>
2025-07-03 12:49:55 +0530

    APIClient _get_base_url_with_base_path method override in SearchConfigurationAPI

--- cognite_toolkit/_cdf_tk/client/api/search_config.py
+++ cognite_toolkit/_cdf_tk/client/api/search_config.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from urllib.parse import urljoin
+
 from cognite.client import CogniteClient
 from cognite.client._api_client import APIClient
 from cognite.client.config import ClientConfig
@@ -10,11 +12,17 @@ from cognite_toolkit._cdf_tk.client.data_classes.search_config import SearchConf
 class SearchConfigurationsAPI(APIClient):
     def __init__(self, config: ClientConfig, api_version: str | None, cognite_client: CogniteClient) -> None:
         super().__init__(config, api_version, cognite_client)
-        # this is hopefully a very temporary fix to avoid the client concatinating the _RESOURCE_PATH with the ordinary /api/ path.
-        self._api_version = None
-        self._RESOURCE_PATH = (
-            f"/apps/v1/projects/{self._cognite_client.config.project}/storage/config/apps/search/views"
-        )
+
+    def _get_base_url_with_base_path(self) -> str:
+        """
+        This method is overridden to provide the correct base path for the Search Configurations API.
+        This method in base class APIClient appends /api/{api_version}/ to the base URL,
+        but for Search Configurations API, we need a different path structure.
+        """
+        base_path = ""
+        if self._api_version:
+            base_path = f"/apps/{self._api_version}/projects/{self._config.project}/storage/config/apps/search/views"
+        return urljoin(self._config.base_url, base_path)
 
     def upsert(self, configuration_update: SearchConfigWrite) -> SearchConfig:
         """Update/Create a Configuration.
@@ -27,7 +35,7 @@ class SearchConfigurationsAPI(APIClient):
 
         """
         res = self._post(
-            url_path=self._RESOURCE_PATH + "/upsert",
+            url_path="/upsert",
             json=configuration_update.dump(),
         )
         return SearchConfig._load(res.json(), cognite_client=self._cognite_client)
@@ -39,5 +47,5 @@ class SearchConfigurationsAPI(APIClient):
             SearchConfigList
 
         """
-        res = self._post(url_path=self._RESOURCE_PATH + "/list")
+        res = self._post(url_path="/list")
         return SearchConfigList._load(res.json()["items"], cognite_client=self._cognite_client)
