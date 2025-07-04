1e7531239c2a13305e54c22a6508bb4ae6d1b76e
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-07-04 10:18:38 +0200

    [CDF-25152]👷Refactor migration command for reuse (#1707)
    # Description
    
    Please describe the change you have made.
    
    ## Changelog
    
    - [ ] Patch
    - [ ] Minor
    - [x] Skip

---
+++ tests/test_unit/test_cdf_tk/test_commands/test_migrate.py
@@ -0,0 +1,131 @@
+from unittest.mock import MagicMock
+
+import pytest
+from cognite.client._api.iam import IAMAPI, ComparableCapability
+from cognite.client.data_classes.capabilities import (
+    Capability,
+    DataModelInstancesAcl,
+    DataModelsAcl,
+    DataSetScope,
+    ProjectCapabilityList,
+    TimeSeriesAcl,
+)
+from cognite.client.data_classes.data_modeling import ViewList
+from cognite.client.exceptions import CogniteAPIError
+
+from cognite_toolkit._cdf_tk.client.data_classes.statistics import InstanceCountsLimits, ProjectStatsAndLimits
+from cognite_toolkit._cdf_tk.client.testing import monkeypatch_toolkit_client
+from cognite_toolkit._cdf_tk.commands._migrate.base import BaseMigrateCommand
+from cognite_toolkit._cdf_tk.exceptions import AuthenticationError, ToolkitMigrationError, ToolkitValueError
+from cognite_toolkit._cdf_tk.tk_warnings import HighSeverityWarning
+
+
+class DummyMigrationCommand(BaseMigrateCommand):
+    """A dummy command for testing purposes."""
+
+    @property
+    def schema_spaces(self) -> list[str]:
+        return ["dummy_space"]
+
+    def source_acl(self, data_set_id: list[int]) -> TimeSeriesAcl:
+        return TimeSeriesAcl(
+            actions=[TimeSeriesAcl.Action.Read, TimeSeriesAcl.Action.Write],
+            scope=DataSetScope(data_set_id),
+        )
+
+
+class TestBaseCommand:
+    def test_validate_access(self) -> None:
+        cmd = DummyMigrationCommand(silent=True)
+        expected_missing = [
+            DataModelInstancesAcl(
+                actions=[
+                    DataModelInstancesAcl.Action.Read,
+                ],
+                scope=DataModelInstancesAcl.Scope.SpaceID(["my_instance_space"]),
+            ),
+            DataModelInstancesAcl(
+                actions=[
+                    DataModelInstancesAcl.Action.Write,
+                ],
+                scope=DataModelInstancesAcl.Scope.SpaceID(["my_instance_space"]),
+            ),
+            DataModelInstancesAcl(
+                actions=[
+                    DataModelInstancesAcl.Action.Write_Properties,
+                ],
+                scope=DataModelInstancesAcl.Scope.SpaceID(["my_instance_space"]),
+            ),
+            DataModelsAcl(actions=[DataModelsAcl.Action.Read], scope=DataModelsAcl.Scope.SpaceID(cmd.schema_spaces)),
+            TimeSeriesAcl(actions=[TimeSeriesAcl.Action.Read], scope=DataSetScope([123])),
+            TimeSeriesAcl(actions=[TimeSeriesAcl.Action.Write], scope=DataSetScope([123])),
+        ]
+
+        with monkeypatch_toolkit_client() as client:
+            existing_capabilities = ProjectCapabilityList([], cognite_client=client)
+
+            def verify_capabilities(desired_capabilities: ComparableCapability) -> list[Capability]:
+                return IAMAPI.compare_capabilities(
+                    existing_capabilities,
+                    desired_capabilities,
+                )
+
+            client.iam.verify_capabilities = verify_capabilities
+            with pytest.raises(AuthenticationError) as exc_info:
+                cmd.validate_access(client, ["my_instance_space"], [123])
+        error = exc_info.value
+        assert isinstance(error, AuthenticationError)
+        assert "Missing required capabilities" in error.args[0]
+        assert error.args[1] == expected_missing
+
+    def test_validate_instance_source_exists_raise(self) -> None:
+        with monkeypatch_toolkit_client() as client:
+            client.data_modeling.views.retrieve.return_value = ViewList([])
+            with pytest.raises(ToolkitMigrationError):
+                BaseMigrateCommand.validate_instance_source_exists(client)
+
+    def test_validate_available_capacity_endpoint_failing(self) -> None:
+        cmd = DummyMigrationCommand(silent=True)
+
+        with monkeypatch_toolkit_client() as client:
+            client.data_modeling.statistics.project.side_effect = CogniteAPIError("Error", 404, "Not Found")
+            cmd.validate_available_capacity(client, 10_000)
+
+        assert len(cmd.warning_list) == 1
+        warning = cmd.warning_list[0]
+        assert isinstance(warning, HighSeverityWarning)
+
+    def test_validate_available_capacity_missing_capacity(self) -> None:
+        cmd = DummyMigrationCommand(silent=True)
+
+        with monkeypatch_toolkit_client() as client:
+            stats = MagicMock(spec=ProjectStatsAndLimits)
+            stats.instances = InstanceCountsLimits(
+                nodes=1000,
+                edges=0,
+                soft_deleted_edges=0,
+                soft_deleted_nodes=0,
+                instances_limit=1500,
+                soft_deleted_instances_limit=10_000,
+            )
+            client.data_modeling.statistics.project.return_value = stats
+            with pytest.raises(ToolkitValueError) as exc_info:
+                cmd.validate_available_capacity(client, 10_000)
+
+        assert "Cannot proceed with migration" in str(exc_info.value)
+
+    def test_validate_available_capacity_sufficient_capacity(self) -> None:
+        cmd = DummyMigrationCommand(silent=True)
+
+        with monkeypatch_toolkit_client() as client:
+            stats = MagicMock(spec=ProjectStatsAndLimits)
+            stats.instances = InstanceCountsLimits(
+                nodes=1000,
+                edges=0,
+                soft_deleted_edges=0,
+                soft_deleted_nodes=0,
+                instances_limit=5_000_000,
+                soft_deleted_instances_limit=100_000_000,
+            )
+            client.data_modeling.statistics.project.return_value = stats
+            cmd.validate_available_capacity(client, 10_000)
