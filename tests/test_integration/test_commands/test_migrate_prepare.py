d50de5508df8a7afeaca98551e17d7791bd47493
anders-albert <anders0albert@gmail.com>
2025-07-04 11:25:02 +0200

    tests: improved tests

--- tests/test_integration/test_commands/test_migrate_prepare.py
+++ tests/test_integration/test_commands/test_migrate_prepare.py
@@ -1,5 +1,7 @@
 from cognite_toolkit._cdf_tk.client import ToolkitClient
 from cognite_toolkit._cdf_tk.commands import MigrationPrepareCommand
+from cognite_toolkit._cdf_tk.commands._migrate.data_model import CONTAINERS, VIEWS
+from cognite_toolkit._cdf_tk.data_classes import ResourceDeployResult
 
 
 class TestMigrateTimeSeriesCommand:
@@ -8,14 +10,21 @@ class TestMigrateTimeSeriesCommand:
         toolkit_client: ToolkitClient,
     ) -> None:
         cmd = MigrationPrepareCommand(silent=True, skip_tracking=True)
+        expected_resources = {"spaces": 1, "containers": len(CONTAINERS), "views": len(VIEWS), "data models": 1}
 
         dry_run_result = cmd.deploy_cognite_migration(toolkit_client, True, verbose=False)
+        actual_dry_run = {
+            key: result.total for key, result in dry_run_result.items() if isinstance(result, ResourceDeployResult)
+        }
+        assert actual_dry_run == expected_resources, "Dry run result does not match expected resources"
 
         actual_result = cmd.deploy_cognite_migration(
             toolkit_client,
             False,
             verbose=False,
         )
+        actual = {
+            key: result.total for key, result in actual_result.items() if isinstance(result, ResourceDeployResult)
+        }
 
-        assert dry_run_result.has_counts, "Dry run should have counts"
-        assert actual_result.has_counts, "Actual should have counts"
+        assert actual == expected_resources, "Actual result does not match expected resources"
