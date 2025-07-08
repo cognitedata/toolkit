cf3fc69e1f14f45e739576639a1441da8458bc6a
anders-albert <anders0albert@gmail.com>
2025-07-08 11:05:41 +0200

    tests: update test to include conversion

--- tests/test_integration/test_toolkit_client/test_migration.py
+++ tests/test_integration/test_toolkit_client/test_migration.py
@@ -3,6 +3,7 @@ from cognite.client.data_classes.data_modeling import NodeApply, NodeApplyList, 
 
 from cognite_toolkit._cdf_tk.client import ToolkitClient
 from cognite_toolkit._cdf_tk.client.data_classes.migration import InstanceSource
+from cognite_toolkit._cdf_tk.tk_warnings import IgnoredValueWarning, catch_warnings
 
 
 @pytest.fixture(scope="session")
@@ -18,6 +19,13 @@ def three_sources(toolkit_client: ToolkitClient, toolkit_space: Space) -> NodeLi
                         {
                             "resourceType": "asset",
                             "id": i,
+                            "preferredConsumerViewId": {
+                                "space": "cdf_cdm",
+                                "externalId": "CogniteAsset",
+                                "version": "v1",
+                            }
+                            if i < 2
+                            else {"invalid": "json"},
                         },
                     )
                 ],
@@ -34,9 +42,19 @@ def three_sources(toolkit_client: ToolkitClient, toolkit_space: Space) -> NodeLi
 
 
 class TestInstanceSourceAPI:
-    def test_retrieve_mappings(self, toolkit_client: ToolkitClient, three_sources) -> None:
+    def test_retrieve_mappings(self, toolkit_client: ToolkitClient, three_sources: NodeList[InstanceSource]) -> None:
         ids = [instance_source.as_asset_centric_id() for instance_source in three_sources]
 
-        retrieved = toolkit_client.migration.instance_source.retrieve(ids)
+        with catch_warnings(IgnoredValueWarning) as warnings:
+            retrieved = toolkit_client.migration.instance_source.retrieve(ids)
 
         assert retrieved.dump() == three_sources.dump(), "Failed to retrieve instance source using asset-centric IDs"
+        assert len(warnings) == 1, "Expected one warning about invalid JSON in preferredConsumerViewId"
+        has_preferred_consumer_view = [
+            item.as_id() for item in retrieved if item.preferred_consumer_view_id is not None
+        ]
+        assert has_preferred_consumer_view == [item.as_id() for item in three_sources[:2]]
+        no_preferred_consumer_view = [item.as_id() for item in retrieved if item.preferred_consumer_view_id is None]
+        assert no_preferred_consumer_view == [three_sources[2].as_id()], (
+            "Expected last item to have no preferred consumer view"
+        )
