0d55c82873ba6ac858dae8ec76652e200159b1be
anders-albert <anders0albert@gmail.com>
2025-07-14 13:08:52 +0200

    refactor; finished Search YAML

--- cognite_toolkit/_cdf_tk/resource_classes/search_config.py
+++ cognite_toolkit/_cdf_tk/resource_classes/search_config.py
@@ -6,7 +6,17 @@ class ViewId(BaseModelResource):
     external_id: str
 
 
+class PropertyConfig(BaseModelResource):
+    property: str
+    disabled: bool | None = None
+    selected: bool | None = None
+    hidden: bool | None = None
+
+
 class SearchConfigYAML(ToolkitResource):
     view: ViewId
     use_as_name: str | None = None
     use_as_description: str | None = None
+    column_layout: list[PropertyConfig] | None = None
+    filter_layout: list[PropertyConfig] | None = None
+    properties_layout: list[PropertyConfig] | None = None
