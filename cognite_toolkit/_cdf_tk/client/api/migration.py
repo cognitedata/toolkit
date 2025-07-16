7d969026adb9d072c08203b1bfcb74f808198aa1
anders-albert <anders0albert@gmail.com>
2025-07-16 08:24:48 +0200

    refactor; stipulate

--- cognite_toolkit/_cdf_tk/client/api/migration.py
+++ cognite_toolkit/_cdf_tk/client/api/migration.py
@@ -1,9 +1,15 @@
 from collections.abc import Sequence
 from itertools import groupby
 
-from cognite.client.data_classes.data_modeling import NodeList, filters, query
+from cognite.client.data_classes.data_modeling import NodeApplyResultList, NodeList, ViewId, filters, query
+from cognite.client.utils.useful_types import SequenceNotStr
 
-from cognite_toolkit._cdf_tk.client.data_classes.migration import AssetCentricId, InstanceSource
+from cognite_toolkit._cdf_tk.client.data_classes.migration import (
+    AssetCentricId,
+    InstanceSource,
+    ViewSource,
+    ViewSourceApply,
+)
 from cognite_toolkit._cdf_tk.utils.collection import chunker_sequence
 
 from .extended_data_modeling import ExtendedInstancesAPI
@@ -52,6 +58,35 @@ class InstanceSourceAPI:
             )
             to_or_filters.append(filters.And(is_resource, is_id))
         return filters.Or(*to_or_filters)
+
+
+class ViewSourceAPI:
+    """API for retrieving instance sources from the data model."""
+
+    def __init__(self, instance_api: ExtendedInstancesAPI) -> None:
+        self._instance_api = instance_api
+
+    def create(self, item: ViewSourceApply | Sequence[ViewSourceApply]) -> NodeApplyResultList:
+        """Create one or more view sources."""
+        raise NotImplementedError()
+
+    def retrieve(self, external_id: str | SequenceNotStr[str]) -> ViewSource | NodeList[ViewSource]:
+        """Retrieve one or more view sources by their external IDs."""
+        raise NotImplementedError()
+
+    def update(self, item: ViewSourceApply | Sequence[ViewSourceApply]) -> ViewSource | NodeList[ViewSource]:
+        """Update a view source or a list of view sources."""
+        raise NotImplementedError()
+
+    def delete(self, external_id: str | SequenceNotStr[str]) -> int:
+        """Delete a view source or a list of view sources by their external IDs."""
+        raise NotImplementedError()
+
+    def list(
+        self, resource_type: str | None = None, view: ViewId | Sequence[ViewId] | None = None, limit: int | None = 25
+    ) -> NodeList[ViewSource]:
+        """List view sources, optionally filtered by resource type and view."""
+        raise NotImplementedError()
 
 
 class MigrationAPI:
