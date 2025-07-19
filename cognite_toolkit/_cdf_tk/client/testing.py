8d9d6cfb74275b45d7c9323f4ddf0be7ecaf8fc1
anders-albert <anders0albert@gmail.com>
2025-07-19 17:09:45 +0200

    fix: update mocking

--- cognite_toolkit/_cdf_tk/client/testing.py
+++ cognite_toolkit/_cdf_tk/client/testing.py
@@ -3,12 +3,17 @@ from contextlib import contextmanager
 from typing import Any
 from unittest.mock import MagicMock
 
+from cognite.client._api.datapoints import DatapointsAPI
+from cognite.client._api.datapoints_subscriptions import DatapointsSubscriptionAPI
+from cognite.client._api.synthetic_time_series import SyntheticDatapointsAPI
 from cognite.client.testing import CogniteClientMock
 
 from cognite_toolkit._cdf_tk.client._toolkit_client import ToolkitClient
 
 from .api.canvas import CanvasAPI
 from .api.dml import DMLAPI
+from .api.extended_data_modeling import ExtendedInstancesAPI
+from .api.extended_timeseries import ExtendedTimeSeriesAPI
 from .api.location_filters import LocationFiltersAPI
 from .api.lookup import (
     AssetLookUpAPI,
@@ -71,6 +76,13 @@ class ToolkitClientMock(CogniteClientMock):
         self.robotics.frames = MagicMock(spec_set=FramesAPI)
         self.robotics.maps = MagicMock(spec_set=MapsAPI)
         self.robotics.capabilities = MagicMock(spec_set=CapabilitiesAPI)
+
+        self.data_modeling.instances = MagicMock(spec_set=ExtendedInstancesAPI)
+
+        self.time_series = MagicMock(spec=ExtendedTimeSeriesAPI)
+        self.time_series.data = MagicMock(spec=DatapointsAPI)
+        self.time_series.data.synthetic = MagicMock(spec_set=SyntheticDatapointsAPI)
+        self.time_series.subscriptions = MagicMock(spec_set=DatapointsSubscriptionAPI)
 
         # This is a helper API, not a real API.
         self.token = TokenAPI(self)
