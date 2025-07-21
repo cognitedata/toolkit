f514622b70099b2f6500eb70892c3ff38de8c12d
anders-albert <anders0albert@gmail.com>
2025-07-21 07:27:21 +0200

    tests: added more tsets

--- tests/test_unit/test_cdf_tk/test_utils/validate_access.py
+++ tests/test_unit/test_cdf_tk/test_utils/validate_access.py
@@ -1,5 +1,12 @@
 import pytest
-from cognite.client.data_classes.capabilities import Capability, DataModelsAcl, ProjectCapability, ProjectCapabilityList
+from cognite.client.data_classes.capabilities import (
+    Capability,
+    DataModelInstancesAcl,
+    DataModelsAcl,
+    ProjectCapability,
+    ProjectCapabilityList,
+    TimeSeriesAcl,
+)
 from cognite.client.data_classes.iam import TokenInspection
 
 from cognite_toolkit._cdf_tk.client.testing import monkeypatch_toolkit_client
@@ -64,6 +71,152 @@ class TestValidateAccess:
             client.iam.token.inspect.return_value = inspection
             validator = ValidateAccess(client, "test the operation")
             result = validator.data_model(["read"], space=space)
+            assert result == expected_result
+
+    @pytest.mark.parametrize(
+        "capabilities, space, expected_error",
+        [
+            pytest.param(
+                [],
+                None,
+                "You have no permission to read instances. This is required to test the operation.",
+                id="No capabilities",
+            ),
+            pytest.param(
+                [
+                    DataModelInstancesAcl(
+                        [DataModelInstancesAcl.Action.Read], DataModelInstancesAcl.Scope.SpaceID(["space1"])
+                    )
+                ],
+                "space2",
+                "You have no permission to read space2 space. This is required to test the operation instances.",
+                id="Space mismatch",
+            ),
+        ],
+    )
+    def test_instances_access_raise(
+        self, capabilities: list[Capability], space: str | None, expected_error: str
+    ) -> None:
+        inspection = self._create_inspection_obj(capabilities)
+
+        with monkeypatch_toolkit_client() as client:
+            client.iam.token.inspect.return_value = inspection
+            validator = ValidateAccess(client, "test the operation")
+            with pytest.raises(AuthorizationError) as exc:
+                validator.instances(["read"], space=space)
+            assert str(exc.value) == expected_error
+
+    @pytest.mark.parametrize(
+        "capabilities, space, expected_result",
+        [
+            pytest.param(
+                [
+                    DataModelInstancesAcl(
+                        [DataModelInstancesAcl.Action.Read], DataModelInstancesAcl.Scope.SpaceID(["space1"])
+                    )
+                ],
+                "space1",
+                None,
+                id="Space match",
+            ),
+            pytest.param(
+                [DataModelInstancesAcl([DataModelInstancesAcl.Action.Read], DataModelInstancesAcl.Scope.All())],
+                None,
+                None,
+                id="All scope",
+            ),
+            pytest.param(
+                [
+                    DataModelInstancesAcl(
+                        [DataModelInstancesAcl.Action.Read], DataModelInstancesAcl.Scope.SpaceID(["space1", "space2"])
+                    )
+                ],
+                None,
+                ["space1", "space2"],
+                id="Limited list of spaces",
+            ),
+        ],
+    )
+    def test_instances_access(
+        self, capabilities: list[Capability], space: str | None, expected_result: list[str] | None
+    ) -> None:
+        inspection = self._create_inspection_obj(capabilities)
+
+        with monkeypatch_toolkit_client() as client:
+            client.iam.token.inspect.return_value = inspection
+            validator = ValidateAccess(client, "test the operation")
+            result = validator.instances(["read"], space=space)
+            assert result == expected_result
+
+    @pytest.mark.parametrize(
+        "capabilities, dataset_id, expected_error",
+        [
+            pytest.param(
+                [],
+                None,
+                "You have no permission to read time series. This is required to test the operation.",
+                id="No capabilities",
+            ),
+            pytest.param(
+                [TimeSeriesAcl([TimeSeriesAcl.Action.Read], TimeSeriesAcl.Scope.DataSet([1]))],
+                2,
+                "You have no permission to read time series in dataset 2. This is required to test the operation.",
+                id="Dataset mismatch",
+            ),
+        ],
+    )
+    def test_timeseries_access_raise(
+        self, capabilities: list[Capability], dataset_id: int | None, expected_error: str
+    ) -> None:
+        inspection = self._create_inspection_obj(capabilities)
+
+        def external_id_lookup(ids: list[int]) -> list[str]:
+            return [str(id_) for id_ in ids]
+
+        with monkeypatch_toolkit_client() as client:
+            client.iam.token.inspect.return_value = inspection
+            client.lookup.data_sets.external_id.side_effect = external_id_lookup
+            client.lookup.assets.external_id.side_effect = external_id_lookup
+            client.lookup.time_series.external_id.side_effect = external_id_lookup
+
+            validator = ValidateAccess(client, "test the operation")
+            with pytest.raises(AuthorizationError) as exc:
+                validator.timeseries(["read"], dataset_id=dataset_id)
+            assert str(exc.value) == expected_error
+
+    @pytest.mark.parametrize(
+        "capabilities, dataset_id, expected_result",
+        [
+            pytest.param(
+                [TimeSeriesAcl([TimeSeriesAcl.Action.Read], TimeSeriesAcl.Scope.DataSet([1, 2]))],
+                1,
+                None,
+                id="Dataset match",
+            ),
+            pytest.param(
+                [TimeSeriesAcl([TimeSeriesAcl.Action.Read], TimeSeriesAcl.Scope.All())],
+                None,
+                None,
+                id="All scope",
+            ),
+        ],
+    )
+    def test_timeseries_access(
+        self, capabilities: list[Capability], dataset_id: int | None, expected_result: dict | None
+    ) -> None:
+        inspection = self._create_inspection_obj(capabilities)
+
+        def external_id_lookup(ids: list[int]) -> list[str]:
+            return [str(id_) for id_ in ids]
+
+        with monkeypatch_toolkit_client() as client:
+            client.iam.token.inspect.return_value = inspection
+            client.lookup.data_sets.external_id.side_effect = external_id_lookup
+            client.lookup.assets.external_id.side_effect = external_id_lookup
+            client.lookup.time_series.external_id.side_effect = external_id_lookup
+
+            validator = ValidateAccess(client, "test the operation")
+            result = validator.timeseries(["read"], dataset_id=dataset_id)
             assert result == expected_result
 
     @staticmethod
