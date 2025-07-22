b6ccdf4c157338b09c490bf3c964c025bb79d16c
anders-albert <anders0albert@gmail.com>
2025-07-22 12:57:38 +0200

    refactor: create implementation structure

--- cognite_toolkit/_cdf_tk/utils/dtype_conversion.py
+++ cognite_toolkit/_cdf_tk/utils/dtype_conversion.py
@@ -1,8 +1,18 @@
+from abc import ABC, abstractmethod
+from collections.abc import Mapping
+from typing import ClassVar
+
+from cognite.client.data_classes.data_modeling.containers import ContainerId
+from cognite.client.data_classes.data_modeling.data_types import ListablePropertyType
 from cognite.client.data_classes.data_modeling.instances import PropertyValueWrite
 from cognite.client.data_classes.data_modeling.views import MappedProperty
 
+_SPECIAL_CASES = {
+    (ContainerId("cdf_cdm", "CogniteTimeSeries"), "type"),
+}
 
-def convert_to_mapped_property(
+
+def convert_to_primary_property(
     value: str | int | float | bool | dict | list, prop: MappedProperty
 ) -> PropertyValueWrite:
     """Convert a string value to the appropriate type based on the provided property type.
@@ -14,4 +24,37 @@ def convert_to_mapped_property(
     Returns:
         PropertyValueWrite: The converted value as a PropertyValue, or None if is_nullable is True and the value is empty.
     """
-    raise NotImplementedError()
+    converter: type[_Converter]
+    if (prop.container, prop.container_property_identifier) in CONVERTER_BY_CONTAINER_PROPERTY:
+        converter = CONVERTER_BY_CONTAINER_PROPERTY[(prop.container, prop.container_property_identifier)]
+    elif prop.type._type in CONVERTER_BY_TYPE:
+        converter = CONVERTER_BY_TYPE[prop.type._type]
+    else:
+        raise TypeError(f"Unsupported property type {prop.type}")
+    if isinstance(prop.type, ListablePropertyType) and prop.type.is_list:
+        raise NotImplementedError(f"Listable property type {prop.type} is not supported")
+    return converter().convert(value)
+
+
+class _Converter(ABC):
+    @abstractmethod
+    def convert(self, value: str | int | float | bool | dict | list) -> PropertyValueWrite:
+        """Convert a value to the appropriate type."""
+        raise NotImplementedError("This method should be implemented by subclasses.")
+
+
+class _SpecialCaseConverter(_Converter, ABC):
+    """Abstract base class for converters handling special cases."""
+
+    container_property: ClassVar[tuple[ContainerId, str]]
+
+
+class _ValueConverter(_Converter, ABC):
+    type: ClassVar[str]
+
+
+CONVERTER_BY_TYPE: Mapping[str, type[_ValueConverter]] = {cls_.type: cls_ for cls_ in _ValueConverter.__subclasses__()}  # type: ignore[type-abstract]
+CONVERTER_BY_CONTAINER_PROPERTY: Mapping[tuple[ContainerId, str], type[_SpecialCaseConverter]] = {
+    cls_.container_property: cls_  # type: ignore[type-abstract]
+    for cls_ in _SpecialCaseConverter.__subclasses__()
+}
