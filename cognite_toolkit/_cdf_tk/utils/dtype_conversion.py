8ee30fdc19d3539b2baa654f598800f27cf81eec
anders-albert <anders0albert@gmail.com>
2025-07-22 10:57:40 +0200

    refactor: extend signature

--- cognite_toolkit/_cdf_tk/utils/dtype_conversion.py
+++ cognite_toolkit/_cdf_tk/utils/dtype_conversion.py
@@ -2,7 +2,9 @@ from cognite.client.data_classes.data_modeling.instances import PropertyValueWri
 from cognite.client.data_classes.data_modeling.views import MappedProperty
 
 
-def convert_to_mapped_property(value: str | int | float | bool, prop: MappedProperty) -> PropertyValueWrite:
+def convert_to_mapped_property(
+    value: str | int | float | bool | dict | list, prop: MappedProperty
+) -> PropertyValueWrite:
     """Convert a string value to the appropriate type based on the provided property type.
 
     Args:
