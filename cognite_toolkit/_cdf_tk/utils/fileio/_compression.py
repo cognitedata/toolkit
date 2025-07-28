87b3e297d0aa2aeb89a35650d1651621b3db0ca3
anders-albert <anders0albert@gmail.com>
2025-07-28 17:38:51 +0200

    feat: added compression classes

---
+++ cognite_toolkit/_cdf_tk/utils/fileio/_compression.py
@@ -0,0 +1,69 @@
+import gzip
+from abc import ABC, abstractmethod
+from collections.abc import Mapping
+from io import TextIOWrapper
+from pathlib import Path
+from typing import ClassVar, Literal
+
+from cognite_toolkit._cdf_tk.exceptions import ToolkitValueError
+from cognite_toolkit._cdf_tk.utils import humanize_collection
+from cognite_toolkit._cdf_tk.utils._auxillary import get_get_concrete_subclasses
+
+
+class Compression(ABC):
+    encoding = "utf-8"
+    newline = "\n"
+    name: ClassVar[str]
+    file_suffix: ClassVar[str]
+
+    def __init__(self, filepath: Path) -> None:
+        self.filepath = filepath
+
+    @abstractmethod
+    def open(self, mode: Literal["r", "w"]) -> TextIOWrapper:
+        """Open the compressed file and return a file-like object."""
+        raise NotImplementedError("This method should be implemented in subclasses.")
+
+    @classmethod
+    def from_filepath(cls, filepath: Path) -> "Compression":
+        if filepath.suffix in COMPRESSION_BY_SUFFIX:
+            return COMPRESSION_BY_SUFFIX[filepath.suffix](filepath=filepath)
+        return NoneCompression(filepath=filepath)
+
+    @classmethod
+    def from_name(cls, compression: str) -> "Compression":
+        if compression in COMPRESSION_BY_NAME:
+            return COMPRESSION_BY_NAME[compression](filepath=Path("dummy"))
+        raise ToolkitValueError(
+            f"Unknown compression type: {compression}. Available types: {humanize_collection(COMPRESSION_BY_NAME.keys())}."
+        )
+
+
+class NoneCompression(Compression):
+    name = "none"
+    file_suffix = ""
+
+    def open(self, mode: Literal["r", "w"]) -> TextIOWrapper:
+        """Open the file without compression."""
+        return self.filepath.open(mode=mode, encoding=self.encoding, newline=self.newline)
+
+
+class GzipCompression(Compression):
+    name = "gzip"
+    file_suffix = ".gz"
+
+    def open(self, mode: Literal["r", "w"]) -> TextIOWrapper:
+        """Open the gzip compressed file."""
+        # MyPy (or gzip) fails to recognize that gzip.open returns a TextIOWrapper
+        return gzip.open(self.filepath, mode=f"{mode}t", encoding=self.encoding, newline=self.newline)  # type: ignore[return-value]
+
+
+COMPRESSION_BY_SUFFIX: Mapping[str, type[Compression]] = {
+    subclass.file_suffix: subclass
+    for subclass in get_get_concrete_subclasses(Compression)  # type: ignore[type-abstract]
+}
+
+COMPRESSION_BY_NAME: Mapping[str, type[Compression]] = {
+    subclass.name: subclass
+    for subclass in get_get_concrete_subclasses(Compression)  # type: ignore[type-abstract]
+}
