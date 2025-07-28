b6e3b6b2a896d4836a8e38979279a8bf5d343240
anders-albert <anders0albert@gmail.com>
2025-07-28 17:47:11 +0200

    tests: added test battery

---
+++ tests/test_unit/test_cdf_tk/test_utils/test_fileio.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+import pytest
+
+from cognite_toolkit._cdf_tk.utils.fileio import COMPRESSION_BY_NAME, COMPRESSION_BY_SUFFIX, Compression
+
+
+class TestCompression:
+    @pytest.mark.parametrize(
+        "compression_name",
+        list(COMPRESSION_BY_NAME.keys()),
+    )
+    def test_read_write_compression_by_name(self, compression_name: str, tmp_path: Path) -> None:
+        compression = Compression.from_name(compression_name)
+        tmp_path = tmp_path / f"test_file.txt{compression.file_suffix}"
+        with compression(tmp_path).open(mode="w") as file:
+            file.write("Test content")
+
+        with compression(tmp_path).open("r") as file:
+            content = file.read()
+
+        assert content == "Test content"
+
+    @pytest.mark.parametrize(
+        "compression_suffix",
+        list(COMPRESSION_BY_SUFFIX.keys()),
+    )
+    def test_read_write_compression_by_suffix(self, compression_suffix: str, tmp_path: Path) -> None:
+        tmp_path = tmp_path / f"test_file.txt{compression_suffix}"
+        with Compression.from_filepath(tmp_path).open(mode="w") as file:
+            file.write("Test content")
+
+        with Compression.from_filepath(tmp_path).open("r") as file:
+            content = file.read()
+
+        assert content == "Test content"
