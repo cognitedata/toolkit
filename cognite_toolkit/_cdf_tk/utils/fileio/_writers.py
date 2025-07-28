7be6f78360945bbfffec57bc9d400f5494b816bc
anders-albert <anders0albert@gmail.com>
2025-07-28 17:56:43 +0200

    feat: added base write class

---
+++ cognite_toolkit/_cdf_tk/utils/fileio/_writers.py
@@ -0,0 +1,112 @@
+import sys
+from abc import ABC, abstractmethod
+from collections import Counter
+from collections.abc import Iterable, Mapping
+from pathlib import Path
+from typing import Any, Generic
+
+from cognite_toolkit._cdf_tk.exceptions import ToolkitValueError
+from cognite_toolkit._cdf_tk.utils._auxillary import get_get_concrete_subclasses
+from cognite_toolkit._cdf_tk.utils.collection import humanize_collection
+from cognite_toolkit._cdf_tk.utils.file import to_directory_compatible
+
+from ._base import T_IO, Chunk, FileIO
+from ._compression import Compression
+
+if sys.version_info >= (3, 11):
+    from typing import Self
+else:
+    from typing_extensions import Self
+
+
+class FileWriter(FileIO, ABC, Generic[T_IO]):
+    def __init__(
+        self, output_dir: Path, kind: str, compression: type[Compression], max_file_size_bytes: int = 128 * 1024 * 1024
+    ) -> None:
+        self.output_dir = output_dir
+        self.kind = kind
+        self.compression_cls = compression
+        self.max_file_size_bytes = max_file_size_bytes
+        self._file_count_by_filename: dict[str, int] = Counter()
+        self._writer_by_filepath: dict[Path, T_IO] = {}
+
+    def write_chunks(self, chunk: Iterable[Chunk], filename: str = "") -> None:
+        filepath = self._get_filepath(filename)
+        writer = self._get_writer(filepath, filename)
+        self._write(writer, chunk)
+
+    def _get_filepath(self, filename: str) -> Path:
+        clean_name = f"{to_directory_compatible(filename)}-" if filename else ""
+        file_count = self._file_count_by_filename[filename]
+        file_path = (
+            self.output_dir
+            / f"{clean_name}part-{file_count:04}.{self.kind}.{self.format}{self.compression_cls.file_suffix}"
+        )
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+        return file_path
+
+    def _get_writer(self, filepath: Path, filename_base: str) -> T_IO:
+        if filepath not in self._writer_by_filepath:
+            self._writer_by_filepath[filepath] = self._create_writer(filepath)
+        elif self._is_above_file_size_limit(filepath, self._writer_by_filepath[filepath]):
+            self._writer_by_filepath[filepath].close()
+            del self._writer_by_filepath[filepath]
+            self._file_count_by_filename[filename_base] += 1
+            new_filepath = self._get_filepath(filename_base)
+            return self._get_writer(new_filepath, filename_base)
+        return self._writer_by_filepath[filepath]
+
+    def __enter__(self) -> Self:
+        self._file_count_by_filename.clear()
+        self._writer_by_filepath.clear()
+        return self
+
+    def __exit__(self, exc_type: type[BaseException] | None, exc_val: BaseException | None, exc_tb: Any | None) -> None:
+        for writer in self._writer_by_filepath.values():
+            writer.close()
+        self._writer_by_filepath.clear()
+        self._file_count_by_filename.clear()
+        return None
+
+    def _is_above_file_size_limit(self, filepath: Path, writer: T_IO) -> bool:
+        """Check if the file size is above the limit."""
+        try:
+            current_position = writer.tell()
+            writer.seek(0, 2)
+            if writer.tell() > self.max_file_size_bytes:
+                return True
+            writer.seek(current_position)
+            return False
+        except (AttributeError, ValueError):
+            return filepath.exists() and filepath.stat().st_size > self.max_file_size_bytes
+
+    @abstractmethod
+    def _create_writer(self, filepath: Path) -> T_IO:
+        """Create a writer for the given file path."""
+        raise NotImplementedError("This method should be implemented in subclasses.")
+
+    @abstractmethod
+    def _write(self, writer: T_IO, chunks: Iterable[Chunk]) -> None:
+        """Write the chunk to the file."""
+        raise NotImplementedError("This method should be implemented in subclasses.")
+
+    @classmethod
+    def create_from_format(
+        cls,
+        format: str,
+        output_dir: Path,
+        kind: str,
+        compression: type[Compression],
+    ) -> "FileWriter":
+        if format in FILE_WRITE_CLS_BY_FORMAT:
+            file_writs_cls = FILE_WRITE_CLS_BY_FORMAT[format]
+            return file_writs_cls(output_dir=output_dir, kind=kind, compression=compression)
+        raise ToolkitValueError(
+            f"Unknown file format: {format}. Available formats: {humanize_collection(FILE_WRITE_CLS_BY_FORMAT.keys())}."
+        )
+
+
+FILE_WRITE_CLS_BY_FORMAT: Mapping[str, type[FileWriter]] = {
+    subclass.format: subclass
+    for subclass in get_get_concrete_subclasses(FileWriter)  # type: ignore[type-abstract]
+}
