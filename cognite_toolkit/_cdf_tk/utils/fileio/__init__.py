786072affc6713c79cb69199b8fec1a735d89452
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-08-08 20:58:04 +0200

    [CDF-25329] 🧑‍💻 FileIO Read base class  (#1799)

--- cognite_toolkit/_cdf_tk/utils/fileio/__init__.py
+++ cognite_toolkit/_cdf_tk/utils/fileio/__init__.py
@@ -1,3 +1,4 @@
+from ._base import CellValue, Chunk, DataType, PrimaryCellValue
 from ._compression import (
     COMPRESSION_BY_NAME,
     COMPRESSION_BY_SUFFIX,
@@ -5,11 +6,21 @@ from ._compression import (
     GzipCompression,
     NoneCompression,
 )
+from ._readers import (
+    FILE_READ_CLS_BY_FORMAT,
+    FileReader,
+)
 
 __all__ = [
     "COMPRESSION_BY_NAME",
     "COMPRESSION_BY_SUFFIX",
+    "FILE_READ_CLS_BY_FORMAT",
+    "CellValue",
+    "Chunk",
     "Compression",
+    "DataType",
+    "FileReader",
     "GzipCompression",
     "NoneCompression",
+    "PrimaryCellValue",
 ]
