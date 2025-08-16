2580827dd66815e4a5155c42cb5b5c21dff07495
anders-albert <anders0albert@gmail.com>
2025-08-16 07:42:03 +0200

    refactor: add upload CLI interface

---
+++ cognite_toolkit/_cdf_tk/apps/_upload_app.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+from typing import Annotated, Any
+
+import typer
+from rich import print
+
+from cognite_toolkit._cdf_tk.commands import UploadCommand
+from cognite_toolkit._cdf_tk.storageio import RawIO
+from cognite_toolkit._cdf_tk.utils.auth import EnvironmentVariables
+
+
+class UploadApp(typer.Typer):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.callback(invoke_without_command=True)(self.upload_main)
+        self.command("raw")(self.upload_raw_cmd)
+
+    @staticmethod
+    def upload_main(ctx: typer.Context) -> None:
+        """Commands to upload data to CDF."""
+        if ctx.invoked_subcommand is None:
+            print("Use [bold yellow]cdf download --help[/] for more information.")
+        return None
+
+    @staticmethod
+    def upload_raw_cmd(
+        ctx: typer.Context,
+        input_dir: Annotated[
+            Path,
+            typer.Argument(
+                help="The directory containing the RAW tables to upload. "
+                "If not specified, the current working directory will be used.",
+                exists=True,
+                file_okay=False,
+                dir_okay=True,
+                resolve_path=True,
+            ),
+        ],
+        verbose: Annotated[
+            bool,
+            typer.Option(
+                "--verbose",
+                "-v",
+                help="Turn on to get more verbose output when running the command",
+            ),
+        ] = False,
+    ) -> None:
+        """This command will download RAW tables from CDF into a temporary directory."""
+        cmd = UploadCommand()
+
+        client = EnvironmentVariables.create_from_environment().get_client()
+        cmd.run(
+            lambda: cmd.upload(
+                io=RawIO(client=client),
+                input_dir=input_dir,
+                verbose=verbose,
+            )
+        )
