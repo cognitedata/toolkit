6d3d7b436aefa8a46e52943f26a2505d0d22fb7a
anders-albert <anders0albert@gmail.com>
2025-08-18 15:39:01 +0200

    tests: added download upload test

--- tests/test_unit/test_cdf_tk/test_storageio/test_asset_centric.py
+++ tests/test_unit/test_cdf_tk/test_storageio/test_asset_centric.py
@@ -1,7 +1,18 @@
+from pathlib import Path
+
 import pytest
-from cognite.client.data_classes import Asset, AssetList, AssetWriteList
+from cognite.client.data_classes import (
+    Asset,
+    AssetList,
+    AssetWriteList,
+    DataSet,
+    DataSetList,
+    LabelDefinition,
+    LabelDefinitionList,
+)
 
 from cognite_toolkit._cdf_tk.client.testing import monkeypatch_toolkit_client
+from cognite_toolkit._cdf_tk.commands import DownloadCommand, UploadCommand
 from cognite_toolkit._cdf_tk.storageio import AssetIO
 from cognite_toolkit._cdf_tk.storageio._selectors import AssetCentricData
 from cognite_toolkit._cdf_tk.utils.collection import chunker
@@ -20,7 +31,7 @@ def some_asset_data() -> AssetList:
                 root_id=123,
                 source="test_source",
                 labels=["my_label"],
-                data_set_id=123,
+                data_set_id=1234,
             )
             for i in range(100)
         ]
@@ -34,7 +45,7 @@ class TestAssetIO:
             client.assets.return_value = chunker(some_asset_data, 10)
             client.assets.aggregate_count.return_value = 100
             client.lookup.data_sets.external_id.return_value = "test_data_set"
-            client.lookup.data_sets.id.return_value = 123
+            client.lookup.data_sets.id.return_value = 1234
 
             io = AssetIO(client)
 
@@ -62,3 +73,44 @@ class TestAssetIO:
                 uploaded_assets.extend(call[0][0])
 
             assert uploaded_assets.dump() == some_asset_data.as_write().dump()
+
+    def test_download_upload_command(self, some_asset_data: AssetList, tmp_path: Path) -> None:
+        selector = AssetCentricData(data_set_external_id=None, hierarchy="test_hierarchy")
+        with monkeypatch_toolkit_client() as client:
+            client.assets.return_value = [some_asset_data]
+            client.assets.aggregate_count.return_value = 100
+            client.lookup.data_sets.external_id.return_value = "test_data_set"
+            client.lookup.data_sets.id.return_value = 1234
+            client.lookup.assets.external_id.return_value = "test_hierarchy"
+            client.lookup.assets.id.return_value = 123
+            client.data_sets.retrieve_multiple.return_value = DataSetList(
+                [DataSet(id=1234, external_id="test_data_set")]
+            )
+            client.labels.retrieve.return_value = LabelDefinitionList(
+                [LabelDefinition(external_id="test_label", name="test_label")]
+            )
+
+            io = AssetIO(client)
+
+            download_command = DownloadCommand(silent=True, skip_tracking=True)
+            upload_command = UploadCommand(silent=True, skip_tracking=True)
+
+            download_command.download(
+                selectors=[selector],
+                io=io,
+                output_dir=tmp_path,
+                verbose=False,
+                file_format=".csv",
+                compression="none",
+                limit=100,
+            )
+
+            upload_command.upload(
+                io=io,
+                input_dir=tmp_path / io.folder_name,
+                ensure_configurations=True,
+                dry_run=False,
+                verbose=False,
+            )
+
+            assert client.assets.create.call_count == 1
