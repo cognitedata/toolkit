660cf56e9f4bd34d85dfeed2bc190244a6f93dc6
anders-albert <anders0albert@gmail.com>
2025-09-02 21:19:33 +0200

    tests: started on test cases

---
+++ tests/test_unit/test_cdf_tk/test_resource_classes/test_datapoint_subscription_yaml.py
@@ -0,0 +1,32 @@
+from collections.abc import Iterable
+from pathlib import Path
+
+import pytest
+
+from cognite_toolkit._cdf_tk.resource_classes.datapoint_subscription import DatapointSubscriptionYAML
+from cognite_toolkit._cdf_tk.tk_warnings.fileread import ResourceFormatWarning
+from cognite_toolkit._cdf_tk.validation import validate_resource_yaml_pydantic
+from tests.test_unit.utils import find_resources
+
+
+def invalid_datapoint_subscription_test_cases() -> Iterable:
+    yield pytest.param(
+        {"name": "Subscription 1"}, {"Missing required field: 'externalId'"}, id="Missing required field: externalId"
+    )
+
+
+class TestDatapointSubscription:
+    @pytest.mark.parametrize("data", list(find_resources("DatapointSubscription")))
+    def test_load_valid_dataset(self, data: dict[str, object]) -> None:
+        loaded = DatapointSubscriptionYAML.model_validate(data)
+
+        assert loaded.model_dump(exclude_unset=True, by_alias=True) == data
+
+    @pytest.mark.parametrize("data, expected_errors", list(invalid_datapoint_subscription_test_cases()))
+    def test_invalid_asset_error_messages(self, data: dict | list, expected_errors: set[str]) -> None:
+        warning_list = validate_resource_yaml_pydantic(data, DatapointSubscriptionYAML, Path("some_file.yaml"))
+        assert len(warning_list) == 1
+        format_warning = warning_list[0]
+        assert isinstance(format_warning, ResourceFormatWarning)
+
+        assert set(format_warning.errors) == expected_errors
