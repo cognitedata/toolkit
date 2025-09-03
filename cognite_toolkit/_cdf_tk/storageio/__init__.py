8b6efc5f7ee9f5034d7d390ef860d56911567562
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-09-03 15:48:34 +0200

    [CDF-25371] Instance IO implementation (download part) (#1875)
    # Description
    
    Stacked on #1876
    
    **Context** StorageIO is an interface for downloading and uploading data
    in Toolkit. This is intended to be used with multiple commands
    download/upload/purge/migration.
    
    This PR implements the download part of the interface for instances.
    This is because this is all we need right now for the purge command. And
    additional benefit is that this PR becomes very light to review.
    
    ## Changelog
    
    - [ ] Patch
    - [x] Skip

--- cognite_toolkit/_cdf_tk/storageio/__init__.py
+++ cognite_toolkit/_cdf_tk/storageio/__init__.py
@@ -1,7 +1,15 @@
 from ._asset_centric import AssetIO
 from ._base import StorageIO, TableStorageIO
+from ._instances import InstanceIO
 from ._raw import RawIO
-from ._selectors import AssetCentricFileSelector, AssetCentricSelector, AssetSubtreeSelector, DataSetSelector
+from ._selectors import (
+    AssetCentricFileSelector,
+    AssetCentricSelector,
+    AssetSubtreeSelector,
+    DataSetSelector,
+    InstanceSelector,
+    InstanceViewSelector,
+)
 
 __all__ = [
     "AssetCentricFileSelector",
@@ -9,6 +17,9 @@ __all__ = [
     "AssetIO",
     "AssetSubtreeSelector",
     "DataSetSelector",
+    "InstanceIO",
+    "InstanceSelector",
+    "InstanceViewSelector",
     "RawIO",
     "StorageIO",
     "TableStorageIO",
