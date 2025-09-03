e26d2ac15267d32cd6215a65ffbcd1e5aadf5cd3
anders-albert <anders0albert@gmail.com>
2025-09-03 11:20:32 +0200

    refactor: implemented interface

--- cognite_toolkit/_cdf_tk/commands/_migrate/data_mapper.py
+++ cognite_toolkit/_cdf_tk/commands/_migrate/data_mapper.py
@@ -1,4 +1,29 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from typing import Generic
+
+from cognite_toolkit._cdf_tk.storageio._base import T_CogniteResourceList, T_Selector, T_WritableCogniteResourceList
 
 
-class DataMapper(ABC): ...
+class DataMapper(Generic[T_Selector, T_WritableCogniteResourceList, T_CogniteResourceList], ABC):
+    def prepare(self, source_selector: T_Selector) -> None:
+        """Prepare the data mapper with the given source selector.
+
+        Args:
+            source_selector: The selector for the source data.
+
+        """
+        # Override in subclass if needed.
+        pass
+
+    @abstractmethod
+    def map_chunk(self, source: T_WritableCogniteResourceList) -> T_CogniteResourceList:
+        """Map a chunk of source data to the target format.
+
+        Args:
+            source: The source data chunk to be mapped.
+
+        Returns:
+            The mapped data chunk in the target format.
+
+        """
+        raise NotImplementedError("Subclasses must implement this method.")
