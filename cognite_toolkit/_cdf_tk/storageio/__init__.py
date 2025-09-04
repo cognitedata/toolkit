81400833a2291936096ea5e6f78fe8083247084d
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-09-04 09:51:22 +0200

    [CDF-25640] 😄Chart StoreageIO (#1888)
    # Description
    
    This PR introduces Chart implementation of the StoreageIO interface.
    This is a prerequisite for adding Chart upload/download command, and it
    will likely be used in a `cdf migrate charts` command.
    
    ## Changelog
    
    - [ ] Patch
    - [x] Skip

--- cognite_toolkit/_cdf_tk/storageio/__init__.py
+++ cognite_toolkit/_cdf_tk/storageio/__init__.py
@@ -1,21 +1,31 @@
+from ._applications import ChartIO
 from ._asset_centric import AssetIO
 from ._base import StorageIO, TableStorageIO
 from ._instances import InstanceIO
 from ._raw import RawIO
 from ._selectors import (
+    AllChartSelector,
     AssetCentricFileSelector,
     AssetCentricSelector,
     AssetSubtreeSelector,
+    ChartFileSelector,
+    ChartOwnerSelector,
+    ChartSelector,
     DataSetSelector,
     InstanceSelector,
     InstanceViewSelector,
 )
 
 __all__ = [
+    "AllChartSelector",
     "AssetCentricFileSelector",
     "AssetCentricSelector",
     "AssetIO",
     "AssetSubtreeSelector",
+    "ChartFileSelector",
+    "ChartIO",
+    "ChartOwnerSelector",
+    "ChartSelector",
     "DataSetSelector",
     "InstanceIO",
     "InstanceSelector",
