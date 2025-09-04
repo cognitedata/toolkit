65e87748c8ca41bc891e29cf67593f96319f74a7
anders-albert <anders0albert@gmail.com>
2025-09-04 22:21:25 +0200

    refactor: review feedback

--- tests/test_unit/test_cdf_tk/test_resource_classes/test_transformation_notification.py
+++ tests/test_unit/test_cdf_tk/test_resource_classes/test_transformation_notification.py
@@ -8,7 +8,7 @@ from cognite_toolkit._cdf_tk.validation import validate_resource_yaml_pydantic
 from tests.test_unit.utils import find_resources
 
 
-class TestTransformationScheduleYAML:
+class TestTransformationNotificationYAML:
     @pytest.mark.parametrize("data", list(find_resources("Notification", "transformations")))
     def test_load_valid_transformation_notification(self, data: dict[str, object]) -> None:
         loaded = TransformationNotificationYAML.model_validate(data)
@@ -22,12 +22,27 @@ class TestTransformationScheduleYAML:
                 {"destination": "example@email.com"},
                 {"Missing required field: 'transformationExternalId'"},
                 id="missing_required_field",
-            )
+            ),
+            pytest.param(
+                {"transformationExternalId": 123, "destination": "example@emai.com"},
+                {
+                    "In field transformationExternalId input should be a valid string. Got 123 of "
+                    "type int. Hint: Use double quotes to force string."
+                },
+                id="wrong_type_field",
+            ),
+            pytest.param(
+                {
+                    "destination": "example@email.com",
+                    "transformationId": 123,
+                    "transformationExternalId": "ext-123",
+                },
+                {"Unused field: 'transformationId'"},
+                id="Specifying transformationId",
+            ),
         ],
     )
-    def test_load_invalid_transformation_notification(
-        self, data: dict[str, object], expected_errors: list[str]
-    ) -> None:
+    def test_load_invalid_transformation_notification(self, data: dict[str, object], expected_errors: set[str]) -> None:
         warning_list = validate_resource_yaml_pydantic(data, TransformationNotificationYAML, Path("some_file.yaml"))
         assert len(warning_list) == 1
         format_warning = warning_list[0]
