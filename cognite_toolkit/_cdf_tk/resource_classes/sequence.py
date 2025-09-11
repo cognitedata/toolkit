b86bfef5695fe8bcfe217c8f7355e3a57da1c384
Aashutosh-cognite <aashutosh.chaubey@cognite.com>
2025-09-11 14:54:50 +0530

    [CDF-24814] Pydantic class for sequence

---
+++ cognite_toolkit/_cdf_tk/resource_classes/sequence.py
@@ -0,0 +1,64 @@
+from typing import Literal
+
+from pydantic import Field
+
+from .base import BaseModelResource, ToolkitResource
+
+
+class SequenceColumnDTO(BaseModelResource):
+    external_id: str = Field(
+        description="The external ID of the column.",
+        max_length=255,
+    )
+    name: str | None = Field(
+        default=None,
+        description="The name of the column.",
+        max_length=255,
+    )
+    description: str | None = Field(
+        default=None,
+        description="The description of the column.",
+        max_length=1000,
+    )
+    metadata: dict[str, str] | None = Field(
+        default=None,
+        description="Custom, application-specific metadata.",
+        max_length=256,
+    )
+    value_type: Literal["STRING", "DOUBLE", "LONG"] | None = Field(
+        default=None,
+        description="What type the datapoints in a column will have.",
+    )
+
+
+class SequenceYAML(ToolkitResource):
+    external_id: str = Field(
+        description="The external ID provided by the client.",
+        max_length=255,
+    )
+    name: str | None = Field(
+        default=None,
+        description="The name of the sequence.",
+        max_length=255,
+    )
+    description: str | None = Field(
+        default=None,
+        description="The description of the sequence.",
+        max_length=1000,
+    )
+    data_set_external_id: str | None = Field(
+        default=None,
+        description="The external ID of the data set that the sequence associated with.",
+    )
+    asset_external_id: str | None = Field(
+        default=None,
+        description="The external ID of the asset that the sequence associated with.",
+    )
+    metadata: dict[str, str] | None = Field(
+        default=None,
+        description="Custom, application-specific metadata.",
+        max_length=256,
+    )
+    columns: list[SequenceColumnDTO] = Field(
+        description="List of column definitions.",
+    )
