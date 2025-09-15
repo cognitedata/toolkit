ef26a009efd38ba5c38cfbfcadda0d7d24c48bb1
anders-albert <anders0albert@gmail.com>
2025-09-15 08:58:35 +0200

    tests: added test

---
+++ tests/test_unit/test_cdf_tk/test_resource_classes/test_hosted_extractor_jobs.py
@@ -0,0 +1,42 @@
+from collections.abc import Iterable
+from pathlib import Path
+
+import pytest
+
+from cognite_toolkit._cdf_tk.resource_classes.hosted_extractor_job import HostedExtractorJobYAML
+from cognite_toolkit._cdf_tk.tk_warnings.fileread import ResourceFormatWarning
+from cognite_toolkit._cdf_tk.validation import validate_resource_yaml_pydantic
+from tests.test_unit.utils import find_resources
+
+
+def invalid_hosted_extractor_job_test_cases() -> Iterable:
+    yield pytest.param(
+        {
+            "externalId": "myJob",
+            "destinationId": "myDest",
+            "sourceId": "mySource",
+            "format": {"type": "invalid", "some_field": "some_value"},
+            "config": {"topicFilter": "some_filter"},
+        },
+        {
+            "In field format invalid type 'invalid'. Expected one of cognite, custom, rockwell or value",
+        },
+        id="Invalid type",
+    )
+
+
+class TestHostedExtractorJobYAML:
+    @pytest.mark.parametrize("data", list(find_resources("Job", resource_dir="hosted_extractors")))
+    def test_load_valid_hosted_extractor_job(self, data: dict[str, object]) -> None:
+        loaded = HostedExtractorJobYAML.model_validate(data)
+
+        assert loaded.model_dump(exclude_unset=True, by_alias=True) == data
+
+    @pytest.mark.parametrize("data, expected_errors", list(invalid_hosted_extractor_job_test_cases()))
+    def test_invalid_hosted_extractor_job_error_messages(self, data: dict | list, expected_errors: set[str]) -> None:
+        warning_list = validate_resource_yaml_pydantic(data, HostedExtractorJobYAML, Path("some_file.yaml"))
+        assert len(warning_list) == 1
+        format_warning = warning_list[0]
+        assert isinstance(format_warning, ResourceFormatWarning)
+
+        assert set(format_warning.errors) == expected_errors
