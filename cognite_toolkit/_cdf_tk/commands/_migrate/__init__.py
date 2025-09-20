4729a8156bb3db86490fef0cccd6fd3123723afb
anders-albert <anders0albert@gmail.com>
2025-09-20 09:08:56 +0200

    refactor: remove references

--- cognite_toolkit/_cdf_tk/commands/_migrate/__init__.py
+++ cognite_toolkit/_cdf_tk/commands/_migrate/__init__.py
@@ -1,4 +1,3 @@
-from .assets import MigrateAssetsCommand
 from .canvas import MigrationCanvasCommand
 from .command import MigrationCommand
 from .files import MigrateFilesCommand
@@ -6,7 +5,6 @@ from .prepare import MigrationPrepareCommand
 from .timeseries import MigrateTimeseriesCommand
 
 __all__ = [
-    "MigrateAssetsCommand",
     "MigrateFilesCommand",
     "MigrateTimeseriesCommand",
     "MigrationCanvasCommand",
