74afdc90f3cf781eec6701bdcf925db8d9094aeb
anders-albert <anders0albert@gmail.com>
2025-09-24 17:02:22 +0200

    feat; implemented project status

--- cognite_toolkit/_cdf_tk/client/api/project.py
+++ cognite_toolkit/_cdf_tk/client/api/project.py
@@ -1,5 +1,6 @@
 from typing import TYPE_CHECKING
 
+from cognite_toolkit._cdf_tk.client.data_classes.project import ProjectStatus
 from cognite_toolkit._cdf_tk.utils.http_client import HTTPClient, ParamRequest
 
 if TYPE_CHECKING:
@@ -7,17 +8,18 @@ if TYPE_CHECKING:
 
 
 class ProjectAPI:
-    def __init__(self, config: ToolkitClientConfig) -> None:
+    def __init__(self, config: "ToolkitClientConfig") -> None:
         self._config = config
         self._http_client = HTTPClient(config, split_items_status_codes=set())
 
-    def info(self) -> dict[str, str]:
+    def status(self) -> ProjectStatus:
         """Retrieve information about the current project."""
         response = self._http_client.request_with_retries(
             ParamRequest(
-                endpoint_url=self._config.create_api_url(""),
-                parameters={"withDataModelingStatus": "true"},
-                method="GET",
+                endpoint_url=f"{self._config.base_url}/api/v1/projects?withDataModelingStatus=true", method="GET"
             )
         )
-        return response.json()
+        response.raise_for_status()
+        body = response.get_first_body()
+        # We expect the API will always return exactly one project when querying by project name
+        return ProjectStatus._load(body["items"][0])  # type: ignore[index,arg-type]
