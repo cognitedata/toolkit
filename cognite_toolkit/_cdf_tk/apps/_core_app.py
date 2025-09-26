7c12067ba6aea158d90156a164b8638dbf995b7e
Pål Rønning <ronpal@users.noreply.github.com>
2025-09-26 16:00:12 +0200

    cleaning up init posters

--- cognite_toolkit/_cdf_tk/apps/_core_app.py
+++ cognite_toolkit/_cdf_tk/apps/_core_app.py
@@ -10,7 +10,6 @@ from dotenv import load_dotenv
 from rich import print
 from rich.panel import Panel
 
-from cognite_toolkit._cdf_tk.commands.init import InitCommand
 from cognite_toolkit._cdf_tk.cdf_toml import CDFToml
 from cognite_toolkit._cdf_tk.client import ToolkitClient
 from cognite_toolkit._cdf_tk.commands import BuildCommand, CleanCommand, DeployCommand
@@ -74,7 +73,33 @@ class CoreApp(typer.Typer):
         Template reference documentation: https://developer.cognite.com/sdks/toolkit/references/configs
         """
         if ctx.invoked_subcommand is None:
-            InitCommand.poster()
+            print(
+                Panel(
+                    "\n".join(
+                        [
+                            "The Cognite Toolkit supports configuration of CDF projects from the command line or in CI/CD pipelines.",
+                            "",
+                            "[bold]Setup:[/]",
+                            "1. Run [underline]cdf repo init[/] [italic]<directory name>[/] to set up a work directory.",
+                            "2. Run [underline]cdf modules init[/] [italic]<directory name>[/] to initialise configuration modules.",
+                            "",
+                            "[bold]Configuration steps:[/]",
+                            "3. Run [underline]cdf build[/] [italic]<directory name>[/] to verify the configuration for your project. Repeat for as many times as needed.",
+                            "   Tip:[underline]cdf modules list[/] [italic]<directory name>[/] gives an overview of all your modules and their status.",
+                            "",
+                            "[bold]Deployment steps:[/]",
+                            "4. Commit the [italic]<directory name>[/] to version control",
+                            "5. Run [underline]cdf auth verify[/] to check that you have access to the relevant CDF project. ",
+                            "    or [underline]cdf auth verify[/] if you have a .env file",
+                            "6. Run [underline]cdf deploy --dry-run[/] to simulate the deployment of the configuration to the CDF project. Review the report provided.",
+                            "7. Run [underline]cdf deploy[/] to deploy the configuration to the CDF project.",
+                        ]
+                    ),
+                    title="Getting started",
+                    style="green",
+                    padding=(1, 2),
+                )
+            )
             return
         if override_env:
             print("  [bold yellow]WARNING:[/] Overriding environment variables with values from .env file...")
--- cognite_toolkit/_cdf_tk/commands/init.py
+++ cognite_toolkit/_cdf_tk/commands/init.py
@@ -6,35 +6,4 @@ from ._base import ToolkitCommand
 
 class InitCommand(ToolkitCommand):
     def execute(self) -> None:
-        InitCommand.poster()
-
-    @staticmethod
-    def poster() -> None:
-        print(
-            Panel(
-                "\n".join(
-                    [
-                        "The Cognite Toolkit supports configuration of CDF projects from the command line or in CI/CD pipelines.",
-                        "",
-                        "[bold]Setup:[/]",
-                        "1. Run [underline]cdf repo init[/] [italic]<directory name>[/] to set up a work directory.",
-                        "2. Run [underline]cdf modules init[/] [italic]<directory name>[/] to initialise configuration modules.",
-                        "",
-                        "[bold]Configuration steps:[/]",
-                        "3. Run [underline]cdf build[/] [italic]<directory name>[/] to verify the configuration for your project. Repeat for as many times as needed.",
-                        "   Tip:[underline]cdf modules list[/] [italic]<directory name>[/] gives an overview of all your modules and their status.",
-                        "",
-                        "[bold]Deployment steps:[/]",
-                        "4. Commit the [italic]<directory name>[/] to version control",
-                        "5. Run [underline]cdf auth verify[/] to check that you have access to the relevant CDF project. ",
-                        "    or [underline]cdf auth verify[/] if you have a .env file",
-                        "6. Run [underline]cdf deploy --dry-run[/] to simulate the deployment of the configuration to the CDF project. Review the report provided.",
-                        "7. Run [underline]cdf deploy[/] to deploy the configuration to the CDF project.",
-                    ]
-                ),
-                title="Getting started",
-                style="green",
-                padding=(1, 2),
-            )
-        )
-       
+        print(Panel("This command is deprecated. Use 'cdf modules init' instead."))
