f09264224282c4add24b3564a6e5d0c856d271ee
anders-albert <anders0albert@gmail.com>
2025-09-27 09:32:57 +0200

    refactor: simplify

--- cognite_toolkit/_cdf_tk/storageio/_base.py
+++ cognite_toolkit/_cdf_tk/storageio/_base.py
@@ -11,7 +11,6 @@ from rich.console import Console
 
 from cognite_toolkit._cdf_tk.client import ToolkitClient
 from cognite_toolkit._cdf_tk.exceptions import ToolkitNotImplementedError
-from cognite_toolkit._cdf_tk.utils.collection import chunker_sequence
 from cognite_toolkit._cdf_tk.utils.fileio import SchemaColumn
 from cognite_toolkit._cdf_tk.utils.http_client import HTTPClient, HTTPMessage, ItemsRequest
 from cognite_toolkit._cdf_tk.utils.useful_types import T_ID, JsonVal, T_Selector, T_WritableCogniteResourceList
@@ -96,6 +95,8 @@ class StorageIO(ABC, Generic[T_ID, T_Selector, T_CogniteResourceList, T_Writable
         """Upload a chunk of data to the storage using a custom HTTP client.
         This ensures that even if one item in the chunk fails, the rest will still be uploaded.
 
+        This assumes that the data_chunk is respecting the CHUNK_SIZE of the storage.
+
         Args:
             data_chunk: The chunk of data to upload, which should be a list of writable Cognite resources.
             http_client: The custom HTTP client to use for the upload.
@@ -105,20 +106,16 @@ class StorageIO(ABC, Generic[T_ID, T_Selector, T_CogniteResourceList, T_Writable
             raise ToolkitNotImplementedError(f"Upload not implemented for {self.KIND} storage.")
 
         config = http_client.config
-        results: list[HTTPMessage] = []
-        for batch in chunker_sequence(data_chunk, self.CHUNK_SIZE):
-            batch_results = http_client.request_with_retries(
-                message=ItemsRequest(
-                    endpoint_url=config.create_api_url(self.UPLOAD_ENDPOINT),
-                    method="POST",
-                    # The dump method from the PySDK always returns JsonVal, but mypy cannot infer that
-                    items=batch.dump(camel_case=True),  # type: ignore[arg-type]
-                    extra_body_fields=dict(self.UPLOAD_EXTRA_ARGS or {}),
-                    as_id=self.as_id,
-                )
+        return http_client.request_with_retries(
+            message=ItemsRequest(
+                endpoint_url=config.create_api_url(self.UPLOAD_ENDPOINT),
+                method="POST",
+                # The dump method from the PySDK always returns JsonVal, but mypy cannot infer that
+                items=data_chunk.dump(camel_case=True),  # type: ignore[arg-type]
+                extra_body_fields=dict(self.UPLOAD_EXTRA_ARGS or {}),
+                as_id=self.as_id,
             )
-            results.extend(batch_results)
-        return results
+        )
 
     @abstractmethod
     def data_to_json_chunk(self, data_chunk: T_WritableCogniteResourceList) -> list[dict[str, JsonVal]]:
