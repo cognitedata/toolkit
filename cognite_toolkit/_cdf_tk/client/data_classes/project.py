c7aa944e6a7b329319a0cb862700967ffbad132f
anders-albert <anders0albert@gmail.com>
2025-09-28 08:58:05 +0200

    feat: added ProjectResourceList

--- cognite_toolkit/_cdf_tk/client/data_classes/project.py
+++ cognite_toolkit/_cdf_tk/client/data_classes/project.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from typing import Any, Literal
 
 from cognite.client import CogniteClient
-from cognite.client.data_classes._base import CogniteResource
+from cognite.client.data_classes._base import CogniteResource, CogniteResourceList
 
 if sys.version_info >= (3, 11):
     from typing import Self
@@ -13,6 +13,8 @@ else:
 
 @dataclass
 class ProjectStatus(CogniteResource):
+    """Project status information."""
+
     data_modeling_status: Literal["HYBRID", "DATA_MODELING_ONLY"]
     url_name: str
 
@@ -22,3 +24,18 @@ class ProjectStatus(CogniteResource):
             data_modeling_status=resource["dataModelingStatus"],
             url_name=resource["urlName"],
         )
+
+
+class ProjectStatusList(CogniteResourceList[ProjectStatus]):
+    """List of ProjectStatus items."""
+
+    _RESOURCE = ProjectStatus
+
+    @property
+    def this_project(self) -> ProjectStatus:
+        """Returns the ProjectStatus of the current project."""
+        project = self._cognite_client.config.project
+        for item in self:
+            if item.url_name == project:
+                return item
+        raise ValueError(f"Project '{project}' not found in the list of projects.")
