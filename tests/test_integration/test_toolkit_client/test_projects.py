19e0e675e69e5afcc2fb353ec912d09c9f6099bb
anders-albert <anders0albert@gmail.com>
2025-09-28 08:59:56 +0200

    tests: updated tests

--- tests/test_integration/test_toolkit_client/test_projects.py
+++ tests/test_integration/test_toolkit_client/test_projects.py
@@ -3,5 +3,6 @@ from cognite_toolkit._cdf_tk.client import ToolkitClient
 
 class TestProjectAPI:
     def test_status(self, toolkit_client: ToolkitClient) -> None:
-        info = toolkit_client.project.status()
-        assert info.data_modeling_status == "HYBRID"
+        result = toolkit_client.project.status()
+        assert result.this_project.url_name == toolkit_client.config.project
+        assert result.this_project.data_modeling_status in ("HYBRID", "DATA_MODELING_ONLY")
