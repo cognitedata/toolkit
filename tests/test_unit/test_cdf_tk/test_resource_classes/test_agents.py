d3cd9ce0be1b730551c9af6881b56e875dbb8339
anders-albert <anders0albert@gmail.com>
2025-10-06 16:11:41 +0200

    tests: added more tests

--- tests/test_unit/test_cdf_tk/test_resource_classes/test_agents.py
+++ tests/test_unit/test_cdf_tk/test_resource_classes/test_agents.py
@@ -1,11 +1,20 @@
 from collections.abc import Iterable
 from pathlib import Path
+from typing import get_args
 
 import pytest
 
 from cognite_toolkit._cdf_tk.constants import MODULES
 from cognite_toolkit._cdf_tk.resource_classes.agent import AgentYAML
+from cognite_toolkit._cdf_tk.resource_classes.agent_tools import (
+    AgentInstanceSpaces,
+    AgentInstanceSpacesDefinition,
+    AgentTool,
+    AgentToolDefinition,
+)
 from cognite_toolkit._cdf_tk.tk_warnings.fileread import ResourceFormatWarning
+from cognite_toolkit._cdf_tk.utils import humanize_collection
+from cognite_toolkit._cdf_tk.utils._auxiliary import get_concrete_subclasses
 from cognite_toolkit._cdf_tk.validation import validate_resource_yaml_pydantic
 from tests.data import COMPLETE_ORG_ALPHA_FLAGS
 from tests.test_unit.utils import find_resources
@@ -36,3 +45,21 @@ class TestAgentYAML:
         assert isinstance(format_warning, ResourceFormatWarning)
 
         assert set(format_warning.errors) == expected_errors
+
+    def test_tools_are_in_union(self) -> None:
+        all_agent_tools = get_concrete_subclasses(AgentToolDefinition)
+        all_union_agent_tools = get_args(AgentTool.__args__[0])
+        missing = set(all_agent_tools) - set(all_union_agent_tools)
+        assert not missing, (
+            f"The following AgentTools subclasses are "
+            f"missing from the AgentTool union: {humanize_collection([cls.__name__ for cls in missing])}"
+        )
+
+    def test_instance_spaces_are_in_union(self) -> None:
+        all_instance_spaces = get_concrete_subclasses(AgentInstanceSpacesDefinition)
+        all_union_instance_spaces = get_args(AgentInstanceSpaces.__args__[0])
+        missing = set(all_instance_spaces) - set(all_union_instance_spaces)
+        assert not missing, (
+            f"The following AgentInstanceSpaces subclasses are "
+            f"missing from the AgentInstanceSpaces union: {humanize_collection([cls.__name__ for cls in missing])}"
+        )
