914a2c4b5e0fc856f04b68e2dc0b9316355753f9
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-10-10 07:10:58 +0200

    😁 Use YAML classes (#2061)
    # Description
    
    These classes are already implemented and well tested. Setting the
    yaml_cls in the CRUD interface will start using this in the build
    command to validate the YAML syntax of the user. The motivation for
    doing this in a separate PR is to avoid merge conflicts.
    
    This is the same type of PR as for example #1944, #1994
    
    **NOTE** This is the last PR of this type. We are done with creating
    YAML classes, so I have added a test to ensure that all ResourceCrud
    have the `yaml_cls` set.
    
    ## Details:
    The testing of the YAML resource classes we use to validate the syntax
    of the user configurations:
    https://github.com/cognitedata/toolkit/tree/main/tests/test_unit/test_cdf_tk/test_resource_classes
    
    Then, by setting yaml_cls in the resource CRUDit will be used in the cdf
    build command to give the user a warning about whether they follow the
    syntax here:
    https://github.com/cognitedata/toolkit/blob/main/cognite_toolkit/_cdf_tk/commands/build_cmd.py#L603
    
    
    ## Changelog
    
    - [x] Patch
    - [ ] Skip
    
    ## cdf
    
    ### Improved
    
    - Warning on YAML syntax errors when running cdf build for resources
    types (alpha features) agents and infield configurations.
    
    ## templates
    
    No changes.

--- cognite_toolkit/_cdf_tk/resource_classes/__init__.py
+++ cognite_toolkit/_cdf_tk/resource_classes/__init__.py
@@ -7,6 +7,7 @@ This is means that we have three set of resource classes we use in Toolkit:
 3. Read resource classes (from cognite-sdk): Represent the read/response format the Cognite resources.
 """
 
+from .agent import AgentYAML
 from .asset import AssetYAML
 from .base import BaseModelResource, ToolkitResource
 from .cognitefile import CogniteFileYAML
@@ -26,6 +27,7 @@ from .hosted_extractor_destination import HostedExtractorDestinationYAML
 from .hosted_extractor_job import HostedExtractorJobYAML
 from .hosted_extractor_mapping import HostedExtractorMappingYAML
 from .hosted_extractor_source import HostedExtractorSourceYAML
+from .infield_v1 import InfieldV1YAML
 from .instance import EdgeYAML, NodeYAML
 from .labels import LabelsYAML
 from .location import LocationYAML
@@ -49,6 +51,7 @@ from .workflow_trigger import WorkflowTriggerYAML
 from .workflow_version import WorkflowVersionYAML
 
 __all__ = [
+    "AgentYAML",
     "AssetYAML",
     "BaseModelResource",
     "CogniteFileYAML",
@@ -71,6 +74,7 @@ __all__ = [
     "HostedExtractorJobYAML",
     "HostedExtractorMappingYAML",
     "HostedExtractorSourceYAML",
+    "InfieldV1YAML",
     "LabelsYAML",
     "LocationYAML",
     "NodeYAML",
