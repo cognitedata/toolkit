a48f13d77fe221dcda0b6f75c987e142ecf588d8
anders-albert <anders0albert@gmail.com>
2025-10-11 09:24:10 +0200

    refactor: remove old selector classes

--- cognite_toolkit/_cdf_tk/storageio/__init__.py
+++ cognite_toolkit/_cdf_tk/storageio/__init__.py
@@ -4,41 +4,17 @@ from ._base import ConfigurableStorageIO, StorageIO, StorageIOConfig, T_Selector
 from ._data_classes import InstanceIdCSVList, InstanceIdRow, ModelList
 from ._instances import InstanceIO
 from ._raw import RawIO
-from ._selectors import (
-    AllChartSelector,
-    AssetCentricFileSelector,
-    AssetCentricSelector,
-    AssetSubtreeSelector,
-    ChartFileSelector,
-    ChartOwnerSelector,
-    ChartSelector,
-    DataSetSelector,
-    InstanceFileSelector,
-    InstanceSelector,
-    InstanceViewSelector,
-)
 
 __all__ = [
-    "AllChartSelector",
-    "AssetCentricFileSelector",
-    "AssetCentricSelector",
     "AssetIO",
-    "AssetSubtreeSelector",
     "BaseAssetCentricIO",
-    "ChartFileSelector",
     "ChartIO",
-    "ChartOwnerSelector",
-    "ChartSelector",
     "ConfigurableStorageIO",
-    "DataSetSelector",
     "EventIO",
     "FileMetadataIO",
-    "InstanceFileSelector",
     "InstanceIO",
     "InstanceIdCSVList",
     "InstanceIdRow",
-    "InstanceSelector",
-    "InstanceViewSelector",
     "ModelList",
     "RawIO",
     "StorageIO",
