e533578f487025206e366c6d2ef15a7fd037d27d
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-10-15 07:48:46 +0200

    🐛Fix get storage io (#2078)
    # Description
    
    This is a small bug found in the testing of the upcoming #2069. It has
    not yet been released so no release necessary.
    
    ## Changelog
    
    - [ ] Patch
    - [x] Skip

--- cognite_toolkit/_cdf_tk/storageio/__init__.py
+++ cognite_toolkit/_cdf_tk/storageio/__init__.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from cognite_toolkit._cdf_tk.utils._auxiliary import get_concrete_subclasses
+from cognite_toolkit._cdf_tk.utils.fileio import COMPRESSION_BY_SUFFIX
 
 from ._applications import CanvasIO, ChartIO
 from ._asset_centric import AssetIO, BaseAssetCentricIO, EventIO, FileMetadataIO, TimeSeriesIO
@@ -14,18 +15,22 @@ from .selectors._base import DataSelector
 STORAGE_IO_CLASSES = get_concrete_subclasses(StorageIO)  # type: ignore[type-abstract]
 
 
-def get_storage_io(selector: DataSelector, kind: str | Path) -> type[StorageIO]:
+def get_storage_io(selector_cls: type[DataSelector], kind: str | Path) -> type[StorageIO]:
     """Get the appropriate StorageIO class based on the type of the provided selector."""
     for cls in STORAGE_IO_CLASSES:
-        if issubclass(type(selector), cls.BASE_SELECTOR) and _is_kind(cls, kind):
+        if issubclass(selector_cls, cls.BASE_SELECTOR) and are_same_kind(cls.KIND, kind):
             return cls
-    raise ValueError(f"No StorageIO found for selector of type {type(selector)}")
+    raise ValueError(f"No StorageIO found for selector of type {selector_cls.__name__}")
 
 
-def _is_kind(cls: type[StorageIO], kind: str | Path) -> bool:
-    if isinstance(kind, Path):
-        return kind.stem.lower().endswith(cls.KIND.lower())
-    return cls.KIND.lower() == kind.lower()
+def are_same_kind(kind: str, kind_or_path: str | Path, /) -> bool:
+    """Check if two kinds are the same, ignoring case and compression suffixes."""
+    if not isinstance(kind_or_path, Path):
+        return kind.casefold() == kind_or_path.casefold()
+    stem = kind_or_path.stem
+    if kind_or_path.suffix in COMPRESSION_BY_SUFFIX:
+        stem = Path(stem).stem
+    return stem.lower().endswith(kind.casefold())
 
 
 __all__ = [
@@ -46,5 +51,6 @@ __all__ = [
     "T_Selector",
     "TableStorageIO",
     "TimeSeriesIO",
+    "are_same_kind",
     "get_storage_io",
 ]
