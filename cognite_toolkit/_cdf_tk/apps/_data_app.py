da1022bd14ce9ddb81388f3a83906894ae433ca5
anders-albert <anders0albert@gmail.com>
2025-10-18 09:39:57 +0200

    refactor: review feedback

--- cognite_toolkit/_cdf_tk/apps/_data_app.py
+++ cognite_toolkit/_cdf_tk/apps/_data_app.py
@@ -11,13 +11,13 @@ from ._upload_app import UploadApp
 class DataApp(typer.Typer):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
-        self.callback(invoke_without_command=True)(self.dump_main)
+        self.callback(invoke_without_command=True)(self.main)
         self.add_typer(DownloadApp(*args, **kwargs), name="download")
         self.add_typer(UploadApp(*args, **kwargs), name="upload")
         self.add_typer(PurgeApp(*args, **kwargs), name="purge")
 
     @staticmethod
-    def dump_main(ctx: typer.Context) -> None:
+    def main(ctx: typer.Context) -> None:
         """Commands to work with data download/upload/purge."""
         if ctx.invoked_subcommand is None:
             print("Use [bold yellow]cdf data --help[/] for more information.")
