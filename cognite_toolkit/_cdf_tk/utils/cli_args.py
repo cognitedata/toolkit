d7fb33b115be5148449c56bcc524b03ed2c24d6b
anders-albert <anders0albert@gmail.com>
2025-10-25 11:06:48 +0200

    tests: and fix

--- cognite_toolkit/_cdf_tk/utils/cli_args.py
+++ cognite_toolkit/_cdf_tk/utils/cli_args.py
@@ -19,13 +19,13 @@ def parse_view_str(view_str: str) -> ViewId:
     ViewId(space='my_space', external_id='my_view', version='v1')
     """
     space_and_rest = view_str.split(":", 1)
-    if len(space_and_rest) != 2:
+    if len(space_and_rest) != 2 or not space_and_rest[0] or not space_and_rest[1]:
         raise ToolkitValueError(
             f"Invalid view string format: '{view_str}'. Expected format 'space:externalId/version'."
         )
     space = space_and_rest[0]
     external_id_and_version = space_and_rest[1].rsplit("/", 1)
-    if len(external_id_and_version) != 2:
+    if len(external_id_and_version) != 2 or not external_id_and_version[0] or not external_id_and_version[1]:
         raise ToolkitValueError(
             f"Invalid view string format: '{view_str}'. Expected format 'space:externalId/version'."
         )
