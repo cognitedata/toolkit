654ebf2241a84bb529338361d17edf20f170ff29
anders-albert <anders0albert@gmail.com>
2025-10-26 10:42:17 +0100

    refactor: asset-centric resource

--- cognite_toolkit/_cdf_tk/utils/useful_types.py
+++ cognite_toolkit/_cdf_tk/utils/useful_types.py
@@ -2,12 +2,14 @@ from collections.abc import Hashable
 from datetime import date, datetime
 from typing import Any, Literal, TypeAlias, TypeVar, get_args
 
+from cognite.client.data_classes import Asset, Event, FileMetadata, Sequence, TimeSeries
 from cognite.client.data_classes._base import WriteableCogniteResourceList
 
 JsonVal: TypeAlias = None | str | int | float | bool | dict[str, "JsonVal"] | list["JsonVal"]
 
 AssetCentricDestinationType: TypeAlias = Literal["assets", "files", "events", "timeseries", "sequences"]
 AssetCentricType: TypeAlias = Literal["asset", "file", "event", "timeseries", "sequence"]
+AssetCentricResource: TypeAlias = Asset | FileMetadata | Event | TimeSeries | Sequence
 DataType: TypeAlias = Literal["string", "integer", "float", "boolean", "json", "date", "timestamp"]
 PythonTypes: TypeAlias = str | int | float | bool | datetime | date | dict[str, Any] | list[Any]
 
