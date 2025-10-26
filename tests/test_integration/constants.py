6a9fa6e3ea22ad263a640db5ede00b4dfd469212
anders-albert <anders0albert@gmail.com>
2025-10-26 12:24:02 +0100

    tests; increase randomness

--- tests/test_integration/constants.py
+++ tests/test_integration/constants.py
@@ -5,7 +5,9 @@ import sys
 # This is needed as we run tests for two different versions of Python in parallel.
 # The platform.system() is not used, but is here in case we start testing on Windows as well.
 # The random number is to avoid conflicts when running tests in parallel (for example, two PRs).
-RUN_UNIQUE_ID = f"{platform.system()}_{sys.version_info.major}_{sys.version_info.minor}_{random.randint(0, 10)!s}"
+RUN_UNIQUE_ID = (
+    f"{platform.system()}_{sys.version_info.major}_{sys.version_info.minor}_{random.randint(0, 100_000_000)!s}"
+)
 
 # These constants are used to set up a raw database and tables with transformations and datasets
 # to populate the asset-centric resources in CDF.
