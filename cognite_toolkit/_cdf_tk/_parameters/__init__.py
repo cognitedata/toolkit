65b99b3e036ac0c1e7833bb4ad308b7fec762681
anders-albert <anders0albert@gmail.com>
2025-10-27 09:01:15 +0100

    refactor: removed reading

--- cognite_toolkit/_cdf_tk/_parameters/__init__.py
+++ cognite_toolkit/_cdf_tk/_parameters/__init__.py
@@ -2,7 +2,6 @@
 
 from .constants import ANY_INT, ANY_STR, ANYTHING
 from .data_classes import ParameterSet, ParameterSpec, ParameterSpecSet, ParameterValue
-from .functions import read_parameter_from_init_type_hints, read_parameters_from_dict
 
 __all__ = [
     "ANYTHING",
@@ -13,6 +12,4 @@ __all__ = [
     "ParameterSpecSet",
     "ParameterSpecSet",
     "ParameterValue",
-    "read_parameter_from_init_type_hints",
-    "read_parameters_from_dict",
 ]
