42065cb38b328a2823215b6a62d0d3dc9167b3f8
anders-albert <anders0albert@gmail.com>
2025-10-31 15:11:55 +0100

    tests: sanitize bug

--- tests/test_unit/test_cdf_tk/test_commands/test_download.py
+++ tests/test_unit/test_cdf_tk/test_commands/test_download.py
@@ -13,18 +13,19 @@ class TestDownloadCommand:
     def test_download_data_in_table_format(self, tmp_path: Path) -> None:
         cmd = DownloadCommand(silent=True, skip_tracking=True)
         with monkeypatch_toolkit_client() as client:
+            dataset = "my/:_data_set"
             client.assets.aggregate_count.return_value = 1
             client.assets.return_value = [
                 AssetList([Asset(id=123, name="asset_123", metadata={"key": "value"}, data_set_id=42)])
             ]
             client.lookup.data_sets.id.return_value = 42
-            client.lookup.data_sets.external_id.return_value = "my_data_set"
+            client.lookup.data_sets.external_id.return_value = dataset
             client.transformations.preview.return_value = TransformationPreviewResult(
                 None, results=[{"key": "key", "key_count": 1}]
             )
 
             cmd.download(
-                selectors=[DataSetSelector(kind="Assets", data_set_external_id="my_data_set")],
+                selectors=[DataSetSelector(kind="Assets", data_set_external_id=dataset)],
                 io=AssetIO(client=client),
                 output_dir=tmp_path,
                 verbose=True,
@@ -38,7 +39,7 @@ class TestDownloadCommand:
             content = list(CSVReader(csv_files[0]).read_chunks_unprocessed())
             assert content == [
                 {
-                    "dataSetExternalId": "my_data_set",
+                    "dataSetExternalId": dataset,
                     "description": "",
                     "externalId": "",
                     "geoLocation": "",
