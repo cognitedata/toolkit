d91b031ea81322b1f50ebdd0c70ba5ffa6ef8328
Aashutosh-cognite <aashutosh.chaubey@cognite.com>
2025-11-06 16:26:24 +0530

    Create Resource interactively

--- cognite_toolkit/_cdf_tk/commands/resources.py
+++ cognite_toolkit/_cdf_tk/commands/resources.py
@@ -3,11 +3,19 @@ from pathlib import Path
 
 import questionary
 import typer
+from questionary import Choice
 from rich import print
 
 from cognite_toolkit._cdf_tk.commands._base import ToolkitCommand
 from cognite_toolkit._cdf_tk.constants import MODULES
-from cognite_toolkit._cdf_tk.cruds import CRUDS_BY_FOLDER_NAME, ContainerCRUD, NodeCRUD, ResourceCRUD, ViewCRUD
+from cognite_toolkit._cdf_tk.cruds import (
+    CRUDS_BY_FOLDER_NAME,
+    RESOURCE_CRUD_LIST,
+    ContainerCRUD,
+    NodeCRUD,
+    ResourceCRUD,
+    ViewCRUD,
+)
 from cognite_toolkit._cdf_tk.data_classes import ModuleDirectories
 from cognite_toolkit._cdf_tk.resource_classes import ToolkitResource
 from cognite_toolkit._cdf_tk.utils.file import yaml_safe_dump
@@ -43,6 +51,94 @@ class ResourcesCommand(ToolkitCommand):
         """
         return f"my_{resource_crud.kind.strip().replace(' ', '_')}"
 
+    def _validate_resource_directories(self, resource_directories: list[str], verbose: bool) -> None:
+        """
+        Validate resource directories.
+        """
+        folder_names: list[str] = list(CRUDS_BY_FOLDER_NAME.keys())
+        for rd in resource_directories:
+            if rd not in folder_names:
+                if verbose and (close_matches := difflib.get_close_matches(rd, folder_names)):
+                    print(
+                        f"[red]{rd} is an invalid resource directory. Did you mean one of the following: {close_matches}?[/red]"
+                    )
+                else:
+                    print(f"[red]{rd} is an invalid resource directory.[/red]")
+                raise typer.Exit()
+
+    def _get_resource_cruds_interactive(
+        self,
+        resource_directories: list[str] | None = None,
+        verbose: bool = False,
+    ) -> list[tuple[type[ResourceCRUD], str]]:
+        """
+        Get resource CRUDs interactively by prompting user to select resource directories and resources.
+        """
+        if not resource_directories:
+            choices = [
+                Choice(title=folder_name, value=(folder_name, cruds))
+                for folder_name, cruds in sorted(CRUDS_BY_FOLDER_NAME.items(), key=lambda x: x[0])
+            ]
+            selected_dirs: list[tuple[str, list[type[ResourceCRUD]]]] | None = questionary.checkbox(
+                "Select resource directories", choices=choices
+            ).ask()
+
+            if not selected_dirs:
+                if verbose:
+                    print("[red]No resource directories selected... Aborting...[/red]")
+                else:
+                    print("[red]Aborting...[/red]")
+                raise typer.Exit()
+
+            resource_directories = [dir_name for dir_name, _ in selected_dirs]
+
+        self._validate_resource_directories(resource_directories, verbose)
+
+        resource_cruds: list[type[ResourceCRUD]] = []
+        for resource_dir in resource_directories:
+            cruds = [
+                crud
+                for crud in CRUDS_BY_FOLDER_NAME[resource_dir]
+                if isinstance(crud, type) and issubclass(crud, ResourceCRUD) and crud in RESOURCE_CRUD_LIST
+            ]
+
+            if not cruds:
+                continue
+
+            choices = [Choice(title=crud.kind, value=crud) for crud in cruds]
+
+            if len(choices) == 1:
+                resource_cruds.append(choices[0].value)  # type: ignore[arg-type]
+                continue
+
+            selected_resources: list[type[ResourceCRUD]] | None = questionary.checkbox(
+                f"What resources would you like to create in {resource_dir}?", choices=choices
+            ).ask()
+
+            if selected_resources:
+                resource_cruds.extend(selected_resources)
+
+        if not resource_cruds:
+            if verbose:
+                print("[red]No resources selected... Aborting...[/red]")
+            else:
+                print("[red]Aborting...[/red]")
+            raise typer.Exit()
+
+        if questionary.confirm("Use default file names for resources?", default=True).ask():
+            return [(crud, self._create_default_file_name(crud)) for crud in resource_cruds]
+        else:
+            result: list[tuple[type[ResourceCRUD], str]] = []
+            for crud in resource_cruds:
+                default_name = self._create_default_file_name(crud)
+                file_name = questionary.text(
+                    f"What is the name of the new {crud.kind} resource file?",
+                    default=default_name,
+                ).ask()
+                if file_name:
+                    result.append((crud, file_name))
+            return result
+
     def _get_resource_cruds(
         self,
         resource_directories: str | list[str],
@@ -56,16 +152,8 @@ class ResourcesCommand(ToolkitCommand):
         if isinstance(resource_directories, str):
             resource_directories = [resource_directories]
 
-        folder_names: list[str] = list(CRUDS_BY_FOLDER_NAME.keys())
-        for rd in resource_directories:
-            if rd not in folder_names:
-                if verbose and (close_matches := difflib.get_close_matches(rd, folder_names)):
-                    print(
-                        f"[red]{rd} is an invalid resource directory. Did you mean one of the following: {close_matches}?[/red]"
-                    )
-                else:
-                    print(f"[red]{rd} is an invalid resource directory.[/red]")
-                raise typer.Exit()
+        self._validate_resource_directories(resource_directories, verbose)
+
         file_name_map: dict[str, str] = {}
         if file_name:
             file_name_map = dict(zip(resources, file_name))
@@ -194,4 +282,10 @@ class ResourcesCommand(ToolkitCommand):
             resource_directories: The resource directories to create the resources in.
             verbose: Whether to print verbose output.
         """
-        print(organization_dir, module_name, resource_directories, verbose)
+        module_path: Path = self._get_module_path(module_name, organization_dir, verbose)
+        resource_cruds: list[tuple[type[ResourceCRUD], str]] = self._get_resource_cruds_interactive(
+            resource_directories, verbose
+        )
+
+        for resource_crud, file_name_str in resource_cruds:
+            self._create_resource_yaml_file(resource_crud, file_name_str, module_path, verbose)
