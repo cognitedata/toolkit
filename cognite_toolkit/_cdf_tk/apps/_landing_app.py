e6ab5ba85833ebeadacea60d422dc5d38a5e46bf
Pål Rønning <ronpal@users.noreply.github.com>
2025-11-06 15:00:42 +0100

    --seven

--- cognite_toolkit/_cdf_tk/apps/_landing_app.py
+++ cognite_toolkit/_cdf_tk/apps/_landing_app.py
@@ -8,7 +8,7 @@ from cognite_toolkit._cdf_tk.commands import InitCommand
 class LandingApp(typer.Typer):
     def __init__(self, *args, **kwargs) -> None:  # type: ignore
         super().__init__(*args, **kwargs)
-        self.command()(self.main_init)
+        # Command is registered directly in _cdf.py, not here
 
     def main_init(
         self,
