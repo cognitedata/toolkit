525b80d32e619e01d50476bec5ed044f120a63a2
Pål Rønning <ronpal@users.noreply.github.com>
2025-11-17 14:19:53 +0100

    Added tests

--- cognite_toolkit/_cdf_tk/apps/_landing_app.py
+++ cognite_toolkit/_cdf_tk/apps/_landing_app.py
@@ -2,7 +2,9 @@ from typing import Annotated
 
 import typer
 
+from cognite_toolkit._cdf_tk.cdf_toml import CDFToml
 from cognite_toolkit._cdf_tk.commands import InitCommand
+from cognite_toolkit._cdf_tk.feature_flags import Flags
 
 
 class LandingApp(typer.Typer):
@@ -19,9 +21,17 @@ class LandingApp(typer.Typer):
                 help="Whether to do a dry-run, do dry-run if present.",
             ),
         ] = False,
+        # TODO: this is a temporary solution to be able to test the functionality
+        # in a new environment, assuming that the toml file doesn't exist yet.
+        # remove this once v.07 is released
         v7: Annotated[
             bool,
-            typer.Option("--seven", "-s", help="Emulate v0.7", hidden=True),
+            typer.Option(
+                "--seven",
+                "-s",
+                help="Emulate v0.7",
+                hidden=(Flags.v07.is_enabled() or not CDFToml.load().is_loaded_from_file),
+            ),
         ] = False,
     ) -> None:
         """Getting started checklist"""
