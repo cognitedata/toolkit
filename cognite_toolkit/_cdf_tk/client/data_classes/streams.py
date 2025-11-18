ff50ad9d2688333a5827ce880a875d3561378458
Aashutosh-cognite <aashutosh.chaubey@cognite.com>
2025-11-18 14:50:17 +0530

    Streams dataclass update

--- cognite_toolkit/_cdf_tk/client/data_classes/streams.py
+++ cognite_toolkit/_cdf_tk/client/data_classes/streams.py
@@ -29,13 +29,13 @@ class StreamRequestList(UserList[StreamRequest], ResourceRequestListProtocol):
     """List of Stream request resources."""
 
     _RESOURCE = StreamRequest
-    items: list[StreamRequest]
+    data: list[StreamRequest]
 
     def __init__(self, initlist: list[StreamRequest] | None = None, **_: Any) -> None:
         super().__init__(initlist or [])
 
     def dump(self, camel_case: bool = True) -> list[dict[str, Any]]:
-        return [item.dump(camel_case) for item in self.items]
+        return [item.dump(camel_case) for item in self.data]
 
     @classmethod
     def load(cls, data: list[dict[str, Any]], cognite_client: CogniteClient | None = None) -> "StreamRequestList":
@@ -83,15 +83,19 @@ class StreamResponse(ResponseResource["StreamRequest"]):
     settings: StreamSettings | None = None
 
     def as_request_resource(self) -> StreamRequest:
-        return StreamRequest(
-            external_id=self.external_id,
-            settings={"template": {"name": self.created_from_template}},
+        return StreamRequest.model_validate(
+            {
+                "externalId": self.external_id,
+                "settings": {"template": {"name": self.created_from_template}},
+            }
         )
 
     def as_write(self) -> StreamRequest:
-        return StreamRequest(
-            external_id=self.external_id,
-            settings={"template": {"name": self.created_from_template}},
+        return StreamRequest.model_validate(
+            {
+                "externalId": self.external_id,
+                "settings": {"template": {"name": self.created_from_template}},
+            }
         )
 
 
