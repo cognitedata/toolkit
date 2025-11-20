71e477bdf28f28d796b9e0b9436efe4107e3f861
anders-albert <anders0albert@gmail.com>
2025-11-20 16:01:38 +0100

    refactor: first pass on implementation

--- cognite_toolkit/_cdf_tk/commands/_migrate/data_mapper.py
+++ cognite_toolkit/_cdf_tk/commands/_migrate/data_mapper.py
@@ -9,17 +9,19 @@ from cognite.client.data_classes.data_modeling import (
     EdgeApply,
     InstanceApply,
     NodeApply,
+    NodeId,
     View,
     ViewId,
 )
 
 from cognite_toolkit._cdf_tk.client import ToolkitClient
 from cognite_toolkit._cdf_tk.client.data_classes.charts import Chart, ChartWrite
+from cognite_toolkit._cdf_tk.client.data_classes.charts_data import ChartCoreTimeseries
 from cognite_toolkit._cdf_tk.client.data_classes.migration import ResourceViewMappingApply
 from cognite_toolkit._cdf_tk.commands._migrate.conversion import DirectRelationCache, asset_centric_to_dm
 from cognite_toolkit._cdf_tk.commands._migrate.data_classes import AssetCentricMapping
 from cognite_toolkit._cdf_tk.commands._migrate.default_mappings import create_default_mappings
-from cognite_toolkit._cdf_tk.commands._migrate.issues import ConversionIssue, MigrationIssue
+from cognite_toolkit._cdf_tk.commands._migrate.issues import ChartMigrationIssue, ConversionIssue, MigrationIssue
 from cognite_toolkit._cdf_tk.commands._migrate.selectors import AssetCentricMigrationSelector
 from cognite_toolkit._cdf_tk.constants import MISSING_INSTANCE_SPACE
 from cognite_toolkit._cdf_tk.exceptions import ToolkitValueError
@@ -129,4 +131,58 @@ class ChartMapper(DataMapper[ChartSelector, Chart, ChartWrite]):
         self.client = client
 
     def map(self, source: Sequence[Chart]) -> Sequence[tuple[ChartWrite | None, MigrationIssue]]:
-        raise NotImplementedError("ChartMapper is not implemented yet.")
+        self._populate_cache(source)
+        output: list[tuple[ChartWrite | None, MigrationIssue]] = []
+        for item in source:
+            mapped_item, issue = self._map_single_item(item)
+            if issue.has_issues:
+                output.append((None, issue))
+            else:
+                output.append((mapped_item, issue))
+        return output
+
+    def _populate_cache(self, source: Sequence[Chart]) -> None:
+        timeseries_ids: set[int] = set()
+        timeseries_external_ids: set[str] = set()
+        for chart in source:
+            for item in chart.data.time_series_collection or []:
+                if item.ts_id:
+                    # We only look-up the internalID if the externalId is missing
+                    timeseries_ids.add(item.ts_id)
+                if item.ts_external_id:
+                    timeseries_external_ids.add(item.ts_external_id)
+        if timeseries_ids:
+            self.client.migration.lookup.time_series(list(timeseries_ids))
+        if timeseries_external_ids:
+            self.client.migration.lookup.time_series(external_id=list(timeseries_external_ids))
+
+    def _map_single_item(self, item: Chart) -> tuple[ChartWrite | None, ChartMigrationIssue]:
+        issue = ChartMigrationIssue(chart_external_id=item.external_id)
+        timeseries_core_collection: list[ChartCoreTimeseries] = []
+        for ts_item in item.data.time_series_collection or []:
+            node_id: NodeId | None = None
+            if ts_item.ts_id:
+                node_id = self.client.migration.lookup.time_series(ts_item.ts_id)
+                if node_id is None and ts_item.ts_external_id:
+                    node_id = self.client.migration.lookup.time_series(external_id=ts_item.ts_external_id)
+                if node_id is None:
+                    issue.missing_timeseries_ids.append(ts_item.ts_id)
+            elif ts_item.ts_external_id:
+                node_id = self.client.migration.lookup.time_series(external_id=ts_item.ts_external_id)
+                if node_id is None:
+                    issue.missing_timeseries_external_ids.append(ts_item.ts_external_id)
+            else:
+                node_id = None
+
+            dumped = ts_item.dump(camel_case=True)
+            dumped.pop("tsId", None)
+            dumped.pop("tsExternalId", None)
+            dumped.pop("originalUnit", None)
+            dumped["nodeReference"] = node_id
+            dumped["viewReference"] = ...
+            core_timeseries = ChartCoreTimeseries._load(dumped)
+            timeseries_core_collection.append(core_timeseries)
+        mapped_chart = item.as_write()
+        mapped_chart.data.core_timeseries_collection = timeseries_core_collection
+        mapped_chart.data.time_series_collection = None
+        return mapped_chart, issue
