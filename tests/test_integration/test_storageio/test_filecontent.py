ec5610ea2a36591ce99b24f454a04ef842501c32
anders-albert <anders0albert@gmail.com>
2025-11-25 06:55:21 +0100

    tests: added failing test

--- tests/test_integration/test_storageio/test_filecontent.py
+++ tests/test_integration/test_storageio/test_filecontent.py
@@ -1,11 +1,17 @@
 from pathlib import Path
 
 from cognite.client.data_classes import FileMetadataWrite
+from cognite.client.data_classes.data_modeling import NodeId, Space
 
 from cognite_toolkit._cdf_tk.client import ToolkitClient
 from cognite_toolkit._cdf_tk.storageio import FileContentIO
 from cognite_toolkit._cdf_tk.storageio._file_content import UploadFileContentItem
-from cognite_toolkit._cdf_tk.storageio.selectors import FileMetadataTemplate, FileMetadataTemplateSelector
+from cognite_toolkit._cdf_tk.storageio.selectors import (
+    FileDataModelingTemplate,
+    FileDataModelingTemplateSelector,
+    FileMetadataTemplate,
+    FileMetadataTemplateSelector,
+)
 from cognite_toolkit._cdf_tk.storageio.selectors._file_content import FILENAME_VARIABLE
 from cognite_toolkit._cdf_tk.utils.http_client import HTTPClient
 from tests.test_integration.constants import RUN_UNIQUE_ID
@@ -47,3 +53,44 @@ class TestFileContentIO:
         finally:
             # Clean up
             toolkit_client.files.delete(external_id=metadata.external_id, ignore_unknown_ids=True)
+
+    def test_upload_dm_file(self, toolkit_client: ToolkitClient, tmp_path: Path, toolkit_space: Space) -> None:
+        my_text_file = tmp_path / "my_dm_file.txt"
+        my_text_file.write_text("This is some test content for data modeling.", encoding="utf-8")
+        metadata = FileMetadataWrite(
+            name="my_dm_file.txt",
+            instance_id=NodeId(
+                space=toolkit_space.space,
+                external_id=f"test_upload_dm_file_001_{RUN_UNIQUE_ID}",
+            ),
+        )
+
+        io = FileContentIO(toolkit_client)
+        try:
+            with HTTPClient(toolkit_client.config) as http_client:
+                io.upload_items(
+                    [
+                        UploadFileContentItem(
+                            source_id=my_text_file.name,
+                            item=metadata,
+                            file_path=my_text_file,
+                            mime_type="text/plain",
+                        )
+                    ],
+                    http_client,
+                    FileDataModelingTemplateSelector(
+                        file_directory=Path("does not matter"),
+                        template=FileDataModelingTemplate(
+                            space=toolkit_space.space,
+                            external_id=FILENAME_VARIABLE,
+                        ),
+                    ),
+                )
+            # Verify upload
+            uploaded_file = toolkit_client.files.retrieve(instance_id=metadata.instance_id)
+            assert uploaded_file is not None
+            assert uploaded_file.name == "my_dm_file.txt"
+            assert uploaded_file.uploaded is True
+        finally:
+            # Clean up
+            toolkit_client.data_modeling.instances.delete(nodes=[metadata.instance_id])
