1ce0870fc54336fa3fe8dd63e6cd5ef375529d5d
anders-albert <anders0albert@gmail.com>
2025-11-25 07:09:08 +0100

    refactor: adjust test

--- tests/test_integration/test_storageio/test_filecontent.py
+++ tests/test_integration/test_storageio/test_filecontent.py
@@ -13,6 +13,7 @@ from cognite_toolkit._cdf_tk.storageio.selectors import (
     FileMetadataTemplateSelector,
 )
 from cognite_toolkit._cdf_tk.storageio.selectors._file_content import FILENAME_VARIABLE
+from cognite_toolkit._cdf_tk.utils.fileio import MultiFileReader
 from cognite_toolkit._cdf_tk.utils.http_client import HTTPClient
 from tests.test_integration.constants import RUN_UNIQUE_ID
 
@@ -54,43 +55,38 @@ class TestFileContentIO:
             # Clean up
             toolkit_client.files.delete(external_id=metadata.external_id, ignore_unknown_ids=True)
 
-    def test_upload_dm_file(self, toolkit_client: ToolkitClient, tmp_path: Path, toolkit_space: Space) -> None:
-        my_text_file = tmp_path / "my_dm_file.txt"
+    def test_read_deserialize_upload_dm_file(
+        self, toolkit_client: ToolkitClient, tmp_path: Path, toolkit_space: Space
+    ) -> None:
+        external_id = f"test_upload_dm_file_001_{RUN_UNIQUE_ID}.txt"
+        my_text_file = tmp_path / external_id
         my_text_file.write_text("This is some test content for data modeling.", encoding="utf-8")
-        metadata = FileMetadataWrite(
-            name="my_dm_file.txt",
-            instance_id=NodeId(
+        instance_id = NodeId(space=toolkit_space.space, external_id=external_id)
+        selector = FileDataModelingTemplateSelector(
+            file_directory=tmp_path,
+            template=FileDataModelingTemplate(
                 space=toolkit_space.space,
-                external_id=f"test_upload_dm_file_001_{RUN_UNIQUE_ID}",
+                external_id=FILENAME_VARIABLE,
             ),
         )
-
         io = FileContentIO(toolkit_client)
+        reader = MultiFileReader([my_text_file])
+
+        read_chunks = list(io.read_chunks(reader, selector))
+        assert len(read_chunks) == 1
+        read_chunk = read_chunks[0]
+
+        upload_content = io.json_chunk_to_data(read_chunk)
+        assert len(upload_content) == 1
+
         try:
             with HTTPClient(toolkit_client.config) as http_client:
-                io.upload_items(
-                    [
-                        UploadFileContentItem(
-                            source_id=my_text_file.name,
-                            item=metadata,
-                            file_path=my_text_file,
-                            mime_type="text/plain",
-                        )
-                    ],
-                    http_client,
-                    FileDataModelingTemplateSelector(
-                        file_directory=Path("does not matter"),
-                        template=FileDataModelingTemplate(
-                            space=toolkit_space.space,
-                            external_id=FILENAME_VARIABLE,
-                        ),
-                    ),
-                )
+                io.upload_items(upload_content, http_client, selector)
+
             # Verify upload
-            uploaded_file = toolkit_client.files.retrieve(instance_id=metadata.instance_id)
+            uploaded_file = toolkit_client.files.retrieve(instance_id=instance_id)
             assert uploaded_file is not None
-            assert uploaded_file.name == "my_dm_file.txt"
             assert uploaded_file.uploaded is True
         finally:
             # Clean up
-            toolkit_client.data_modeling.instances.delete(nodes=[metadata.instance_id])
+            toolkit_client.data_modeling.instances.delete(nodes=instance_id)
