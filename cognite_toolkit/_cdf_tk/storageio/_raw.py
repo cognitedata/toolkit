d468c62b89f892afc3aaaed95cdfd8a690ffe3ce
Anders Albert <60234212+doctrino@users.noreply.github.com>
2025-12-01 07:02:25 +0100

    [CDF-26158] 🚸Validate raw column (#2266)
    # Description
    
    Small check
    
    ## Bump
    
    - [x] Patch
    - [ ] Skip
    
    ## Changelog
    
    ### Improved
    
    - If you upload a RAW table in csv or parquet format and specify which
    column to use for key using `cdf data upload dir`, Toolkit now validates
    that the column is in csv/parquet file.

--- cognite_toolkit/_cdf_tk/storageio/_raw.py
+++ cognite_toolkit/_cdf_tk/storageio/_raw.py
@@ -1,4 +1,5 @@
 from collections.abc import Iterable, Sequence
+from itertools import chain
 from uuid import uuid4
 
 from cognite.client.data_classes import Row, RowWrite
@@ -6,6 +7,8 @@ from cognite.client.data_classes import Row, RowWrite
 from cognite_toolkit._cdf_tk.cruds import RawDatabaseCRUD, RawTableCRUD
 from cognite_toolkit._cdf_tk.exceptions import ToolkitValueError
 from cognite_toolkit._cdf_tk.utils import sanitize_filename
+from cognite_toolkit._cdf_tk.utils.collection import chunker
+from cognite_toolkit._cdf_tk.utils.fileio import MultiFileReader
 from cognite_toolkit._cdf_tk.utils.http_client import HTTPClient, HTTPMessage, ItemsRequest
 from cognite_toolkit._cdf_tk.utils.useful_types import JsonVal
 
@@ -96,3 +99,26 @@ class RawIO(
         if selector is not None and selector.key is not None and selector.key in row:
             key = str(row.pop(selector.key))
         return RowWrite(key=key, columns=row)
+
+    @classmethod
+    def read_chunks(
+        cls, reader: MultiFileReader, selector: RawTableSelector
+    ) -> Iterable[list[tuple[str, dict[str, JsonVal]]]]:
+        if not reader.is_table or selector.key is None:
+            yield from super().read_chunks(reader, selector)
+            return
+        data_name = "row" if reader.is_table else "line"
+        # Validate that the key exists in all files
+        for input_file in sorted(reader.input_files, key=reader._part_no):
+            iterable = reader.reader_class(input_file).read_chunks()
+            try:
+                first = next(iterable)
+            except StopIteration:
+                continue
+            if selector.key not in first:
+                raise ToolkitValueError(
+                    f"Column '{selector.key}' not found in file {input_file.as_posix()!r}. Please ensure the specified column exists."
+                )
+            full_iterator = chain([first], iterable)
+            line_numbered_iterator = ((f"{data_name} {i}", row) for i, row in enumerate(full_iterator, start=1))
+            yield from chunker(line_numbered_iterator, cls.CHUNK_SIZE)
