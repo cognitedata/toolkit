cd0f54c508db7fd768ef0b51b5a18ec5ed7dbd19
Erik Angelsen <95421177+erikangelsen-cognite@users.noreply.github.com>
2025-12-02 16:09:41 +0100

    Infield cdmv2 (#2245)
    Config for the full infield CDM experience.
    - New config container: InFieldCDMConfig
    - Similar expereince as the infield_quickstart config, but it holds way
    more configurations and is structured better.
    
    
    
    ## Bump
    
    - [x] Patch
    - [ ] Skip
    
    ## Changelog
    
    ### Added
    
    - [alpha] Infield configuration `InFieldCDMConfig`.
    
    ---------
    
    Co-authored-by: anders-albert <anders0albert@gmail.com>
    Co-authored-by: Anders Albert <60234212+doctrino@users.noreply.github.com>

--- cognite_toolkit/_cdf_tk/client/testing.py
+++ cognite_toolkit/_cdf_tk/client/testing.py
@@ -21,7 +21,7 @@ from .api.extended_files import ExtendedFileMetadataAPI
 from .api.extended_functions import ExtendedFunctionsAPI
 from .api.extended_raw import ExtendedRawAPI
 from .api.extended_timeseries import ExtendedTimeSeriesAPI
-from .api.infield import InfieldAPI, InfieldConfigAPI
+from .api.infield import InfieldAPI, InFieldCDMConfigAPI, InfieldConfigAPI
 from .api.location_filters import LocationFiltersAPI
 from .api.lookup import (
     AssetLookUpAPI,
@@ -84,6 +84,7 @@ class ToolkitClientMock(CogniteClientMock):
         self.functions.schedules = MagicMock(spec_set=FunctionSchedulesAPI)
         self.infield = MagicMock(spec=InfieldAPI)
         self.infield.config = MagicMock(spec_set=InfieldConfigAPI)
+        self.infield.cdm_config = MagicMock(spec_set=InFieldCDMConfigAPI)
 
         self.project = MagicMock(spec_set=ProjectAPI)
 
