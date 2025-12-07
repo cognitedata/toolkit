4e2f9716404de9e29d3f7aa252950e46dd6fb2f4
anders-albert <anders0albert@gmail.com>
2025-12-07 14:10:07 +0100

    refactor; remove items

--- cognite_toolkit/_cdf_tk/utils/http_client/_data_classes2.py
+++ cognite_toolkit/_cdf_tk/utils/http_client/_data_classes2.py
@@ -1,22 +1,18 @@
 import gzip
-import json
 import sys
 from abc import ABC, abstractmethod
-from collections.abc import Hashable
-from typing import Any, Generic, Literal, TypeVar
+from typing import Any, Literal
 
 import httpx
 from cognite.client import global_config
-from pydantic import BaseModel, ConfigDict, Field, JsonValue, TypeAdapter, model_validator
-from pydantic.alias_generators import to_camel
+from pydantic import BaseModel, JsonValue, TypeAdapter, model_validator
 
-from cognite_toolkit._cdf_tk.utils.http_client._tracker import ItemsRequestTracker
 from cognite_toolkit._cdf_tk.utils.useful_types import PrimitiveType
 
 if sys.version_info >= (3, 11):
-    from typing import Self
+    pass
 else:
-    from typing_extensions import Self
+    pass
 
 
 class HTTPResult2(BaseModel): ...
@@ -102,89 +98,3 @@ class RequestMessage2(BaseRequestMessage):
             if not global_config.disable_gzip:
                 data = gzip.compress(data.encode("utf-8"))
         return data
-
-
-class ItemsResultMessage2(BaseModel):
-    ids: list[Hashable]
-
-
-class ItemsFailedRequestMessage2(ItemsResultMessage2):
-    error_message: str
-
-
-class ItemsSuccessResponseMessage2(ItemsResultMessage2):
-    status_code: int
-    body: str
-    content: bytes
-
-
-class ItemsFailedResponseMessage2(ItemsResultMessage2):
-    status_code: int
-    error: ErrorDetails2
-    body: str
-
-
-class BaseModelObject(BaseModel):
-    """Base class for all object. This includes resources and nested objects."""
-
-    # We allow extra fields to support forward compatibility.
-    model_config = ConfigDict(alias_generator=to_camel, extra="allow")
-
-    def dump(self, camel_case: bool = True) -> dict[str, Any]:
-        """Dump the resource to a dictionary.
-
-        This is the default serialization method for request resources.
-        """
-        return self.model_dump(mode="json", by_alias=camel_case)
-
-    @classmethod
-    def _load(cls, resource: dict[str, Any]) -> "Self":
-        """Load method to match CogniteResource signature."""
-        return cls.model_validate(resource)
-
-
-class RequestResource(BaseModelObject, ABC):
-    @abstractmethod
-    def as_id(self) -> Hashable: ...
-
-
-T_RequestResource = TypeVar("T_RequestResource", bound=RequestResource)
-
-
-def _set_default_tracker(data: dict[str, Any]) -> ItemsRequestTracker:
-    if "tracker" not in data or data["tracker"] is None:
-        return ItemsRequestTracker(data.get("max_failures_before_abort", 50))
-    return data["tracker"]
-
-
-class ItemsRequest2(Generic[T_RequestResource], BaseRequestMessage):
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-    items: list[T_RequestResource]
-    extra_body_fields: dict[str, JsonValue] | None = None
-    max_failures_before_abort: int = 50
-    tracker: ItemsRequestTracker = Field(init=False, default_factory=_set_default_tracker, exclude=True)
-
-    @property
-    def content(self) -> str:
-        body: dict[str, Any] = {
-            "items": [
-                item.model_dump(exclude_unset=False, by_alias=True, exclude_none=False, mode="json")
-                for item in self.items
-            ]
-        }
-        if self.extra_body_fields:
-            body.update(self.extra_body_fields)
-        return json.dumps(body)
-
-    def split(self, status_attempts: int) -> list["ItemsRequest2[T_RequestResource]"]:
-        """Split the request into multiple requests with a single item each."""
-        mid = len(self.items) // 2
-        if mid == 0:
-            return [self]
-        self.tracker.register_failure()
-        messages: list[ItemsRequest2[T_RequestResource]] = []
-        for part in (self.items[:mid], self.items[mid:]):
-            new_request = self.model_copy(update={"items": part, "status_attempt": status_attempts})
-            new_request.tracker = self.tracker
-            messages.append(new_request)
-        return messages
