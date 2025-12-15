c194564217669c71b64749c4aa896f93031c965f
anders-albert <anders0albert@gmail.com>
2025-12-15 07:43:18 +0100

    refactor: first pass on implementation

--- cognite_toolkit/_cdf_tk/client/api/assets.py
+++ cognite_toolkit/_cdf_tk/client/api/assets.py
@@ -1,9 +1,21 @@
 from collections.abc import Sequence
+from dataclasses import dataclass
 
 from cognite.client.utils.useful_types import SequenceNotStr
 
 from cognite_toolkit._cdf_tk.client.cdf_client._client import CDFClient
 from cognite_toolkit._cdf_tk.client.data_classes.assets import AssetRequest, AssetResponse
+from cognite_toolkit._cdf_tk.utils.http_client import (
+    ItemsFailedRequest2,
+    ItemsFailedResponse2,
+)
+
+
+@dataclass
+class HTTPResult:
+    successes: list[AssetResponse]
+    failed_requests: list[ItemsFailedRequest2]
+    failed_responses: list[ItemsFailedResponse2]
 
 
 class AssetsAPI:
@@ -24,9 +36,21 @@ class AssetsAPI:
         Returns:
             list[AssetResponse]: The created assets.
         """
-        raise NotImplementedError()
+        return self._cdf_client.request_resource_items(
+            items=assets,
+            endpoint_url=self._cdf_client.config.create_api_url(self.ENDPOINT),
+            method="POST",
+            request_limit=self.CREATE_REQUEST_LIMIT,
+            response_type=AssetResponse,
+        )
 
-    def retrieve(self, ids: Sequence[int], external_ids: SequenceNotStr[str]) -> list[AssetResponse]:
+    def retrieve(
+        self,
+        ids: Sequence[int],
+        external_ids: SequenceNotStr[str],
+        ignore_unknown_ids: bool = False,
+        aggregated_properties: bool = False,
+    ) -> list[AssetResponse]:
         """Retrieve assets by their IDs or external IDs.
 
         Args:
@@ -35,7 +59,17 @@ class AssetsAPI:
         Returns:
             list[AssetResponse]: The retrieved assets.
         """
-        raise NotImplementedError()
+        return self._cdf_client.request_resource_items(
+            items=...,
+            endpoint_url=self._cdf_client.config.create_api_url(f"{self.ENDPOINT}/byids"),
+            method="POST",
+            request_limit=self.RETRIEVE_REQUEST_LIMIT,
+            response_type=AssetResponse,
+            extra_body_fields={
+                "ignoreUnknownIds": ignore_unknown_ids,
+                "aggregatedProperties": ["childCount", "depth", "path"] if aggregated_properties else [],
+            },
+        )
 
     def update(self, assets: Sequence[AssetRequest]) -> list[AssetResponse]:
         """Update assets.
@@ -45,7 +79,13 @@ class AssetsAPI:
         Returns:
             list[AssetResponse]: The updated assets.
         """
-        raise NotImplementedError()
+        self._cdf_client.request_resource_items(
+            items=assets,
+            endpoint_url=self._cdf_client.config.create_api_url(f"{self.ENDPOINT}/update"),
+            method="POST",
+            request_limit=self.RETRIEVE_REQUEST_LIMIT,
+            response_type=AssetResponse,
+        )
 
     def delete(
         self,
@@ -60,4 +100,14 @@ class AssetsAPI:
             ids (Sequence[int]): The IDs of the assets to delete.
             external_ids (Sequence[str]): The external IDs of the assets to delete.
         """
-        raise NotImplementedError()
+        self._cdf_client.request_resource_items(
+            items=...,
+            endpoint_url=self._cdf_client.config.create_api_url(f"{self.ENDPOINT}/delete"),
+            method="POST",
+            request_limit=self.RETRIEVE_REQUEST_LIMIT,
+            response_type=AssetResponse,
+            extra_body_fields={
+                "recursive": recursive,
+                "ignoreUnknownIds": ignore_unknown_ids,
+            },
+        )
--- cognite_toolkit/_cdf_tk/client/cdf_client/_client.py
+++ cognite_toolkit/_cdf_tk/client/cdf_client/_client.py
@@ -1,22 +1,29 @@
+import time
 from collections.abc import Sequence
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import Future, ThreadPoolExecutor
+from typing import Literal
 
+from pydantic import JsonValue, TypeAdapter
 from rich import Console
 
+from cognite_toolkit._cdf_tk.client.data_classes.base import T_ResponseResource
+from cognite_toolkit._cdf_tk.utils.collection import chunker_sequence
 from cognite_toolkit._cdf_tk.utils.http_client import (
     HTTPClient,
     HTTPResult2,
     ItemsRequest2,
     ItemsResultMessage2,
     RequestMessage2,
+    RequestResource,
 )
+from cognite_toolkit._cdf_tk.utils.http_client._data_classes2 import ItemsResultList
 
 
 class CDFClient:
     def __init__(self, http_client: HTTPClient, console: Console, max_workers: int = 8) -> None:
         self.http_client = http_client
         self.console = console
-        self._config = http_client.config
+        self.config = http_client.config
         self._pool = ThreadPoolExecutor(max_workers=max_workers, thread_name_prefix="cdf")
 
     def _close(self) -> None:
@@ -39,12 +46,29 @@ class CDFClient:
                 f"max_concurrent {max_concurrent} exceeds the thread pool max_workers {self._pool._max_workers}"
             )
         to_request = list(requests)
+        futures: list[Future] = []
         results: list[HTTPResult2] = []
-        raise NotImplementedError()
+        concurrent_count = 0
+        while to_request or futures:
+            if max_concurrent is None or concurrent_count < max_concurrent:
+                item = to_request.pop()
+                concurrent_count += 1
+                futures.append(self._pool.submit(self.http_client.request_single_retries, item))
+            else:
+                ongoing_futures: list[Future] = []
+                for future in futures:
+                    if future.done():
+                        results.append(future.result())
+                        concurrent_count -= 1
+                    else:
+                        ongoing_futures.append(future)
+                futures = ongoing_futures
+                time.sleep(0.5)
+        return results
 
     def request_items_concurrent(
         self, requests: Sequence[ItemsRequest2], max_concurrent: int | None = None
-    ) -> list[ItemsResultMessage2]:
+    ) -> ItemsResultList:
         """Send multiple item requests concurrently.
 
         Args:
@@ -58,4 +82,61 @@ class CDFClient:
             raise ValueError(
                 f"max_concurrent {max_concurrent} exceeds the thread pool max_workers {self._pool._max_workers}"
             )
-        raise NotImplementedError()
+        to_request = list(requests)
+        futures: list[Future] = []
+        results = ItemsResultList()
+        concurrent_count = 0
+        while to_request or futures:
+            if to_request and (max_concurrent is None or concurrent_count < max_concurrent):
+                item = to_request.pop()
+                concurrent_count += 1
+                futures.append(self._pool.submit(self.http_client.request_items, item))
+            else:
+                ongoing_futures: list[Future] = []
+                for future in futures:
+                    if future.done():
+                        result = future.result()
+                        for result_item in result:
+                            if isinstance(result_item, ItemsRequest2):
+                                to_request.append(result_item)
+                            elif isinstance(result_item, ItemsResultMessage2):
+                                results.append(result_item)
+                            else:
+                                raise TypeError(f"Unexpected result type: {type(result)}")
+                        concurrent_count -= 1
+                    else:
+                        ongoing_futures.append(future)
+                futures = ongoing_futures
+                time.sleep(0.5)
+        return results
+
+    def request_resource_items(
+        self,
+        items: Sequence[RequestResource],
+        endpoint_url: str,
+        method: Literal["GET", "POST", "PATCH", "DELETE", "PUT"],
+        request_limit: int,
+        response_type: type[T_ResponseResource],
+        extra_body_fields: dict[str, JsonValue] | None = None,
+        max_concurrent: int | None = None,
+    ) -> list[T_ResponseResource]:
+        """Send item requests for resources.
+
+        Args:
+            items (Sequence[RequestResource]): The resources to send in the requests.
+            endpoint_url (str): The endpoint URL for the requests.
+            method (str): The HTTP method for the requests.
+            request_limit (int): The maximum number of items per request.
+            response_type (type[T_ResponseResource]): The type of the response resources.
+            max_concurrent (int | None): The maximum number of concurrent requests. If None, uses the default from the thread pool.
+
+        Returns:
+            list[T_ResponseResource]: The response resources.
+        """
+        request_items = [
+            ItemsRequest2(endpoint_url=endpoint_url, method=method, items=chunk, extra_body_fields=extra_body_fields)
+            for chunk in chunker_sequence(items, request_limit)
+        ]
+        response = self.request_items_concurrent(request_items, max_concurrent)
+        response.raise_for_status()
+        return TypeAdapter(list[response_type]).validate_python(response.get_items())
