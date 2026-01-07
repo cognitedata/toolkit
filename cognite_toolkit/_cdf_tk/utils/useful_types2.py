650767ee4aa4d45bbbd31490d41c7d2c42d4e207
anders-albert <anders0albert@gmail.com>
2026-01-07 15:06:52 +0100

    refactor; update

--- cognite_toolkit/_cdf_tk/utils/useful_types2.py
+++ cognite_toolkit/_cdf_tk/utils/useful_types2.py
@@ -2,11 +2,13 @@
 
 from typing import TypeAlias, TypeVar
 
-from cognite.client.data_classes import Annotation, Event, FileMetadata, TimeSeries
+from cognite.client.data_classes import Annotation, FileMetadata
 
 from cognite_toolkit._cdf_tk.client.data_classes.asset import AssetResponse
+from cognite_toolkit._cdf_tk.client.data_classes.event import EventResponse
+from cognite_toolkit._cdf_tk.client.data_classes.timeseries import TimeSeriesResponse
 
-AssetCentricResource: TypeAlias = AssetResponse | FileMetadata | Event | TimeSeries
-AssetCentricResourceExtended: TypeAlias = AssetResponse | FileMetadata | Event | TimeSeries | Annotation
+AssetCentricResource: TypeAlias = AssetResponse | FileMetadata | EventResponse | TimeSeriesResponse
+AssetCentricResourceExtended: TypeAlias = AssetResponse | FileMetadata | EventResponse | TimeSeriesResponse | Annotation
 T_AssetCentricResource = TypeVar("T_AssetCentricResource", bound=AssetCentricResource)
 T_AssetCentricResourceExtended = TypeVar("T_AssetCentricResourceExtended", bound=AssetCentricResourceExtended)
