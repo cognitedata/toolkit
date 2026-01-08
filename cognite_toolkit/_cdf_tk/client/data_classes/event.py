14d78dcc149bc70c84639593c18b4f8dcbf90eb4
anders-albert <anders0albert@gmail.com>
2026-01-08 13:07:11 +0100

    refactor: simplify

--- cognite_toolkit/_cdf_tk/client/data_classes/event.py
+++ cognite_toolkit/_cdf_tk/client/data_classes/event.py
@@ -1,9 +1,9 @@
-from cognite_toolkit._cdf_tk.client.data_classes.base import RequestResource, ResponseResource
+from cognite_toolkit._cdf_tk.client.data_classes.base import BaseModelObject, RequestResource, ResponseResource
 
 from .identifiers import ExternalId, InternalOrExternalId
 
 
-class EventRequest(RequestResource):
+class Event(BaseModelObject):
     external_id: str | None = None
     data_set_id: int | None = None
     start_time: int | None = None
@@ -21,17 +21,10 @@ class EventRequest(RequestResource):
         return ExternalId(external_id=self.external_id)
 
 
-class EventResponse(ResponseResource[EventRequest]):
-    external_id: str | None = None
-    data_set_id: int | None = None
-    start_time: int | None = None
-    end_time: int | None = None
-    type: str | None = None
-    subtype: str | None = None
-    description: str | None = None
-    metadata: dict[str, str] | None = None
-    asset_ids: list[int] | None = None
-    source: str | None = None
+class EventRequest(Event, RequestResource): ...
+
+
+class EventResponse(Event, ResponseResource[EventRequest]):
     id: int
     created_time: int
     last_updated_time: int
--- cognite_toolkit/_cdf_tk/client/data_classes/timeseries.py
+++ cognite_toolkit/_cdf_tk/client/data_classes/timeseries.py
@@ -1,10 +1,10 @@
-from cognite_toolkit._cdf_tk.client.data_classes.base import RequestResource, ResponseResource
+from cognite_toolkit._cdf_tk.client.data_classes.base import BaseModelObject, RequestResource, ResponseResource
 
 from .identifiers import ExternalId, InternalOrExternalId
 from .instance_api import NodeReference
 
 
-class TimeSeriesRequest(RequestResource):
+class TimeSeries(BaseModelObject):
     external_id: str | None = None
     name: str | None = None
     is_sting: bool = False
@@ -23,21 +23,13 @@ class TimeSeriesRequest(RequestResource):
         return ExternalId(external_id=self.external_id)
 
 
-class TimeSeriesResponse(ResponseResource[TimeSeriesRequest]):
+class TimeSeriesRequest(TimeSeries, RequestResource): ...
+
+
+class TimeSeriesResponse(TimeSeries, ResponseResource[TimeSeriesRequest]):
     id: int
     instance_id: NodeReference | None = None
-    external_id: str | None = None
-    name: str | None = None
-    is_sting: bool
-    metadata: dict[str, str] | None = None
-    unit: str | None = None
     type: str
-    unit_external_id: str | None = None
-    asset_id: int | None = None
-    is_step: bool
-    description: str | None = None
-    security_categories: list[int] | None = None
-    data_set_id: int | None = None
     created_time: int
     last_updated_time: int
 
