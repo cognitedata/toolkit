847a77d98ef01bebd139d3aa74463478c57720af
anders-albert <anders0albert@gmail.com>
2026-01-18 08:00:40 +0100

    feat; first pass

---
+++ cognite_toolkit/_cdf_tk/client/resource_classes/resource_view_mapping.py
@@ -0,0 +1,31 @@
+from cognite_toolkit._cdf_tk.client.resource_classes.base import (
+    BaseModelObject,
+    RequestResource,
+    ResponseResource,
+)
+
+from .data_modeling import ViewReference
+from .identifiers import ExternalId
+
+
+class ResourceViewMapping(BaseModelObject):
+    external_id: str
+    resource_type: str
+    view_id: ViewReference
+    property_mapping: dict[str, str]
+
+    def as_id(self) -> ExternalId:
+        return ExternalId(external_id=self.external_id)
+
+
+class ResourceViewMappingRequest(ResourceViewMapping, RequestResource):
+    existing_version: int | None = None
+
+
+class ResourceViewMappingResponse(ResourceViewMapping, ResponseResource[ResourceViewMappingRequest]):
+    version: int
+    created_time: int
+    last_updated_time: int
+
+    def as_request_resource(self) -> ResourceViewMappingRequest:
+        return ResourceViewMappingRequest.model_validate(self.dump(), extra="ignore")
