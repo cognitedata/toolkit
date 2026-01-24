971af2a008611bce7679c384b8f2eefa1eb6f3a5
anders-albert <anders0albert@gmail.com>
2026-01-24 08:26:20 +0100

    tests: updaet tests

--- tests/test_integration/test_utils/test_http_client.py
+++ tests/test_integration/test_utils/test_http_client.py
@@ -1,36 +1,32 @@
 import json
 
 from cognite_toolkit._cdf_tk.client import ToolkitClientConfig
-from cognite_toolkit._cdf_tk.client.http_client import HTTPClient, ParamRequest, SimpleBodyRequest, SuccessResponse
+from cognite_toolkit._cdf_tk.client.http_client import HTTPClient, RequestMessage2, SuccessResponse2
 
 
 class TestHttpClient:
     def test_get_request(self, toolkit_client_config: ToolkitClientConfig) -> None:
         config = toolkit_client_config
         with HTTPClient(config) as client:
-            result = client.request(
-                ParamRequest(
+            response = client.request_single(
+                RequestMessage2(
                     endpoint_url=config.create_api_url(""),
                     method="GET",
                 )
             )
 
-        assert len(result) == 1
-        response = result[0]
-        assert isinstance(response, SuccessResponse)
+        assert isinstance(response, SuccessResponse2)
         assert response.status_code == 200
 
     def test_post_request(self, toolkit_client_config: ToolkitClientConfig) -> None:
         config = toolkit_client_config
         with HTTPClient(config) as client:
-            result = client.request(
-                SimpleBodyRequest(
+            response = client.request_single(
+                RequestMessage2(
                     endpoint_url=config.create_api_url("timeseries/list"), method="POST", body_content={"limit": 1}
                 )
             )
-        assert len(result) == 1
-        response = result[0]
-        assert isinstance(response, SuccessResponse)
+        assert isinstance(response, SuccessResponse2)
         assert response.status_code == 200
         response_body = json.loads(response.body)
         assert "items" in response_body
