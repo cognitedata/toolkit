3140973d393763b416a54c96e38e34db898c1009
anders-albert <anders0albert@gmail.com>
2026-02-06 22:27:51 +0100

    tests: smoke tests

--- tests_smoke/test_client/conftest.py
+++ tests_smoke/test_client/conftest.py
@@ -2,35 +2,38 @@ import pytest
 
 from cognite_toolkit._cdf_tk.client import ToolkitClient
 from cognite_toolkit._cdf_tk.client.resource_classes.data_modeling import (
+    ContainerPropertyDefinition,
     ContainerRequest,
-    DataModelRequest,
-    SpaceRequest,
-    ViewRequest,
     ContainerResponse,
     SpaceResponse,
-    ContainerPropertyDefinition,
-    TextProperty, ViewResponse, ViewCorePropertyRequest,
+    TextProperty,
+    ViewCorePropertyRequest,
+    ViewRequest,
+    ViewResponse,
 )
-from tests_smoke.constants import SMOKE_SPACE, SMOKE_TEST_VIEW_EXTERNAL_ID, SMOKE_TEST_CONTAINER_EXTERNAL_ID
+from tests_smoke.constants import SMOKE_SPACE, SMOKE_TEST_CONTAINER_EXTERNAL_ID, SMOKE_TEST_VIEW_EXTERNAL_ID
 
 
 @pytest.fixture(scope="session")
 def smoke_container(toolkit_client: ToolkitClient, smoke_space: SpaceResponse) -> ContainerResponse:
-    return toolkit_client.tool.containers.create([
+    return toolkit_client.tool.containers.create(
+        [
             ContainerRequest(
                 space=SMOKE_SPACE,
                 external_id=SMOKE_TEST_CONTAINER_EXTERNAL_ID,
                 name="Toolkit Smoke Test Container",
-                properties={
-                    "name": ContainerPropertyDefinition(type=TextProperty())
-                }
+                properties={"name": ContainerPropertyDefinition(type=TextProperty())},
             )
         ]
-    )
+    )[0]
+
 
 @pytest.fixture(scope="session")
-def smoke_view(toolkit_client: ToolkitClient, smoke_space: SpaceResponse, smoke_container: ContainerResponse) -> ViewResponse:
-    return toolkit_client.tool.views.create([
+def smoke_view(
+    toolkit_client: ToolkitClient, smoke_space: SpaceResponse, smoke_container: ContainerResponse
+) -> ViewResponse:
+    return toolkit_client.tool.views.create(
+        [
             ViewRequest(
                 space=SMOKE_SPACE,
                 external_id=SMOKE_TEST_VIEW_EXTERNAL_ID,
@@ -38,10 +41,9 @@ def smoke_view(toolkit_client: ToolkitClient, smoke_space: SpaceResponse, smoke_
                 name="Toolkit Smoke Test View",
                 properties={
                     "name": ViewCorePropertyRequest(
-                        container=smoke_container.as_id(),
-                        container_property_identifier="name"
+                        container=smoke_container.as_id(), container_property_identifier="name"
                     )
-                }
+                },
             )
         ]
     )
