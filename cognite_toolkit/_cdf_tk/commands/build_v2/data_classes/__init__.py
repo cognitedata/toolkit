723868e5ab465d7676c184a77eba6c9f79de409e
anders-albert <anders0albert@gmail.com>
2026-02-10 13:21:36 +0100

    fix: import

--- cognite_toolkit/_cdf_tk/commands/build_v2/data_classes/__init__.py
+++ cognite_toolkit/_cdf_tk/commands/build_v2/data_classes/__init__.py
@@ -3,8 +3,8 @@ from ._built_results import BuiltResult
 from ._insights import ConsistencyError, Insight, InsightList, ModelSyntaxError, Recommendation
 from ._module_results import ModuleResult
 from ._modules_list import ModuleList
+from ._read_module import ReadModule
 from ._selected_module import SelectedModule
-from .read_module import ReadModule
 
 __all__ = [
     "BuiltModule",
