e4b49f6fe36d3860f391abbe7eeb4757175c34d5
anders-albert <anders0albert@gmail.com>
2026-02-10 13:49:07 +0100

    tests: finished test

--- tests/test_unit/test_cdf_tk/test_commands/test_buildv2/test_command.py
+++ tests/test_unit/test_cdf_tk/test_commands/test_buildv2/test_command.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from cognite_toolkit._cdf_tk.commands import BuildV2Command
 from cognite_toolkit._cdf_tk.commands.build_v2.data_classes import BuildParameters
+from cognite_toolkit._cdf_tk.cruds import SpaceCRUD
 
 
 class TestBuildCommand:
@@ -9,13 +10,13 @@ class TestBuildCommand:
         cmd = BuildV2Command()
 
         org = tmp_path / "org"
-        org.mkdir()
+        resource_file = org / "modules" / "my_module" / SpaceCRUD.folder_name / f"my_space.{SpaceCRUD.kind}.yaml"
+        resource_file.parent.mkdir(parents=True)
+        space_yaml = """space: my_space
+name: My Space"""
+        resource_file.write_text(space_yaml)
 
-        parameters = BuildParameters(
-            organization_dir=org,
-            build_dir=tmp_path / "build",
-            build_env_name="dev",
-        )
+        parameters = BuildParameters(organization_dir=org, build_dir=tmp_path / "build", build_env_name="dev")
 
         result = cmd.build_folder(parameters)
 
