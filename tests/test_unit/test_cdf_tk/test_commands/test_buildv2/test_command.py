ecd55c9088dcdd74492aaf0adba6dc9e352813fe
anders-albert <anders0albert@gmail.com>
2026-02-10 14:29:25 +0100

    tests: improve tests

--- tests/test_unit/test_cdf_tk/test_commands/test_buildv2/test_command.py
+++ tests/test_unit/test_cdf_tk/test_commands/test_buildv2/test_command.py
@@ -9,15 +9,21 @@ class TestBuildCommand:
     def test_end_to_end(self, tmp_path: Path) -> None:
         cmd = BuildV2Command()
 
+        # Set up a simple organization with modules folder.
         org = tmp_path / "org"
         resource_file = org / "modules" / "my_module" / SpaceCRUD.folder_name / f"my_space.{SpaceCRUD.kind}.yaml"
         resource_file.parent.mkdir(parents=True)
         space_yaml = """space: my_space
-name: My Space"""
+name: My Space
+"""
         resource_file.write_text(space_yaml)
-
-        parameters = BuildParameters(organization_dir=org, build_dir=tmp_path / "build", build_env_name="dev")
+        build_dir = tmp_path / "build"
+        parameters = BuildParameters(organization_dir=org, build_dir=build_dir, build_env_name="dev")
 
         result = cmd.build_folder(parameters)
 
         assert result
+
+        built_space = list(build_dir.rglob(f"*.{SpaceCRUD.kind}.yaml"))
+        assert len(built_space) == 1
+        assert built_space[0].read_text() == space_yaml
