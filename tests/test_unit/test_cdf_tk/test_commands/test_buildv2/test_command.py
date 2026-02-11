8b78807943538fd288dd01bf36082aff44c89383
anders-albert <anders0albert@gmail.com>
2026-02-11 13:17:08 +0100

    tests: update

--- tests/test_unit/test_cdf_tk/test_commands/test_buildv2/test_command.py
+++ tests/test_unit/test_cdf_tk/test_commands/test_buildv2/test_command.py
@@ -18,7 +18,7 @@ name: My Space
 """
         resource_file.write_text(space_yaml)
         build_dir = tmp_path / "build"
-        parameters = BuildParameters(organization_dir=org, build_dir=build_dir, config_yaml_name="dev")
+        parameters = BuildParameters(organization_dir=org, build_dir=build_dir)
 
         result = cmd.build(parameters)
 
@@ -40,9 +40,9 @@ name: My Space
 """
         resource_file.write_text(space_yaml)
         build_dir = tmp_path / "build"
-        parameters = BuildParameters(organization_dir=org, build_dir=build_dir, build_env_name="dev")
+        parameters = BuildParameters(organization_dir=org, build_dir=build_dir)
 
-        result = cmd.build_folder(parameters)
+        result = cmd.build(parameters)
 
         assert result
 
