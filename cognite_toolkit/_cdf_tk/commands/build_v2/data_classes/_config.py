c32c296351d4ea4e9929784f365ae8ad2d27e2d2
anders-albert <anders0albert@gmail.com>
2026-02-11 15:29:03 +0100

    refactor: first pass

--- cognite_toolkit/_cdf_tk/commands/build_v2/data_classes/_config.py
+++ cognite_toolkit/_cdf_tk/commands/build_v2/data_classes/_config.py
@@ -1,10 +1,21 @@
-from typing import ClassVar
+import os
+from typing import ClassVar, Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, JsonValue
+
+
+class Environment(BaseModel):
+    name: str = "dev"
+    project: str = Field(default_factory=lambda: os.environ.get("CDF_PROJECT", "UNKNOWN"))
+    validation_type: Literal["dev", "prod"] = "dev"
+    selected: list[str] | None = None
 
 
 class ConfigYAML(BaseModel):
     filename: ClassVar[str] = "config.{name}.yaml"
+
+    environment: Environment = Field(default_factory=Environment)
+    variables: dict[str, JsonValue] | None = None
 
     @classmethod
     def get_filename(cls, name: str) -> str:
