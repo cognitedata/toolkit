8b7b0943174513ea6f3794905ff478490fcf1250
nikokaoja <nikola.vasiljevic@cognite.com>
2026-02-12 15:39:29 +0100

    fix test

--- tests/test_unit/test_cdf_tk/test_commands/test_buildv2/test_command.py
+++ tests/test_unit/test_cdf_tk/test_commands/test_buildv2/test_command.py
@@ -144,11 +144,11 @@ class TestValidateBuildParameters:
         console = MagicMock(spec=Console)
         if expected_error:
             with pytest.raises(ToolkitError) as exc_info:
-                BuildV2Command.validate_build_parameters(parameters, console, user_args)
+                BuildV2Command._validate_build_parameters(parameters, console, user_args)
 
             assert expected_error in str(exc_info.value).replace("\\", "/")  # Normalize paths for windows
         else:
-            BuildV2Command.validate_build_parameters(parameters, console, user_args)
+            BuildV2Command._validate_build_parameters(parameters, console, user_args)
 
 
 class TestReadParameters:
