c1141ddd5111eb6f2f4dd3e0436e52a5e6e831e6
anders-albert <anders0albert@gmail.com>
2026-02-13 13:45:05 +0100

    refactor: cleanup

--- cognite_toolkit/_cdf_tk/resource_classes/base.py
+++ cognite_toolkit/_cdf_tk/resource_classes/base.py
@@ -1,24 +1,8 @@
-from dataclasses import dataclass
-from typing import ClassVar
-
-from cognite.client.data_classes._base import CogniteResource
-from cognite.client.utils._text import to_camel_case
 from pydantic import BaseModel
+from pydantic.alias_generators import to_camel
 
 
-class BaseModelResource(BaseModel, alias_generator=to_camel_case, extra="forbid"): ...
+class BaseModelResource(BaseModel, alias_generator=to_camel, extra="forbid"): ...
 
 
-class ToolkitResource(BaseModelResource):
-    _cdf_resource: ClassVar[type[CogniteResource]]
-
-    def as_cognite_sdk_resource(self) -> CogniteResource:
-        """Convert the model to a CDF resource."""
-        return self._cdf_resource.load(self.model_dump(exclude_unset=True))
-
-
-@dataclass(frozen=True)
-class UnusedFieldWarning(Warning):
-    """Warning for unused fields in the model."""
-
-    field_names: frozenset[str]
+class ToolkitResource(BaseModelResource): ...
