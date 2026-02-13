814a7355a6856653dcbadd97758928eca63e6720
anders-albert <anders0albert@gmail.com>
2026-02-13 08:02:14 +0100

    tests: first tests

--- tests/test_unit/test_cdf_tk/test_commands/test_buildv2/test_module_source_parser.py
+++ tests/test_unit/test_cdf_tk/test_commands/test_buildv2/test_module_source_parser.py
@@ -1,1 +1,23 @@
-class TestModuleSourceParser: ...
+from pathlib import Path
+
+import pytest
+
+from cognite_toolkit._cdf_tk.commands.build_v2._module_source_parser import ModuleSourceParser
+
+
+class TestModuleSourceParser:
+    @pytest.mark.parametrize(
+        "yaml_files, expected_modules",
+        [
+            pytest.param(
+                [
+                    "modules/moduleA/data_modeling/my.Space.yaml",
+                ],
+                {"modules/moduleA": ["modules/moduleA/data_modeling/my.Space.yaml"]},
+                id="Single module with one YAML file",
+            )
+        ],
+    )
+    def test_find_modules(self, yaml_files: list[str], expected_modules: dict[str, list[str]]):
+        actual_modules = ModuleSourceParser._find_modules([Path(yaml_file) for yaml_file in yaml_files])
+        assert actual_modules == expected_modules
