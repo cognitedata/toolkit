da7a13cc4bc9b5660c92c7bb70c1fb755a3fb36b
Anders Albert <60234212+doctrino@users.noreply.github.com>
2026-02-19 13:54:08 +0100

    [prep CDF-27040] 🪨Adjust ModuleSource for import function (#2535)
    # Description
    
    In the `_parse_module_sources`, we currently lose information about
    which resource folder a file belongs to. This PR ensures that we keep
    that information such that we do not have to search for it in the
    `_import_module` function.
    
    ## Bump
    
    - [ ] Patch
    - [x] Skip

--- cognite_toolkit/_cdf_tk/commands/build_v2/_module_source_parser.py
+++ cognite_toolkit/_cdf_tk/commands/build_v2/_module_source_parser.py
@@ -1,6 +1,6 @@
-from collections import defaultdict
+from collections.abc import Iterable
 from pathlib import Path
-from typing import Any
+from typing import Any, cast
 
 from cognite_toolkit._cdf_tk.commands.build_v2.data_classes import (
     AbsoluteDirPath,
@@ -8,10 +8,11 @@ from cognite_toolkit._cdf_tk.commands.build_v2.data_classes import (
     ModelSyntaxError,
     ModuleSource,
     RelativeDirPath,
+    RelativeFilePath,
 )
 from cognite_toolkit._cdf_tk.commands.build_v2.data_classes._module import BuildVariable
 from cognite_toolkit._cdf_tk.constants import EXCL_FILES
-from cognite_toolkit._cdf_tk.cruds import CRUDS_BY_FOLDER_NAME_INCLUDE_ALPHA
+from cognite_toolkit._cdf_tk.cruds import CRUDS_BY_FOLDER_NAME_INCLUDE_ALPHA, ResourceTypes
 
 
 class ModuleSourceParser:
@@ -23,34 +24,26 @@ class ModuleSourceParser:
         self.organization_dir = organization_dir
         self.errors = InsightList()
 
-    def parse(self, yaml_files: list[RelativeDirPath], variables: dict[str, Any]) -> list[ModuleSource]:
-        files_by_module, orphans = self._find_modules(yaml_files)
-        errors = self._validate_modules(list(files_by_module.keys()), self.selected_modules, orphans)
+    def parse(self, yaml_files: list[RelativeFilePath], variables: dict[str, Any]) -> list[ModuleSource]:
+        source_by_module_id, orphans = self._find_modules(yaml_files, self.organization_dir)
+        module_ids = list(source_by_module_id.keys())
+        errors = self._validate_modules(module_ids, self.selected_modules, orphans)
         if errors:
             self.errors.extend(errors)
             return []
-        selected_modules = self._select_modules(files_by_module, self.selected_modules)
-        build_variables, errors = self._parse_variables(variables, set(files_by_module.keys()), set(selected_modules))
+        selected_modules = self._select_modules(module_ids, self.selected_modules)
+        build_variables, errors = self._parse_variables(variables, set(module_ids), set(selected_modules))
         if errors:
             self.errors.extend(errors)
             return []
         module_sources: list[ModuleSource] = []
         for module in selected_modules:
-            source = ModuleSource(
-                path=self.organization_dir / module,
-                id=module,
-                resource_files=[self.organization_dir / resource_file for resource_file in files_by_module[module]],
-            )
+            source = source_by_module_id[module]
             module_build_variables = build_variables.get(module, [])
             if module_build_variables:
                 for iteration, module_variable in enumerate(module_build_variables, start=1):
                     module_sources.append(
-                        source.model_copy(
-                            update={
-                                "variables": module_variable,
-                                "iteration": iteration,
-                            }
-                        )
+                        source.model_copy(update={"variables": module_variable, "iteration": iteration})
                     )
             else:
                 module_sources.append(source)
@@ -58,27 +51,37 @@ class ModuleSourceParser:
 
     @classmethod
     def _find_modules(
-        cls, yaml_files: list[RelativeDirPath]
-    ) -> tuple[dict[RelativeDirPath, list[RelativeDirPath]], list[RelativeDirPath]]:
+        cls, yaml_files: list[RelativeFilePath], organization_dir: Path
+    ) -> tuple[dict[RelativeDirPath, ModuleSource], list[RelativeDirPath]]:
         """Organizes YAML files by their module (top-level folder in the modules directory)."""
-        files_by_module: dict[RelativeDirPath, list[RelativeDirPath]] = defaultdict(list)
+        source_by_module_id: dict[RelativeDirPath, ModuleSource] = {}
         orphan_files: list[RelativeDirPath] = []
         for yaml_file in yaml_files:
             if yaml_file.name in EXCL_FILES:
                 continue
-            module_path = cls._get_module_path_from_resource_file_path(yaml_file)
-            if module_path:
-                files_by_module[module_path].append(yaml_file)
+            relative_module_path, resource_folder = cls._get_module_path_from_resource_file_path(yaml_file)
+            if relative_module_path and resource_folder:
+                if relative_module_path not in source_by_module_id:
+                    source_by_module_id[relative_module_path] = ModuleSource(
+                        path=organization_dir / relative_module_path,
+                        id=relative_module_path,
+                    )
+                source = source_by_module_id[relative_module_path]
+                if resource_folder not in source.resource_files_by_folder:
+                    source.resource_files_by_folder[resource_folder] = []
+                source.resource_files_by_folder[resource_folder].append(organization_dir / yaml_file)
             else:
                 orphan_files.append(yaml_file)
-        return dict(files_by_module), orphan_files
+        return source_by_module_id, orphan_files
 
     @staticmethod
-    def _get_module_path_from_resource_file_path(resource_file: Path) -> Path | None:
+    def _get_module_path_from_resource_file_path(resource_file: Path) -> tuple[Path | None, ResourceTypes | None]:
         for parent in resource_file.parents:
             if parent.name in CRUDS_BY_FOLDER_NAME_INCLUDE_ALPHA:
-                return parent.parent
-        return None
+                # We know that all keys in CRUDS_BY_FOLDER_NAME_INCLUDE_ALPHA are valid ResourceTypes,
+                # so this cast is safe.
+                return parent.parent, cast(ResourceTypes, parent.name)
+        return None, None
 
     @classmethod
     def _validate_modules(
@@ -96,11 +99,11 @@ class ModuleSourceParser:
 
     @classmethod
     def _select_modules(
-        cls, files_by_module: dict[RelativeDirPath, list[RelativeDirPath]], selection: set[RelativeDirPath | str]
+        cls, module_paths: Iterable[RelativeDirPath], selection: set[RelativeDirPath | str]
     ) -> list[RelativeDirPath]:
         return [
             module_path
-            for module_path in files_by_module.keys()
+            for module_path in module_paths
             if module_path in selection
             or module_path.name in selection
             or any(parent in selection for parent in module_path.parents)
