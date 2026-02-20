51628e49d9261f35f0fa13b72fdf240c9e6bc1e2
Nikola Vasiljevic <35523348+nikokaoja@users.noreply.github.com>
2026-02-20 12:26:54 +0100

    🔨🔨🔨Introduction of Toolkit rules (#2554)
    # Description
    
    This PR brings new module focused on definition of Toolkit Rules. In
    addition rules orchestrator and registry has been developed providing an
    elegant way to perform post-syntax validation on a module level.
    Accordingly build command v2 has been updated.
    
    ## Bump
    
    - [ ] Patch
    - [x] Skip

---
+++ cognite_toolkit/_cdf_tk/rules/__init__.py
@@ -0,0 +1,5 @@
+from ._base import ToolkitResourceRule
+from ._dummy import DummyDataModelRule
+from ._orchestrator import RulesOrchestrator
+
+__all__ = ["DummyDataModelRule", "RulesOrchestrator", "ToolkitResourceRule"]
