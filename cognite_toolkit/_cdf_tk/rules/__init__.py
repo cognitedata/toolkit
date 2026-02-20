36c9092dde14318386394d156210da0cce325fbb
nikokaoja <nikola.vasiljevic@cognite.com>
2026-02-20 13:10:59 +0100

    update tests

--- cognite_toolkit/_cdf_tk/rules/__init__.py
+++ cognite_toolkit/_cdf_tk/rules/__init__.py
@@ -1,5 +1,6 @@
 from ._base import ToolkitResourceRule
 from ._dummy import DummyDataModelRule
 from ._orchestrator import RulesOrchestrator
+from ._workflow import WorkflowDatasetMissing
 
-__all__ = ["DummyDataModelRule", "RulesOrchestrator", "ToolkitResourceRule"]
+__all__ = ["DummyDataModelRule", "RulesOrchestrator", "ToolkitResourceRule", "WorkflowDatasetMissing"]
