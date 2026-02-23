00b503d02f4ccb1e6fe59e132bae73efbc2dabfa
anders-albert <anders0albert@gmail.com>
2026-02-23 15:17:00 +0100

    refactor: remove unused

--- cognite_toolkit/_cdf_tk/client/resource_classes/data_modeling/_constraints.py
+++ cognite_toolkit/_cdf_tk/client/resource_classes/data_modeling/_constraints.py
@@ -1,8 +1,8 @@
 from abc import ABC
 from typing import Annotated, Any, Literal
 
-from pydantic import Field, TypeAdapter, field_serializer, model_serializer
-from pydantic_core.core_schema import FieldSerializationInfo, SerializerFunctionWrapHandler
+from pydantic import Field, TypeAdapter, field_serializer
+from pydantic_core.core_schema import FieldSerializationInfo
 
 from cognite_toolkit._cdf_tk.client._resource_base import BaseModelObject
 
@@ -11,12 +11,6 @@ from ._references import ContainerReference
 
 class ConstraintDefinition(BaseModelObject, ABC):
     constraint_type: str
-
-    @model_serializer(mode="wrap")  # type: ignore[type-var]
-    def serialize(self, handler: SerializerFunctionWrapHandler, info: FieldSerializationInfo) -> dict[str, Any]:
-        # Always serialize constraint_type, even if model_dump(exclude_unset=True)
-        serialized = handler(self)
-        return {"constraintType" if info.by_alias else "constraint_type": self.constraint_type, **serialized}
 
 
 class UniquenessConstraintDefinition(ConstraintDefinition):
--- cognite_toolkit/_cdf_tk/client/resource_classes/data_modeling/_indexes.py
+++ cognite_toolkit/_cdf_tk/client/resource_classes/data_modeling/_indexes.py
@@ -1,8 +1,7 @@
 from abc import ABC
-from typing import Annotated, Any, Literal
+from typing import Annotated, Literal
 
-from pydantic import Field, TypeAdapter, model_serializer
-from pydantic_core.core_schema import FieldSerializationInfo, SerializerFunctionWrapHandler
+from pydantic import Field, TypeAdapter
 
 from cognite_toolkit._cdf_tk.client._resource_base import BaseModelObject
 
@@ -10,12 +9,6 @@ from cognite_toolkit._cdf_tk.client._resource_base import BaseModelObject
 class IndexDefinition(BaseModelObject, ABC):
     index_type: str
     properties: list[str]
-
-    @model_serializer(mode="wrap")  # type: ignore[type-var]
-    def serialize(self, handler: SerializerFunctionWrapHandler, info: FieldSerializationInfo) -> dict[str, Any]:
-        # Always serialize as {"indexType": self.index_type}, even if model_dump(exclude_unset=True)
-        serialized = handler(self)
-        return {"indexType" if info.by_alias else "index_type": self.index_type, **serialized}
 
 
 class BtreeIndex(IndexDefinition):
