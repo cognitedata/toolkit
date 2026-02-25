adb72f5aeaa99b75ec2c9c1e96add570cab02374
anders-albert <anders0albert@gmail.com>
2026-02-25 11:43:57 +0100

    refactor; added metadata object

---
+++ cognite_toolkit/_cdf_tk/client/_types.py
@@ -0,0 +1,10 @@
+from typing import Annotated, TypeAlias
+
+from pydantic import BeforeValidator
+
+
+def _keys_as_string(value: dict) -> dict[str, str]:
+    return {str(k): str(v) for k, v in value.items()}
+
+
+Metadata: TypeAlias = Annotated[dict[str, str], BeforeValidator(_keys_as_string)]
