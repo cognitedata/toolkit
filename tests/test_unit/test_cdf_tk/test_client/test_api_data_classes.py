ea5534218bebb1a4b335bdb54c74bbe8c646af9e
anders-albert <anders0albert@gmail.com>
2026-02-25 11:45:01 +0100

    tests: added failing tests

--- tests/test_unit/test_cdf_tk/test_client/test_api_data_classes.py
+++ tests/test_unit/test_cdf_tk/test_client/test_api_data_classes.py
@@ -9,6 +9,7 @@ from cognite_toolkit._cdf_tk.client.resource_classes.asset import AssetRequest
 from cognite_toolkit._cdf_tk.client.resource_classes.datapoint_subscription import (
     DatapointSubscriptionRequest,
 )
+from cognite_toolkit._cdf_tk.client.resource_classes.dataset import DataSetRequest
 from cognite_toolkit._cdf_tk.client.resource_classes.filemetadata import FileMetadataResponse
 from cognite_toolkit._cdf_tk.client.resource_classes.group import GroupResponse
 from cognite_toolkit._cdf_tk.client.resource_classes.identifiers import PrincipalId
@@ -335,3 +336,14 @@ class TestSimulatorRoutineRevision:
                 "steadyStateDetection": [],
             },
         }
+
+
+class TestDataSetRequest:
+    def test_read_non_string_metadata(self) -> None:
+        data = {
+            "externalId": "dataset_1",
+            "name": "Dataset 1",
+            "metadata": {"archived": True},  # Non-string value
+        }
+        dataset_request = DataSetRequest.model_validate(data)
+        assert dataset_request.dump() == data
