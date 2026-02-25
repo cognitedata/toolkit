e8b6025e60937670e95f630adaa26f4e7816b84b
anders-albert <anders0albert@gmail.com>
2026-02-25 11:51:48 +0100

    stryle

--- cognite_toolkit/_cdf_tk/client/_types.py
+++ cognite_toolkit/_cdf_tk/client/_types.py
@@ -3,8 +3,8 @@ from typing import Annotated, TypeAlias
 from pydantic import BeforeValidator
 
 
-def _keys_as_string(value: dict) -> dict[str, str]:
+def _keys_and_values_as_string(value: dict) -> dict[str, str]:
     return {str(k): str(v) for k, v in value.items()}
 
 
-Metadata: TypeAlias = Annotated[dict[str, str], BeforeValidator(_keys_as_string)]
+Metadata: TypeAlias = Annotated[dict[str, str], BeforeValidator(_keys_and_values_as_string)]
