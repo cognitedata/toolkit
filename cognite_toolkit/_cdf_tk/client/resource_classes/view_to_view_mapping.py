ba6ca997d19bac85b38029aba7a8c28d2afaa7f7
Anders Albert <60234212+doctrino@users.noreply.github.com>
2026-03-03 11:34:54 +0100

    [CDF-27220, CDF-27221] 😃 Instance to Instance conversion (#2598)
    # Description
    
    Instance to instance conversion. This is needed for InField migration. I
    ended up splitting this into two methods, one for container properites
    and one for ocnnections. PR is +200 code and +300 tests.
    
    ## Bump
    
    - [ ] Patch
    - [x] Skip

--- cognite_toolkit/_cdf_tk/client/resource_classes/view_to_view_mapping.py
+++ cognite_toolkit/_cdf_tk/client/resource_classes/view_to_view_mapping.py
@@ -3,8 +3,7 @@ from typing import Any
 from pydantic import Field, field_serializer
 
 from cognite_toolkit._cdf_tk.client._resource_base import BaseModelObject
-
-from .data_modeling import ViewId
+from cognite_toolkit._cdf_tk.client.identifiers import ViewId
 
 
 class ViewToViewMapping(BaseModelObject):
@@ -21,3 +20,9 @@ class ViewToViewMapping(BaseModelObject):
     @field_serializer("source_view", "destination_view", mode="plain")
     def serialize_view_id(self, view_id: ViewId) -> dict[str, Any]:
         return {**view_id.dump(), "type": "view"}
+
+    def get_destination_property(self, source_property: str) -> str | None:
+        dest_prop_id = self.property_mapping.get(source_property)
+        if not dest_prop_id and self.map_identical_id_properties:
+            return source_property
+        return dest_prop_id
