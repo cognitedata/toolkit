96bac3787888ccc61b239e84e6cc2d3f686d5e52
Anders Albert <60234212+doctrino@users.noreply.github.com>
2026-03-04 09:09:26 +0100

    [CDF-26982] ➡ InField data migration (#2625)
    # Description
    
    Please describe the change you have made.
    
    ## Bump
    
    - [x] Patch
    - [ ] Skip
    
    ## Changelog
    
    ### Added
    
    - [alpha] Support for migrating InField data using the `cdf migrate
    infield-data` command.

--- cognite_toolkit/_cdf_tk/commands/_migrate/creators.py
+++ cognite_toolkit/_cdf_tk/commands/_migrate/creators.py
@@ -33,6 +33,7 @@ from cognite_toolkit._cdf_tk.client.resource_classes.data_modeling import (
 from cognite_toolkit._cdf_tk.client.resource_classes.dataset import DataSetResponse
 from cognite_toolkit._cdf_tk.client.resource_classes.infield import (
     INFIELD_ON_CDM_DATA_MODEL,
+    DataStorage,
     InFieldCDMLocationConfigRequest,
 )
 from cognite_toolkit._cdf_tk.client.resource_classes.location_filter import LocationFilterRequest
@@ -425,13 +426,13 @@ class InfieldV2ConfigCreator(MigrationCreator):
             #    add to Validation in cdf build.
             data_filters[key] = {"instanceSpaces": [config.source_data_instance_space]}
 
-        data_storage: dict[str, JsonValue] = {
-            "rootLocation": root_node.dump(include_instance_type=False),
+        data_storage = DataStorage(
+            root_location=root_node.dump(include_instance_type=False),
             # Todo: This does not work. We need to require a new space. If not the migration will
             #    make legacy broken as it will update lastUpdatedTime of all data which InField depends on
             #    matching the user.
-            "appInstanceSpace": config.app_data_instance_space,
-        }
+            app_instance_space=config.app_data_instance_space,
+        )
         view_mappings: dict[str, JsonValue] = {}
         for key, default_view in [
             ("asset", ViewId(space="cdf_cdm", external_id="CogniteAsset", version="v1")),
