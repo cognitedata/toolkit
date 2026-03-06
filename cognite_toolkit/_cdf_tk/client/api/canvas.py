cec835f0ad664da80dd062416b7c006ffe2539c4
anders-albert <anders0albert@gmail.com>
2026-03-06 11:45:47 +0100

    refactor; update

--- cognite_toolkit/_cdf_tk/client/api/canvas.py
+++ cognite_toolkit/_cdf_tk/client/api/canvas.py
@@ -1,8 +1,8 @@
 from collections.abc import Sequence
-from typing import Any
 
 from cognite_toolkit._cdf_tk.client.api.instances import MultiWrappedInstancesAPI
 from cognite_toolkit._cdf_tk.client.http_client import HTTPClient
+from cognite_toolkit._cdf_tk.client.identifiers import InstanceDefinitionId, ViewId
 from cognite_toolkit._cdf_tk.client.resource_classes.canvas import (
     ANNOTATION_EDGE_TYPE_REF,
     CANVAS_ANNOTATION_VIEW_ID,
@@ -19,11 +19,18 @@ from cognite_toolkit._cdf_tk.client.resource_classes.canvas import (
     IndustrialCanvasRequest,
     IndustrialCanvasResponse,
 )
-from cognite_toolkit._cdf_tk.client.resource_classes.data_modeling._query import QueryResponseUntyped
-from cognite_toolkit._cdf_tk.client.resource_classes.instance_api import (
-    TypedInstanceIdentifier,
-    move_properties,
+from cognite_toolkit._cdf_tk.client.resource_classes.data_modeling._query import (
+    QueryEdgeExpression,
+    QueryEdgeTableExpression,
+    QueryNodeExpression,
+    QueryNodeTableExpression,
+    QueryRequest,
+    QueryResponseUntyped,
+    QuerySelect,
+    QuerySelectSource,
+    QueryThrough,
 )
+from cognite_toolkit._cdf_tk.client.resource_classes.data_modeling._wrapped import move_properties
 
 _QUERY_LIMIT = 1000
 
@@ -41,108 +48,119 @@ class IndustrialCanvasConfigAPI(MultiWrappedInstancesAPI[IndustrialCanvasRequest
     def __init__(self, http_client: HTTPClient) -> None:
         super().__init__(http_client, query_chunk=1)
 
-    def _retrieve_query(self, items: Sequence[TypedInstanceIdentifier]) -> dict[str, Any]:
-        return {
-            "with": {
-                self._CANVAS_REF: {
-                    "limit": len(items),
-                    "nodes": {
-                        "filter": {
-                            "instanceReferences": [
-                                {"space": item.space, "externalId": item.external_id} for item in items
-                            ]
-                        },
-                    },
-                },
-                self._SOLUTION_TAGS_REF: {
-                    "limit": _QUERY_LIMIT,
-                    "nodes": {
-                        "from": self._CANVAS_REF,
-                        "through": {
-                            "source": CANVAS_VIEW_ID.dump(),
-                            "identifier": "solutionTags",
-                        },
-                    },
-                },
-                self._ANNOTATION_EDGES_REF: {
-                    "limit": _QUERY_LIMIT,
-                    "edges": {
-                        "from": self._CANVAS_REF,
-                        "direction": "outwards",
-                        "filter": {
+    def _retrieve_query(self, items: Sequence[InstanceDefinitionId]) -> QueryRequest:
+        canvas_vid = ViewId(
+            space=CANVAS_VIEW_ID.space, external_id=CANVAS_VIEW_ID.external_id, version=CANVAS_VIEW_ID.version
+        )
+        annotation_vid = ViewId(
+            space=CANVAS_ANNOTATION_VIEW_ID.space,
+            external_id=CANVAS_ANNOTATION_VIEW_ID.external_id,
+            version=CANVAS_ANNOTATION_VIEW_ID.version,
+        )
+        solution_tag_vid = ViewId(
+            space=SOLUTION_TAG_VIEW_ID.space,
+            external_id=SOLUTION_TAG_VIEW_ID.external_id,
+            version=SOLUTION_TAG_VIEW_ID.version,
+        )
+        container_ref_vid = ViewId(
+            space=CONTAINER_REFERENCE_VIEW_ID.space,
+            external_id=CONTAINER_REFERENCE_VIEW_ID.external_id,
+            version=CONTAINER_REFERENCE_VIEW_ID.version,
+        )
+        fdm_ref_vid = ViewId(
+            space=FDM_INSTANCE_CONTAINER_REFERENCE_VIEW_ID.space,
+            external_id=FDM_INSTANCE_CONTAINER_REFERENCE_VIEW_ID.external_id,
+            version=FDM_INSTANCE_CONTAINER_REFERENCE_VIEW_ID.version,
+        )
+
+        return QueryRequest(
+            with_={
+                self._CANVAS_REF: QueryNodeExpression(
+                    limit=len(items),
+                    nodes=QueryNodeTableExpression(
+                        filter={"instanceReferences": [item.dump(include_instance_type=False) for item in items]},
+                    ),
+                ),
+                self._SOLUTION_TAGS_REF: QueryNodeExpression(
+                    limit=_QUERY_LIMIT,
+                    nodes=QueryNodeTableExpression(
+                        from_=self._CANVAS_REF,
+                        through=QueryThrough(source=canvas_vid, identifier="solutionTags"),
+                    ),
+                ),
+                self._ANNOTATION_EDGES_REF: QueryEdgeExpression(
+                    limit=_QUERY_LIMIT,
+                    edges=QueryEdgeTableExpression(
+                        from_=self._CANVAS_REF,
+                        direction="outwards",
+                        filter={
                             "equals": {
                                 "property": ["edge", "type"],
                                 "value": ANNOTATION_EDGE_TYPE_REF,
                             }
                         },
-                        "nodeFilter": {
-                            "hasData": [CANVAS_ANNOTATION_VIEW_ID.dump()],
-                        },
-                    },
-                },
-                self._CONTAINER_REF_EDGES_REF: {
-                    "limit": _QUERY_LIMIT,
-                    "edges": {
-                        "from": self._CANVAS_REF,
-                        "direction": "outwards",
-                        "filter": {
+                        node_filter={"hasData": [annotation_vid.dump()]},
+                    ),
+                ),
+                self._CONTAINER_REF_EDGES_REF: QueryEdgeExpression(
+                    limit=_QUERY_LIMIT,
+                    edges=QueryEdgeTableExpression(
+                        from_=self._CANVAS_REF,
+                        direction="outwards",
+                        filter={
                             "equals": {
                                 "property": ["edge", "type"],
                                 "value": CONTAINER_REFERENCE_EDGE_TYPE_REF,
                             }
                         },
-                        "nodeFilter": {
-                            "hasData": [CONTAINER_REFERENCE_VIEW_ID.dump()],
-                        },
-                    },
-                },
-                self._FDM_REF_EDGES_REF: {
-                    "limit": _QUERY_LIMIT,
-                    "edges": {
-                        "from": self._CANVAS_REF,
-                        "direction": "outwards",
-                        "filter": {
+                        node_filter={"hasData": [container_ref_vid.dump()]},
+                    ),
+                ),
+                self._FDM_REF_EDGES_REF: QueryEdgeExpression(
+                    limit=_QUERY_LIMIT,
+                    edges=QueryEdgeTableExpression(
+                        from_=self._CANVAS_REF,
+                        direction="outwards",
+                        filter={
                             "equals": {
                                 "property": ["edge", "type"],
                                 "value": FDM_CONTAINER_REFERENCE_EDGE_TYPE_REF,
                             }
                         },
-                        "nodeFilter": {
-                            "hasData": [FDM_INSTANCE_CONTAINER_REFERENCE_VIEW_ID.dump()],
-                        },
-                    },
-                },
-                self._ANNOTATIONS_REF: {
-                    "limit": _QUERY_LIMIT,
-                    "nodes": {"from": self._ANNOTATION_EDGES_REF},
-                },
-                self._CONTAINER_REFS_REF: {
-                    "limit": _QUERY_LIMIT,
-                    "nodes": {"from": self._CONTAINER_REF_EDGES_REF},
-                },
-                self._FDM_REFS_REF: {
-                    "limit": _QUERY_LIMIT,
-                    "nodes": {"from": self._FDM_REF_EDGES_REF},
-                },
+                        node_filter={"hasData": [fdm_ref_vid.dump()]},
+                    ),
+                ),
+                self._ANNOTATIONS_REF: QueryNodeExpression(
+                    limit=_QUERY_LIMIT,
+                    nodes=QueryNodeTableExpression(from_=self._ANNOTATION_EDGES_REF),
+                ),
+                self._CONTAINER_REFS_REF: QueryNodeExpression(
+                    limit=_QUERY_LIMIT,
+                    nodes=QueryNodeTableExpression(from_=self._CONTAINER_REF_EDGES_REF),
+                ),
+                self._FDM_REFS_REF: QueryNodeExpression(
+                    limit=_QUERY_LIMIT,
+                    nodes=QueryNodeTableExpression(from_=self._FDM_REF_EDGES_REF),
+                ),
             },
-            "select": {
-                self._CANVAS_REF: {
-                    "sources": [{"source": CANVAS_VIEW_ID.dump(), "properties": ["*"]}],
-                },
-                self._SOLUTION_TAGS_REF: {
-                    "sources": [{"source": SOLUTION_TAG_VIEW_ID.dump(), "properties": ["*"]}],
-                },
-                self._ANNOTATIONS_REF: {
-                    "sources": [{"source": CANVAS_ANNOTATION_VIEW_ID.dump(), "properties": ["*"]}],
-                },
-                self._CONTAINER_REFS_REF: {
-                    "sources": [{"source": CONTAINER_REFERENCE_VIEW_ID.dump(), "properties": ["*"]}],
-                },
-                self._FDM_REFS_REF: {
-                    "sources": [{"source": FDM_INSTANCE_CONTAINER_REFERENCE_VIEW_ID.dump(), "properties": ["*"]}],
-                },
+            select={
+                self._CANVAS_REF: QuerySelect(
+                    sources=[QuerySelectSource(source=canvas_vid, properties=["*"])],
+                ),
+                self._SOLUTION_TAGS_REF: QuerySelect(
+                    sources=[QuerySelectSource(source=solution_tag_vid, properties=["*"])],
+                ),
+                self._ANNOTATIONS_REF: QuerySelect(
+                    sources=[QuerySelectSource(source=annotation_vid, properties=["*"])],
+                ),
+                self._CONTAINER_REFS_REF: QuerySelect(
+                    sources=[QuerySelectSource(source=container_ref_vid, properties=["*"])],
+                ),
+                self._FDM_REFS_REF: QuerySelect(
+                    sources=[QuerySelectSource(source=fdm_ref_vid, properties=["*"])],
+                ),
             },
-        }
+        )
 
     def _validate_query_response(self, query_response: QueryResponseUntyped) -> list[IndustrialCanvasResponse]:
         annotations = [
