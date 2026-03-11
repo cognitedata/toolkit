b5ef6d480c03f2df11b9a7bf5e107e0b743de9d7
anders-albert <anders0albert@gmail.com>
2026-03-11 08:08:33 +0100

    refactor; set up

--- tests/test_unit/test_cdf_tk/test_commands/test_migration_cmd/test_infield_mapping.py
+++ tests/test_unit/test_cdf_tk/test_commands/test_migration_cmd/test_infield_mapping.py
@@ -1,7 +1,14 @@
-from cognite_toolkit._cdf_tk.commands._migrate.infield_data_mappings import create_infield_data_mappings
+from cognite_toolkit._cdf_tk.commands._migrate.infield_data_mappings import (
+    create_infield_data_mappings,
+    create_infield_schedule_selector,
+)
 
 
 class TestInFieldMapping:
     def test_mapping_validation(self) -> None:
         mappings = create_infield_data_mappings(extra="forbid")
         assert mappings, "Mapping creation should not raise an error"
+
+    def test_schedule_selector_validation(self) -> None:
+        selector = create_infield_schedule_selector()
+        assert selector, "Schedule selector creation should not raise an error"
