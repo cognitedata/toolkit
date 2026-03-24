19ec510bdc4ef9a907368426285bab55ff31924f
Magnus Schjølberg <magnus.schjolberg@cognitedata.com>
2026-03-24 12:06:34 +0100

    Move TestTimeWindowsMs to test_time.py

--- tests/test_unit/test_cdf_tk/test_commands/test_migration_cmd/test_migration_io.py
+++ tests/test_unit/test_cdf_tk/test_commands/test_migration_cmd/test_migration_io.py
@@ -16,7 +16,6 @@ from cognite_toolkit._cdf_tk.commands._migrate.migration_io import (
 from cognite_toolkit._cdf_tk.commands._migrate.selectors import MigrationCSVFileSelector
 from cognite_toolkit._cdf_tk.storageio import AssetIO, Page
 from cognite_toolkit._cdf_tk.storageio.selectors import ThreeDModelIdSelector
-from cognite_toolkit._cdf_tk.utils.time import time_windows_ms
 
 
 @pytest.fixture(scope="module")
@@ -229,16 +228,3 @@ class TestThreeDAssetMappingMigrationIO:
 
         with pytest.raises(NotImplementedError):
             io.data_to_json_chunk(MagicMock())
-
-
-class TestTimeWindowsMs:
-    def test_none_interval_returns_single_window(self) -> None:
-        assert time_windows_ms(0, 1_000, None) == [(0, 1_000)]
-
-    def test_splits_evenly_by_interval(self) -> None:
-        hour_ms = 60 * 60 * 1000
-        now_ms = 3 * hour_ms
-        windows = time_windows_ms(0, now_ms, hour_ms)
-        assert windows == [(0, hour_ms), (hour_ms, 2 * hour_ms), (2 * hour_ms, now_ms)]
-        for i in range(len(windows) - 1):
-            assert windows[i][1] == windows[i + 1][0]
