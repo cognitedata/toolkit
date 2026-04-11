6f852b502116158df379bf792ec9e941fdb5bb29
anders-albert <anders0albert@gmail.com>
2026-04-11 11:02:00 +0200

    refactor; extend

--- cognite_toolkit/_cdf_tk/client/resource_classes/chart.py
+++ cognite_toolkit/_cdf_tk/client/resource_classes/chart.py
@@ -1,10 +1,27 @@
-from typing import Literal, TypeAlias
+from collections.abc import Set
+from typing import Literal, TypeAlias, get_args
 
-from cognite_toolkit._cdf_tk.client._resource_base import BaseModelObject, RequestResource, ResponseResource
+from pydantic import Field
+
+from cognite_toolkit._cdf_tk.client._resource_base import (
+    BaseModelObject,
+    RequestResource,
+    ResponseResource,
+)
 from cognite_toolkit._cdf_tk.client.identifiers import ExternalId
+from cognite_toolkit._cdf_tk.client.resource_classes.chart_monitoring_job import (
+    ChartMonitoringJobRequest,
+    ChartMonitoringJobResponse,
+)
+from cognite_toolkit._cdf_tk.client.resource_classes.chart_scheduled_calculation import (
+    ChartScheduledCalculationRequest,
+    ChartScheduledCalculationResponse,
+)
 from cognite_toolkit._cdf_tk.client.resource_classes.charts_data import ChartData
 
 Visibility: TypeAlias = Literal["PUBLIC", "PRIVATE"]
+BackendService: TypeAlias = Literal["monitoring_jobs", "scheduled_calculations"]
+BACKEND_SERVICES: Set[BackendService] = frozenset(get_args(BackendService))
 
 
 class Chart(BaseModelObject, extra="allow"):
@@ -16,10 +33,14 @@ class Chart(BaseModelObject, extra="allow"):
         return ExternalId(external_id=self.external_id)
 
 
-class ChartRequest(Chart, RequestResource, extra="allow"): ...
+class ChartRequest(Chart, RequestResource, extra="allow"):
+    monitoring_jobs: list[ChartMonitoringJobRequest] | None = Field(None, exclude=True)
+    scheduled_calculations: list[ChartScheduledCalculationRequest] | None = Field(None, exclude=True)
 
 
 class ChartResponse(Chart, ResponseResource[ChartRequest], extra="allow"):
+    monitoring_jobs: list[ChartMonitoringJobResponse] | None = Field(None, exclude=True)
+    scheduled_calculations: list[ChartScheduledCalculationResponse] | None = Field(None, exclude=True)
     created_time: int
     last_updated_time: int
     owner_id: str
@@ -27,3 +48,11 @@ class ChartResponse(Chart, ResponseResource[ChartRequest], extra="allow"):
     @classmethod
     def request_cls(cls) -> type[ChartRequest]:
         return ChartRequest
+
+    def as_request_resource(self, include: Set[BackendService] = BACKEND_SERVICES) -> ChartRequest:
+        chart_request = super().as_request_resource()
+        if self.monitoring_jobs is not None and "monitoring_jobs" in include:
+            chart_request.monitoring_jobs = [job.as_request_resource() for job in self.monitoring_jobs]
+        if self.scheduled_calculations is not None and "scheduled_calculations" in include:
+            chart_request.scheduled_calculations = [calc.as_request_resource() for calc in self.scheduled_calculations]
+        return chart_request
