ad912bd4b2a7fed28186711287ffcfa2b351516b
anders-albert <anders0albert@gmail.com>
2026-04-14 06:55:26 +0200

    refactor

--- cognite_toolkit/_cdf_tk/storageio/_applications.py
+++ cognite_toolkit/_cdf_tk/storageio/_applications.py
@@ -457,7 +457,7 @@ class ChartIO(UploadableStorageIO[ChartSelector, ChartResponse, ChartRequest]):
                 # on one CDF project, and uploaded on another, we need up update the internal ID to point to the new one.
                 # Note that we persisted the internal ID of the CDF project we downloaded from, so we can do an easy
                 # lookup.
-                # Furthermore, the Chats UI does not seem to clean up MonitoringJobReferences when monitoring jobs
+                # Furthermore, the Charts UI does not seem to clean up MonitoringJobReferences when monitoring jobs
                 # are deleted, so we can have references pointing to non-existing monitoring jobs. Currently,
                 # we just let these job references to be and do not touch them.
                 new_references: list[MonitoringJobReference] = []
