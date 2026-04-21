de4772583b5af82af72938560165b346b627bf18
anders-albert <anders0albert@gmail.com>
2026-04-21 15:37:52 +0200

    tests: update exception list

--- tests_smoke/test_client/test_cdf_resource_api.py
+++ tests_smoke/test_client/test_cdf_resource_api.py
@@ -9,8 +9,10 @@ import pytest
 
 from cognite_toolkit._cdf_tk.client import ToolkitClient
 from cognite_toolkit._cdf_tk.client._resource_base import ResponseResource, T_ResponseResource
+from cognite_toolkit._cdf_tk.client.api.alert_channels import AlertChannelsAPI
 from cognite_toolkit._cdf_tk.client.api.annotations import AnnotationsAPI
 from cognite_toolkit._cdf_tk.client.api.chart_scheduled_calculations import ChartScheduledCalculationsAPI
+from cognite_toolkit._cdf_tk.client.api.charts_folders import ChartFoldersAPI
 from cognite_toolkit._cdf_tk.client.api.charts_monitoring_job import ChartMonitoringJobsAPI
 from cognite_toolkit._cdf_tk.client.api.cognite_files import CogniteFilesAPI
 from cognite_toolkit._cdf_tk.client.api.data_product_versions import DataProductVersionsAPI
@@ -313,6 +315,9 @@ NOT_GENERIC_TESTED: Set[type[CDFResourceAPI]] = frozenset(
         # Requires a timeseries.
         ChartMonitoringJobsAPI,
         ChartScheduledCalculationsAPI,
+        # Manually tested
+        ChartFoldersAPI,
+        AlertChannelsAPI,
     }
 )
 
