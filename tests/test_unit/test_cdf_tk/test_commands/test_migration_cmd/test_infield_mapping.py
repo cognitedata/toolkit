4861769434d42e20fb241f218cab6b15e67f6054
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2026-04-30 17:28:35 +0200

    Remove banal test

--- tests/test_unit/test_cdf_tk/test_commands/test_migration_cmd/test_infield_mapping.py
+++ tests/test_unit/test_cdf_tk/test_commands/test_migration_cmd/test_infield_mapping.py
@@ -12,9 +12,3 @@ class TestInFieldMapping:
     def test_schedule_selector_validation(self) -> None:
         selector = create_infield_schedule_selector()
         assert selector, "Schedule selector creation should not raise an error"
-
-    def test_skip_observations_filters_mapping(self) -> None:
-        all_mappings = create_infield_data_mappings()
-        filtered = [m for m in all_mappings if m.destination_view.external_id != "FieldObservation"]
-        assert len(filtered) == len(all_mappings) - 1
-        assert all(m.destination_view.external_id != "FieldObservation" for m in filtered)
