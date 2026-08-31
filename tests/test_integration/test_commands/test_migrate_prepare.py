c32bb1ad9568b90f35ab69b0d354033d8406804f
anders-albert <anders0albert@gmail.com>
2026-08-31 19:14:54 +0200

    tests: updated tests

--- tests/test_integration/test_commands/test_migrate_prepare.py
+++ tests/test_integration/test_commands/test_migrate_prepare.py
@@ -2,7 +2,6 @@ from cognite_toolkit._cdf_tk.client import ToolkitClient
 from cognite_toolkit._cdf_tk.commands import MigrationPrepareCommand
 from cognite_toolkit._cdf_tk.commands._migrate.data_model import CONTAINERS, VIEWS
 from cognite_toolkit._cdf_tk.commands._migrate.default_mappings import create_default_mappings
-from cognite_toolkit._cdf_tk.data_classes import ResourceDeployResult
 
 
 class TestMigrateTimeSeriesCommand:
@@ -20,9 +19,7 @@ class TestMigrateTimeSeriesCommand:
         }
 
         dry_run_result = cmd.deploy_cognite_migration(toolkit_client, True, verbose=False)
-        actual_dry_run = {
-            key: result.total for key, result in dry_run_result.items() if isinstance(result, ResourceDeployResult)
-        }
+        actual_dry_run = {result.resource_name: result.total_count for result in dry_run_result}
         assert actual_dry_run == expected_resources, "Dry run result does not match expected resources"
 
         actual_result = cmd.deploy_cognite_migration(
@@ -30,8 +27,6 @@ class TestMigrateTimeSeriesCommand:
             False,
             verbose=False,
         )
-        actual = {
-            key: result.total for key, result in actual_result.items() if isinstance(result, ResourceDeployResult)
-        }
+        actual = {result.resource_name: result.total_count for result in actual_result}
 
         assert actual == expected_resources, "Actual result does not match expected resources"
