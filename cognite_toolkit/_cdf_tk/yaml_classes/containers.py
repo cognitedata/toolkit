4fc7dd7561a5d6d60cbbd03f828f7374ff67307d
Anders Albert <60234212+doctrino@users.noreply.github.com>
2026-09-01 14:06:55 +0200

    [CDF-28531] 😛Improve validation record container (#3181)
    # Description
    
    See below
    
    ## Bump
    
    - [x] Patch
    - [ ] Skip
    
    ## Changelog
    
    ### Improved
    
    - When building record containers, Toolkit now checks that you do not
    use indexes, constraints, or invalid properties (CDF external
    properties).

--- cognite_toolkit/_cdf_tk/yaml_classes/containers.py
+++ cognite_toolkit/_cdf_tk/yaml_classes/containers.py
@@ -2,7 +2,7 @@ import re
 from typing import Literal
 
 from pydantic import Field, field_validator, model_serializer
-from pydantic_core.core_schema import SerializationInfo, SerializerFunctionWrapHandler
+from pydantic_core.core_schema import SerializationInfo, SerializerFunctionWrapHandler, ValidationInfo
 
 from cognite_toolkit._cdf_tk.client.resource_classes.data_modeling import ContainerId
 from cognite_toolkit._cdf_tk.constants import (
@@ -94,3 +94,27 @@ class ContainerYAML(ToolkitResource):
         if self.indexes:
             serialized_data["indexes"] = {k: v.model_dump(**vars(info)) for k, v in self.indexes.items()}
         return serialized_data
+
+    @field_validator("indexes", "constraints", mode="after")
+    @classmethod
+    def indexes_and_constraints_not_supported_for_record_containers(
+        cls, val: dict[str, str] | None, info: ValidationInfo
+    ) -> dict[str, str] | None:
+        """Validate that indexes and constraints are not set for record containers"""
+        if info.data.get("used_for") == "record" and val is not None:
+            raise ValueError("Indexes and constraints are not supported for record containers.")
+        return val
+
+    @field_validator("properties", mode="after")
+    @classmethod
+    def external_cdf_references_not_supported_for_record_containers(
+        cls, val: dict[str, ContainerPropertyDefinition], info: ValidationInfo
+    ) -> dict[str, ContainerPropertyDefinition]:
+        """Validate that external CDF references are not set for record containers"""
+        if info.data.get("used_for") == "record":
+            for key, prop in val.items():
+                if prop.type.type in {"timeseries", "file", "sequence"}:
+                    raise ValueError(
+                        f"External CDF references are not supported for record containers. Property '{key}' is of type 'externalCdfReference'."
+                    )
+        return val
