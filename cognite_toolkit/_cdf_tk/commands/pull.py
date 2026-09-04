075701ab293587e8da3d4f70712a51ab087bd818
anders-albert <anders0albert@gmail.com>
2026-09-04 12:18:55 +0200

    refactor; deleted unused classe

--- cognite_toolkit/_cdf_tk/commands/pull.py
+++ cognite_toolkit/_cdf_tk/commands/pull.py
@@ -1,19 +1,14 @@
-import itertools
 import json
 import re
 import sys
 import tempfile
-import uuid
-from collections import Counter, UserList, defaultdict
-from dataclasses import dataclass, field
+from collections import Counter, defaultdict
+from dataclasses import dataclass
 from pathlib import Path
-from typing import Any, Union
+from typing import Any
 
 import yaml
-from rich import print
 from rich.console import Console, Group, RenderableType
-from rich.markdown import Markdown
-from rich.panel import Panel
 
 from cognite_toolkit._cdf_tk.client import ToolkitClient
 from cognite_toolkit._cdf_tk.client._resource_base import Identifier, ResponseResource, T_Identifier
@@ -40,8 +35,6 @@ from cognite_toolkit._cdf_tk.ui import (
     hanging_indent,
 )
 from cognite_toolkit._cdf_tk.utils import (
-    YAMLComment,
-    YAMLWithComments,
     humanize_collection,
     read_yaml_content,
     safe_read,
@@ -52,9 +45,9 @@ from cognite_toolkit._cdf_tk.utils.file import safe_rmtree, yaml_safe_dump
 from ._base import ToolkitCommand
 
 if sys.version_info >= (3, 11):
-    from typing import Self
+    pass
 else:
-    from typing_extensions import Self
+    pass
 
 _VARIABLE_PATTERN = re.compile(r"\{\{(.+?)\}\}")
 # The encoding and newline characters to use when writing files
@@ -63,336 +56,6 @@ _VARIABLE_PATTERN = re.compile(r"\{\{(.+?)\}\}")
 # version control, the diff will be easier to read.
 ENCODING = "utf-8"
 NEWLINE = "\n"
-
-
-@dataclass
-class Variable:
-    placeholder: str | None = None
-    name: str | None = None
-    source_value: str | None = None
-
-
-@dataclass
-class ResourceProperty:
-    """This represents a single property in a CDF resource file.
-
-    Args:
-        key_path: The path to the property in the resource file.
-        build_value: The value of the property in the local resource file build file.
-        cdf_value: The value of the property in the CDF resource.
-        variables: A list of variables that are used in the property value.
-    """
-
-    key_path: tuple[str | int, ...]
-    build_value: float | int | str | bool | None = None
-    cdf_value: float | int | str | bool | None = None
-    variables: list[Variable] = field(default_factory=list)
-
-    @property
-    def value(self) -> float | int | str | bool | None:
-        if self.has_variables:
-            return self.variables[0].source_value
-        return self.cdf_value or self.build_value
-
-    @property
-    def has_variables(self) -> bool:
-        return bool(self.variables)
-
-    @property
-    def is_changed(self) -> bool:
-        return (
-            self.build_value != self.cdf_value
-            and self.build_value is not None
-            and self.cdf_value is not None
-            and not self.has_variables
-        )
-
-    @property
-    def is_added(self) -> bool:
-        return self.build_value is None and self.cdf_value is not None
-
-    @property
-    def is_cannot_change(self) -> bool:
-        return (
-            self.build_value != self.cdf_value
-            and not self.has_variables
-            and self.build_value is not None
-            and self.cdf_value is not None
-        )
-
-    def __str__(self) -> str:
-        key_str = ".".join(map(str, self.key_path))
-        if self.is_added:
-            return f"ADDED: '{key_str}: {self.cdf_value}'"
-        elif self.is_changed:
-            return f"CHANGED: '{key_str}: {self.build_value} -> {self.cdf_value}'"
-        elif self.is_cannot_change:
-            return f"CANNOT CHANGE (contains variables): '{key_str}: {self.build_value} -> {self.cdf_value}'"
-        else:
-            return f"UNCHANGED: '{key_str}: {self.build_value}'"
-
-
-class ResourceYAMLDifference(YAMLWithComments[tuple[Union[str, int], ...], ResourceProperty]):
-    """This represents a YAML file that contains resources and their properties.
-
-    It is used to compare a local resource file with a CDF resource.
-    """
-
-    def __init__(
-        self,
-        items: dict[tuple[str | int, ...], ResourceProperty],
-        comments: dict[tuple[str, ...], YAMLComment] | None = None,
-    ) -> None:
-        super().__init__(items or {})
-        self._comments = comments or {}
-
-    def _get_comment(self, key: tuple[str, ...]) -> YAMLComment | None:
-        return self._comments.get(key)
-
-    @classmethod
-    def load(cls, build_content: str, source_content: str) -> Self:
-        comments = cls._extract_comments(build_content)
-        build = read_yaml_content(build_content)
-        build_flatten = cls._flatten(build)
-        items: dict[tuple[str | int, ...], ResourceProperty] = {}
-        for key, value in build_flatten.items():
-            items[key] = ResourceProperty(
-                key_path=key,
-                build_value=value,
-            )
-
-        source_content, variable_by_placeholder = cls._replace_variables(source_content)
-        source = read_yaml_content(source_content)
-        source_items = cls._flatten(source)
-        for key, value in source_items.items():
-            for placeholder, variable in variable_by_placeholder.items():
-                if placeholder in str(value):
-                    items[key].variables.append(
-                        Variable(
-                            placeholder=placeholder,
-                            name=variable_by_placeholder[placeholder],
-                            source_value=str(value),
-                        )
-                    )
-        return cls(items, comments)
-
-    @classmethod
-    def _flatten(
-        cls, raw: dict[str, Any] | list[dict[str, Any]]
-    ) -> dict[tuple[str | int, ...], str | int | float | bool | None]:
-        if isinstance(raw, dict):
-            return cls._flatten_dict(raw)
-        elif isinstance(raw, list):
-            raise NotImplementedError()
-        else:
-            raise ValueError(f"Expected a dictionary or list, got {type(raw)}")
-
-    @classmethod
-    def _flatten_dict(
-        cls, raw: dict[str, Any], key_path: tuple[str | int, ...] = ()
-    ) -> dict[tuple[str | int, ...], str | int | float | bool | None]:
-        items: dict[tuple[str | int, ...], str | int | float | bool | None] = {}
-        for key, value in raw.items():
-            if key == "scopes":
-                # Hack to handle that scopes is a list variable
-                items[(*key_path, key)] = value
-            elif isinstance(value, dict):
-                items.update(cls._flatten_dict(value, (*key_path, key)))
-            elif isinstance(value, list):
-                for i, item in enumerate(value):
-                    if isinstance(item, dict):
-                        items.update(cls._flatten_dict(item, (*key_path, key, i)))
-                    else:
-                        items[(*key_path, key, i)] = item
-            else:
-                items[(*key_path, key)] = value
-        return items
-
-    @classmethod
-    def _replace_variables(cls, content: str) -> tuple[str, dict[str, str]]:
-        variable_by_placeholder: dict[str, str] = {}
-        seen: set[str] = set()
-        for match in _VARIABLE_PATTERN.finditer(content):
-            variable = match.group(1)
-            if variable in seen:
-                continue
-            placeholder = f"VARIABLE_{uuid.uuid4().hex[:8]}"
-            content = content.replace(f"{{{{{variable}}}}}", placeholder)
-            variable_by_placeholder[placeholder] = variable
-            seen.add(variable)
-        return content, variable_by_placeholder
-
-    def update_cdf_resource(self, cdf_resource: dict[str, Any]) -> None:
-        for key, value in self._flatten_dict(cdf_resource).items():
-            if key in self:
-                self[key].cdf_value = value
-            else:
-                self[key] = ResourceProperty(key_path=key, cdf_value=value)
-
-    def dump(self) -> dict[Any, Any]:
-        dumped: dict[Any, Any] = {}
-        for key, prop in self.items():
-            current = dumped
-            for part, next_part in itertools.pairwise(key):
-                if isinstance(part, int) and isinstance(current, list) and len(current) < part + 1:
-                    current.append({})
-                    current = current[part]
-                elif isinstance(part, int) and isinstance(current, list) and part < len(current):
-                    current = current[part]
-                elif isinstance(part, str) and isinstance(next_part, str):
-                    current = current.setdefault(part, {})
-                elif isinstance(part, str) and isinstance(next_part, int):
-                    current = current.setdefault(part, [])
-                else:
-                    raise ValueError(f"Expected a string or int, got {type(part)}")
-            if isinstance(key[-1], int) and isinstance(current, list):
-                current.append(prop.value)
-            elif isinstance(key[-1], str) and isinstance(current, dict):
-                current[key[-1]] = prop.value
-            else:
-                raise ValueError(f"Expected a string or int, got {type(key[-1])}")
-        return dumped
-
-    def dump_yaml_with_comments(self, indent_size: int = 2) -> str:
-        """Dump a config dictionary to a yaml string"""
-        dumped_with_comments = self._dump_yaml_with_comments(indent_size, False)
-        for key, prop in self.items():
-            for variable in prop.variables:
-                if variable.placeholder:
-                    dumped_with_comments = dumped_with_comments.replace(
-                        variable.placeholder, f"{{{{{variable.name}}}}}"
-                    )
-        return dumped_with_comments
-
-    def display(self, title: str | None = None) -> None:
-        added = [prop for prop in self.values() if prop.is_added]
-        changed = [prop for prop in self.values() if prop.is_changed]
-        cannot_change = [prop for prop in self.values() if prop.is_cannot_change]
-        unchanged = [
-            prop for prop in self.values() if not prop.is_added and not prop.is_changed and not prop.is_cannot_change
-        ]
-
-        content: list[str] = []
-        if added:
-            content.append("\n**Added properties**(Either set in CDF UI or default values set by CDF):")
-            content.extend([f" - {prop}" for prop in added])
-        if changed:
-            content.append("\n**Changed properties:**")
-            content.extend([f" - {prop}" for prop in changed])
-        if cannot_change:
-            content.append("\n**Cannot change properties**")
-            content.extend([f" - {prop}" for prop in cannot_change])
-        if unchanged:
-            content.append(f"\n**{len(unchanged)} properties unchanged**")
-
-        print(Panel.fit(Markdown("\n".join(content), justify="left"), title=title or "Resource differences"))
-
-
-@dataclass
-class Line:
-    line_no: int
-    build_value: str | None = None
-    source_value: str | None = None
-    cdf_value: str | None = None
-    variables: list[str] | None = None
-
-    @property
-    def value(self) -> str:
-        if self.variables:
-            if self.source_value is None:
-                raise ValueError("Source value should be set if there are variables")
-            return self.source_value
-        value = self.cdf_value or self.build_value
-        if value is None:
-            raise ValueError("CDF value or build value should be set")
-        return value
-
-    @property
-    def is_changed(self) -> bool:
-        return (
-            self.build_value != self.cdf_value
-            and self.build_value is not None
-            and self.cdf_value is not None
-            and self.variables is None
-        )
-
-    @property
-    def is_added(self) -> bool:
-        return self.build_value is None and self.cdf_value is not None
-
-    @property
-    def is_cannot_change(self) -> bool:
-        return (
-            self.build_value != self.cdf_value
-            and self.variables is not None
-            and self.build_value is not None
-            and self.cdf_value is not None
-        )
-
-
-class TextFileDifference(UserList):
-    def __init__(self, lines: list[Line] | None) -> None:
-        super().__init__(lines or [])
-
-    @classmethod
-    def load(cls, build_content: str, source_content: str) -> Self:
-        lines = []
-        # Build and source content should have the same number of lines
-        for no, (build, source) in enumerate(zip(build_content.splitlines(), source_content.splitlines())):
-            variables = [v.group(1) for v in _VARIABLE_PATTERN.finditer(source)] or None
-            lines.append(
-                Line(
-                    line_no=no + 1,
-                    build_value=build,
-                    source_value=source,
-                    variables=variables,
-                )
-            )
-        return cls(lines)
-
-    def update_cdf_content(self, cdf_content: str) -> None:
-        for i, line in enumerate(cdf_content.splitlines()):
-            if i < len(self):
-                self[i].cdf_value = line
-            else:
-                self.append(Line(cdf_value=line, line_no=i + 1))
-
-    def dump(self) -> str:
-        return "\n".join(line.value for line in self) + "\n"
-
-    def display(self, title: str | None = None) -> None:
-        added = [line for line in self if line.is_added]
-        changed = [line for line in self if line.is_changed]
-        cannot_change = [line for line in self if line.is_cannot_change]
-        unchanged_count = len(self) - len(added) - len(changed) - len(cannot_change)
-
-        content: list[str] = []
-        if added:
-            content.append("\n**Added lines**")
-            if len(added) == 1:
-                content.append(f" - Line {added[0].line_no}: '{added[0].cdf_value}'")
-            else:
-                content.append(f" - Line {added[0].line_no} - {added[-1].line_no}: {len(added)} lines")
-        if changed:
-            content.append("\n**Changed lines**")
-            if len(changed) == 1:
-                content.append(f" - Line {changed[0].line_no}: '{changed[0].source_value}' -> '{changed[0].cdf_value}'")
-            else:
-                content.append(f" - Line {changed[0].line_no} - {changed[-1].line_no}: {len(changed)} lines")
-        if cannot_change:
-            content.append("\n**Cannot change lines**")
-            if len(cannot_change) == 1:
-                content.append(
-                    f" - Line {cannot_change[0].line_no}: '{cannot_change[0].source_value}' -> '{cannot_change[0].cdf_value}'"
-                )
-            else:
-                content.append(
-                    f" - Line {cannot_change[0].line_no} - {cannot_change[-1].line_no}: {len(cannot_change)} lines"
-                )
-        if unchanged_count != 0:
-            content.append(f"\n**{unchanged_count} lines unchanged**")
-
-        print(Panel.fit(Markdown("\n".join(content), justify="left"), title=title or "File differences"))
 
 
 @dataclass
