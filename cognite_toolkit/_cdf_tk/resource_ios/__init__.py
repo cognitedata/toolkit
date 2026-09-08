7ec4197cd8a88760a8909a1d8cecb78d824a0202
Anders Albert <60234212+doctrino@users.noreply.github.com>
2026-09-08 11:02:21 +0200

    [CDF-28566]  🪒 Remove data crud (#3214)
    # Description
    
    With the removal of the ResourceDataLoader classes we can now drop
    `pandas` as a dependency.
    
    ## Bump
    
    - [x] Patch
    - [ ] Skip
    
    ## Changelog
    
    ### Removed
    
    - `pandas` is no longer a required dependency for Toolkit.

--- cognite_toolkit/_cdf_tk/resource_ios/__init__.py
+++ cognite_toolkit/_cdf_tk/resource_ios/__init__.py
@@ -17,8 +17,7 @@ from typing import Literal, TypeAlias
 
 from cognite_toolkit._cdf_tk.feature_flags import FeatureFlag, Flags
 
-from ._base_ios import DataCRUD, Loader, ResourceContainerIO, ResourceIO
-from ._data_cruds import DatapointsCRUD, FileCRUD, RawFileCRUD
+from ._base_ios import Loader, ResourceContainerIO, ResourceIO
 from ._resource_ios import (
     AgentIO,
     AppIO,
@@ -119,10 +118,9 @@ CRUDS_BY_FOLDER_NAME: defaultdict[str, list[type[Loader]]] = defaultdict(list)
 for _loader in itertools.chain(
     ResourceIO.__subclasses__(),
     ResourceContainerIO.__subclasses__(),
-    DataCRUD.__subclasses__(),
     GroupIO.__subclasses__(),
 ):
-    if _loader in [ResourceIO, ResourceContainerIO, DataCRUD, GroupIO]:
+    if _loader in [ResourceIO, ResourceContainerIO, GroupIO]:
         # Skipping base classes
         continue
     # MyPy bug: https://github.com/python/mypy/issues/4717
@@ -150,7 +148,6 @@ RESOURCE_CRUD_BY_FOLDER_NAME_BY_KIND: dict[str, dict[str, type[ResourceIO]]] = {
 CRUD_LIST = list(itertools.chain.from_iterable(CRUDS_BY_FOLDER_NAME.values()))
 RESOURCE_CRUD_LIST = [loader for loader in CRUD_LIST if issubclass(loader, ResourceIO)]
 RESOURCE_CRUD_CONTAINER_LIST = [loader for loader in CRUD_LIST if issubclass(loader, ResourceContainerIO)]
-RESOURCE_DATA_CRUD_LIST = [loader for loader in CRUD_LIST if issubclass(loader, DataCRUD)]
 KINDS_BY_FOLDER_NAME: dict[str, set[str]] = {}
 for crud in CRUD_LIST:
     if crud.folder_name not in KINDS_BY_FOLDER_NAME:
@@ -208,7 +205,6 @@ __all__ = [
     "RESOURCE_CRUD_BY_FOLDER_NAME",
     "RESOURCE_CRUD_CONTAINER_LIST",
     "RESOURCE_CRUD_LIST",
-    "RESOURCE_DATA_CRUD_LIST",
     "_EXCLUDED_CRUDS",
     "AgentIO",
     "AppIO",
@@ -216,19 +212,16 @@ __all__ = [
     "AssetIO",
     "CogniteFileCRUD",
     "ContainerCRUD",
-    "DataCRUD",
     "DataModelIO",
     "DataProductIO",
     "DataProductVersionIO",
     "DataSetsIO",
     "DatapointSubscriptionIO",
-    "DatapointsCRUD",
     "EdgeCRUD",
     "EventIO",
     "ExternalDataSourceIO",
     "ExtractionPipelineConfigIO",
     "ExtractionPipelineIO",
-    "FileCRUD",
     "FileMetadataCRUD",
     "FunctionIO",
     "FunctionScheduleIO",
@@ -245,7 +238,6 @@ __all__ = [
     "LocationFilterIO",
     "NodeCRUD",
     "RawDatabaseCRUD",
-    "RawFileCRUD",
     "RawTableCRUD",
     "RelationshipIO",
     "ResourceContainerIO",
--- cognite_toolkit/_cdf_tk/resource_ios/_base_ios.py
+++ cognite_toolkit/_cdf_tk/resource_ios/_base_ios.py
@@ -24,7 +24,6 @@ from cognite_toolkit._cdf_tk.yaml_classes import ToolkitResource
 
 if TYPE_CHECKING:
     from cognite_toolkit._cdf_tk.commands.build_v2.data_classes import BuildVariable, ResourceType
-    from cognite_toolkit._cdf_tk.data_classes import BuildEnvironment
 
 if sys.version_info >= (3, 11):
     from typing import Self
@@ -501,27 +500,4 @@ class ResourceContainerIO(ResourceIO[T_Identifier, T_RequestResource, T_Response
 
     @abstractmethod
     def drop_data(self, ids: Sequence[T_Identifier]) -> int:
-        raise NotImplementedError
-
-
-class DataCRUD(Loader, ABC):
-    """This is the base class for all data CRUD..
-
-    A data CRUD is a CRUD that uploads data to CDF. It will typically depend on a
-    resource container that stores the data. For example, the datapoints crud depends
-    on the timeseries CRUD.
-
-    It has only one required method:
-        - upload: Uploads the data to CDF.
-
-    class attributes:
-        item_name: The name of the item that is stored in the resource container. This should be set in the subclass.
-            It is used to display messages when running operations.
-
-    """
-
-    item_name: str
-
-    @abstractmethod
-    def upload(self, state: "BuildEnvironment", dry_run: bool) -> Iterable[tuple[str, int]]:
         raise NotImplementedError
