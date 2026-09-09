535c929e75db3a1865212d4579f820776379e6d5
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2026-09-09 10:42:16 +0200

    Add space valdiation

--- cognite_toolkit/_cdf_tk/yaml_classes/location.py
+++ cognite_toolkit/_cdf_tk/yaml_classes/location.py
@@ -75,7 +75,11 @@ class LocationYAML(ToolkitResource):
     )
     instance_spaces: list[str] | None = Field(default=None, description="The list of spaces that instances are in")
     user_data_instance_space: str | None = Field(
-        default=None, description="The space where user-uploaded data is stored."
+        default=None,
+        description="The space where user-uploaded data is stored.",
+        min_length=1,
+        max_length=43,
+        pattern=SPACE_FORMAT_PATTERN,
     )
     scene: Scenes | None = Field(default=None, description="The scene config for the location.")
     asset_centric: AssetCentricResource | None = Field(
