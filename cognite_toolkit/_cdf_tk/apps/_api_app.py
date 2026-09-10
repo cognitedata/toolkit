61790fc0d7d7a4829b555adc0de27bde67bb3e43
anders-albert <anders0albert@gmail.com>
2026-09-10 14:28:51 +0200

    refactor: use new instances api command.

--- cognite_toolkit/_cdf_tk/apps/_api_app.py
+++ cognite_toolkit/_cdf_tk/apps/_api_app.py
@@ -2,6 +2,9 @@ from typing import Any
 
 import typer
 
+from cognite_toolkit._cdf_tk.commands import InstancesAPICommand
+from cognite_toolkit._cdf_tk.utils.auth import EnvironmentVariables
+
 from ._helpers import print_help_if_no_subcommand
 
 DEFAULT_LIST_LIMIT = 25
@@ -15,7 +18,7 @@ class APIApp(typer.Typer):
 
     @staticmethod
     def main(ctx: typer.Context) -> None:
-        """Plugin to work with data in CDF"""
+        """Commands to call CDF APIs"""
         print_help_if_no_subcommand(ctx)
 
 
@@ -23,33 +26,36 @@ class InstancesApp(typer.Typer):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self.callback(invoke_without_command=True)(self.main)
+        self.command()(self.list)
 
     @staticmethod
     def main(ctx: typer.Context) -> None:
-        """Plugin to work with data in CDF"""
+        """Commands to work with data modeling instances"""
         print_help_if_no_subcommand(ctx)
 
     @staticmethod
     def list(
-        view: str | None = typer.Option(
-            None,
+        view: str = typer.Option(
+            "cdf_cdm:CogniteDescribable/v1",
             "--view",
             "-v",
-            help="The properties to get for each instance. If not specified, only the instance properties are returned - not any container properties.",
+            help="The properties to get for each instance, given as 'space:externalId/version'. ",
         ),
         filter: str | None = typer.Option(
             None,
             "--filter",
             "-f",
-            help="A filter expression to filter the instances. If not specified, all instances are returned.",
+            help="A YAML/JSON filter expression to filter the instances.",
         ),
         limit: int = typer.Option(
             DEFAULT_LIST_LIMIT,
             "--limit",
             "-l",
-            help="Maximum number of instances to return. If not specified, the default limit is 25.",
+            help="Maximum number of instances to return.",
             max=1000,
         ),
     ) -> None:
         """List instances in CDF"""
-        ...
+        client = EnvironmentVariables.create_from_environment().get_client()
+        cmd = InstancesAPICommand()
+        cmd.run(lambda: cmd.list(client, view=view, filter=filter, limit=limit))
