75b91b3d89ac055cbcc3a58dda5bbafb7081606f
anders-albert <anders0albert@gmail.com>
2026-09-15 15:24:42 +0200

    refactor; fix up

--- cognite_toolkit/_cdf_tk/commands/build_v2/data_classes/_module.py
+++ cognite_toolkit/_cdf_tk/commands/build_v2/data_classes/_module.py
@@ -68,7 +68,7 @@ class BuildVariable(BaseModel):
         elif file_suffix == ".sql":
             if isinstance(substitution, list):
                 substitution = self._format_list_as_sql_tuple(substitution)
-        elif file_suffix in (".md", ".py"):
+        elif file_suffix == ".md":
             if isinstance(substitution, list):
                 substitution = ", ".join(str(item) for item in substitution)
         else:
